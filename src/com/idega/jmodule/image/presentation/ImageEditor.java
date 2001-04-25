a6b6945f9c7d44f8b17a75280606069738b66ed0
Pall Helgason <palli@idega.com>
2001-04-25 20:02:41 +0000

    no message

--- src/com/idega/jmodule/image/presentation/ImageEditor.java
+++ src/com/idega/jmodule/image/presentation/ImageEditor.java
@@ -6,9 +6,23 @@ import com.idega.jmodule.object.interfaceobject.*;
 import com.idega.jmodule.image.presentation.*;
 
 public class ImageEditor extends JModuleObject{
+private boolean refresh = false;
+private boolean showAll = true;
+
   public void main(ModuleInfo modinfo)throws Exception{
+    String sRefresh = modinfo.getRequest().getParameter("refresh");
     ImageBrowser browser = new ImageBrowser();
-    browser.setShowAll(true);
+    browser.setShowAll(showAll);
+
+    if( (sRefresh!=null) || refresh ) browser.refresh();
     add(browser);
+  }
+
+  public void refresh(){
+    this.refresh=true;
+  }
+
+  public void setShowAll(boolean showAll){
+    this.showAll = showAll;
   }
 }
