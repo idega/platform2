0d84ec9c858f8cbab9a0481f88e2f2102fd63ef6
Eirikur Hrafnsson <eiki@idega.com>
2001-05-08 13:41:56 +0000

    no message

---
+++ src/com/idega/jmodule/image/business/ImageProperties.java
@@ -0,0 +1,47 @@
+ package com.idega.block.building.business;
+
+
+ /**
+  * Title:
+  * Description:
+  * Copyright:    Copyright (c) 2001
+  * Company:      idega multimedia
+  * @author       <a href="mailto:aron@idega.is">aron@idega.is</a>
+  * @version 1.0
+  */
+
+ public class ImageProperties{
+    private String sName,sContentType,sPath,sWebPath;
+
+    public ImageProperties(String name,String contenttype,String path,String webpath){
+      this.sContentType = contenttype;
+      this.sName = name;
+      this.sPath = path;
+      this.sWebPath = webpath;
+    }
+    public String getName(){
+      return sName;
+    }
+    public void setName(String name){
+      this.sName = name;
+    }
+    public String getContentType(){
+      return sContentType;
+    }
+    public void setContentType(String cont){
+       this.sContentType = cont;
+    }
+    public String getPath(){
+      return sPath;
+    }
+    public void getPath(String path){
+      this.sPath = path;
+    }
+     public String getWebPath(){
+      return sWebPath;
+    }
+    public void getWebPath(String path){
+      this.sWebPath = path;
+    }
+
+  }
