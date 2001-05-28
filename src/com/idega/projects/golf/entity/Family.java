aff41492be8ee786cd2fa94262e90fb0228e52c6
Thorhallur Helgason <laddi@idega.com>
2001-05-28 16:19:26 +0000

    no message

--- src/com/idega/projects/golf/entity/Family.java
+++ src/com/idega/projects/golf/entity/Family.java
@@ -6,31 +6,25 @@ import java.sql.*;
 
 public class Family extends GolfEntity{
 
-	public Family(){
-		super();
-
-	}
-
-	public Family(int id)throws SQLException{
-		super(id);
-	}
-
-	public void initializeAttributes(){
-		addAttribute(getIDColumnName());
-		addAttribute("name","Fjölskylda",true,true,"java.lang.String");
-	}
-
-	public String getEntityName(){
-		return "family";
-	}
-
-	public String getName(){
-		return getStringColumnValue("name");
-	}
-
-	public void setName(String name){
-		setColumn("name", name);
-	}
+  public Family(){
+    super();
+  }
+  public Family(int id)throws SQLException{
+    super(id);
+  }
+  public void initializeAttributes(){
+    addAttribute(getIDColumnName());
+    addAttribute("name","Fjölskylda",true,true,"java.lang.String");
+  }
+  public String getEntityName(){
+    return "family";
+  }
+  public String getName(){
+    return getStringColumnValue("name");
+  }
+  public void setName(String name){
+    setColumn("name", name);
+  }
 
 
 }
