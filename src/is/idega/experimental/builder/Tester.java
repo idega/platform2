5f079d49c6f2c5237b6b5ba33127f9c63d45e2f7
Pall Helgason <palli@idega.com>
2001-06-06 11:15:20 +0000

    Moved the campus files from com.idega.projects to is.idega.projects

--- src/is/idega/experimental/builder/Tester.java
+++ src/is/idega/experimental/builder/Tester.java
@@ -1,17 +1,12 @@
 package is.idega.experimental.builder;
 
 import com.idega.builder.data.IBPage;
-import com.idega.data.BlobWrapper;
-import com.idega.servlet.IWPresentationServlet;
 import java.sql.SQLException;
 import java.io.IOException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
-import java.io.DataOutputStream;
 import javax.servlet.GenericServlet;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -22,19 +17,28 @@ public class Tester extends GenericServlet {
   }
 
   public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+    loadImage();
+  }
+
+  public void saveImage() {
+   try {
+      IBPage page = new IBPage();
+  		InputStream stream = new FileInputStream("c:\\programming\\source\\idega\\is\\idega\\experimental\\builder\\page2.xml");
+
+      page.setName("Test2");
+      page.setPageValue(stream);
+      page.insert();
+    }
+    catch(SQLException e) {
+      e.printStackTrace();
+    }
+    catch(FileNotFoundException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void loadImage() {
     try {
-//      IBPage page = new IBPage();
-      //BlobWrapper wrap = new BlobWrapper(page,"page_value");
-
-      //wrap.setInputStreamForBlobWrite(new FileInputStream("c:\\programming\\source\\idega\\is\\idega\\experimental\\builder\\page2.xml"));
-//		InputStream stream = new FileInputStream("c:\\programming\\source\\idega\\is\\idega\\experimental\\builder\\page2.xml");
-
-//      page.setID(3);
-//      page.setName("Test2");
-      //page.setPageValue(wrap);
-//      page.setPageValue(stream);
-//      page.insert();
-
       System.out.println("Getting page 23");
 
       IBPage page = new IBPage(23);
@@ -44,49 +48,22 @@ public class Tester extends GenericServlet {
 
       java.io.FileOutputStream file = new java.io.FileOutputStream("c:\\temp.xml");
       int buffersize=1;
-      byte	buffer[]= new byte[buffersize];
-      int		noRead	= 0;
-      while ( noRead != -1 ){
-        noRead	= myInputStream.read( buffer, 0, buffersize );
+      byte buffer[]= new byte[buffersize];
+      int noRead	= 0;
+      while (noRead != -1) {
+        noRead = myInputStream.read(buffer,0,buffersize);
         file.write(buffer);
       }
       file.close();
 
-
-/*                  response.setContentType("text/xml");
-
-               //   DataOutputStream output = new DataOutputStream( response.getOutputStream() );
-
-                          int buffersize=1;
-                          byte	buffer[]= new byte[buffersize];
-                          int		noRead	= 0;
-
-                          noRead	= myInputStream.read( buffer, 0, buffersize );
-                          int i=0;
-                          //Write out the file to the browser
-                          while ( noRead != -1 ){
-System.out.println("Reading stuff");
-                 //                 output.write( buffer, 0, noRead );
-                                  response.getWriter().print((char)buffer[0]);
-                                  noRead	= myInputStream.read( buffer, 0, buffersize );
-
-                          }
-
-
-
-//                          output.flush();
-//                          output.close();*/
-                          myInputStream.close();
-
-
-
-
+      myInputStream.close();
     }
     catch(SQLException e) {
       e.printStackTrace();
     }
-    catch(FileNotFoundException e2) {
-      e2.printStackTrace();
+    catch(IOException e) {
+      e.printStackTrace();
     }
   }
 }
+
