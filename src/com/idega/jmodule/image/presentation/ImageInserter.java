4e9d61607abdbd6489345d39d6019461fda41090
Aron Birkir Gudmundsson <aron@idega.com>
2001-06-15 11:19:37 +0000

    no message

--- src/com/idega/jmodule/image/presentation/ImageInserter.java
+++ src/com/idega/jmodule/image/presentation/ImageInserter.java
@@ -24,7 +24,8 @@ import com.idega.jmodule.image.business.*;
 public class ImageInserter extends JModuleObject{
 
 private int imageId = -1;
-private String imSessionImageName = "image_id";
+private String imSessionImageName =null;
+private String sHiddenInputName = null;
 private String defaultImageURL = "/pics/news/x.gif";
 private String adminURL = "/image/insertimage.jsp";
 private String nameOfWindow = "Ný Mynd";
@@ -33,6 +34,8 @@ private int maxImageWidth = 140;
 private boolean hasUseBox = true;
 
 public ImageInserter(){
+  this.imSessionImageName="image_id";
+  this.sHiddenInputName = "image_id";
 }
 
 public ImageInserter(int imageId) {
@@ -41,11 +44,13 @@ public ImageInserter(int imageId) {
 
 public ImageInserter(String imSessionImageName) {
   this.imSessionImageName=imSessionImageName;
+  this.sHiddenInputName = imSessionImageName;
 }
 
 public ImageInserter(int imageId, String imSessionImageName) {
   this.imageId=imageId;
   this.imSessionImageName=imSessionImageName;
+  this.sHiddenInputName = imSessionImageName;
 }
 
   public void main(ModuleInfo modinfo)throws Exception{
@@ -73,7 +78,7 @@ public ImageInserter(int imageId, String imSessionImageName) {
         imageAdmin.addParameter("im_image_session_name",imSessionImageName);
         if ( imageId != -1 )  imageAdmin.addParameter("image_id",imageId);
 
-      HiddenInput hidden = new HiddenInput(imSessionImageName,imageId+"");
+      HiddenInput hidden = new HiddenInput(sHiddenInputName,imageId+"");
       CheckBox insertImage = new CheckBox("insertImage","Y");
         insertImage.setChecked(true);
 
@@ -100,6 +105,13 @@ public ImageInserter(int imageId, String imSessionImageName) {
     this.sUseBoxString = sUseBoxString;
   }
 
+  public void setHiddenInputName(String name){
+    sHiddenInputName = name;
+  }
+  public String getHiddenInputName(){
+    return sHiddenInputName;
+  }
+
   public void setDefaultImageURL(String defaultImageURL){
     this.defaultImageURL = defaultImageURL;
   }
@@ -118,5 +130,8 @@ public ImageInserter(int imageId, String imSessionImageName) {
 
   public void setImSessionImageName(String imSessionImageName) {
     this.imSessionImageName=imSessionImageName;
+  }
+  public String getImSessionImageName() {
+    return this.imSessionImageName;
   }
 }
