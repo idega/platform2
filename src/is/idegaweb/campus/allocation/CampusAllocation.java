adcdc74c03adeffaa8f65495edf3d934c1cc6875
Aron Birkir Gudmundsson <aron@idega.com>
2001-06-25 09:59:12 +0000

    no message

--- src/is/idegaweb/campus/allocation/CampusAllocation.java
+++ src/is/idegaweb/campus/allocation/CampusAllocation.java
@@ -1,5 +1,5 @@
 /*
- * $Id: CampusAllocation.java,v 1.1 2001/06/22 11:34:14 aron Exp $
+ * $Id: CampusAllocation.java,v 1.2 2001/06/25 09:59:12 aron Exp $
  *
  * Copyright (C) 2001 Idega hf. All Rights Reserved.
  *
@@ -10,10 +10,14 @@
 package is.idegaweb.campus.allocation;
 
 import com.idega.jmodule.object.textObject.*;
+import com.idega.jmodule.object.interfaceobject.*;
 import com.idega.jmodule.object.Table;
 import com.idega.jmodule.object.ModuleObject;
 import com.idega.jmodule.object.ModuleInfo;
 import com.idega.block.finance.presentation.*;
+import com.idega.block.application.data.*;
+import com.idega.block.application.business.ApplicationFinder;
+import java.util.List;
 
 /**
  *
@@ -33,8 +37,10 @@ public class CampusAllocation extends KeyEditor{
   protected void control(ModuleInfo modinfo){
 
       this.makeView();
-      this.addHeader((this.makeLinkTable(0)));
-      this.addHeader((Text.getBreak()));
+      if(isAdmin)
+        this.addMain((makeSubjectTable()));
+      else
+        this.addMain(new Text("Ekki Réttindi"));
 
   }
 
@@ -69,5 +75,50 @@ public class CampusAllocation extends KeyEditor{
       LinkTable.add(Link5,5,1);
     }
     return LinkTable;
+  }
+
+  public ModuleObject makeSubjectTable(){
+    List L = ApplicationFinder.ListOfSubject();
+    Table Frame = new Table(3,2);
+      Frame.setCellpadding(0);
+      Frame.setCellspacing(0);
+    Table Left = new Table();
+      Left.setCellpadding(0);
+      Left.setCellspacing(0);
+    Table Right = new Table();
+      Right.setCellpadding(0);
+      Right.setCellspacing(0);
+    Frame.add(Left,1,1);
+    Frame.add(Right,3,1);
+    if(L != null){
+      String sParameter = "app_sub_id";
+      ApplicationSubject AS;
+      int len = L.size();
+      for (int i = 0; i < len; i++) {
+        AS = (ApplicationSubject) L.get(i);
+        RadioButton R = new RadioButton(sParameter,String.valueOf(AS.getID()));
+        Left.add(R,1,i+1);
+        Left.add(AS.getName(),2,i+1);
+      }
+    }
+    else{
+      Left.add(new Text("Ekkert úthlutunartímabil til"));
+    }
+    Link Approve = new Link("Samþykkja nýskráðar umsóknir");
+    Link Contracts = new Link("Samningar");
+    Link Waitinglist = new Link("Biðlistar");
+    Link RoughOrder = new Link("Grófraða úthlutun");
+    Link Allocate = new Link("Úthluta");
+    Link Subject = new Link("Nýtt umsóknartímabil");
+
+    Right.add(Approve,1,1);
+    Right.add(Contracts,1,2);
+    Right.add(Waitinglist,1,3);
+    Right.add(RoughOrder,1,4);
+    Right.add(Allocate,1,5);
+    Right.add(Subject,1,6);
+
+    return Frame;
+
   }
 }
