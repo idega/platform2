f00e2d42e09d6320d9fb9a63eb1e48e85431eaf0
Aron Birkir Gudmundsson <aron@idega.com>
2001-07-12 10:33:14 +0000

    no message

--- src/is/idegaweb/campus/allocation/CampusAllocation.java
+++ src/is/idegaweb/campus/allocation/CampusAllocation.java
@@ -1,5 +1,5 @@
 /*
- * $Id: CampusAllocation.java,v 1.3 2001/06/25 21:55:05 palli Exp $
+ * $Id: CampusAllocation.java,v 1.4 2001/07/12 10:33:14 aron Exp $
  *
  * Copyright (C) 2001 Idega hf. All Rights Reserved.
  *
@@ -46,34 +46,6 @@ public class CampusAllocation extends KeyEditor{
 
   public ModuleObject makeLinkTable(int menuNr){
     Table LinkTable = new Table(6,1);
-    int last = 6;
-    LinkTable.setWidth("100%");
-    LinkTable.setCellpadding(2);
-    LinkTable.setCellspacing(1);
-    LinkTable.setColor(this.DarkColor);
-    LinkTable.setWidth(last,"100%");
-    Link Link1 = new Link("Heim");
-    Link1.setFontColor(this.LightColor);
-    Link1.addParameter(this.strAction,String.valueOf(this.ACT1));
-    Link Link2 = new Link("Bókhaldsliðir","/finance/accountkey.jsp");
-    Link2.setFontColor(this.LightColor);
-    Link2.addParameter(this.strAction,String.valueOf(this.ACT2));
-    Link Link3 = new Link("Gjaldliðir","/finance/tariffkey.jsp");
-    Link3.setFontColor(this.LightColor);
-    Link3.addParameter(this.strAction,String.valueOf(this.ACT3));
-    Link Link4 = new Link("Gjöld","/finance/tariff.jsp");
-    Link4.setFontColor(this.LightColor);
-    Link4.addParameter(this.strAction,String.valueOf(this.ACT4));
-    Link Link5 = new Link("Álagning","/finance/assessment.jsp");
-    Link5.setFontColor(this.LightColor);
-    Link5.addParameter(this.strAction,String.valueOf(this.ACT5));
-    if(isAdmin){
-      LinkTable.add(Link1,1,1);
-      LinkTable.add(Link2,2,1);
-      LinkTable.add(Link3,3,1);
-      LinkTable.add(Link4,4,1);
-      LinkTable.add(Link5,5,1);
-    }
     return LinkTable;
   }
 
