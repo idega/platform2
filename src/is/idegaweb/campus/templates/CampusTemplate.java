aec61c09b6916dabb34649918057143311b9359e
Thorhallur Helgason <laddi@idega.com>
2001-07-13 16:00:55 +0000

    no message

--- src/is/idegaweb/campus/templates/CampusTemplate.java
+++ src/is/idegaweb/campus/templates/CampusTemplate.java
@@ -1,5 +1,5 @@
 /*
- * $Id: CampusTemplate.java,v 1.3 2001/07/13 12:54:41 laddi Exp $
+ * $Id: CampusTemplate.java,v 1.4 2001/07/13 16:00:55 laddi Exp $
  *
  * Copyright (C) 2001 Idega hf. All Rights Reserved.
  *
@@ -32,19 +32,17 @@ public abstract class CampusTemplate extends MainTemplate{
 
     Login login = new Login();
       login.setLayout(Login.LAYOUT_STACKED);
-      login.setTryAgainImageUrl("/pics/IS/inn.gif");
       login.setUserTextColor("#000000");
-      login.setLoggedOnTextColor("#000000");
       login.setPasswordTextColor("#000000");
       login.setHeight("110");
       login.setWidth("100");
-      login.setLoginImageUrl("/pics/IS/inn.gif");
-      login.setLogOutImageUrl("/pics/IS/ut.gif");
       login.setUserTextSize(1);
       login.setPasswordTextSize(1);
       login.setStyle("font-family: Verdana; font-size: 8pt; border: 1 solid #000000");
       login.setInputLength(14);
       login.setSubmitButtonAlignment("right");
+      login.setUserText("Nafn:");
+      login.setPasswordText("Lykilorð:");
 
     addMenuTitle(new Image("/pics/titles/IS/menutitle.gif"));
     addRightTitle(new Image("/pics/titles/IS/logintitle.gif"));
