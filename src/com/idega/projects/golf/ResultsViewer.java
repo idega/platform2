fab881c122d05680772353b388afb5616ef006ae
Thorhallur Helgason <laddi@idega.com>
2001-07-19 22:03:02 +0000

    no message

--- src/com/idega/projects/golf/ResultsViewer.java
+++ src/com/idega/projects/golf/ResultsViewer.java
@@ -341,17 +341,25 @@ private Table resultTable;
        }
       }
       else {
-        if ( !championship ) {
+        if ( this.tournamentGroupID == -1 ) {
+          if ( !championship ) {
+            TournamentResults tournResults = new TournamentResults(tournamentID,sortBy,tournamentGroupID,tournamentRounds,gender);
+                tournResults.sortBy(orderBy);
+
+            resultTable.add(tournResults,1,1);
+          }
+          else {
+            resultTable.setAlignment(1,1,"center");
+            resultTable.add(Text.getBreak());
+            resultTable.add(Text.getBreak());
+            resultTable.add("Veldu hvern einstakan flokk úr listanum hér að ofan til að sjá stöðuna innan hans",1,1);
+          }
+        }
+        else {
           TournamentResults tournResults = new TournamentResults(tournamentID,sortBy,tournamentGroupID,tournamentRounds,gender);
               tournResults.sortBy(orderBy);
 
           resultTable.add(tournResults,1,1);
-        }
-        else {
-          resultTable.setAlignment(1,1,"center");
-          resultTable.add(Text.getBreak());
-          resultTable.add(Text.getBreak());
-          resultTable.add("Veldu hvern einstakan flokk úr listanum hér að ofan til að sjá stöðuna innan hans",1,1);
         }
       }
     }
