ab133aa2822b05e046190dd9886c72396a5939e2
Aron Birkir Gudmundsson <aron@idega.com>
2001-08-14 10:01:58 +0000

    no message

---
+++ src/is/idegaweb/campus/entity/CampusPhone.java
@@ -0,0 +1,65 @@
+package is.idegaweb.campus.entity;
+
+import com.idega.data.GenericEntity;
+import java.sql.SQLException;
+
+
+/**
+ * Title:
+ * Description:
+ * Copyright:    Copyright (c) 2000-2001 idega.is All Rights Reserved
+ * Company:      idega
+  *@author <a href="mailto:aron@idega.is">Aron Birkir</a>
+ * @version 1.1
+ */
+
+public class CampusPhone extends GenericEntity {
+
+  public CampusPhone() {
+  }
+  public CampusPhone(int id) throws SQLException{
+    super(id);
+  }
+  public void initializeAttributes() {
+    addAttribute(getIDColumnName());
+    addAttribute(getColumnNamePhoneNumber(),"Phone number",true,true,String.class);
+    addAttribute(getColumnNameApartmentId(),"Apartment",true,true,Integer.class,"one-to-one",com.idega.block.building.data.Apartment.class);
+    addAttribute(getColumnNameDateInstalled(),"Cypher Key",true,true,java.sql.Date.class);
+    addAttribute(getColumnNameDateResigned(),"Admin email",true,true,java.sql.Date.class);
+  }
+  public String getEntityName() {
+    return getEntityTableName();
+  }
+
+  public static String getEntityTableName(){return "CAM_PHONE";}
+  public static String getColumnNamePhoneNumber(){return "PHONE_NUMBER";}
+  public static String getColumnNameApartmentId(){return "BU_APARTMENT_ID";}
+  public static String getColumnNameDateInstalled(){return "DATE_INSTALLED";}
+  public static String getColumnNameDateResigned(){return "DATE_RESIGNED";}
+
+
+  public void setPhoneNumber(String number){
+    setColumn(getColumnNamePhoneNumber(),number);
+  }
+  public String getPhoneNumber(){
+    return getStringColumnValue(getColumnNamePhoneNumber());
+  }
+  public int getApartmentId(){
+    return getIntColumnValue(getColumnNameApartmentId());
+  }
+  public void setApartmentId(int id){
+    setColumn(getColumnNameApartmentId(),id);
+  }
+  public void setDateInstalled(java.sql.Date date){
+    setColumn(getColumnNameDateInstalled(),date);
+  }
+  public java.sql.Date getDateInstalled(){
+    return((java.sql.Date)getColumnValue(getColumnNameDateInstalled()));
+  }
+  public void setDateResigned(java.sql.Date date){
+    setColumn(getColumnNameDateResigned(),date);
+  }
+  public java.sql.Date getDateResigned(){
+    return((java.sql.Date)getColumnValue(getColumnNameDateResigned()));
+  }
+}
