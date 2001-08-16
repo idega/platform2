a6cd0fcfebc1691a435dbdcab27f3d171e3070d5
Thorhallur Helgason <laddi@idega.com>
2001-08-16 10:51:49 +0000

    no message

--- src/com/idega/block/trade/stockroom/business/SupplierManager.java
+++ src/com/idega/block/trade/stockroom/business/SupplierManager.java
@@ -3,10 +3,13 @@ package com.idega.block.trade.stockroom.business;
 import com.idega.block.trade.stockroom.data.*;
 import com.idega.core.accesscontrol.data.PermissionGroup;
 import com.idega.core.accesscontrol.business.AccessControl;
+import com.idega.core.accesscontrol.business.LoginDBHandler;
 import com.idega.core.user.data.User;
 import com.idega.core.user.business.UserBusiness;
 import com.idega.core.data.*;
 import com.idega.util.idegaTimestamp;
+import java.util.*;
+import java.sql.*;
 
 
 /**
@@ -29,7 +32,7 @@ public class SupplierManager {
   }
 
 
-  public Supplier createSupplier(String name, String description, int[] addressIds, int[] phoneIds, int[] emailIds) throws Exception {
+  public static Supplier createSupplier(String name, String userName, String password, String description, int[] addressIds, int[] phoneIds, int[] emailIds) throws Exception {
 
     boolean isUpdate = false;
     SupplierStaffGroup sGroup = new SupplierStaffGroup();
@@ -41,11 +44,13 @@ public class SupplierManager {
     supp.setName(name);
     supp.setDescription(description);
     supp.setGroupId(sGroup.getID());
+    supp.setIsValid(true);
 
     supp.insert();
 
     UserBusiness uBus = new UserBusiness();
     User user = uBus.insertUser(name,"","- admin",name+" - admin","Supplier administrator",null,idegaTimestamp.RightNow());
+    LoginDBHandler.createLogin(user.getID(), userName, password);
 
     sGroup.addTo(user);
 
@@ -54,7 +59,7 @@ public class SupplierManager {
     AccessControl ac = new AccessControl();
     int permissionGroupID = ac.createPermissionGroup(name+" - admins", "Supplier administator group", "", userIDs ,null);
 
-    sGroup.addTo(PermissionGroup.class, permissionGroupID);
+    //sGroup.addTo(PermissionGroup.class, permissionGroupID);
 
     if(addressIds != null){
       for (int i = 0; i < addressIds.length; i++) {
@@ -78,8 +83,43 @@ public class SupplierManager {
 
   }
 
+  public static void invalidateSupplier(Supplier supplier) throws SQLException {
+    supplier.setIsValid(false);
+    supplier.update();
+  }
 
+  public static void validateSupplier(Supplier supplier) throws SQLException {
+    supplier.setIsValid(true);
+    supplier.update();
+  }
 
+  public void updateSupplier(Supplier supplier, String name, String description, int[] addressIds, int[] phoneIds, int[] emailIds) throws Exception {
+    supplier.setName(name);
+    supplier.setDescription(description);
+    supplier.update();
+/*
+    supplier.reverseRemoveFrom(Address.getStaticInstance(Address.class));
+
+    if(addressIds != null){
+      for (int i = 0; i < addressIds.length; i++) {
+        supplier.addTo(Address.class, addressIds[i]);
+      }
+    }
+
+    if(phoneIds != null){
+      if (phoneIds.length == 0) {
+        supplier.reverseRemoveFrom(Phone.getStaticInstance(Phone.class));
+      }
+    }
+
+    if(emailIds != null){
+      if (emailIds.length == 0) {
+        supplier.reverseRemoveFrom(Phone.getStaticInstance(Email.class));
+      }
+    }
+*/
+
+  }
 
 
 
--- src/com/idega/block/trade/stockroom/data/Supplier.java
+++ src/com/idega/block/trade/stockroom/data/Supplier.java
@@ -1,10 +1,11 @@
 package com.idega.block.trade.stockroom.data;
 
 import java.sql.*;
+import java.util.*;
 import com.idega.data.GenericEntity;
 import com.idega.core.data.*;
+import com.idega.data.*;
 import com.idega.block.employment.data.EmployeeGroup;
-
 /**
  * Title:        IW Trade
  * Description:
@@ -29,9 +30,11 @@ public class Supplier extends GenericEntity{
     addAttribute(getColumnNameName(), "Name", true, true, String.class);
     addAttribute(getColumnNameDescription(), "Lýsing", true, true, String.class,500);
     addAttribute(getColumnNameGroupID(),"Hópur", true, true, Integer.class, "many_to_one", SupplierStaffGroup.class);
+    addAttribute(getColumnNameIsValid(),"Í notkun",true, true, Boolean.class);
 
     this.addManyToManyRelationShip(Address.class,"SR_SUPPLIER_IC_ADDRESS");
     this.addManyToManyRelationShip(Phone.class,"SR_SUPPLIER_IC_PHONE");
+    this.addManyToManyRelationShip(Email.class,"SR_SUPPLIER_IC_EMAIL");
 //    this.addManyToManyRelationShip(PriceCategory.class, "SR_SUPPLIER_PRICE_CATEGORY");
     this.addManyToManyRelationShip(ProductCategory.class, "SR_SUPPLIER_PRODUCT_CATEGORY" );
   }
@@ -39,10 +42,15 @@ public class Supplier extends GenericEntity{
   public void insertStartData()throws Exception{
   }
 
+  public void setDefaultValues() {
+    setIsValid(true);
+  }
+
   public static String getSupplierTableName(){return "SR_SUPPLIER";}
   public static String getColumnNameName() {return "NAME";}
-  public static String getColumnNameDescription() {return "NAME";}
+  public static String getColumnNameDescription() {return "DESCRIPTION";}
   public static String getColumnNameGroupID() {return "IC_GROUP_ID";}
+  public static String getColumnNameIsValid() {return "IS_VALID";}
 
   public String getEntityName(){
     return getSupplierTableName();
@@ -73,7 +81,80 @@ public class Supplier extends GenericEntity{
     return getIntColumnValue(getColumnNameGroupID());
   }
 
-/*
+  public void setIsValid(boolean isValid) {
+    setColumn(getColumnNameIsValid(),isValid);
+  }
+
+  public boolean getIsValid() {
+    return getBooleanColumnValue(getColumnNameIsValid());
+  }
+
+  public Address getAddress() throws SQLException {
+    Address address = null;
+    List addr = getAddresses();
+    if (addr !=null) {
+      address = (Address) addr.get(addr.size() -1);
+    }
+    return address;
+  }
+
+  public List getAddresses() throws SQLException{
+    return EntityFinder.findRelated(this,Address.getStaticInstance(Address.class));
+  }
+
+
+  public List getPhones() throws SQLException {
+    return EntityFinder.findRelated(this,Phone.getStaticInstance(Phone.class));
+  }
+
+  public List getHomePhone() throws SQLException {
+    return getPhones(Phone.getHomeNumberID());
+  }
+
+  public List getFaxPhone() throws SQLException {
+    return getPhones(Phone.getFaxNumberID());
+  }
+
+  public List getWorkPhone() throws SQLException {
+    return getPhones(Phone.getWorkNumberID());
+  }
+
+  public List getMobilePhone() throws SQLException {
+    return getPhones(Phone.getMobileNumberID());
+  }
+
+  public List getPhones(int PhoneTypeId) throws SQLException{
+    Vector phones = new Vector();
+    List allPhones = getPhones();
+    if (allPhones != null) {
+      Phone temp = null;
+      for (int i = 0; i < allPhones.size(); i++) {
+        temp = (Phone) allPhones.get(i);
+        if (temp.getPhoneTypeId() == PhoneTypeId) {
+          phones.add(temp);
+        }
+      }
+    }
+    return phones;
+  }
+
+  public Email getEmail() throws SQLException{
+    Email email = null;
+    List emails = getEmails();
+    if (emails != null) {
+      email = (Email) emails.get(emails.size() - 1);
+    }
+    return email;
+  }
+
+  public List getEmails() throws SQLException {
+    return EntityFinder.findRelated(this,Email.getStaticInstance(Email.class));
+  }
+
+  public static Supplier[] getValidSuppliers() throws SQLException {
+    return (Supplier[]) Supplier.getStaticInstance(Supplier.class).findAllByColumnOrdered(Supplier.getColumnNameIsValid(),"Y",Supplier.getColumnNameName());
+  }
+  /*
   public Address getAddress() {
       return (Address) getColumnValue("IC_ADDRESS_ID"); where address_type is st_supplier_address
   }
