443b43aa49dae4513169c4c03493e99a7d3fdb57
Aron Birkir Gudmundsson <aron@idega.com>
2001-08-22 12:33:17 +0000

    no message

--- src/com/idega/block/building/business/BuildingCacher.java
+++ src/com/idega/block/building/business/BuildingCacher.java
@@ -10,6 +10,7 @@ import java.util.Vector;
 import java.sql.SQLException;
 import java.util.Hashtable;
 import java.util.Iterator;
+import com.idega.util.idegaTimestamp;
 
 
 
@@ -17,6 +18,9 @@ public class BuildingCacher {
 
   private static List Complexes,Buildings,Floors,Categories,Types,Apartments;
   private static Hashtable hComplexes,hBuildings,hFloors,hCategories,hTypes,hApartments;
+  private static boolean setToReload = false;
+  private static idegaTimestamp lastReloaded = idegaTimestamp.RightNow();
+  private static int reloadCount = 0;
 
   public BuildingCacher() {
   }
@@ -80,82 +84,136 @@ public class BuildingCacher {
 
   public static void reload(){
     initializeLodgings();
+    lastReloaded = idegaTimestamp.RightNow();
+    reloadCount++;
+    setToReload = false;
+  }
+
+  public static void setToReloadNextTimeReferenced(){
+    setToReload = true;
+  }
+
+  public static idegaTimestamp getLastReloaded(){
+    return lastReloaded;
+  }
+
+  public static int getReloadedCount(){
+    return reloadCount;
   }
 
   public static List getBuildings(){
-    if(hComplexes == null){
+    if(hComplexes == null || setToReload){
       reload();
     }
     return Buildings;
   }
   public static List getComplexes(){
-    if(hBuildings == null){
+    if(hBuildings == null || setToReload){
       reload();
     }
     return Complexes;
   }
   public static List getFloors(){
-    if(hFloors == null){
+    if(hFloors == null || setToReload){
       reload();
     }
     return Floors;
   }
   public static List getTypes(){
-    if(hTypes == null){
+    if(hTypes == null || setToReload){
       reload();
     }
     return Types;
   }
   public static List getCategories(){
-    if(hCategories == null){
+    if(hCategories == null || setToReload){
       reload();
     }
     return Categories;
   }
   public static List getApartments(){
-    if(hApartments == null){
+    if(hApartments == null || setToReload){
       reload();
     }
     return Apartments;
   }
 
+  public static Hashtable hashOfBuildings(){
+    if(hComplexes == null || setToReload){
+      reload();
+    }
+    return hBuildings;
+  }
+  public static Hashtable hashOfComplexes(){
+    if(hBuildings == null || setToReload){
+      reload();
+    }
+    return hComplexes;
+  }
+  public static Hashtable hashOfFloors(){
+    if(hFloors == null || setToReload){
+      reload();
+    }
+    return hFloors;
+  }
+  public static Hashtable hashOfTypes(){
+    if(hTypes == null || setToReload){
+      reload();
+    }
+    return hTypes;
+  }
+  public static Hashtable hashOfCategories(){
+    if(hCategories == null || setToReload){
+      reload();
+    }
+    return hCategories;
+  }
+  public static Hashtable hashOfApartments(){
+    if(hApartments == null || setToReload){
+      reload();
+    }
+    return hApartments;
+  }
+
+
+
   public static Complex getComplex(int id){
-    if(hComplexes == null){
+    if(hComplexes == null || setToReload){
       reload();
     }
     return (Complex)hComplexes.get(new Integer(id));
   }
 
   public static Building getBuilding(int id){
-    if(hBuildings == null){
+    if(hBuildings == null || setToReload){
       reload();
     }
     return (Building)hBuildings.get(new Integer(id));
   }
 
   public static Floor getFloor(int id){
-    if(hFloors == null){
+    if(hFloors == null || setToReload){
       reload();
     }
     return (Floor)hFloors.get(new Integer(id));
   }
 
   public static ApartmentCategory getApartmentCategory(int id){
-    if(hCategories == null){
+    if(hCategories == null || setToReload){
       reload();
     }
     return (ApartmentCategory)hCategories.get(new Integer(id));
   }
 
   public static ApartmentType getApartmentType(int id){
-    if(hTypes == null){
+    if(hTypes == null || setToReload){
       reload();
     }
     return (ApartmentType)hTypes.get(new Integer(id));
   }
 
   public static Apartment getApartment(int id){
-    if(hApartments == null){
+    if(hApartments == null || setToReload){
       reload();
     }
     return (Apartment)hApartments.get(new Integer(id));
