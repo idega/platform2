b06851d12b7de8c88c604f94819036959537f7ae
Thorhallur Helgason <laddi@idega.com>
2001-08-27 11:08:42 +0000

    no message

---
+++ src/is/idega/idegaweb/travel/data/Tour.java
@@ -0,0 +1,63 @@
+package is.idega.travel.data;
+
+import com.idega.data.*;
+import java.sql.*;
+
+
+/**
+ * Title:        idegaWeb TravelBooking
+ * Description:
+ * Copyright:    Copyright (c) 2001
+ * Company:      idega
+ * @author <a href="mailto:gimmi@idega.is">Grimur Jonsson</a>
+ * @version 1.0
+ */
+
+public class Tour extends GenericEntity {
+
+  public Tour() {
+    super();
+  }
+
+  public void initializeAttributes() {
+    addAttribute(getIDColumnName(),"Service_id",true,true,Integer.class,"one-to-one",Service.class);
+    addAttribute(getHotelPickupColumnName(), "Hotel pick-up", true, true, Boolean.class);
+    addAttribute(getHotelPickupTimeColumnName(), "Hotel pick-up time", true, true, Timestamp.class);
+
+  }
+  public String getEntityName() {
+    return getTripTableName();
+  }
+
+
+  public boolean getIsHotelPickup() {
+    return getHotelPickup();
+  }
+
+  public boolean getHotelPickup() {
+    return getBooleanColumnValue(getHotelPickupColumnName());
+  }
+
+  public void setIsHotelPickup(boolean pickup) {
+    setHotelPickup(pickup);
+  }
+
+  public void setHotelPickup(boolean pickup) {
+    setColumn(getHotelPickupColumnName(),pickup);
+  }
+
+  public void setHotelPickupTime(Timestamp pickupTime) {
+    setColumn(getHotelPickupTimeColumnName(), pickupTime);
+  }
+
+  public Timestamp getHotelPickupTime() {
+    return (Timestamp) getColumnValue(getHotelPickupTimeColumnName());
+  }
+
+
+  public static String getTripTableName() {return "TB_TRIP";}
+  public static String getHotelPickupColumnName() {return "HOTEL_PICKUP";}
+  public static String getHotelPickupTimeColumnName() {return "HOTEL_PICKUP_TIME";}
+//  public static String getColumnName() {return "";}
+
+}
