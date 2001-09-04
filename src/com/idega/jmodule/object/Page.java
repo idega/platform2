063e53c98d03fc7228fc1880f43803768902bd8f
Pall Helgason <palli@idega.com>
2001-09-04 12:02:34 +0000

    no message

--- src/com/idega/jmodule/object/Page.java
+++ src/com/idega/jmodule/object/Page.java
@@ -1,5 +1,5 @@
 /*
- * $Id: Page.java,v 1.18 2001/08/27 20:37:37 gummi Exp $
+ * $Id: Page.java,v 1.19 2001/09/04 12:02:46 palli Exp $
  *
  * Copyright (C) 2001 Idega hf. All Rights Reserved.
  *
@@ -22,7 +22,6 @@ import com.idega.idegaweb.IWMainApplication;
 *@author <a href="mailto:tryggvi@idega.is">Tryggvi Larusson</a>
 *@version 1.2
 */
-
 public class Page extends ModuleObjectContainer {
   private String title;
   private Script theAssociatedScript;
@@ -39,13 +38,12 @@ public class Page extends ModuleObjectContainer {
   private String styleSheetURL = "/style/style.css";
   private boolean addStyleSheet = false;
   private Hashtable frameProperties;
-  private boolean isTemplate=false;
-
-
+  private boolean isTemplate_ = false;
+  private boolean isPage_ = true;
+  private boolean isDraft_ = false;
 
   protected static final String ROWS_PROPERTY = "ROWS";
 
-  //private static final String IW_FRAME_PARAMETER="idegaweb_frame_page_s";
   protected static final String IW_PAGE_KEY = "idegaweb_page";
   public static final String IW_FRAME_STORAGE_PARMETER = "idegaweb_frame_page";
   public static final String IW_FRAME_CLASS_PARAMETER = "idegaweb_frame_class";
@@ -53,98 +51,161 @@ public class Page extends ModuleObjectContainer {
   private final static String START_TAG="<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\" \"http://www.w3.org/TR/REC-html40/loose.dtd\">\n<html>";
   private final static String END_TAG="</html>";
 
-
-
-
+  /**
+   *
+   */
   public Page() {
     this("");
   }
 
+  /**
+   *
+   */
   public Page(String s) {
     super();
     setTitle(s);
   }
 
+  /**
+   *
+   */
   public void setBackgroundColor(String color) {
     setAttribute("bgcolor",color);
   }
 
+  /**
+   *
+   */
   public void setTextColor(String color) {
     setAttribute("text",color);
   }
 
+  /**
+   *
+   */
   public void setAlinkColor(String color) {
     setAttribute("alink",color);
   }
 
+  /**
+   *
+   */
   public void setHoverColor(String color) {
     setAttribute("alink",color);
     this.hoverColor = color;
   }
 
+  /**
+   *
+   */
   public void setTextDecoration(String textDecoration) {
     this.textDecoration = textDecoration;
   }
 
+  /**
+   *
+   */
   public void setStyleSheetURL(String styleSheetURL) {
     this.addStyleSheet = true;
     this.styleSheetURL = styleSheetURL;
   }
 
+  /**
+   *
+   */
   public void setVlinkColor(String color) {
     setAttribute("vlink",color);
     this.visitedColor = color;
   }
 
+  /**
+   *
+   */
   public void setLinkColor(String color) {
     setAttribute("link",color);
     this.linkColor = color;
   }
 
+  /**
+   *
+   */
   public void setPageFontFace(String TextFontFace) {
     this.pageStyleFont = TextFontFace;
   }
 
+  /**
+   *
+   */
   public void setPageFontSize(String TextFontSize) {
     this.pageStyleFont = TextFontSize;
   }
 
+  /**
+   *
+   */
   public void setPageFontStyle (String TextFontStyle) {
     this.pageStyleFontStyle = TextFontStyle;
   }
 
+  /**
+   *
+   */
   public String getPageFontFace() {
     return this.pageStyleFont;
   }
 
+  /**
+   *
+   */
   public String getPageFontSize() {
     return this.pageStyleFont;
   }
 
+  /**
+   *
+   */
   public String getPageFontStyle () {
     return this.pageStyleFontStyle;
   }
 
+  /**
+   *
+   */
   public void setTitle(String title) {
     setName(title);
   }
 
+  /**
+   *
+   */
   public void setMarginWidth(int width) {
     setAttribute("marginwidth",Integer.toString(width));
   }
 
+  /**
+   *
+   */
   public void setMarginHeight(int height) {
     setAttribute("marginheight",Integer.toString(height));
   }
 
+  /**
+   *
+   */
   public void setLeftMargin(int leftmargin) {
     setAttribute("leftmargin",Integer.toString(leftmargin));
   }
 
+  /**
+   *
+   */
   public void setTopMargin(int topmargin) {
     setAttribute("topmargin",Integer.toString(topmargin));
   }
 
+  /**
+   *
+   */
   public void setAllMargins(int allMargins) {
     setMarginWidth(allMargins);
     setMarginHeight(allMargins);
@@ -152,37 +213,61 @@ public class Page extends ModuleObjectContainer {
     setTopMargin(allMargins);
   }
 
+  /**
+   *
+   */
   public String getTitle() {
     return getName();
   }
 
+  /**
+   *
+   */
   public void setAssociatedScript(Script myScript) {
     theAssociatedScript = myScript;
   }
 
+  /*
+   *
+   */
   private void initializeAssociatedScript() {
     if(theAssociatedScript == null) {
       theAssociatedScript = new Script();
     }
   }
 
+  /**
+   *
+   */
   public Script getAssociatedScript() {
     initializeAssociatedScript();
     return this.theAssociatedScript;
   }
 
+  /**
+   *
+   */
   public void setBackgroundImage(String imageURL) {
     setAttribute("background",imageURL);
   }
 
+  /**
+   *
+   */
   public void setBackgroundImage(Image backgroundImage) {
     setAttribute("background",backgroundImage.getURL());
   }
 
+  /**
+   *
+   */
   public void setOnLoad(String action) {
     setAttribute("onLoad",action);
   }
 
+  /**
+   *
+   */
   public void setOnUnLoad(String action) {
     setAttribute("onUnLoad",action);
   }
@@ -194,22 +279,32 @@ public class Page extends ModuleObjectContainer {
     setOnLoad("window.close()");
   }
 
+  /**
+   *
+   */
   public void setToGoBack() {
     setOnLoad("history.go(-1)");
   }
 
 
-  //Sets the parent (caller) window to reload on Unload
+  /**
+   * Sets the parent (caller) window to reload on Unload
+   */
   public void setParentToReload() {
     setOnUnLoad("window.opener.location.reload()");
   }
 
-  // Displaying an Alert
-  // aron@idega.is
+  /**
+   * Displaying an Alert
+   * aron@idega.is
+   */
   public void setToLoadAlert(String sMessage) {
     setOnLoad("alert('"+sMessage+"')");
   }
 
+  /**
+   *
+   */
   public boolean doPrint(ModuleInfo modinfo) {
     boolean returnBoole;
     if (modinfo.getParameter("idegaspecialrequesttype") == null) {
@@ -225,29 +320,47 @@ public class Page extends ModuleObjectContainer {
     return returnBoole;
   }
 
+  /*
+   *
+   */
   private void setDefaultAttributes(ModuleInfo modinfo) {
     if (!isAttributeSet("bgcolor")) {
       setBackgroundColor(modinfo.getDefaultBackgroundColor());
     }
   }
 
+  /**
+   *
+   */
   public void setToReload() {
     doReload = true;
   }
 
+  /**
+   *
+   */
   public void setToRedirect(String URL) {
     zeroWait = true;
     setToRedirect(URL,0);
   }
 
+  /**
+   *
+   */
   public void setToRedirect(String URL,int secondInterval) {
     redirectInfo = "" + secondInterval + " ;URL=" + URL;
   }
 
+  /**
+   *
+   */
   public String getRedirectInfo() {
     return redirectInfo;
   }
 
+  /*
+   *
+   */
   protected void prepareClone(ModuleObject newObjToCreate) {
    super.prepareClone(newObjToCreate);
    Page newPage = (Page)newObjToCreate;
@@ -264,6 +377,9 @@ public class Page extends ModuleObjectContainer {
    newPage.hoverColor = this.hoverColor;
   }
 
+  /**
+   *
+   */
   public synchronized Object clone() {
     Page obj = null;
     try {
@@ -290,9 +406,9 @@ public class Page extends ModuleObjectContainer {
     return obj;
   }
 
-
-
-
+  /**
+   *
+   */
   public void main(ModuleInfo modinfo) throws Exception {
     if (doReload) {
       if(modinfo.getSession().getAttribute("idega_special_reload") != null) {
@@ -305,6 +421,9 @@ public class Page extends ModuleObjectContainer {
     }
   }
 
+  /*
+   *
+   */
   protected boolean isChildOfOtherPage(){
     boolean isInsideOtherPage = false;
     ModuleObject parent = getParentObject();
@@ -326,6 +445,9 @@ public class Page extends ModuleObjectContainer {
     }
   }
 
+  /**
+   *
+   */
   public void print(ModuleInfo modinfo) throws Exception {
     initVariables(modinfo);
     setDefaultAttributes(modinfo);
@@ -334,9 +456,6 @@ public class Page extends ModuleObjectContainer {
 
 
     if (getLanguage().equals("HTML")) {
-      //StringBuffer buf= new StringBuffer();
-      //buf.append("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\" \"http://www.w3.org/TR/REC-html40/loose.dtd\">\n<html>");
-
       if(!isInsideOtherPage){
         println(getStartTag());
         if(zeroWait) {
@@ -346,10 +465,6 @@ public class Page extends ModuleObjectContainer {
         if (getAssociatedScript() != null) {
           getAssociatedScript()._print(modinfo);
         }
-        //println("\n<meta http-equiv=\"content-type\" content=\"text/html; charset=iso-8859-1\">\n<meta name=\"generator\" content=\"idega arachnea 1.2\">\n<meta name=\"author\" content=\"idega.is\">\n<meta name=\"copyright\" content=\"idega.is\">\n");
-        //if (getRedirectInfo() != null) {
-        //  println("<meta http-equiv=\"refresh\" content=\""+getRedirectInfo()+"\">");
-        //}
 
         println(getMetaInformation(modinfo));
         println("<title>"+getTitle()+"</title>");
@@ -377,14 +492,12 @@ public class Page extends ModuleObjectContainer {
         println("\n</body>");
         println(getEndTag());
       }
-        //}
     }
     else if (getLanguage().equals("WML")) {
       println("<?xml version=\"1.0\"?>");
       println("<!DOCTYPE wml PUBLIC \"-//WAPFORUM//DTD WML 1.1//EN\" \"http://www.wapforum.org/DTD/wml_1.1.xml\">");
       println("<wml>");
       println("<card title=\""+getTitle()+"\" id=\"card1\">");
-
 
       //Catch all exceptions that are thrown in print functions of objects stored inside
       try {
@@ -403,93 +516,82 @@ public class Page extends ModuleObjectContainer {
     }
   }
 
+  /**
+   *
+   */
   public void setProperty(String key, String values[]) {
     if (key.equalsIgnoreCase("title")) {
       setTitle(values[0]);
     }
   }
 
+  /**
+   *
+   */
   public static String getStartTag(){
     return START_TAG;
   }
 
+  /**
+   *
+   */
   public static String getEndTag(){
     return END_TAG;
   }
 
+  /**
+   *
+   */
   public String getMetaInformation(ModuleInfo modinfo){
-      String theReturn = "\n<meta http-equiv=\"content-type\" content=\"text/html; charset=iso-8859-1\">\n<meta name=\"generator\" content=\"idegaWeb 1.3\">\n<meta name=\"author\" content=\"idega.is\">\n<meta name=\"copyright\" content=\"idega.is\">\n";
-      if (getRedirectInfo() != null) {
-        theReturn += "<meta http-equiv=\"refresh\" content=\""+getRedirectInfo()+"\">";
-      }
-      return theReturn;
+    String theReturn = "\n<meta http-equiv=\"content-type\" content=\"text/html; charset=iso-8859-1\">\n<meta name=\"generator\" content=\"idegaWeb 1.3\">\n<meta name=\"author\" content=\"idega.is\">\n<meta name=\"copyright\" content=\"idega.is\">\n";
+    if (getRedirectInfo() != null) {
+      theReturn += "<meta http-equiv=\"refresh\" content=\""+getRedirectInfo()+"\">";
+    }
+    return theReturn;
   }
 
   /**
    * Used to find the Page object to be printed in top of the current page
    */
   public static Page getPage(ModuleInfo modinfo){
-      /*String frameKey = modinfo.getParameter(IW_FRAME_STORAGE_PARMETER);
-
-      if(frameKey!=null){
-
-        Page page = getPage(getFrameStorageInfo(modinfo),modinfo);
-        return page;
-        //return getPageFromSession(modinfo,frameKey);
-      }*/
-
-      //else{
-      /**
-       * Inside a top level page:
-       */
-       Page page =  (Page) IWCoreServlet.retrieveObject(IW_PAGE_KEY);
-       return page;
-      //}
-
-
-      /*Page page = (Page)retrieveObject("idega_page");
-        if (page==null){
-          String servletName = this.getServletConfig().getServletName();
-          String attributeKey =servletName+"_idega_page";
-          Page newPage = (Page)getServletContext().getAttribute(attributeKey);
-          //System.out.println("AttributeKey="+attributeKey+" for getPage()");
-          page = (Page)newPage.clone();
-          storeObject("idega_page",page);
-        }
-        return page;*/
+    Page page =  (Page) IWCoreServlet.retrieveObject(IW_PAGE_KEY);
+    return page;
   }
 
+  /**
+   *
+   */
   public static Page loadPage(ModuleInfo modinfo){
-      String classKey = modinfo.getParameter(IW_FRAME_CLASS_PARAMETER);
-      String frameKey = modinfo.getParameter(IW_FRAME_STORAGE_PARMETER);
+    String classKey = modinfo.getParameter(IW_FRAME_CLASS_PARAMETER);
+    String frameKey = modinfo.getParameter(IW_FRAME_STORAGE_PARMETER);
 
-      if(frameKey!=null){
-
-        Page page = getPage(getFrameStorageInfo(modinfo),modinfo);
-        System.out.println("Page 1");
+    if(frameKey!=null){
+      Page page = getPage(getFrameStorageInfo(modinfo),modinfo);
+      System.out.println("Page 1");
+      return page;
+    }
+    else if(classKey!=null){
+      try{
+      Page page = (Page)Class.forName(classKey).newInstance();
+      return page;
+      }
+      catch(Exception e){
+        Page page = new Page();
+        page.add("Page invalid");
+        page.addBreak();
+        page.add(e.getClass().getName()+"Message: "+e.getMessage());
+        e.printStackTrace();
         return page;
-        //return getPageFromSession(modinfo,frameKey);
       }
-      else if(classKey!=null){
-        try{
-        Page page = (Page)Class.forName(classKey).newInstance();
-        return page;
-        }
-        catch(Exception e){
-          Page page = new Page();
-          page.add("Page invalid");
-          page.addBreak();
-          page.add(e.getClass().getName()+"Message: "+e.getMessage());
-          e.printStackTrace();
-          return page;
-        }
-      }
-      else{
-        return new Page();
-      }
+    }
+    else{
+      return new Page();
+    }
   }
 
-
+  /*
+   *
+   */
   private static FrameStorageInfo getFrameStorageInfo(ModuleInfo modinfo){
     String key = modinfo.getParameter(IW_FRAME_STORAGE_PARMETER);
     FrameStorageInfo info =  (FrameStorageInfo)modinfo.getSessionAttribute(key);
@@ -499,97 +601,98 @@ public class Page extends ModuleObjectContainer {
     return info;
   }
 
-
-
+  /*
+   *
+   */
   private static Page getPage(FrameStorageInfo info,ModuleInfo modinfo){
-      String key = info.getStorageKey();
-      Page theReturn = (Page)modinfo.getSessionAttributeWeak(key);
-      if(theReturn ==null){
-        try{
-          theReturn = (Page)info.getFrameClass().newInstance();
-        }
-        catch(Exception ex){
-          if(theReturn==null){
-            theReturn = new Page("Expired");
-            theReturn.add("This page has expired");
-          }
-          ex.printStackTrace();
-        }
-        storePage(theReturn,modinfo);
+    String key = info.getStorageKey();
+    Page theReturn = (Page)modinfo.getSessionAttributeWeak(key);
+    if (theReturn ==null){
+      try{
+        theReturn = (Page)info.getFrameClass().newInstance();
       }
-      return theReturn;
+      catch(Exception ex){
+        if(theReturn==null){
+          theReturn = new Page("Expired");
+          theReturn.add("This page has expired");
+        }
+        ex.printStackTrace();
+      }
+      storePage(theReturn,modinfo);
+    }
+    return theReturn;
   }
 
-
-
+  /**
+   *
+   */
   public static void storePage(Page page,ModuleInfo modinfo){
-      String storageKey = page.getID();
-      String infoKey=storageKey;
-      FrameStorageInfo info = new FrameStorageInfo(storageKey,page.getClass());
-      modinfo.setSessionAttribute(infoKey,info);
-      modinfo.setSessionAttributeWeak(storageKey,page);
-    }
+    String storageKey = page.getID();
+    String infoKey=storageKey;
+    FrameStorageInfo info = new FrameStorageInfo(storageKey,page.getClass());
+    modinfo.setSessionAttribute(infoKey,info);
+    modinfo.setSessionAttributeWeak(storageKey,page);
+  }
 
-
+  /**
+   *
+   */
   public static void setTopPage(Page page){
     IWCoreServlet.storeObject(IW_PAGE_KEY,page);
   }
 
+  /**
+   *
+   */
   public static boolean isRequestingTopPage(ModuleInfo modinfo){
     return !modinfo.isParameterSet(IW_FRAME_STORAGE_PARMETER);
   }
 
-  /*
-  protected void setFrameProperty(String propertyName,String propertyValue){
-    if(frameProperties ==null){
-      frameProperties = new Hashtable();
-    }
-    frameProperties.put(propertyName,propertyValue);
+  /**
+   * Sets this page to be a template page
+   */
+  public void setIsTemplate() {
+    isTemplate_ = true;
+    isPage_ = false;
+    isDraft_ = false;
   }
 
-  protected void setFrameProperty(String propertyName){
-    setFrameProperty(propertyName,slash);
+  /**
+   * Sets this page to be a "normal" page
+   */
+  public void setIsPage() {
+    isTemplate_ = false;
+    isPage_ = true;
+    isDraft_ = false;
   }
 
-  protected String getFrameProperty(String propertyName){
-    if(frameProperties == null){
-      return null;
-    }
-    return (String)frameProperties.get(propertyName);
+  /**
+   * Sets this page to be a draft
+   */
+  public void setIsDraft() {
+    isTemplate_ = false;
+    isPage_ = false;
+    isDraft_ = true;
   }
 
-  protected String getFramePropertiesString(){
-    StringBuffer returnString = new StringBuffer();
-    String Attribute ="";
-    if (this.attributes != null) {
-      Enumeration e = frameProperties.keys();
-      while (e.hasMoreElements()) {
-        Attribute = (String)e.nextElement();
-        if(!Attribute.equals(ROWS_PROPERTY)){
-          returnString.append(" ");
-          returnString.append(Attribute);
-
-          String AttributeValue = (String)frameProperties.get(Attribute);
-          if(!AttributeValue.equals(slash)){
-
-            returnString.append("=\"");
-            returnString.append(AttributeValue);
-            returnString.append("\" ");
-
-          }
-
-        }
-      }
-    }
-    return returnString.toString();
-  }
-  */
-
-  public void setTemplate(boolean isTemlpate){
-    this.isTemplate=isTemplate;
+  /**
+   *
+   */
+  public boolean getIsTemplate(){
+    return isTemplate_;
   }
 
-  public boolean isTemplate(){
-    return isTemplate;
+  /**
+   *
+   */
+  public boolean getIsPage(){
+    return isPage_;
+  }
+
+  /**
+   *
+   */
+  public boolean getIsDraft(){
+    return isDraft_;
   }
 }
