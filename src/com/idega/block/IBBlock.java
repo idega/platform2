eefc28105e694029c6ad4fd20b1a8340c053ca14
Aron Birkir Gudmundsson <aron@idega.com>
2001-09-20 01:24:40 +0000

    no message

---
+++ src/com/idega/block/IBBlock.java
@@ -0,0 +1,16 @@
+package com.idega.block;
+
+/**
+ * Title:
+ * Description:
+ * Copyright:    Copyright (c) 2000-2001 idega.is All Rights Reserved
+ * Company:      idega
+  *@author <a href="mailto:aron@idega.is">Aron Birkir</a>
+ * @version 1.1
+ */
+
+public interface IBBlock {
+
+  public boolean deleteBlock(int ICObjectInstanceId);
+
+}
