17f36376bc76685260ace5c75ddcd28d1610af9f
Thorhallur Helgason <laddi@idega.com>
2001-11-02 02:15:43 +0000

    no message

---
+++ src/com/idega/block/calendar/business/CalendarViewHandler.java
@@ -0,0 +1,36 @@
+package com.idega.block.calendar.business;
+
+import java.util.List;
+import com.idega.presentation.PresentationObject;
+import com.idega.presentation.IWContext;
+import com.idega.presentation.ui.DropdownMenu;
+import com.idega.presentation.text.Text;
+import com.idega.builder.handler.PropertyHandler;
+
+/**
+ * Title:        idegaclasses
+ * Description:
+ * Copyright:    Copyright (c) 2001
+ * Company:      idega
+ * @author <a href="tryggvi@idega.is">Tryggvi Larusson</a>
+ * @version 1.0
+ */
+
+public class CalendarViewHandler implements PropertyHandler {
+
+  public CalendarViewHandler() {
+  }
+  public List getDefaultHandlerTypes() {
+    return null;
+  }
+  public PresentationObject getHandlerObject(String name,String value,IWContext iwc){
+    DropdownMenu menu = new DropdownMenu(name);
+    menu.addMenuElement("","Select:");
+    menu.addMenuElement(CalendarBusiness.DAY,CalendarBusiness.PARAMETER_DAY);
+    menu.addMenuElement(CalendarBusiness.MONTH,CalendarBusiness.PARAMETER_MONTH);
+    menu.addMenuElement(CalendarBusiness.YEAR,CalendarBusiness.PARAMETER_YEAR);
+    menu.setSelectedElement(value);
+    return menu;
+  }
+
+}
