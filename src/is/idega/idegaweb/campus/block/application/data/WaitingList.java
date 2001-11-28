ecba9204eaa50718cdee79eda45b8521c3b1884e
Pall Helgason <palli@idega.com>
2001-11-28 23:49:21 +0000

    no message

--- src/is/idega/idegaweb/campus/block/application/data/WaitingList.java
+++ src/is/idega/idegaweb/campus/block/application/data/WaitingList.java
@@ -1,5 +1,5 @@
 /*
- * $Id: WaitingList.java,v 1.1 2001/11/08 14:43:05 aron Exp $
+ * $Id: WaitingList.java,v 1.2 2001/11/28 23:49:21 palli Exp $
  *
  * Copyright (C) 2001 Idega hf. All Rights Reserved.
  *
@@ -11,6 +11,7 @@ package is.idega.idegaweb.campus.block.application.data;
 
 
 import com.idega.data.GenericEntity;
+import java.sql.Date;
 import java.sql.SQLException;
 
 /**
@@ -25,6 +26,8 @@ public class WaitingList extends GenericEntity {
   private static final String applicantId_ = "app_applicant_id";
   private static final String order_ = "ordered";
   private static final String type_ = "list_type";
+  private static final String lastConfirmation_ = "last_confirmation";
+  private static final String numberOfRejections_ = "number_of_rejections";
 
   public WaitingList() {
     super();
@@ -41,6 +44,8 @@ public class WaitingList extends GenericEntity {
     addAttribute(applicantId_,"Applicant",true,true,"java.lang.Integer","one-to-many","com.idega.block.application.data.Applicant");
     addAttribute(order_,"Order",true,true,"java.lang.Integer");
     addAttribute(type_,"Waiting list type",true,true,"java.lang.String");
+    addAttribute(lastConfirmation_,"Last confirmation date",true,true,"java.sql.Date");
+    addAttribute(numberOfRejections_,"Number of rejections",true,true,"java.lang.Integer");
     setMaxLength(type_,1);
   }
 
@@ -71,6 +76,15 @@ public class WaitingList extends GenericEntity {
   public static String getOrderColumnName() {
     return(order_);
   }
+
+  public static String getLastConfirmationColumnName() {
+    return(lastConfirmation_);
+  }
+
+  public static String getNumberOfRejectionsColumnName() {
+    return(numberOfRejections_);
+  }
+
 
   public void setComplexId(int id) {
     setColumn(complexId_,id);
@@ -122,5 +136,25 @@ public class WaitingList extends GenericEntity {
 
   public Integer getOrder() {
     return(getIntegerColumnValue(order_));
+  }
+
+  public void setLastConfirmationDate(Date date) {
+    setColumn(lastConfirmation_,date);
+  }
+
+  public Date getLastConfirmationDate() {
+    return((Date)getColumnValue(lastConfirmation_));
+  }
+
+  public void setNumberOfRejections(int count) {
+    setColumn(numberOfRejections_,count);
+  }
+
+  public void setNumberOfRejections(Integer count) {
+    setColumn(numberOfRejections_,count);
+  }
+
+  public int getNumberOfRejections() {
+    return(getIntColumnValue(numberOfRejections_));
   }
 }
