161c60da176b1a06a39df1da5c3fed0fbacaa5fb
Pall Helgason <palli@idega.com>
2002-01-14 11:28:57 +0000

    no message

--- src/is/idega/idegaweb/campus/block/request/data/Request.java
+++ src/is/idega/idegaweb/campus/block/request/data/Request.java
@@ -1,5 +1,5 @@
 /*
- * $Id: Request.java,v 1.2 2002/01/08 12:45:29 aron Exp $
+ * $Id: Request.java,v 1.3 2002/01/14 11:28:57 palli Exp $
  *
  * Copyright (C) 2001 Idega hf. All Rights Reserved.
  *
@@ -21,11 +21,11 @@ import java.sql.Timestamp;
  */
 public class Request extends GenericEntity {
   private static final String ENTITY_NAME = "re_request";
-  private static final String REQUEST_TYPE_ID = "re_request_type_id";
   private static final String USER_ID = "user_id";
   private static final String DESCRIPTION = "description";
   private static final String DATE_SENT = "date_sent";
   private static final String DATE_PROCESSED = "date_processed";
+  private static final String REQUEST_TYPE = "re_request_type";
   private static final String STATUS = "status";
 
   public Request() {
@@ -38,12 +38,13 @@ public class Request extends GenericEntity {
 
   public void initializeAttributes() {
     addAttribute(getIDColumnName());
-    addAttribute(getColumnRequestTypeId(),"Request type id",true,true,Integer.class,GenericEntity.MANY_TO_ONE,RequestType.class);
     addAttribute(getColumnUserId(),"User id",true,true,Integer.class,GenericEntity.MANY_TO_ONE,User.class);
     addAttribute(getColumnDescription(),"Description",true,true,String.class);
     addAttribute(getColumnDateSent(),"Date sent",true,true,Timestamp.class);
     addAttribute(getColumnDateProcessed(),"Date processed",true,true,Timestamp.class);
+    addAttribute(getColumnRequestType(),"Request type",true,true,String.class);
     addAttribute(getColumnStatus(),"Status",true,true,String.class);
+    setMaxLength(getColumnRequestType(),1);
     setMaxLength(getColumnStatus(),1);
     setMaxLength(getColumnDescription(),4000);
   }
@@ -52,8 +53,8 @@ public class Request extends GenericEntity {
     return(ENTITY_NAME);
   }
 
-  public static String getColumnRequestTypeId() {
-    return(REQUEST_TYPE_ID);
+  public static String getColumnRequestType() {
+    return(REQUEST_TYPE);
   }
 
   public static String getColumnUserId() {
@@ -76,16 +77,12 @@ public class Request extends GenericEntity {
     return(STATUS);
   }
 
-  public int getRequestTypeId() {
-    return(getIntColumnValue(getColumnRequestTypeId()));
+  public int getRequestType() {
+    return(getIntColumnValue(getColumnRequestType()));
   }
 
-  public void setRequestTypeId(int type) {
-    setColumn(getColumnRequestTypeId(),type);
-  }
-
-  public void setRequestTypeId(Integer type) {
-    setColumn(getColumnRequestTypeId(),type);
+  public void setRequestType(String type) {
+    setColumn(getColumnRequestType(),type);
   }
 
   public int getUserId() {
