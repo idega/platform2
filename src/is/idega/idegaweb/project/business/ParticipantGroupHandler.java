dff3174836f016374cf267fffa79847594da43b4
Gudmundur Agust Saemundsson <gummi@idega.com>
2002-02-07 00:45:27 +0000

    no message

---
+++ src/is/idega/idegaweb/project/business/ParticipantGroupHandler.java
@@ -0,0 +1,67 @@
+package is.idega.idegaweb.project.business;
+
+import com.idega.builder.handler.PropertyHandler;
+import com.idega.presentation.ui.DropdownMenu;
+import com.idega.presentation.PresentationObject;
+import com.idega.presentation.IWContext;
+import com.idega.builder.dynamicpagetrigger.business.DPTTriggerBusiness;
+import com.idega.builder.dynamicpagetrigger.data.PageTriggerInfo;
+import is.idega.idegaweb.project.data.IPCategoryType;
+import java.util.Iterator;
+import java.util.List;
+import java.sql.SQLException;
+
+
+/**
+ * @author <a href="gummi@idega.is">Guðmundur Ágúst Sæmundsson</a>
+ * @version 1.0
+ */
+
+public class ParticipantGroupHandler implements PropertyHandler {
+  /**
+   *
+   */
+  public ParticipantGroupHandler() {
+  }
+
+  /**
+   *
+   */
+  public List getDefaultHandlerTypes() {
+    return(null);
+  }
+
+  /**
+   *
+   */
+  public PresentationObject getHandlerObject(String name, String stringValue, IWContext iwc) {
+    DropdownMenu menu = new DropdownMenu(name);
+    menu.addMenuElement(-1,"Select");
+
+    try {
+      /**
+       * @todo link to project app (hardcoded dpt_id 1)
+       */
+      List list = DPTTriggerBusiness.getInstance().getDPTPermissionGroups(new PageTriggerInfo(ProjectBusiness.tmpHardcodedPageTriggerInfoId));
+      if (list != null) {
+        Iterator iter = list.iterator();
+        while (iter.hasNext()) {
+          IPCategoryType item = (IPCategoryType)iter.next();
+          menu.addMenuElement(item.getID(),item.getName());
+        }
+      }
+    }
+    catch (SQLException ex) {
+      ex.printStackTrace();
+    }
+    menu.setSelectedElement(stringValue);
+
+    return(menu);
+  }
+
+  /**
+   *
+   */
+  public void onUpdate(String values[], IWContext iwc) {
+  }
+}
