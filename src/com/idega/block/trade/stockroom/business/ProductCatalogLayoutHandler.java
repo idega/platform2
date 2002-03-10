8e2c4e3a55e49f4e2c1b3e2b41db741a19e77a2a
Grimur Jonsson <gimmi@idega.com>
2002-03-10 20:38:27 +0000

    no message

--- src/com/idega/block/trade/stockroom/business/ProductCatalogLayoutHandler.java
+++ src/com/idega/block/trade/stockroom/business/ProductCatalogLayoutHandler.java
@@ -1,55 +1,58 @@
 /*
- * $Id: ProductCatalogLayoutHandler.java,v 1.1 2002/03/09 12:37:36 gimmi Exp $
+ *  $Id: ProductCatalogLayoutHandler.java,v 1.2 2002/03/10 20:38:27 gimmi Exp $
  *
- * Copyright (C) 2001 Idega hf. All Rights Reserved.
+ *  Copyright (C) 2001 Idega hf. All Rights Reserved.
  *
- * This software is the proprietary information of Idega hf.
- * Use is subject to license terms.
+ *  This software is the proprietary information of Idega hf.
+ *  Use is subject to license terms.
  *
  */
 package com.idega.block.trade.stockroom.business;
-
-import com.idega.block.trade.stockroom.presentation.ProductCatalogLayoutProductList;
-import com.idega.block.trade.stockroom.presentation.ProductCatalogLayoutSingleFile;
 import java.util.List;
 import com.idega.presentation.PresentationObject;
 import com.idega.presentation.IWContext;
 import com.idega.presentation.ui.DropdownMenu;
 import com.idega.builder.handler.PropertyHandler;
+import com.idega.block.trade.stockroom.presentation.ProductCatalogLayoutProductList;
+import com.idega.block.trade.stockroom.presentation.ProductCatalogLayoutSingleFile;
+import com.idega.block.trade.stockroom.presentation.ProductCatalogLayoutExpandedList;
 
 /**
- * @author <a href="tryggvi@idega.is">Tryggvi Larusson</a>
- * @version 1.0
+ *@author     <a href="tryggvi@idega.is">Tryggvi Larusson</a>
+ *@created    10. mars 2002
+ *@version    1.0
  */
 public class ProductCatalogLayoutHandler implements PropertyHandler {
-  /**
-   *
-   */
-  public ProductCatalogLayoutHandler() {
-  }
+ /**
+  */
+ public ProductCatalogLayoutHandler() { }
 
-  /**
-   *
-   */
-  public List getDefaultHandlerTypes() {
-    return(null);
-  }
+ /**
+  *@return    The defaultHandlerTypes value
+  */
+ public List getDefaultHandlerTypes() {
+  return ( null );
+ }
 
-  /**
-   *
-   */
-  public PresentationObject getHandlerObject(String name, String value, IWContext iwc) {
-    DropdownMenu menu = new DropdownMenu(name);
-    menu.addMenuElement("","Select:");
-    menu.addMenuElement(ProductCatalogLayoutSingleFile.class.getName(), "Single file");
-    menu.addMenuElement(ProductCatalogLayoutProductList.class.getName(), "Product list");
-    menu.setSelectedElement(value);
-    return(menu);
-  }
+ /**
+  *@param  name   Description of the Parameter
+  *@param  value  Description of the Parameter
+  *@param  iwc    Description of the Parameter
+  *@return        The handlerObject value
+  */
+ public PresentationObject getHandlerObject( String name, String value, IWContext iwc ) {
+  DropdownMenu menu = new DropdownMenu( name );
+  menu.addMenuElement( "", "Select:" );
+  menu.addMenuElement( ProductCatalogLayoutSingleFile.class.getName(), "Single file" );
+  menu.addMenuElement( ProductCatalogLayoutProductList.class.getName(), "Product list" );
+  menu.addMenuElement( ProductCatalogLayoutExpandedList.class.getName(), "Expanded list" );
+  menu.setSelectedElement( value );
+  return ( menu );
+ }
 
-  /**
-   *
-   */
-  public void onUpdate(String values[], IWContext iwc) {
-  }
+ /**
+  *@param  values  Description of the Parameter
+  *@param  iwc     Description of the Parameter
+  */
+ public void onUpdate( String values[], IWContext iwc ) { }
 }
