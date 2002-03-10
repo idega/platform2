fc3247c605dbd483b737dd61b061d8969449a0e5
Grimur Jonsson <gimmi@idega.com>
2002-03-10 10:35:14 +0000

    no message

---
+++ src/com/idega/block/trade/stockroom/business/ProductViewerLayoutHandler.java
@@ -0,0 +1,50 @@
+/*
+ * $Id: ProductViewerLayoutHandler.java,v 1.3 2002/03/10 10:35:39 gimmi Exp $
+ *
+ * Copyright (C) 2001 Idega hf. All Rights Reserved.
+ *
+ * This software is the proprietary information of Idega hf.
+ * Use is subject to license terms.
+ *
+ */
+package com.idega.block.trade.stockroom.business;
+
+import com.idega.block.trade.stockroom.presentation.ProductViewerLayoutIdega;
+import java.util.List;
+import com.idega.presentation.PresentationObject;
+import com.idega.presentation.IWContext;
+import com.idega.presentation.ui.DropdownMenu;
+import com.idega.builder.handler.PropertyHandler;
+
+/**
+ * @author <a href="tryggvi@idega.is">Tryggvi Larusson</a>
+ * @version 1.0
+ */
+public class ProductViewerLayoutHandler implements PropertyHandler {
+  /**
+   *
+   */
+  public ProductViewerLayoutHandler() {
+  }
+
+  public List getDefaultHandlerTypes() {
+    return(null);
+  }
+
+  /**
+   *
+   */
+  public PresentationObject getHandlerObject(String name, String value, IWContext iwc) {
+    DropdownMenu menu = new DropdownMenu(name);
+    menu.addMenuElement("","Select:");
+    menu.addMenuElement(ProductViewerLayoutIdega.class.getName(), "Idega Portal");
+    menu.setSelectedElement(value);
+    return(menu);
+  }
+
+  /**
+   *
+   */
+  public void onUpdate(String values[], IWContext iwc) {
+  }
+}
