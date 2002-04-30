a51c1af161864cf53f86010e0fcac783035cec7e
Gudmundur Agust Saemundsson <gummi@idega.com>
2002-04-30 14:25:48 +0000

    no message

---
+++ src/com/idega/user/data/UserGroupRepresentative.java
@@ -0,0 +1,8 @@
+package com.idega.user.data;
+
+import javax.ejb.*;
+
+public interface UserGroupRepresentative extends com.idega.user.data.Group
+{
+ public java.lang.String getGroupTypeValue();
+}
