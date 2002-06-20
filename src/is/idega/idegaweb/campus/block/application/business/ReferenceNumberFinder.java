6de558342a153673bf70ee53e421f1fe00bd4372
Aron Birkir Gudmundsson <aron@idega.com>
2002-06-20 14:20:06 +0000

    no message

---
+++ src/is/idega/idegaweb/campus/block/application/business/ReferenceNumberFinder.java
@@ -0,0 +1,44 @@
+package is.idega.idegaweb.campus.block.application.business;
+
+import com.idega.idegaweb.IWApplicationContext;
+import com.idega.block.application.business.ReferenceNumberHandler;
+import com.idega.util.CypherText;
+/**
+ * <p>Title: </p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2002</p>
+ * <p>Company: </p>
+ * @author <br><a href="mailto:aron@idega.is">Aron Birkir</a><br>
+ * @version 1.0
+ */
+
+public class ReferenceNumberFinder {
+
+  private static ReferenceNumberFinder finder;
+
+  private String key = "";
+  private CypherText cypherText;
+  private ReferenceNumberHandler refHandler;
+
+  private ReferenceNumberFinder(IWApplicationContext iwac){
+    System.err.println("Creating Finder");
+    refHandler = new ReferenceNumberHandler();
+    cypherText = new CypherText();
+    key = refHandler.getCypherKey(iwac);
+  }
+
+  public static ReferenceNumberFinder getInstance(IWApplicationContext iwac){
+    if(finder ==null)
+      finder = new ReferenceNumberFinder(iwac);
+    return finder;
+
+  }
+
+  public String lookup(int applicationId){
+    String id = Integer.toString(applicationId);
+    while(id.length() < 6)
+      id = "0"+id;
+    return cypherText.doCyper(id,key);
+  }
+
+}
