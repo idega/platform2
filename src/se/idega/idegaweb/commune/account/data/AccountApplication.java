7c96ff272d7d2d33803e0dd33b5176530d6d9d2d
Tryggvi Larusson <tryggvi.larusson@gmail.com>
2002-07-03 16:38:17 +0000

    *** empty log message ***

---
+++ src/se/idega/idegaweb/commune/account/data/AccountApplication.java
@@ -0,0 +1,11 @@
+package se.idega.idegaweb.commune.account.data;
+
+import javax.ejb.*;
+
+public interface AccountApplication extends com.idega.block.process.data.Case
+{
+ public void setEmail(java.lang.String p0) throws java.rmi.RemoteException;
+ public java.lang.String getEmail() throws java.rmi.RemoteException;
+ public void setApplicantName(java.lang.String p0) throws java.rmi.RemoteException;
+ public java.lang.String getApplicantName() throws java.rmi.RemoteException;
+}
