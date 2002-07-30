eca98cc9b611092933a1ba5d1657e3de6689e133
Aron Birkir Gudmundsson <aron@idega.com>
2002-07-30 12:23:13 +0000

    added school admin root group business

--- src/se/idega/idegaweb/commune/business/CommuneUserBusiness.java
+++ src/se/idega/idegaweb/commune/business/CommuneUserBusiness.java
@@ -10,4 +10,5 @@ public interface CommuneUserBusiness extends com.idega.business.IBOService
  public com.idega.user.data.User createSchoolAdministrator(java.lang.String p0,java.lang.String p1,java.lang.String p2,com.idega.block.school.data.School p3)throws javax.ejb.FinderException,javax.ejb.CreateException, java.rmi.RemoteException;
  public com.idega.user.data.User createCitizenByPersonalIDIfDoesNotExist(String firstName, String middleName, String lastName,String personalID) throws javax.ejb.CreateException,java.rmi.RemoteException;
  public com.idega.user.data.Group getRootCitizenGroup()throws javax.ejb.CreateException, javax.ejb.FinderException, java.rmi.RemoteException;
+ public com.idega.user.data.Group getRootSchoolAdministratorGroup()throws  javax.ejb.CreateException, javax.ejb.FinderException,java.rmi.RemoteException;
 }
