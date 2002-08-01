62f546a43541ba78c35552247eb0ac98374c75d7
Aron Birkir Gudmundsson <aron@idega.com>
2002-08-01 18:51:25 +0000

    *** empty log message ***

--- src/is/idega/idegaweb/campus/block/phone/business/PhoneFileHandler.java
+++ src/is/idega/idegaweb/campus/block/phone/business/PhoneFileHandler.java
@@ -114,163 +114,164 @@ public class PhoneFileHandler {
 
 
   public void process(File PhoneFile)throws java.rmi.RemoteException{
+  	process3(PhoneFile);
 
-    Map M = PhoneFinder.mapOfAccountsByPhoneNumber();
-    // If we can assess something
-    if( M != null ){
-      try{
-
-        FileReader fin = new FileReader(PhoneFile);
-        LineNumberReader lin = new LineNumberReader(fin);
-
-        javax.transaction.TransactionManager t = com.idega.transaction.IdegaTransactionManager.getInstance();
-        try{
-          t.begin();
-
-          String line = null;
-          StringTokenizer st;
-          StringBuffer sbError = new StringBuffer(),sbNoAccount = new StringBuffer();;
-          String anumber,snumber,bnumber;
-          int nightsec,daysec,sec;
-          float price,totPrice = 0;
-          idegaTimestamp stamp;
-          int count = 10;
-          int linecount = 1, noAccountCount = 0,errorCount = 0,numberCount = 0;
-          Vector vError = new Vector();
-          Vector vNoAccount = new Vector();
-          Hashtable phoneNumbers = new Hashtable();
-          AccountPhoneEntry ape;
-          Integer iAccountId;
-          Account eAccount;
-          boolean cont = false;
-          while( (line = lin.readLine()) != null ){//&& count != 0){
-            cont = false;
-            st = new StringTokenizer(line,";");
-            if(st.countTokens() == 8){
-              ape = ((com.idega.block.finance.data.AccountPhoneEntryHome)com.idega.data.IDOLookup.getHomeLegacy(AccountPhoneEntry.class)).createLegacy();
-              try{
-                anumber = st.nextToken().trim();
-                snumber = st.nextToken().trim();
-                bnumber = st.nextToken().trim();
-                String s = st.nextToken().trim();
-                stamp = parseStamp(s);
-                nightsec = Integer.parseInt(st.nextToken());
-                daysec = Integer.parseInt(st.nextToken());
-                sec = Integer.parseInt(st.nextToken());
-                price = Float.parseFloat(st.nextToken());
-                ape.setLastUpdated(idegaTimestamp.getTimestampRightNow());
-                ape.setDayDuration(daysec);
-                ape.setNightDuration(nightsec);
-                ape.setDuration(sec);
-                ape.setMainNumber(anumber);
-                ape.setPhonedStamp(stamp.getTimestamp());
-                ape.setPhoneNumber(bnumber);
-                ape.setPrice(price);
-                ape.setSubNumber(snumber);
-                ape.setStatus(com.idega.block.finance.data.AccountPhoneEntryBMPBean.statusUnread);
-                cont = true;
-              }
-              catch(Exception ex){
-                System.err.println("error in line "+linecount);
-                cont = false;
-              }
-              // valid line in file
-              if(cont){
-                String number = ape.getSubNumber();
-                // account for phonenumber exist
-                if(phoneNumbers.containsKey(number)){
-                  Integer ncount = (Integer) phoneNumbers.get(number);
-                  phoneNumbers.put( number , new Integer(ncount.intValue()+1));
-                }
-                else{
-                  phoneNumbers.put( number, new Integer(1));
-                  numberCount++;
-                }
-
-                if(M!=null && M.containsKey(number)){
-                  //iAccountId = (Integer) M.get(number);
-                  eAccount = (Account) M.get(number);
-                  ape.setAccountId(((Integer)eAccount.getPrimaryKey()).intValue());
-                  ape.setStatus(com.idega.block.finance.data.AccountPhoneEntryBMPBean.statusRead);
-                  eAccount.addAmount(new Float(ape.getPrice()));
-                  ape.insert();
-                  eAccount.store();
-                  totPrice += ape.getPrice();
-                }
-                // account for phonenumber doesn´t exist
-                else{
-                  sbNoAccount.append(line);
-                  sbNoAccount.append("\n");
-                  noAccountCount++;
-                }
-              }
-              // invalid line in file
-              else{
-                 System.err.println("error in line "+linecount +" : parsing error");
-                 sbError.append(line);
-                 sbError.append("\n");
-                 errorCount++;
-              }
-
-
-            }
-            else{
-              System.err.println("error in line "+linecount +" : too few columns");
-              sbError.append(line);
-              sbError.append("\n");
-              errorCount++;
-            }
-            count--;
-            linecount++;
-          }// while
-          PhoneFileInfo pfi = ((is.idega.idegaweb.campus.block.phone.data.PhoneFileInfoHome)com.idega.data.IDOLookup.getHomeLegacy(PhoneFileInfo.class)).createLegacy();
-          pfi.setDateRead(idegaTimestamp.getTimestampRightNow());
-          pfi.setLineCount(linecount-1);
-          pfi.setErrorCount(errorCount );
-          pfi.setNoAccountCount(noAccountCount );
-          pfi.setFileName(PhoneFile.getName());
-          pfi.setNumberCount(numberCount);
-          pfi.setTotalAmount(totPrice);
-          pfi.insert();
-
-          if(errorCount > 0){
-            FileWriter out = new FileWriter(new File(PhoneFile.getParentFile(),"e_"+PhoneFile.getName()));
-            BufferedWriter bout = new BufferedWriter(out);
-            bout.write(sbError.toString());
-            bout.close();
-            out.close();
-          }
-          if(noAccountCount > 0){
-            FileWriter out = new FileWriter(new File(PhoneFile.getParentFile(),"n_"+PhoneFile.getName()));
-            BufferedWriter bout = new BufferedWriter(out);
-            bout.write(sbNoAccount.toString());
-            bout.close();
-            out.close();
-          }
-
-
-          t.commit();
-        }
-        catch(Exception e){
-          try {
-              t.rollback();
-            }
-            catch(javax.transaction.SystemException ex) {
-              ex.printStackTrace();
-            }
-            e.printStackTrace();
-        }
-      }
-      catch(FileNotFoundException fnfe){
-        fnfe.printStackTrace();
-      }
-      /*catch(IOException fnfe){
-        fnfe.printStackTrace();
-      }*/
-    }
-    else{
-      System.err.println("no accounts behind phonenumbers");
-    }
+//    Map M = PhoneFinder.mapOfAccountsByPhoneNumber();
+//    // If we can assess something
+//    if( M != null ){
+//      try{
+//
+//        FileReader fin = new FileReader(PhoneFile);
+//        LineNumberReader lin = new LineNumberReader(fin);
+//
+//        javax.transaction.TransactionManager t = com.idega.transaction.IdegaTransactionManager.getInstance();
+//        try{
+//          t.begin();
+//
+//          String line = null;
+//          StringTokenizer st;
+//          StringBuffer sbError = new StringBuffer(),sbNoAccount = new StringBuffer();;
+//          String anumber,snumber,bnumber;
+//          int nightsec,daysec,sec;
+//          float price,totPrice = 0;
+//          idegaTimestamp stamp;
+//          int count = 10;
+//          int linecount = 1, noAccountCount = 0,errorCount = 0,numberCount = 0;
+//          Vector vError = new Vector();
+//          Vector vNoAccount = new Vector();
+//          Hashtable phoneNumbers = new Hashtable();
+//          AccountPhoneEntry ape;
+//          Integer iAccountId;
+//          Account eAccount;
+//          boolean cont = false;
+//          while( (line = lin.readLine()) != null ){//&& count != 0){
+//            cont = false;
+//            st = new StringTokenizer(line,";");
+//            if(st.countTokens() == 8){
+//              ape = ((com.idega.block.finance.data.AccountPhoneEntryHome)com.idega.data.IDOLookup.getHomeLegacy(AccountPhoneEntry.class)).createLegacy();
+//              try{
+//                anumber = st.nextToken().trim();
+//                snumber = st.nextToken().trim();
+//                bnumber = st.nextToken().trim();
+//                String s = st.nextToken().trim();
+//                stamp = parseStamp(s);
+//                nightsec = Integer.parseInt(st.nextToken());
+//                daysec = Integer.parseInt(st.nextToken());
+//                sec = Integer.parseInt(st.nextToken());
+//                price = Float.parseFloat(st.nextToken());
+//                ape.setLastUpdated(idegaTimestamp.getTimestampRightNow());
+//                ape.setDayDuration(daysec);
+//                ape.setNightDuration(nightsec);
+//                ape.setDuration(sec);
+//                ape.setMainNumber(anumber);
+//                ape.setPhonedStamp(stamp.getTimestamp());
+//                ape.setPhoneNumber(bnumber);
+//                ape.setPrice(price);
+//                ape.setSubNumber(snumber);
+//                ape.setStatus(com.idega.block.finance.data.AccountPhoneEntryBMPBean.statusUnread);
+//                cont = true;
+//              }
+//              catch(Exception ex){
+//                System.err.println("error in line "+linecount);
+//                cont = false;
+//              }
+//              // valid line in file
+//              if(cont){
+//                String number = ape.getSubNumber();
+//                // account for phonenumber exist
+//                if(phoneNumbers.containsKey(number)){
+//                  Integer ncount = (Integer) phoneNumbers.get(number);
+//                  phoneNumbers.put( number , new Integer(ncount.intValue()+1));
+//                }
+//                else{
+//                  phoneNumbers.put( number, new Integer(1));
+//                  numberCount++;
+//                }
+//
+//                if(M!=null && M.containsKey(number)){
+//                  //iAccountId = (Integer) M.get(number);
+//                  eAccount = (Account) M.get(number);
+//                  ape.setAccountId(((Integer)eAccount.getPrimaryKey()).intValue());
+//                  ape.setStatus(com.idega.block.finance.data.AccountPhoneEntryBMPBean.statusRead);
+//                  eAccount.addAmount(new Float(ape.getPrice()));
+//                  ape.insert();
+//                  eAccount.store();
+//                  totPrice += ape.getPrice();
+//                }
+//                // account for phonenumber doesn´t exist
+//                else{
+//                  sbNoAccount.append(line);
+//                  sbNoAccount.append("\n");
+//                  noAccountCount++;
+//                }
+//              }
+//              // invalid line in file
+//              else{
+//                 System.err.println("error in line "+linecount +" : parsing error");
+//                 sbError.append(line);
+//                 sbError.append("\n");
+//                 errorCount++;
+//              }
+//
+//
+//            }
+//            else{
+//              System.err.println("error in line "+linecount +" : too few columns");
+//              sbError.append(line);
+//              sbError.append("\n");
+//              errorCount++;
+//            }
+//            count--;
+//            linecount++;
+//          }// while
+//          PhoneFileInfo pfi = ((is.idega.idegaweb.campus.block.phone.data.PhoneFileInfoHome)com.idega.data.IDOLookup.getHomeLegacy(PhoneFileInfo.class)).createLegacy();
+//          pfi.setDateRead(idegaTimestamp.getTimestampRightNow());
+//          pfi.setLineCount(linecount-1);
+//          pfi.setErrorCount(errorCount );
+//          pfi.setNoAccountCount(noAccountCount );
+//          pfi.setFileName(PhoneFile.getName());
+//          pfi.setNumberCount(numberCount);
+//          pfi.setTotalAmount(totPrice);
+//          pfi.insert();
+//
+//          if(errorCount > 0){
+//            FileWriter out = new FileWriter(new File(PhoneFile.getParentFile(),"e_"+PhoneFile.getName()));
+//            BufferedWriter bout = new BufferedWriter(out);
+//            bout.write(sbError.toString());
+//            bout.close();
+//            out.close();
+//          }
+//          if(noAccountCount > 0){
+//            FileWriter out = new FileWriter(new File(PhoneFile.getParentFile(),"n_"+PhoneFile.getName()));
+//            BufferedWriter bout = new BufferedWriter(out);
+//            bout.write(sbNoAccount.toString());
+//            bout.close();
+//            out.close();
+//          }
+//
+//
+//          t.commit();
+//        }
+//        catch(Exception e){
+//          try {
+//              t.rollback();
+//            }
+//            catch(javax.transaction.SystemException ex) {
+//              ex.printStackTrace();
+//            }
+//            e.printStackTrace();
+//        }
+//      }
+//      catch(FileNotFoundException fnfe){
+//        fnfe.printStackTrace();
+//      }
+//      /*catch(IOException fnfe){
+//        fnfe.printStackTrace();
+//      }*/
+//    }
+//    else{
+//      System.err.println("no accounts behind phonenumbers");
+//    }
   }
 
   public void process3(File PhoneFile)throws java.rmi.RemoteException{
