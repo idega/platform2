543d4f94a23d8629b52668184ba73754c20bb9bc
Tryggvi Larusson <tryggvi.larusson@gmail.com>
2002-08-29 13:28:49 +0000

    added a primary key column

--- src/com/idega/block/staff/data/StaffMetaBMPBean.java
+++ src/com/idega/block/staff/data/StaffMetaBMPBean.java
@@ -32,6 +32,9 @@ public class StaffMetaBMPBean extends com.idega.block.staff.data.StaffMetaDataBM
     }
 
     public void initializeAttributes(){
+    	//Adding fake primary key
+    	addAttribute(getIDColumnName());
+
       addAttribute(getColumnNameUserID(),"User",true,true,Integer.class,"many-to-one",com.idega.core.user.data.User.class);
       addAttribute(getColumnNameLocaleId(), "Locale", true, true, Integer.class,"many_to_one",com.idega.core.data.ICLocale.class);
       addAttribute(getColumnNameAttribute(),"Attribute",true,true,String.class);
