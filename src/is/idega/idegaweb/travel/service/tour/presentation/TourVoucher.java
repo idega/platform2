e47bc43ffc6d57462c6dcbbd8838c1f6dfd79837
Grimur Jonsson <gimmi@idega.com>
2002-09-09 08:54:22 +0000

    no message

--- src/is/idega/idegaweb/travel/service/tour/presentation/TourVoucher.java
+++ src/is/idega/idegaweb/travel/service/tour/presentation/TourVoucher.java
@@ -22,8 +22,8 @@ public class TourVoucher extends Voucher{
   }
 
   public void main(IWContext iwc) throws Exception{
-    setupVoucher(iwc);
     super.main(iwc);
+    setupVoucher(iwc);
   }
   protected void setupVoucher(IWContext iwc) throws RemoteException {
     IWResourceBundle iwrb = super.getResourceBundle(iwc);
