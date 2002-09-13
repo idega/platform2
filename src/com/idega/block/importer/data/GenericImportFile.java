50c8668faf66d9f71754ed11026088b8fd540631
Eirikur Hrafnsson <eiki@idega.com>
2002-09-13 01:35:18 +0000

    added valueseparator stuff

--- src/com/idega/block/importer/data/GenericImportFile.java
+++ src/com/idega/block/importer/data/GenericImportFile.java
@@ -1,11 +1,17 @@
 package com.idega.block.importer.data;
 
-import com.idega.block.importer.business.*;
-import com.idega.util.Timer;
-import java.util.Collection;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.StringTokenizer;
 
-import java.io.*;
+import com.idega.block.importer.business.NoRecordsException;
+import com.idega.util.Timer;
+import com.idega.util.text.TextSoap;
 
 /**
  * <p>Title: IdegaWeb classes</p>
@@ -17,149 +23,235 @@ import java.io.*;
  */
 
 public class GenericImportFile implements ImportFile{
+	
+	  private File file;
+	  private String recordDilimiter = "\n";
+	  private String valueSeparator = ";";
+	  private String ignoreIfFoundValue = " ";
+	  private FileReader fr;
+	  private BufferedReader br;
+	  private boolean addNewLineAfterRecord = false;
 
-  private File file;
-  private String recordDilimiter = "\n";
-  private FileReader fr;
-  private BufferedReader br;
+	
+	  public GenericImportFile(File file) {
+	    this.file = file;
+	  }
+	
+	  /**
+	  *
+	  * @return the String value of recordDilimiter.
+	  */
+	  public String getRecordDilimiter(){
+	        return recordDilimiter;
+	  }
+	
+	  /**
+	  *
+	  * @param aRecordDilimiter - the new value for recordDilimiter
+	  */
+	  public void setRecordDilimiter(String aRecordDilimiter){
+	        recordDilimiter = aRecordDilimiter;
+	  }
+	
+	
+	 // public Collection getRecords(){return null;}
+	 // public Object getRecordAtIndex(int i){return null;}
+	
+	 /**
+	  * This method works like an iterator but
+	  */
+	 public Object getNextRecord(){
+	  String line;
+	  StringBuffer buf = new StringBuffer();
+	
+	  try {
+	    if( fr == null ){
+	      fr = new FileReader(getFile());
+	      br = new BufferedReader(fr);
+	    }
+	
+	    while ( ( (line=br.readLine()) != null ) && ( line.indexOf(getRecordDilimiter())== -1 ) ){
+	      buf.append(line);
+	      if( addNewLineAfterRecord ){
+	      	buf.append('\n');
+	      }
+	    }
+	
+	    return buf.toString();
+	  }
+	  catch( FileNotFoundException ex ){
+	    ex.printStackTrace(System.err);
+	    return null;
+	  }
+	  catch( IOException ex ){
+	    ex.printStackTrace(System.err);
+	    return null;
+	  }
+	
+	
+	 }
+	
+	 /**
+	  * @deprecated
+	  * This method parses the file into records (ArrayList) and returns the complete list.<p>
+	  *  it throws a NoRecordsFoundException if no records where found.
+	  */
+	  public Collection getRecords() throws NoRecordsException{
+	
+	    try{
+	      fr = new FileReader(getFile());
+	      br = new BufferedReader(fr);
+	      String line;
+	      StringBuffer buf = new StringBuffer();
+	      ArrayList list = new ArrayList();
+	
+	      int cnt = 0;
+	      int records = 0;
+	
+	      Timer clock = new Timer();
+	      clock.start();
+	
+	      while ( (line=br.readLine()) != null){
+	        if( buf == null ){
+	          buf = new StringBuffer();
+	        }
+	
+	        buf.append(line);
+	
+	        /**@todo this should be an option with a setMethod?**/
+	    if(addNewLineAfterRecord){
+	    	buf.append('\n');
+	    }
+	
+	    if( line.indexOf(getRecordDilimiter())!= -1 ){
+	      records++;
+	      if( (records % 1000) == 0 ){
+	        System.out.println("Importer: Reading record nr.: "+records+" from file "+getFile().getName());
+	      }
+	
+	     list.add(buf.toString());
+	     buf = null;
+	    }
+	
+	    cnt++;
+	  }
+	
+	  line = null;
+	  buf = null;
+	
+	  br.close();
+	  fr = null;
+	  br = null;
+	
+	  clock.stop();
+	
+	  if( records == 0 ){
+	   throw new NoRecordsException("No records where found in the selected file"+file.getAbsolutePath());
+	  }
+	
+	  //System.gc();
+	
+	  System.out.println("Time for operation: "+clock.getTime()+" ms  OR "+((int)(clock.getTime()/1000))+" s");
+	  System.out.println("Number of Lines: "+cnt);
+	  System.out.println("Number of records = "+records);
+	
+	      return list;
+	    }
+	    catch( FileNotFoundException ex ){
+	      ex.printStackTrace(System.err);
+	      return null;
+	    }
+	    catch( IOException ex ){
+	      ex.printStackTrace(System.err);
+	      return null;
+	    }
+	
+	  }
+	
+	
+	
+	
+	  protected File getFile(){
+	    return file;
+	  }
 
-  public GenericImportFile(File file) {
-    this.file = file;
-  }
-
-  /**
-  *
-  * @return the String value of recordDilimiter.
-  */
-  public String getRecordDilimiter(){
-        return recordDilimiter;
-  }
-
-  /**
-  *
-  * @param aRecordDilimiter - the new value for recordDilimiter
-  */
-  public void setRecordDilimiter(String aRecordDilimiter){
-        recordDilimiter = aRecordDilimiter;
-  }
-
-
- // public Collection getRecords(){return null;}
- // public Object getRecordAtIndex(int i){return null;}
-
- /**
-  * This method works like an iterator but
-  */
- public Object getNextRecord(){
-  String line;
-  StringBuffer buf = new StringBuffer();
-
-  try {
-    if( fr == null ){
-      fr = new FileReader(getFile());
-      br = new BufferedReader(fr);
-    }
-
-    while ( ( (line=br.readLine()) != null ) && ( line.indexOf(getRecordDilimiter())== -1 ) ){
-      buf.append(line);
-      /**@todo this should be an option with a setMethod?**/
-      buf.append('\n');
-    }
-
-    return buf.toString();
-  }
-  catch( FileNotFoundException ex ){
-    ex.printStackTrace(System.err);
-    return null;
-  }
-  catch( IOException ex ){
-    ex.printStackTrace(System.err);
-    return null;
-  }
+	/**
+	 * Returns the addNewLineAfterRecord.
+	 * @return boolean
+	 */
+	public boolean isAddNewLineAfterRecord() {
+		return addNewLineAfterRecord;
+	}
+	
+	/**
+	 * Sets if to add a \n after each record
+	 * @param addNewLineAfterRecord The addNewLineAfterRecord to set
+	 */
+	public void setAddNewLineAfterRecord(boolean addNewLineAfterRecord) {
+		this.addNewLineAfterRecord = addNewLineAfterRecord;
+	}
+	
+	/**
+	 * Returns the valueSeparator.
+	 * @return String
+	 */
+	public String getValueSeparator() {
+		return valueSeparator;
+	}
+	
+	/**
+	 * Sets the file.
+	 * @param file The file to set
+	 */
+	public void setFile(File file) {
+		this.file = file;
+	}
+	
+	/**
+	 * Sets the valueSeparator.
+	 * @param valueSeparator The valueSeparator to set
+	 */
+	public void setValueSeparator(String valueSeparator) {
+		this.valueSeparator = valueSeparator;
+	}
 
 
- }
-
- /**
-  * @deprecated
-  * This method parses the file into records (ArrayList) and returns the complete list.<p>
-  *  it throws a NoRecordsFoundException if no records where found.
-  */
-  public Collection getRecords() throws NoRecordsException{
-
-    try{
-      fr = new FileReader(getFile());
-      br = new BufferedReader(fr);
-      String line;
-      StringBuffer buf = new StringBuffer();
-      ArrayList list = new ArrayList();
-
-      int cnt = 0;
-      int records = 0;
-
-      Timer clock = new Timer();
-      clock.start();
-
-      while ( (line=br.readLine()) != null){
-        if( buf == null ){
-          buf = new StringBuffer();
-        }
-
-        buf.append(line);
-
-        /**@todo this should be an option with a setMethod?**/
-        buf.append('\n');
-
-        if( line.indexOf(getRecordDilimiter())!= -1 ){
-          records++;
-          if( (records % 1000) == 0 ){
-            System.out.println("Importer: Reading record nr.: "+records+" from file "+getFile().getName());
-          }
-
-         list.add(buf.toString());
-         buf = null;
-        }
-
-        cnt++;
-      }
-
-      line = null;
-      buf = null;
-
-      br.close();
-      fr = null;
-      br = null;
-
-      clock.stop();
-
-      if( records == 0 ){
-       throw new NoRecordsException("No records where found in the selected file"+file.getAbsolutePath());
-      }
-
-      //System.gc();
-
-      System.out.println("Time for operation: "+clock.getTime()+" ms  OR "+((int)(clock.getTime()/1000))+" s");
-      System.out.println("Number of Lines: "+cnt);
-      System.out.println("Number of records = "+records);
-
-      return list;
-    }
-    catch( FileNotFoundException ex ){
-      ex.printStackTrace(System.err);
-      return null;
-    }
-    catch( IOException ex ){
-      ex.printStackTrace(System.err);
-      return null;
-    }
-
-  }
+	/**
+	 * Method getValueAtIndexFromRecordString. Uses the valueSeparator and a stringtokenizer to read and return a value at a selected index.
+	 * @param index The index starts at 1
+	 * @param recordString
+	 * @return The value at the selected index or null.
+	 */
+	public String getValueAtIndexFromRecordString(int index, String recordString){		
+		int i = 1;
+		recordString = TextSoap.findAndReplace(recordString,valueSeparator+valueSeparator,valueSeparator+ignoreIfFoundValue+valueSeparator);
+		StringTokenizer tokens = new StringTokenizer(recordString,valueSeparator);
+		String value = null;
+		while( tokens.hasMoreTokens() && i<=index ){
+			value = tokens.nextToken();	
+			System.out.println("GenericImportFile : index = "+index+" value = "+value);
+			i++;
+		}
+		
+		
+		return value;
+	} 
 
 
+	/**
+	 * Returns the ignoreIfFoundValue.
+	 * @return String
+	 */
+	public String getIgnoreIfFoundValue() {
+		return ignoreIfFoundValue;
+	}
 
-
-  protected File getFile(){
-    return file;
-  }
+	/**
+	 * Sets the ignoreIfFoundValue. When calling getValueAtIndexFromRecordString if this string is found the method will return null.
+	 * @param ignoreIfFoundValue The ignoreIfFoundValue to set
+	 */
+	public void setIgnoreIfFoundValue(String ignoreIfFoundValue) {
+		this.ignoreIfFoundValue = ignoreIfFoundValue;
+	}
 
 }
