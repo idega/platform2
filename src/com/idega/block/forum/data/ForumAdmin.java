dc9744c5c2cec0fde688ce3c5298b8a46c13cc1f
Thorhallur Helgason <laddi@idega.com>
2002-10-14 16:15:22 +0000

    Removed all old Forum classes.

--- src/com/idega/block/forum/data/ForumAdmin.java
+++
@@ -1,9 +0,0 @@
-package com.idega.block.forum.data;
-
-import javax.ejb.*;
-
-public interface ForumAdmin extends com.idega.data.IDOLegacyEntity
-{
- public boolean getUseForums();
- public void setUseForums(boolean p0);
-}
--- src/com/idega/block/forum/data/ForumConfig.java
+++
@@ -1,7 +0,0 @@
-package com.idega.block.forum.data;
-
-import javax.ejb.*;
-
-public interface ForumConfig extends com.idega.data.IDOLegacyEntity
-{
-}
