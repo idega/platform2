864e12ec53dd288e6f44eca6a826b24656a0700c
Pall Helgason <palli@idega.com>
2002-10-16 01:25:51 +0000

    Added javascript, but it is disabled. Small fixes left.

--- src/se/idega/idegaweb/commune/childcare/presentation/ChildCareApplicationForm.java
+++ src/se/idega/idegaweb/commune/childcare/presentation/ChildCareApplicationForm.java
@@ -57,7 +57,6 @@ public class ChildCareApplicationForm extends CommuneBlock {
 
 	private final static String ERROR_NO_CHECKS = "cca_no_checks";
 
-
 	private final static String SUBJECT = "cca_subject";
 	private final static String PROVIDERS = "cca_providers";
 	private final static String LAST_NAME = "cca_last_name";
@@ -79,7 +78,7 @@ public class ChildCareApplicationForm extends CommuneBlock {
 	private final static String NOT_LOGGED_IN = "cca_not_logged_in";
 	private final static String APPLICATION_INSERTED = "cca_application_ok";
 	private final static String APPLICATION_FAILURE = "cca_application_failed";
-	
+
 	private final static String EMAIL_PROVIDER_SUBJECT = "cca_provider_email_subject";
 	private final static String EMAIL_PROVIDER_MESSAGE = "cca_provider_email_message";
 
@@ -88,10 +87,10 @@ public class ChildCareApplicationForm extends CommuneBlock {
 	protected IWBundle _iwb;
 	protected IWResourceBundle _iwrb;
 
-	protected int _valProvider[] = {-1, -1, -1, -1, -1};
-	protected int _valArea[] = {-1, -1, -1, -1, -1};
-	protected String _valDate[] = {null, null, null, null, null};
-	
+	protected int _valProvider[] = { -1, -1, -1, -1, -1 };
+	protected int _valArea[] = { -1, -1, -1, -1, -1 };
+	protected String _valDate[] = { null, null, null, null, null };
+
 	protected Collection _areas = null;
 	protected Collection _providers = null;
 
@@ -145,7 +144,7 @@ public class ChildCareApplicationForm extends CommuneBlock {
 			}
 		}
 		else if (iwc.isParameterSet(PARAM_CHECK_ID)) {
-			return ACTION_VIEW_FORM;		
+			return ACTION_VIEW_FORM;
 		}
 		else {
 			return ACTION_VIEW_CHECKS;
@@ -159,7 +158,7 @@ public class ChildCareApplicationForm extends CommuneBlock {
 	 */
 	private void viewChecks(IWContext iwc) {
 		Collection checks = null;
-		
+
 		try {
 			checks = getCheckBusiness(iwc).findAllApprovedChecksByUser(_user);
 		}
@@ -167,11 +166,11 @@ public class ChildCareApplicationForm extends CommuneBlock {
 		}
 		catch (Exception e) {
 		}
-		
+
 		if (checks != null && !checks.isEmpty()) {
 			Iterator it = checks.iterator();
 			while (it.hasNext()) {
-				Check check = (Check)it.next();
+				Check check = (Check) it.next();
 				User child = null;
 				try {
 					child = getCheckBusiness(iwc).getUserById(check.getChildId());
@@ -180,26 +179,26 @@ public class ChildCareApplicationForm extends CommuneBlock {
 				}
 				catch (Exception e) {
 				}
-				
+
 				Link link = null;
 				try {
 					if (child != null) {
 						link = new Link(child.getName());
-						link.addParameter(PARAM_CHECK_ID, ((Integer) check.getPrimaryKey()).toString());						
+						link.addParameter(PARAM_CHECK_ID, ((Integer) check.getPrimaryKey()).toString());
 					}
 				}
-				catch (RemoteException e) {					
+				catch (RemoteException e) {
 				}
-				
+
 				if (link != null) {
 					add(link);
 					add(Text.BREAK);
 				}
-			}			
+			}
 		}
 		else {
-			add(getErrorText(localize(ERROR_NO_CHECKS,"This user has no checks")));
-		}			
+			add(getErrorText(localize(ERROR_NO_CHECKS, "This user has no checks")));
+		}
 	}
 
 	private void viewForm(IWContext iwc) {
@@ -207,7 +206,7 @@ public class ChildCareApplicationForm extends CommuneBlock {
 		form.setName(PARAM_FORM_NAME);
 
 		String checkId = iwc.getParameter(PARAM_CHECK_ID);
-		form.addParameter(PARAM_CHECK_ID,checkId);
+		form.addParameter(PARAM_CHECK_ID, checkId);
 		Check check = null;
 		try {
 			check = getCheckBusiness(iwc).getCheck(new Integer(checkId).intValue());
@@ -216,7 +215,7 @@ public class ChildCareApplicationForm extends CommuneBlock {
 		}
 		catch (Exception e) {
 		}
-		
+
 		User child = null;
 		if (check != null) {
 			try {
@@ -235,19 +234,19 @@ public class ChildCareApplicationForm extends CommuneBlock {
 		nameTable1.setColor(2, 1, getBackgroundColor());
 		nameTable1.add(getSmallText(localize(LAST_NAME, "Last name")), 1, 1);
 		nameTable1.add(getSmallText(localize(FIRST_NAME, "First name")), 2, 1);
-		if (child != null) {				
+		if (child != null) {
 			try {
 				nameTable1.add(getSmallText(child.getLastName()), 1, 2);
 				nameTable1.add(getSmallText(child.getFirstName()), 2, 2);
 			}
 			catch (RemoteException e) {
 			}
-			
+
 			try {
-				form.addParameter(PARAM_CHILD_ID,((Integer)child.getPrimaryKey()).intValue());
+				form.addParameter(PARAM_CHILD_ID, ((Integer) child.getPrimaryKey()).intValue());
 			}
 			catch (RemoteException e) {
-			}			
+			}
 		}
 
 		Table nameTable2 = new Table(2, 2);
@@ -261,10 +260,10 @@ public class ChildCareApplicationForm extends CommuneBlock {
 			try {
 				Address address = getCheckBusiness(iwc).getUserAddress(child);
 				PostalCode code = getCheckBusiness(iwc).getUserPostalCode(child);
-				if (address != null) 
+				if (address != null)
 					nameTable2.add(getSmallText(address.getStreetName() + " " + address.getStreetNumber()), 1, 2);
 				if (code != null)
-					nameTable2.add(getSmallText(code.getPostalCode() + " " + code.getName()), 2, 2);				
+					nameTable2.add(getSmallText(code.getPostalCode() + " " + code.getName()), 2, 2);
 			}
 			catch (RemoteException e) {
 			}
@@ -300,7 +299,7 @@ public class ChildCareApplicationForm extends CommuneBlock {
 			DateInput date = new DateInput(PARAM_DATE + "_" + i);
 			date.setToCurrentDate();
 			areaDrop.setAttribute("style", getSmallTextFontStyle());
-//			areaDrop.setToSubmit();
+			//			areaDrop.setToSubmit();
 			providerDrop.setAttribute("style", getSmallTextFontStyle());
 			date.setStyleAttribute("style", getSmallTextFontStyle());
 			providerText = getSmallText(provider + " " + i);
@@ -315,7 +314,7 @@ public class ChildCareApplicationForm extends CommuneBlock {
 		add(nameTable2);
 		add(Text.BREAK);
 		form.add(inputTable);
-		
+
 		add(form);
 	}
 
@@ -326,10 +325,10 @@ public class ChildCareApplicationForm extends CommuneBlock {
 		boolean done = false;
 		if (business != null) {
 			try {
-				String subject = localize(EMAIL_PROVIDER_SUBJECT,"Child care application");
-				String message = localize(EMAIL_PROVIDER_MESSAGE,"You have received a new childcare application");
-				
-				done = business.insertApplications(_user,_valProvider,_valDate,new Integer(checkId).intValue(),new Integer(childId).intValue(),subject,message);
+				String subject = localize(EMAIL_PROVIDER_SUBJECT, "Child care application");
+				String message = localize(EMAIL_PROVIDER_MESSAGE, "You have received a new childcare application");
+
+				done = business.insertApplications(_user, _valProvider, _valDate, new Integer(checkId).intValue(), new Integer(childId).intValue(), subject, message);
 			}
 			catch (RemoteException e) {
 				e.printStackTrace();
@@ -411,10 +410,10 @@ public class ChildCareApplicationForm extends CommuneBlock {
 			return null;
 		}
 	}
-	
+
 	private CheckBusiness getCheckBusiness(IWContext iwc) throws Exception {
 		return (CheckBusiness) com.idega.business.IBOLookup.getServiceInstance(iwc, CheckBusiness.class);
-	}	
+	}
 
 	private ChildCareBusiness getChildCareBusiness(IWContext iwc) {
 		try {
@@ -424,17 +423,238 @@ public class ChildCareApplicationForm extends CommuneBlock {
 			return null;
 		}
 	}
-	
+
 	private boolean checkParameters(IWContext iwc) {
 		for (int i = 0; i < 5; i++) {
-			_valProvider[i] = iwc.isParameterSet(PARAM_PROVIDER + "_" + (i + 1))?Integer.parseInt(iwc.getParameter(PARAM_PROVIDER + "_" + (i + 1))):-1;
-			_valArea[i] = iwc.isParameterSet(PARAM_AREA + "_" + (i + 1))?Integer.parseInt(iwc.getParameter(PARAM_PROVIDER + "_" + (i + 1))):-1;
+			_valProvider[i] = iwc.isParameterSet(PARAM_PROVIDER + "_" + (i + 1)) ? Integer.parseInt(iwc.getParameter(PARAM_PROVIDER + "_" + (i + 1))) : -1;
+			_valArea[i] = iwc.isParameterSet(PARAM_AREA + "_" + (i + 1)) ? Integer.parseInt(iwc.getParameter(PARAM_PROVIDER + "_" + (i + 1))) : -1;
 			_valDate[i] = iwc.getParameter(PARAM_DATE + "_" + (i + 1));
 		}
 		/**
 		 * @todo Setja inn tékk á þessum breytum
 		 */
 
-		return true;	
+		return true;
+	}
+
+	private String getFilterCallerScript(IWContext iwc, String areaName, String schoolName) {
+		StringBuffer script = new StringBuffer("changeFilter(");
+		//		script.append(Type);
+		//		script.append(",");
+		//		script.append("this.form.elements['");
+		//		script.append(typeName);
+		//		script.append("'],");
+		script.append("this.form.elements['");
+		script.append(areaName);
+		script.append("'],");
+		script.append("this.form.elements['");
+		script.append(schoolName);
+		script.append("'])");
+		return script.toString();
+	}
+
+	private String getInitFilterCallerScript(IWContext iwc, String areaName, String schoolName, int areaSel, int schoolSel) {
+		StringBuffer script = new StringBuffer("initFilter(");
+		//		script.append("document.forms['").append(PARAM_FORM_NAME).append("'].elements['");
+		//		script.append(typeName);
+		//		script.append("'],");
+		script.append("document.forms['").append(PARAM_FORM_NAME).append("'].elements['");
+		script.append(areaName);
+		script.append("'],");
+		script.append("document.forms['").append(PARAM_FORM_NAME).append("'].elements['");
+		script.append(schoolName);
+		script.append("'],");
+		//		script.append(typeSel);
+		//		script.append(",");
+		script.append(areaSel);
+		script.append(",");
+		script.append(schoolSel);
+		script.append(")");
+		return script.toString();
+	}
+
+	/*
+	  private String getFilterScript(IWContext iwc)throws java.rmi.RemoteException{
+	    StringBuffer s = new StringBuffer();
+	    s.append("function changeFilter(index,type,area,school){").append(" \n\t");
+	    s.append("var typeSelect = type;").append(" \n\t");
+	    s.append("var areaSelect = area;").append(" \n\t");
+	    s.append("var schoolSelect = school;").append(" \n\t");
+	    s.append("var selected = 0;").append(" \n\t");
+	    s.append("if(index == 1){").append(" \n\t\t");
+	    s.append("selected = typeSelect.options[typeSelect.selectedIndex].value;").append("\n\t\t");
+	    s.append("areaSelect.options.length = 0;").append("\n\t\t");
+	    s.append("schoolSelect.options.length = 0;").append("\n\t\t");
+	    s.append("areaSelect.options[areaSelect.options.length] = new Option(\"");
+	    s.append(iwrb.getLocalizedString("choose_area","Choose Area")).append("\",\"-1\",true,true);").append("\n\t\t");
+	    //s.append("schoolSelect.options[schoolSelect.options.length] = new Option(\"Veldu skóla\",\"-1\",true,true);").append("\n\t");
+	    s.append("}else if(index == 2){").append(" \n\t\t");
+	    s.append("selected = areaSelect.options[areaSelect.selectedIndex].value;").append("\n\t\t");
+	    s.append("schoolSelect.options.length = 0;").append("\n\t\t");
+	    s.append("schoolSelect.options[schoolSelect.options.length] = new Option(\"");
+	    s.append(iwrb.getLocalizedString("choose_school","Choose School")).append("\",\"-1\",true,true);").append("\n\t");
+	    s.append("} else if(index == 3){").append("\n\t\t");
+	    s.append("selected = schoolSelect.options[schoolSelect.selectedIndex].value;").append("\n\t");
+	    s.append("}").append("\n\t\t\t");
+	
+	    // Data Filling ::
+	
+	    StringBuffer t = new StringBuffer("if(index==1){\n\t");
+	    StringBuffer a = new StringBuffer("else if(index==2){\n\t");
+	    StringBuffer c = new StringBuffer("else if(index==3){\n\t");
+	
+	    Collection Types = this.schoolTypes;
+	    if(Types!=null && !Types.isEmpty()){
+	      Iterator iter = Types.iterator();
+	      SchoolType type;
+	      SchoolArea area;
+	      School school;
+	      Collection areas;
+	      Collection schools;
+	      // iterate through schooltypes
+	      while(iter.hasNext()){
+	        type = (SchoolType) iter.next();
+	
+	        Integer tPK = (Integer) type.getPrimaryKey();
+	        //System.err.println("checking type "+tPK.toString());
+	        areas = getSchoolAreasWithType(iwc,tPK.intValue());
+	        if(areas!=null && !areas.isEmpty()){
+	          Iterator iter2 = areas.iterator();
+	          t.append("if(selected == \"").append(tPK.toString()).append("\"){").append("\n\t\t");
+	
+	           Hashtable aHash = new Hashtable();
+	
+	          // iterate through areas whithin types
+	          while(iter2.hasNext()) {
+	            area = (SchoolArea) iter2.next();
+	            Integer aPK = (Integer)area.getPrimaryKey();
+	            // System.err.println("checking area "+aPK.toString());
+	            if(!aHash.containsKey(aPK)){
+	            	aHash.put(aPK,aPK);
+		            schools = getSchoolByAreaAndType(iwc,aPK.intValue(),tPK.intValue());
+		            if(schools!=null ){
+		              Iterator iter3 = schools.iterator();
+		              a.append("if(selected == \"").append(aPK.toString()).append("\"){").append("\n\t\t");
+		              Hashtable hash = new Hashtable();
+		              // iterator through schools whithin area and type
+		              while(iter3.hasNext()){
+		                school = (School) iter3.next();
+		                String pk = school.getPrimaryKey().toString();
+		                //System.err.println("checking school "+pk.toString());
+		                if(!hash.containsKey(pk)){
+			                a.append("schoolSelect.options[schoolSelect.options.length] = new Option(\"");
+			                a.append(school.getSchoolName()).append("\",\"");
+			                a.append(pk).append("\");\n\t\t");
+			                hash.put(pk,pk);
+		                }
+	
+		              }
+		              a.append("}\n\t\t");
+		        	}
+	            }
+	            else{
+	            System.err.println("shools empty");
+	          }
+	          t.append("areaSelect.options[areaSelect.options.length] = new Option(\"");
+	          t.append(area.getSchoolAreaName()).append("\",\"");
+	          t.append(area.getPrimaryKey().toString()).append("\");").append("\n\t\t");;
+	
+	          }
+	          t.append("}\n\t");
+	        }
+	        else
+	           System.err.println("areas empty");
+	      }
+	    }
+	    else
+	      System.err.println("types empty");
+	
+	
+	    s.append("\n\n");
+	
+	
+	    t.append("\n\t }");
+	    a.append("\n\t }");
+	    c.append("\n\t }");
+	
+	    s.append(t.toString());
+	    s.append(a.toString());
+	    s.append(c.toString());
+	    s.append("\n}");
+	
+	    return s.toString();
+	  }
+	*/
+	public String getInitFilterScript() {
+		StringBuffer s = new StringBuffer();
+		s.append("function initFilter(area,school,area_sel,school_sel){ \n  ");
+		//		s.append("changeFilter( 1 ,type,area,school); \n  ");
+		//		s.append("type.selectedIndex = type_sel; \n  ");
+		s.append("changeFilter(1,area,school); \n  ");
+		s.append("area.selectedIndex = area_sel; \n  ");
+		s.append("changeFilter(2,area,school); \n ");
+		s.append("school.selectedIndex = school_sel; \n}");
+		return s.toString();
+	}
+	/*
+		public String getSchoolCheckScript(){
+		  StringBuffer s = new StringBuffer();
+			s.append("\nfunction checkApplication(){\n\t");
+			//s.append("\n\t\t alert('").append("checking choices").append("');");
+			s.append("\n\t var currSchool = ").append("document.").append(prmForm).append(".elements['").append(prmPreSchool).append("'];");
+			s.append("\n\t var dropOne = ").append("document.").append(prmForm).append(".elements['").append(prmFirstSchool).append("'];");
+			s.append("\n\t var dropTwo = ").append("document.").append(prmForm).append(".elements['").append(prmSecondSchool).append("'];");
+			s.append("\n\t var dropThree = ").append("document.").append(prmForm).append(".elements['").append(prmThirdSchool).append("'];");
+			s.append("\n\t var gradeDrop = ").append("document.").append(prmForm).append(".elements['").append(prmPreGrade).append("'];");
+			s.append("\n\t var one = ").append("dropOne.options[dropOne.selectedIndex].value;");
+			s.append("\n\t var two = ").append("dropTwo.options[dropTwo.selectedIndex].value;");
+			s.append("\n\t var  three = ").append("dropThree.options[dropThree.selectedIndex].value;");
+			s.append("\n\t var  year = gradeDrop.options?").append("gradeDrop.options[gradeDrop.selectedIndex].value").append(":")
+			.append("document.sch_app_the_frm.elements['").append(prmPreGrade).append("'].value;");
+			s.append("\n\t var  school = currSchool.options?").append("currSchool.options[currSchool.selectedIndex].value").append(":")
+			.append("document.sch_app_the_frm.elements['").append(prmPreSchool).append("'].value;");
+			// current school check
+			s.append("\n\t if(school <= 0){");
+			String msg1 = iwrb.getLocalizedString("school_choice.must_set_current_school","You must provide current shool");
+			s.append("\n\t\t\t alert('").append(msg1).append("');");
+			s.append("\n\t\t ");
+			s.append("\n\t }");
+	
+			// year check
+			s.append("\n\t else if(year <= 0 && school > 0){");
+			String msg2 = iwrb.getLocalizedString("school_choice.must_set_grade","You must provide current shool year");
+			s.append("\n\t\t\t alert('").append(msg2).append("');");
+			s.append("\n\t\t ");
+			s.append("\n\t }");
+	
+			// schoolchoices checked
+			s.append("\n\t else if(one && two && three){");
+			s.append("\n\t if(one == two || two == three || three == one){");
+			String msg = iwrb.getLocalizedString("school_school.must_not_be_the_same","Please do not choose the same school more than once");
+			s.append("\n\t\t\t alert('").append(msg).append("');");
+			s.append("\n\t\t\t return false;");
+			s.append("\n\t\t }");
+			s.append("\n\t }");
+			s.append("\n\t else{");
+			s.append("\n\t\t alert('").append("no choices").append("');");
+			s.append("\n\t\t return false;");
+			s.append("\n\t }");
+			//s.append("\n\t\t alert('").append("nothing wrong").append("');");
+			s.append("\n\t return true;");
+			s.append("\n}\n");
+			return s.toString();
+		}
+		*/
+
+	private String getMySubmitScript() {
+		StringBuffer s = new StringBuffer();
+		s.append("\n function MySubmit(){");
+		s.append("\n\t if(checkApplication()) {");
+		s.append("\n\t\t document.").append(PARAM_FORM_NAME).append(".elements['").append(PARAM_FORM_SUBMIT).append("'].value='true';");
+		s.append("\n\t\t document.").append(PARAM_FORM_NAME).append(".submit();");
+		s.append("\n\t }");
+		s.append("\n}\n");
+
+		return s.toString();
 	}
 }
