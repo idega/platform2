468bc4a744b11f429085b40666efc3857d1a1fe4
Thorhallur Helgason <laddi@idega.com>
2002-11-01 16:03:56 +0000

    Removed method setOwner().

--- src/se/idega/idegaweb/commune/account/citizen/data/CitizenAccount.java
+++ src/se/idega/idegaweb/commune/account/citizen/data/CitizenAccount.java
@@ -9,7 +9,6 @@ import se.idega.idegaweb.commune.account.data.AccountApplication;
 public interface CitizenAccount extends IDOEntity, Case, AccountApplication {
     String getCaseCodeDescription () throws RemoteException;
     String getCaseCodeKey () throws RemoteException;
-    void setOwner ();
 
     String getApplicantName () throws RemoteException;
     String getEmail () throws RemoteException;
