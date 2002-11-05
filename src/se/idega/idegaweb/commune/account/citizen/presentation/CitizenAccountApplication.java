25f27c61dbbac3d15ed77e7305253a54101297e4
Staffan Noteberg <staffan.noteberg@rekursiv.se>
2002-11-05 12:03:51 +0000

    n/a

--- src/se/idega/idegaweb/commune/account/citizen/presentation/CitizenAccountApplication.java
+++ src/se/idega/idegaweb/commune/account/citizen/presentation/CitizenAccountApplication.java
@@ -1,5 +1,5 @@
 /*
- * $Id: CitizenAccountApplication.java,v 1.10 2002/11/04 11:27:47 staffan Exp $
+ * $Id: CitizenAccountApplication.java,v 1.11 2002/11/05 12:03:51 staffan Exp $
  *
  * Copyright (C) 2002 Idega hf. All Rights Reserved.
  *
@@ -32,7 +32,8 @@ public class CitizenAccountApplication extends CommuneBlock {
 	private final static int ACTION_SUBMIT_SIMPLE_FORM = 1;
 	private final static int ACTION_SUBMIT_UNKNOWN_CITIZEN_FORM = 2;
 
-	private final static String PARAM_NAME = "caa_name";
+	private final static String PARAM_FIRST_NAME = "caa_first_name";
+	private final static String PARAM_LAST_NAME = "caa_last_name";
 	private final static String PARAM_PID = "caa_pid";
 	private final static String PARAM_BIRTH_YEAR = "caa_birth_year";
 	private final static String PARAM_BIRTH_MONTH = "caa_birth_month";
@@ -44,6 +45,10 @@ public class CitizenAccountApplication extends CommuneBlock {
     private final static String PARAM_STREET = "caa_street";
     private final static String PARAM_ZIP_CODE = "caa_zip_code";
     private final static String PARAM_CITY = "caa_city";
+
+    private final static String SSN_KEY = "caa_ssn";
+    private final static String SSN_DEFAULT
+        = "Personnummer (år/månad/dag - fyra sist siffror)";
 
     private final static String PARAM_CUSTODIAN1_PID = "caa_custodian1_pid";
     private final static String PARAM_CUSTODIAN1_CIVIL_STATUS
@@ -95,137 +100,41 @@ public class CitizenAccountApplication extends CommuneBlock {
 		}    
 	}
 
-    private void addSimpleInputs (final IWContext iwc, final Table inputTable) {
-		final DropdownMenu yearInput = new DropdownMenu (PARAM_BIRTH_YEAR);
-        final Calendar rightNow = Calendar.getInstance();
-        final int currentYear = rightNow.get (Calendar.YEAR);
-        addDropdownInput (iwc, PARAM_BIRTH_YEAR, currentYear - 110, currentYear,
-                          inputTable, 1, 2);
-        addDropdownInput (iwc, PARAM_BIRTH_MONTH, 12, 1, inputTable, 1, 2);
-        addDropdownInput (iwc, PARAM_BIRTH_DAY, 31, 1, inputTable, 1, 2);
-        addSingleInput (iwc, PARAM_PID, "Personnummer", inputTable, 4,
-                        _isPIDError, 1, 1);
-        addSingleInput (iwc, PARAM_EMAIL, "E-post", inputTable, 40,
-                        _isEmailError, 2, 1);
-        addSingleInput (iwc, PARAM_PHONE_HOME, "Telefon (hem)", inputTable, 20,
-                        _isPhoneHomeError, 1, 3);
-        addSingleInput (iwc, PARAM_PHONE_WORK, "Telefon (jobb/mobil)",
-                        inputTable, 20, false, 2, 3);
-    }
-
-    private void addDropdownInput
-        (final IWContext iwc, final String paramId, final int startId,
-         final int stopId, final Table inputTable, final int col,
-         final int row) {
-        final boolean forward = startId < stopId;
-        final DropdownMenu dropdown = new DropdownMenu (paramId);
-        int i = startId;
-        while (forward ? i <= stopId : i >= stopId) {
-            final String iAsString = new Integer (i).toString ();
-            dropdown.addMenuElementFirst (iAsString, iAsString);
-            i += (forward ? 1 : -1);
-        }
-		if (iwc.isParameterSet (paramId)) {
-		   dropdown.setSelectedElement (iwc.getParameter (paramId));
-        }
-        inputTable.add (dropdown, col, row);
-    }
-                                   
-    private void addGenderDropdownInput
-        (final IWContext iwc, final Table inputTable, final int col,
-         final int row) {
-        final String subject = localize (PARAM_GENDER, "Kön");
-        final Text text = getSmallText (subject);
-        inputTable.add (text, col, row);
-        try {
-            final CitizenAccountBusiness business
-                    = (CitizenAccountBusiness) IBOLookup.getServiceInstance
-                    (iwc, CitizenAccountBusiness.class);
-            final Gender [] genders = business.getGenders ();
-            final DropdownMenu dropdown = new DropdownMenu (PARAM_GENDER);
-            for (int i = 0; i < genders.length; i++) {
-                final String nameInDb = genders [i].getName ();
-                final String name = localize ("caa_" + nameInDb, nameInDb);
-                final String id = genders [i].getPrimaryKey ().toString ();
-                dropdown.addMenuElementFirst (id, name);
-            }
-            if (iwc.isParameterSet (PARAM_GENDER)) {
-                dropdown.setSelectedElement (iwc.getParameter (PARAM_GENDER));
-            }
-            inputTable.add (dropdown, col, row + 1);
-        } catch (RemoteException e) {
-            e.printStackTrace ();
-        }
-    }
-                                   
-    private void addSingleInput
-        (final IWContext iwc, final String paramId, final String defaultText,
-         final Table inputTable, final int maxLength, final boolean isError,
-         final int col, final int row) {
-
-        final TextInput textInput = new TextInput (paramId);
-		textInput.setMaxlength (maxLength);
-		textInput.setStyleClass (getSmallTextFontStyle ());
-		final String param = iwc.getParameter (paramId);
-		if (param != null) {
-			textInput.setContent (param);
-        }
-        final String subject = localize (paramId, defaultText);
-        final Text text = isError ? getSmallErrorText (subject)
-                : getSmallText (subject);
-        inputTable.add (text, col, row);
-        inputTable.add (textInput, col, row + 1);
-    }
-
-    private void addSubmitButton (final IWContext iwc, final String submitId,
-                                  final String defaultText,
-                                  final Table inputTable, final int row) {
-		inputTable.setAlignment(2, row, "right");
-        final String text = localize(submitId, defaultText);
-		final SubmitButton submitButton = new SubmitButton
-                (getBundle(iwc).getImageButton (text), submitId);
-		submitButton.setStyleClass(getLinkFontStyle());
-		inputTable.add(submitButton, 2, row);
-    }
-
 	private void viewUnknownCitizenApplicationForm (IWContext iwc) {
 		Form accountForm = new Form();
 
-		Table inputTable = new Table(2, 17);
-		inputTable.setCellspacing(2);
-		inputTable.setCellpadding(4);
-		inputTable.setColor(getBackgroundColor());
-
-        addSimpleInputs (iwc, inputTable);
-
-        addSingleInput (iwc, PARAM_NAME, "Namn", inputTable, 40, false, 1, 5);
-        addSingleInput (iwc, PARAM_STREET, "Gatuadress", inputTable, 40, false,
-                        1, 7);
-        addGenderDropdownInput (iwc, inputTable, 2, 7);
-        addSingleInput (iwc, PARAM_ZIP_CODE, "Postnummer", inputTable, 40,
-                        false, 1, 9);
-        addSingleInput (iwc, PARAM_CITY, "Postadress", inputTable, 40, false, 2,
-                        9);
-
+		Table table = createTable ();
+        addSimpleInputs (table, iwc);
+        int row = 9;
+        addHeader (table, row++, false, PARAM_FIRST_NAME, "Förnamn");
+        addSingleInput (table, row++, iwc, PARAM_FIRST_NAME, 40);
+        addHeader (table, row++, false, PARAM_LAST_NAME, "Efternamn");
+        addSingleInput (table, row++, iwc, PARAM_LAST_NAME, 40);
+        addHeader (table, row++, false, PARAM_STREET, "Gatuadress");
+        addSingleInput (table, row++, iwc, PARAM_STREET, 40);
+        addHeader (table, row++, false, PARAM_ZIP_CODE, "Postnummer");
+        addSingleInput (table, row++, iwc, PARAM_ZIP_CODE, 40);
+        addHeader (table, row++, false, PARAM_CITY, "Postort");
+        addSingleInput (table, row++, iwc, PARAM_CITY, 40);
+        addGenderDropdownInput (table, row++, iwc);
+        row++;
         final Text custodianHeader1
                 = getLocalizedHeader (CUSTODIAN1_KEY, "Vårdnadshavare 1");
-
-        inputTable.add (custodianHeader1, 1, 11);
-        addSingleInput (iwc, PARAM_CUSTODIAN1_PID, "Personnummer", inputTable,
-                        40, false, 1, 12);
-        addSingleInput (iwc, PARAM_CUSTODIAN1_CIVIL_STATUS, "Civilstånd",
-                        inputTable, 40, false, 2, 12);
+        table.add (custodianHeader1, 1, row++);
+        addSsnInput (table, row++, iwc, "_custodian1");
+        row++;
+        addHeader (table, row++, false, PARAM_CUSTODIAN1_CIVIL_STATUS, "Civilstånd");
+        addSingleInput (table, row++, iwc, PARAM_CUSTODIAN1_CIVIL_STATUS, 40);
 
         final Text custodianHeader2
                 = getLocalizedHeader (CUSTODIAN2_KEY, "Vårdnadshavare 2");
-        inputTable.add (custodianHeader2, 1, 14);
-        addSingleInput (iwc, PARAM_CUSTODIAN2_PID, "Personnummer", inputTable,
-                        40, false, 1, 15);
-        addSingleInput (iwc, PARAM_CUSTODIAN2_CIVIL_STATUS, "Civilstånd",
-                        inputTable, 40, false, 2, 15);
-
-        addSubmitButton (iwc, PARAM_UNKNOWN_CITIZEN_FORM_SUBMIT,
-                         "Submit application", inputTable, 17);
+        table.add (custodianHeader2, 1, row++);
+        addSsnInput (table, row++, iwc, "_custodian2");
+        row++;
+        addHeader (table, row++, false, PARAM_CUSTODIAN2_CIVIL_STATUS, "Civilstånd");
+        addSingleInput (table, row++, iwc, PARAM_CUSTODIAN2_CIVIL_STATUS, 40);
+        addSubmitButton (table, row, iwc, PARAM_UNKNOWN_CITIZEN_FORM_SUBMIT,
+                         "Submit application");
 
 		if (_isError) {
 			if (_errorMsg != null) {
@@ -241,30 +150,17 @@ public class CitizenAccountApplication extends CommuneBlock {
 				accountForm.add(errorTable);
 			}
 		}
-		accountForm.add(inputTable);
+		accountForm.add(table);
 
 		add(accountForm);
     }
 
 	private void viewSimpleApplicationForm(IWContext iwc) {
-		Form accountForm = new Form();
-
-		Table inputTable = new Table(2, 6);
-		inputTable.setCellspacing(2);
-		inputTable.setCellpadding(4);
-		inputTable.setAlignment(2, 6, "right");
-		inputTable.setColor(getBackgroundColor());
-
-        addSimpleInputs (iwc, inputTable);
-
-		SubmitButton submitButton
-                = new SubmitButton(getBundle(iwc).getImageButton
-                                   (localize(PARAM_SIMPLE_FORM_SUBMIT,
-                                             "Submit application")),
-                                   PARAM_SIMPLE_FORM_SUBMIT);
-		submitButton.setStyleClass(getLinkFontStyle());
-
-		inputTable.add(submitButton, 2, 6);
+		final Table table = createTable ();
+        addSimpleInputs (table, iwc);
+        addSubmitButton (table, 9, iwc, PARAM_SIMPLE_FORM_SUBMIT,
+                         "Skicka ansökan");
+		final Form accountForm = new Form();
 		if (_isError) {
 			if (_errorMsg != null) {
 				Table errorTable = new Table(1, 1);
@@ -279,7 +175,7 @@ public class CitizenAccountApplication extends CommuneBlock {
 				accountForm.add(errorTable);
 			}
 		}
-		accountForm.add(inputTable);
+		accountForm.add(table);
 
 		add(accountForm);
 	}
@@ -351,7 +247,8 @@ public class CitizenAccountApplication extends CommuneBlock {
 	}
 
     private void submitUnknownCitizenForm (final IWContext iwc) {
-		final String name = iwc.getParameter (PARAM_NAME);
+		final String name = iwc.getParameter (PARAM_FIRST_NAME) + " "
+                + iwc.getParameter (PARAM_LAST_NAME);
         final int genderId
                 = new Integer (iwc.getParameter (PARAM_GENDER)).intValue ();
 		final String pid = iwc.getParameter (PARAM_PID);
@@ -436,6 +333,121 @@ public class CitizenAccountApplication extends CommuneBlock {
 		else
 			add(new Text(localize(TEXT_APPLICATION_SUBMITTED,
                                   "Application submitted")));
+    }
+
+
+    private void addSimpleInputs (final Table table, final IWContext iwc) {
+        addSsnInput (table, 1, iwc, "");
+        addHeader (table, 3, _isEmailError, PARAM_EMAIL, "E-post");
+        addSingleInput (table, 4, iwc, PARAM_EMAIL, 40);
+        addHeader (table, 5, _isPhoneHomeError, PARAM_PHONE_HOME, "Telefon (hem)");
+        addSingleInput (table, 6, iwc, PARAM_PHONE_HOME, 20);
+        addHeader (table, 7, false, PARAM_PHONE_WORK,
+                   "Telefon (jobb/mobil)");
+        addSingleInput (table, 8, iwc, PARAM_PHONE_WORK, 20);
+    }
+
+    private Table createTable () {
+        final Table table = new Table ();
+		table.setCellspacing (2);
+		table.setCellpadding (4);
+		table.setColor (getBackgroundColor ());
+        return table;
+    }
+
+    private void addSsnInput (final Table table, final int row,
+                              final IWContext iwc, final String paramPostfix) {
+        final String subject = localize (SSN_KEY, SSN_DEFAULT);
+        table.add (getSmallText (subject), 1, row);
+        final Calendar rightNow = Calendar.getInstance();
+        final int currentYear = rightNow.get (Calendar.YEAR);
+        addDropdownInput (table, row + 1, iwc, PARAM_BIRTH_YEAR + paramPostfix,
+                          currentYear - 110, currentYear - 18);
+        table.add (new Text (" / "), 1, row + 1);
+        addDropdownInput (table, row + 1, iwc, PARAM_BIRTH_MONTH + paramPostfix,
+                          12, 1);
+        table.add (new Text (" / "), 1, row + 1);
+        addDropdownInput (table, row + 1, iwc, PARAM_BIRTH_DAY + paramPostfix,
+                          31, 1);
+        table.add (new Text (" - "), 1, row + 1);
+        addSingleInput (table, row + 1, iwc, PARAM_PID + paramPostfix, 4);
+    }
+
+    private void addDropdownInput
+        (final Table table, final int row, final IWContext iwc,
+         final String paramId, final int startId, final int stopId) {
+        final boolean forward = startId < stopId;
+        final DropdownMenu dropdown = new DropdownMenu (paramId);
+        int i = startId;
+        while (forward ? i <= stopId : i >= stopId) {
+            final String iAsString = new Integer (i).toString ();
+            dropdown.addMenuElementFirst (iAsString, iAsString);
+            i += (forward ? 1 : -1);
+        }
+		if (iwc.isParameterSet (paramId)) {
+		   dropdown.setSelectedElement (iwc.getParameter (paramId));
+        }
+        table.add (dropdown, 1, row);
+    }
+                                   
+    private void addGenderDropdownInput
+        (final Table table, final int row, final IWContext iwc) {
+        final String subject = localize (PARAM_GENDER, "Kön");
+        final Text text = getSmallText (subject);
+        table.add (text, 1, row);
+        try {
+            final CitizenAccountBusiness business
+                    = (CitizenAccountBusiness) IBOLookup.getServiceInstance
+                    (iwc, CitizenAccountBusiness.class);
+            final Gender [] genders = business.getGenders ();
+            final DropdownMenu dropdown = new DropdownMenu (PARAM_GENDER);
+            for (int i = 0; i < genders.length; i++) {
+                final String nameInDb = genders [i].getName ();
+                final String name = localize ("caa_" + nameInDb, nameInDb);
+                final String id = genders [i].getPrimaryKey ().toString ();
+                dropdown.addMenuElementFirst (id, name);
+            }
+            if (iwc.isParameterSet (PARAM_GENDER)) {
+                dropdown.setSelectedElement (iwc.getParameter (PARAM_GENDER));
+            }
+            table.add (dropdown, 1, row + 1);
+        } catch (RemoteException e) {
+            e.printStackTrace ();
+        }
+    }
+                                   
+    private void addSingleInput
+        (final Table table, final int row ,final IWContext iwc,
+         final String paramId, final int maxLength) {
+        final TextInput textInput = new TextInput (paramId);
+		textInput.setMaxlength (maxLength);
+		textInput.setStyleClass (getSmallTextFontStyle ());
+		final String param = iwc.getParameter (paramId);
+		if (param != null) {
+			textInput.setContent (param);
+        }
+        table.add (textInput, 1, row);
+    }
+    
+    private void addHeader
+        (final Table table, final int row, final boolean isError,
+         final String paramId, final String defaultText) {
+        final String header = localize (paramId, defaultText);
+        final Text text = isError ? getSmallErrorText (header)
+                : getSmallText (header);
+        table.add (text, 1, row);
+    }
+
+
+    private void addSubmitButton (final Table table, final int row,
+                                  final IWContext iwc, final String submitId,
+                                  final String defaultText) {
+		table.setAlignment (1, row, "right");
+        final String text = localize (submitId, defaultText);
+		final SubmitButton submitButton = new SubmitButton
+                (getBundle(iwc).getImageButton (text), submitId);
+		submitButton.setStyleClass (getLinkFontStyle());
+		table.add (submitButton, 1, row);
     }
 
     private static String getSsn (final IWContext iwc) {
