a32d2b9a74b1c9f8b26b4d71197880739c7a0b0e
Grimur Jonsson <gimmi@idega.com>
2003-02-09 13:22:49 +0000

    Added getLocalizedMessage...

--- src/com/idega/block/tpos/business/TPosException.java
+++ src/com/idega/block/tpos/business/TPosException.java
@@ -1,5 +1,5 @@
 /*
- * $Id: TPosException.java,v 1.3 2002/04/06 19:07:37 tryggvil Exp $
+ * $Id: TPosException.java,v 1.4 2003/02/09 13:22:49 gimmi Exp $
  *
  * Copyright (C) 2002 Idega hf. All Rights Reserved.
  *
@@ -8,6 +8,8 @@
  *
  */
 package com.idega.block.tpos.business;
+
+import com.idega.idegaweb.IWResourceBundle;
 
 /**
  * @author <a href="mail:palli@idega.is">Pall Helgason</a>
@@ -72,5 +74,53 @@ public class TPosException extends Exception {
    */
   public String getDisplayError() {
     return(_displayError);
+  }
+  
+  public String getLocalizedMessage(IWResourceBundle iwrb) {
+		System.out.println("TPOS errormessage = " + this.getErrorMessage());
+		System.out.println("number = " + this.getErrorNumber());
+		System.out.println("display = " + this.getDisplayError());
+		int number = Integer.parseInt(this.getErrorNumber());
+		switch (number) {
+			case 6:
+			case 12:
+			case 19:
+				return (iwrb.getLocalizedString("travel.creditcard_number_incorrect","Creditcard number incorrect"));
+			case 10:
+			case 22:
+			case 74:
+				return (iwrb.getLocalizedString("travel.creditcard_type_not_accepted","Creditcard type not accepted"));
+			case 17:
+			case 18:
+				return (iwrb.getLocalizedString("travel.creditcard_is_expired","Creditcard is expired"));
+			case 48:
+			case 49:
+			case 50:
+			case 51:
+			case 56:
+			case 57:
+			case 76:
+			case 79:
+			case 2002:
+			case 2010:
+				return (iwrb.getLocalizedString("travel.cannot_connect_to_cps","Could not connect to Central Payment Server"));
+			case 7:
+			case 37:
+			case 69:
+			case 75:
+				return (iwrb.getLocalizedString("travel.creditcard_autorization_failed","Authorization failed"));
+			/*case 69:
+				display.setText(e.getErrorMessage());
+				break;*/
+			case 20:
+			case 31:
+				return (iwrb.getLocalizedString("travel.transaction_not_permitted","Transaction not permitted"));
+			case 99999:
+				return (iwrb.getLocalizedString("travel.booking_was_not_confirmed_try_again_later","Booking was not confirmed. Please try again later"));
+			default:
+				return (iwrb.getLocalizedString("travel.cannot_connect","Cannot communicate with server"));
+		}
+
+
   }
 }
