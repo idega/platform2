8b374f8d4ef5851400c466b2f3a5331194d46b25
Pall Helgason <palli@idega.com>
2003-04-02 13:21:29 +0000

    Added userchooser to this

--- src/is/idega/idegaweb/member/presentation/GroupBoardTab.java
+++ src/is/idega/idegaweb/member/presentation/GroupBoardTab.java
@@ -8,24 +8,21 @@
  */
 package is.idega.idegaweb.member.presentation;
 
-import is.idega.idegaweb.member.business.plugins.ClubInformationPluginBusiness;
-
 import java.rmi.RemoteException;
 import java.util.Hashtable;
 
 import javax.ejb.FinderException;
 
-import com.idega.idegaweb.IWApplicationContext;
 import com.idega.idegaweb.IWResourceBundle;
 import com.idega.presentation.IWContext;
 import com.idega.presentation.Table;
 import com.idega.presentation.text.Text;
-import com.idega.presentation.ui.DateInput;
-import com.idega.presentation.ui.TextInput;
 import com.idega.user.data.Group;
 import com.idega.user.data.GroupHome;
+import com.idega.user.data.User;
+import com.idega.user.data.UserHome;
+import com.idega.user.presentation.UserChooserBrowser;
 import com.idega.user.presentation.UserGroupTab;
-import com.idega.util.IWTimestamp;
 
 /**
  * @author palli
@@ -39,9 +36,9 @@ public class GroupBoardTab extends UserGroupTab {
 	private static final String TAB_NAME = "board_tab_name";
 	private static final String DEFAULT_TAB_NAME = "Club Board";
 	
-	private TextInput _ssn1Field;
-	private TextInput _ssn2Field;
-	private TextInput _ssn3Field;
+	private UserChooserBrowser _ssn1Field;
+	private UserChooserBrowser _ssn2Field;
+	private UserChooserBrowser _ssn3Field;
 
 	private Text _ssn1Text;
 	private Text _ssn2Text;
@@ -72,9 +69,9 @@ public class GroupBoardTab extends UserGroupTab {
 	 * @see com.idega.user.presentation.UserGroupTab#initializeFieldNames()
 	 */
 	public void initializeFieldNames() {
-		_ssn1FieldName = "board_ssn1";
-		_ssn2FieldName = "board_ssn2";
-		_ssn3FieldName = "board_ssn3";
+		_ssn1FieldName = "board_member1";
+		_ssn2FieldName = "board_member2";
+		_ssn3FieldName = "board_member3";
 	}
 
 	/* (non-Javadoc)
@@ -91,18 +88,40 @@ public class GroupBoardTab extends UserGroupTab {
 	 * @see com.idega.user.presentation.UserGroupTab#updateFieldsDisplayStatus()
 	 */
 	public void updateFieldsDisplayStatus() {
-		_ssn1Field.setContent((String) fieldValues.get(_ssn1FieldName));
-		_ssn2Field.setContent((String) fieldValues.get(_ssn2FieldName));
-		_ssn3Field.setContent((String) fieldValues.get(_ssn3FieldName));
+		User user1 = null;
+		User user2 = null;
+		User user3 = null;
+		String ssn1 = (String) fieldValues.get(_ssn1FieldName);
+		String ssn2 = (String) fieldValues.get(_ssn2FieldName);
+		String ssn3 = (String) fieldValues.get(_ssn3FieldName);
+		try {
+			UserHome home = (UserHome) com.idega.data.IDOLookup.getHome(User.class);
+			if (ssn1 != null && !ssn1.equals(""))
+				user1 = (User) (home.findByPrimaryKey(new Integer(ssn1)));
+			if (ssn2 != null && !ssn2.equals(""))
+				user2 = (User) (home.findByPrimaryKey(new Integer(ssn2)));
+			if (ssn3 != null && !ssn3.equals(""))
+				user3 = (User) (home.findByPrimaryKey(new Integer(ssn3)));
+
+			if (ssn1 != null && !ssn1.equals(""))		
+				_ssn1Field.setSelectedUser(ssn1,user1.getName());
+			if (ssn2 != null && !ssn2.equals(""))		
+				_ssn2Field.setSelectedUser(ssn2,user2.getName());
+			if (ssn3 != null && !ssn3.equals(""))		
+				_ssn3Field.setSelectedUser(ssn3,user3.getName());
+		}
+		catch(Exception e) {
+		}
+		
 	}
 
 	/* (non-Javadoc)
 	 * @see com.idega.user.presentation.UserGroupTab#initializeFields()
 	 */
 	public void initializeFields() {
-		_ssn1Field = new TextInput(_ssn1FieldName);
-		_ssn2Field = new TextInput(_ssn2FieldName);
-		_ssn3Field = new TextInput(_ssn3FieldName);
+		_ssn1Field = new UserChooserBrowser(_ssn1FieldName);
+		_ssn2Field = new UserChooserBrowser(_ssn2FieldName);
+		_ssn3Field = new UserChooserBrowser(_ssn3FieldName);
 	}
 
 	/* (non-Javadoc)
@@ -112,37 +131,22 @@ public class GroupBoardTab extends UserGroupTab {
 			IWContext iwc = IWContext.getInstance();
 		IWResourceBundle iwrb = getResourceBundle(iwc);
 
-		_ssn1Text = new Text(iwrb.getLocalizedString(_ssn1FieldName, "SSN1") + ":");
-		_ssn2Text = new Text(iwrb.getLocalizedString(_ssn2FieldName, "SSN2") + ":");
-		_ssn3Text = new Text(iwrb.getLocalizedString(_ssn3FieldName, "SSN3") + ":");
+		_ssn1Text = new Text(iwrb.getLocalizedString(_ssn1FieldName, "Member 1") + ":");
+		_ssn2Text = new Text(iwrb.getLocalizedString(_ssn2FieldName, "Member 2") + ":");
+		_ssn3Text = new Text(iwrb.getLocalizedString(_ssn3FieldName, "Member 3") + ":");
 	}
 
 	/* (non-Javadoc)
 	 * @see com.idega.user.presentation.UserGroupTab#lineUpFields()
 	 */
 	public void lineUpFields() {
-		Table t = new Table(2, 18);
+		Table t = new Table(2, 3);
 		t.add(_ssn1Text, 1, 1);
 		t.add(_ssn1Field, 2, 1);
-		t.add("Nafn :",1,2);
-		t.add("Aðsetur :",1,3);
-		t.add("Sími :",1,4);
-		t.add("Fax :",1,5);
-		t.add("Tölvupóstfang :",1,6);
-		t.add(_ssn2Text, 1, 7);
-		t.add(_ssn2Field, 2, 7);
-		t.add("Nafn :",1,8);
-		t.add("Aðsetur :",1,9);
-		t.add("Sími :",1,10);
-		t.add("Fax :",1,11);
-		t.add("Tölvupóstfang :",1,12);
-		t.add(_ssn3Text, 1, 13);
-		t.add(_ssn3Field, 2, 13);
-		t.add("Nafn :",1,14);
-		t.add("Aðsetur :",1,15);
-		t.add("Sími :",1,16);
-		t.add("Fax :",1,17);
-		t.add("Tölvupóstfang :",1,18);
+		t.add(_ssn2Text, 1, 2);
+		t.add(_ssn2Field, 2, 2);
+		t.add(_ssn3Text, 1, 3);
+		t.add(_ssn3Field, 2, 3);
 
 		add(t);
 	}
@@ -187,9 +191,9 @@ public class GroupBoardTab extends UserGroupTab {
 			String ssn2 = (String) fieldValues.get(_ssn2FieldName);
 			String ssn3 = (String) fieldValues.get(_ssn3FieldName);
 
-			group.setMetaData("BOARD_SSN1", ssn1);
-			group.setMetaData("BOARD_SSN2", ssn2);
-			group.setMetaData("BOARD_SSN3", ssn3);
+			group.setMetaData("BOARD_MEMBER1", ssn1);
+			group.setMetaData("BOARD_MEMBER2", ssn2);
+			group.setMetaData("BOARD_MEMBER3", ssn3);
 
 			group.store();
 		}
