c8475f93a0b3d7c1c0fb32eead29c8c4bedcf882
Eirikur Hrafnsson <eiki@idega.com>
2003-04-14 18:44:11 +0000

    A collection of the static variables used in the member system like group type names.

---
+++ src/is/idega/idegaweb/member/util/IWMemberConstants.java
@@ -0,0 +1,39 @@
+package is.idega.idegaweb.member.util;
+
+/**
+ * A collection of the static variables used in the member system like group type names.
+ * @author <a href="mailto:eiki@idega.is">Eirikur Hrafnsson</a>
+ *
+ */
+public final class IWMemberConstants {
+	public static final String GROUP_TYPE_FEDERATION = "iwme_federation";
+	public static final String GROUP_TYPE_FEDERATION_STAFF = "iwme_federation_staff";
+	public static final String GROUP_TYPE_FEDERATION_COMMITTEE = "iwme_federation_committee";
+	public static final String GROUP_TYPE_UNION = "iwme_union";
+	public static final String GROUP_TYPE_UNION_STAFF = "iwme_union_staff";
+	public static final String GROUP_TYPE_UNION_COMMITTEE = "iwme_union_committee";
+	public static final String GROUP_TYPE_REGIONAL_UNION = "iwme_regional_union";
+	public static final String GROUP_TYPE_REGIONAL_UNION_STAFF = "iwme_regional_union_staff";
+	public static final String GROUP_TYPE_REGIONAL_UNION_COMMITTEE = "iwme_regional_union_committee";
+	public static final String GROUP_TYPE_REGIONAL_UNION_COLLECTION = "iwme_regional_union_collection";
+	public static final String GROUP_TYPE_LEAGUE= "iwme_league";
+	public static final String GROUP_TYPE_LEAGUE_STAFF = "iwme_league_staff";
+	public static final String GROUP_TYPE_LEAGUE_COMMITTEE = "iwme_league_committee";
+	public static final String GROUP_TYPE_LEAGUE_COLLECTION = "iwme_league_collection";
+	public static final String GROUP_TYPE_CLUB = "iwme_club";
+	public static final String GROUP_TYPE_CLUB_MEMBER = "iwme_club_member";
+	public static final String GROUP_TYPE_CLUB_PLAYER = "iwme_club_player";
+	public static final String GROUP_TYPE_CLUB_TRAINER = "iwme_club_trainer";
+	public static final String GROUP_TYPE_CLUB_STAFF = "iwme_club_staff";
+	public static final String GROUP_TYPE_CLUB_COMMITTEE = "iwme_club_committee";
+	public static final String GROUP_TYPE_CLUB_COMMITTEE_MAIN = "iwme_club_main_committee";
+	public static final String GROUP_TYPE_CLUB_DIVISION = "iwme_club_division";
+	public static final String GROUP_TYPE_CLUB_DIVISION_TRAINER = "iwme_club_division_trainer";
+	public static final String GROUP_TYPE_CLUB_DIVISION_STAFF = "iwme_club_division_staff";
+	public static final String GROUP_TYPE_CLUB_DIVISION_COMMITTEE = "iwme_club_division_committee";
+	public static final String GROUP_TYPE_CLUB_DIVISION_TEMPLATE = "iwme_club_division_template";
+	public static final String GROUP_TYPE_TEMPORARY = "iwme_temporary";
+	public static final String GROUP_TYPE_ALIAS = "alias";
+	public static final String GROUP_TYPE_GENERAL = "general";
+
+}
