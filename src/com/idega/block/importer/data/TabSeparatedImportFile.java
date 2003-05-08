54c690f7d3cb63088adf54e17298b0e72142f5e2
Joakim Johnsson <joakim@idega.com>
2003-05-08 17:35:50 +0000

    *** empty log message ***

--- src/com/idega/block/importer/data/TabSeparatedImportFile.java
+++ src/com/idega/block/importer/data/TabSeparatedImportFile.java
@@ -5,9 +5,7 @@ import java.io.File;
 /**
  * @author <a href="mailto:joakim@idega.is">Joakim Johnson</a>
  * To add this to the "File Type" dropdown for the import function, execute the following SQL:
- * insert into im_file_class values(4, 'Tab separatede file', 
- * 'com.idega.block.importer.data.TabSeparatedImportFile',
- * 'A tab separated file read. By default each record is separated with a new line character and each value is separated by a tab, but it can be adjusted by properties.')
+ * insert into im_file_class values(4, 'Tab separatede file', 'com.idega.block.importer.data.TabSeparatedImportFile', 'A tab separated file read. By default each record is separated with a new line character and each value is separated by a tab, but it can be adjusted by properties.')
  * 
  * Note that the "4" value in the SQL might have to be adjusted in the sql, 
  * depending on the number of records already inserted in the table. 
