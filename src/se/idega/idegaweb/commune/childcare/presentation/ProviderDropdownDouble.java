edc8a1ef53da91dca4673a738db97cf7b4397cb4
Thorhallur Helgason <laddi@idega.com>
2003-05-19 08:37:43 +0000

    A class to get providers in a double dropdown.

---
+++ src/se/idega/idegaweb/commune/childcare/presentation/ProviderDropdownDouble.java
@@ -0,0 +1,55 @@
+/*
+ * Created on 8.5.2003
+ */
+package se.idega.idegaweb.commune.childcare.presentation;
+
+import com.idega.block.school.data.School;
+import com.idega.presentation.ui.SelectDropdownDouble;
+
+/**
+ * @author laddi
+ */
+public class ProviderDropdownDouble extends SelectDropdownDouble {
+
+	/**
+	 * 
+	 */
+	public ProviderDropdownDouble() {
+		super();
+	}
+
+	/**
+	 * @param primaryName
+	 * @param secondaryName
+	 */
+	public ProviderDropdownDouble(String primaryName, String secondaryName) {
+		super(primaryName, secondaryName);
+	}
+
+	/**
+	 * @see com.idega.presentation.ui.SelectDropdownDouble#getValue(java.lang.Object)
+	 */
+	protected String getValue(Object value) {
+		try {
+			School school = (School) value;
+			return school.getSchoolName();
+		}
+		catch (ClassCastException e) {
+			return (String) value;
+		}
+	}
+
+	/**
+	 * @see com.idega.presentation.ui.SelectDropdownDouble#getKey(java.lang.Object)
+	 */
+	protected String getKey(Object key) {
+		try {
+			School school = (School) key;
+			return school.getPrimaryKey().toString();
+		}
+		catch (ClassCastException e) {
+			return (String) key;
+		}
+	}
+
+}
