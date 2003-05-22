9b6521a159230cd6eef025a2f0fc52f79f83c411
Thorhallur Helgason <laddi@idega.com>
2003-05-22 21:27:34 +0000

    A brilliant button that does everything.  Only thing missing is the kitchen sink...

---
+++ src/se/idega/idegaweb/commune/presentation/BusinessLaunchButton.java
@@ -0,0 +1,46 @@
+/*
+ * $Id: BusinessLaunchButton.java,v 1.1 2003/05/22 21:27:34 laddi Exp $
+ *
+ * Copyright (C) 2002 Idega hf. All Rights Reserved.
+ *
+ * This software is the proprietary information of Idega hf.
+ * Use is subject to license terms.
+ *
+ */
+package se.idega.idegaweb.commune.presentation;
+
+
+import se.idega.idegaweb.commune.childcare.presentation.ChildCareBlock;
+
+import com.idega.presentation.IWContext;
+import com.idega.presentation.ui.Form;
+import com.idega.presentation.ui.SubmitButton;
+
+/**
+ * This class does something very clever.....
+ * 
+ * @author <a href="palli@idega.is">Pall Helgason</a>
+ * @version 1.0
+ */
+public class BusinessLaunchButton extends ChildCareBlock {
+	protected static final String SUBMIT = "detonate";
+	
+	protected void control(IWContext iwc) {
+		if (iwc.isParameterSet(SUBMIT)) {
+			getBusiness().convertOldQueue(iwc.getCurrentUser());
+		}
+		
+		displayForm();			
+	}
+
+	protected void displayForm() {
+		Form form = new Form();
+		SubmitButton button = new SubmitButton(SUBMIT,"Kill application");
+		form.add(button);
+		add(form);	
+	}
+
+	public void init(IWContext iwc) {
+		control(iwc);
+	}
+}
