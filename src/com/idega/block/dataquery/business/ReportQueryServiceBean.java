2f9d8ab3886930d9bc01ebae5dc1edfcac8b505e
Aron Birkir Gudmundsson <aron@idega.com>
2003-05-22 10:27:38 +0000

    *** empty log message ***

---
+++ src/com/idega/block/dataquery/business/ReportQueryServiceBean.java
@@ -0,0 +1,16 @@
+/*
+ * Created on May 22, 2003
+ *
+ * To change this generated comment go to 
+ * Window>Preferences>Java>Code Generation>Code Template
+ */
+package com.idega.block.dataquery.business;
+
+import com.idega.business.IBOServiceBean;
+
+/**
+ * @author aron
+ */
+public class ReportQueryServiceBean extends IBOServiceBean {
+
+}
