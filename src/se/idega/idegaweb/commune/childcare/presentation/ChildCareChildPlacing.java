a153bff2d83b82c166d905fc1d5027d3bd193492
Thorhallur Helgason <laddi@idega.com>
2003-05-27 06:34:48 +0000

    Shows information about child care placings for a child.

---
+++ src/se/idega/idegaweb/commune/childcare/presentation/ChildCareChildPlacing.java
@@ -0,0 +1,177 @@
+/*
+ * Created on 26.5.2003
+ */
+package se.idega.idegaweb.commune.childcare.presentation;
+
+import java.rmi.RemoteException;
+import java.util.Collection;
+import java.util.Iterator;
+
+import com.idega.block.school.data.School;
+import com.idega.block.school.data.SchoolClass;
+import com.idega.block.school.data.SchoolClassMember;
+import com.idega.core.data.Address;
+import com.idega.core.data.Email;
+import com.idega.core.data.Phone;
+import com.idega.presentation.IWContext;
+import com.idega.presentation.Table;
+import com.idega.presentation.text.Link;
+import com.idega.presentation.ui.GenericButton;
+import com.idega.user.data.User;
+import com.idega.util.IWTimestamp;
+import com.idega.util.PersonalIDFormatter;
+
+/**
+ * @author laddi
+ */
+public class ChildCareChildPlacing extends ChildCareBlock {
+
+	/**
+	 * @see se.idega.idegaweb.commune.childcare.presentation.ChildCareBlock#init(com.idega.presentation.IWContext)
+	 */
+	public void init(IWContext iwc) throws Exception {
+		Table table = new Table(1,5);
+		table.setCellpadding(0);
+		table.setCellspacing(0);
+		table.setWidth(getWidth());
+		table.setHeight(2, 12);
+		table.setHeight(4, 12);
+		
+		GenericButton back = (GenericButton) getStyledInterface(new GenericButton("back",localize("back","Back")));
+		if (getResponsePage() != null)
+			back.setPageToOpen(getResponsePage());
+
+		if (getSession().getUserID() != -1) {
+			table.add(getInformationTable(iwc), 1, 1);
+			table.add(getPlacingsTable(iwc), 1, 3);
+			table.add(back, 1, 5);
+		}
+		else {
+			table.add(getLocalizedHeader("child_care.no_child_or_application_found","No child or application found."), 1, 1);
+			table.add(back, 1, 3);
+		}
+		
+		add(table);
+	}
+	
+	protected Table getPlacingsTable(IWContext iwc) throws RemoteException {
+		Table table = new Table();
+		table.setWidth(getWidth());
+		table.setCellpadding(getCellpadding());
+		table.setCellspacing(getCellspacing());
+		table.setColumns(4);
+		table.setRowColor(1, getHeaderColor());
+		int column = 1;
+		int row = 1;
+			
+		table.add(getLocalizedSmallHeader("child_care.provider","Provider"), column++, row);
+		table.add(getLocalizedSmallHeader("child_care.group","Group"), column++, row);
+		table.add(getLocalizedSmallHeader("child_care.valid_from","Valid from"), column++, row);
+		table.add(getLocalizedSmallHeader("child_care.removed","Removed"), column++, row++);
+		
+		SchoolClassMember member;
+		SchoolClass group;
+		School provider;
+		IWTimestamp validFrom;
+		IWTimestamp terminated = null;
+		
+		Collection placings = getBusiness().getSchoolBusiness().findClassMemberInSchool(getSession().getChildID(), getSession().getChildCareID());
+		Iterator iter = placings.iterator();
+		while (iter.hasNext()) {
+			column = 1;
+			member = (SchoolClassMember) iter.next();
+			group = member.getSchoolClass();
+			provider = group.getSchool();
+			validFrom = new IWTimestamp(member.getRegisterDate());
+			if (member.getRemovedDate() != null)
+				terminated = new IWTimestamp(member.getRemovedDate());
+
+			if (row % 2 == 0)
+				table.setRowColor(row, getZebraColor1());
+			else
+				table.setRowColor(row, getZebraColor2());
+	
+			table.add(getSmallText(provider.getSchoolName()), column++, row);
+			table.add(getSmallText(group.getSchoolClassName()), column++, row);
+			table.add(getSmallText(validFrom.getLocaleDate(iwc.getCurrentLocale(), IWTimestamp.SHORT)), column++, row);
+			if (member.getRemovedDate() != null)
+				table.add(getSmallText(terminated.getLocaleDate(iwc.getCurrentLocale(), IWTimestamp.SHORT)), column++, row);
+			else
+				table.add(getSmallText("-"), column++, row++);
+		}
+		table.setColumnAlignment(3, Table.HORIZONTAL_ALIGN_CENTER);
+		table.setColumnAlignment(4, Table.HORIZONTAL_ALIGN_CENTER);
+
+		return table;
+	}
+
+	protected Table getInformationTable(IWContext iwc) throws RemoteException {
+		Table table = new Table();
+		table.setWidth(Table.HUNDRED_PERCENT);
+		table.setCellpadding(0);
+		table.setCellspacing(0);
+		table.setColumns(3);
+		table.setWidth(1, "100");
+		table.setWidth(2, "6");
+		int row = 1;
+		
+		User child = getBusiness().getUserBusiness().getUser(getSession().getChildID());
+		if (child != null) {
+			Address address = getBusiness().getUserBusiness().getUsersMainAddress(child);
+			Collection parents = getBusiness().getUserBusiness().getParentsForChild(child);
+			
+			table.add(getLocalizedSmallHeader("child_care.child","Child"), 1, row);
+			table.add(getSmallText(child.getNameLastFirst(true)), 3, row);
+			table.add(getSmallText(" - "), 3, row);
+			table.add(getSmallText(PersonalIDFormatter.format(child.getPersonalID(), iwc.getCurrentLocale())), 3, row++);
+			
+			if (address != null) {
+				table.add(getLocalizedSmallHeader("child_care.address","Address"), 1, row);
+				table.add(getSmallText(address.getStreetAddress()), 3, row);
+				if (address.getPostalAddress() != null)
+					table.add(getSmallText(", "+address.getPostalAddress()), 3, row);
+				row++;
+			}
+			
+			table.setHeight(row++, 12);
+			
+			if (parents != null) {
+				table.add(getLocalizedSmallHeader("child_care.parents","Parents"), 1, row);
+				Phone phone;
+				Email email;
+
+				Iterator iter = parents.iterator();
+				while (iter.hasNext()) {
+					User parent = (User) iter.next();
+					address = getBusiness().getUserBusiness().getUsersMainAddress(parent);
+					email = getBusiness().getUserBusiness().getEmail(parent);
+					phone = getBusiness().getUserBusiness().getHomePhone(parent);
+
+					table.add(getSmallText(parent.getNameLastFirst(true)), 3, row);
+					table.add(getSmallText(" - "), 3, row);
+					table.add(getSmallText(PersonalIDFormatter.format(parent.getPersonalID(), iwc.getCurrentLocale())), 3, row++);
+			
+					if (address != null) {
+						table.add(getSmallText(address.getStreetAddress()), 3, row);
+						if (address.getPostalAddress() != null)
+							table.add(getSmallText(", "+address.getPostalAddress()), 3, row);
+						row++;
+					}
+					if (phone != null && phone.getNumber() != null) {
+						table.add(getSmallText(localize("child_care.phone","Phone")+": "), 3, row);
+						table.add(getSmallText(phone.getNumber()), 3, row++);
+					}
+					if (email != null && email.getEmailAddress() != null) {
+						Link link = getSmallLink(email.getEmailAddress());
+						link.setURL("mailto:"+email.getEmailAddress(), false, false);
+						table.add(link, 3, row++);
+					}
+			
+					table.setHeight(row++, 12);
+				}
+			}
+		}
+		
+		return table;
+	}
+}
