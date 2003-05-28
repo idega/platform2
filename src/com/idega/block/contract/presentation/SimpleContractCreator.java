311ac8dbd70824c24f390a84b790524fba42c608
Roar Skullestad <roar@idega.com>
2003-05-28 00:29:54 +0000

    Simple block for creating contracts

---
+++ src/com/idega/block/contract/presentation/SimpleContractCreator.java
@@ -0,0 +1,67 @@
+/*
+ * Created on 27.5.2003
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package com.idega.block.contract.presentation;
+
+import java.util.Iterator;
+import java.util.List;
+
+import com.idega.block.contract.business.ContractFinder;
+import com.idega.block.contract.business.ContractBusiness;
+import com.idega.block.contract.data.ContractCategory;
+import com.idega.idegaweb.IWResourceBundle;
+import com.idega.presentation.Block;
+import com.idega.presentation.IWContext;
+import com.idega.presentation.ui.DropdownMenu;
+import com.idega.presentation.ui.Form;
+import com.idega.presentation.ui.SubmitButton;
+import com.idega.user.data.User;
+
+/**
+ * @author Roar
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public class SimpleContractCreator extends Block {
+	
+	private static String IW_BUNDLE_IDENTIFIER="com.idega.block.contract";
+	private static String PARAM_MENU="scc_category";
+	private static String PARAM_SUBMIT="scc_submit";
+
+	public void main(IWContext iwc){
+		IWResourceBundle iwrb = this.getResourceBundle(iwc);
+		add(iwrb.getLocalizedString("scc.selectcon","Select contract:"));
+		Form form = new Form();
+		add(form);
+		DropdownMenu menu = new DropdownMenu(PARAM_MENU);
+		form.add(menu);
+		List categories = ContractFinder.listOfContractCategories();
+		for (Iterator iter = categories.iterator(); iter.hasNext();) {
+			ContractCategory element = (ContractCategory) iter.next();
+			menu.addMenuElement(element.getID(),element.getName());
+		}
+		if(iwc.isParameterSet(PARAM_MENU))
+		{
+			int categoryID = Integer.parseInt(iwc.getParameter(PARAM_MENU));
+			createContract(categoryID,iwc.getCurrentUser());
+		}
+		form.add(new SubmitButton(PARAM_SUBMIT,iwrb.getLocalizedString("scc.createcon","Create contract")));
+	}
+	
+	/**
+	 * @param categoryID
+	 * @param user
+	 */
+	private void createContract(int categoryId, User user) {
+		int userID = user.getID();
+		ContractBusiness.createAndPrintContract(userID,categoryId);
+	}
+
+	public String getBundleIdentifier(){
+		return this.IW_BUNDLE_IDENTIFIER;
+	}
+}
