7256d50fff3508541d0caf59bf84c575427e80b3
Roar Skullestad <roar@idega.com>
2003-05-28 00:26:40 +0000

    Changed to abstract class Added setNextAction()

--- src/se/idega/block/pki/data/NBSSignedEntity.java
+++ src/se/idega/block/pki/data/NBSSignedEntity.java
@@ -14,11 +14,35 @@ import java.sql.Date;
  * To change the template for this generated type comment go to
  * Window>Preferences>Java>Code Generation>Code and Comments
  */
-public interface NBSSignedEntity {
-	void setXmlSignedData(String data);
-	void setSignedBy(int userId);
-	void setSignedDate(Date time);
-	void setSignedFlag(boolean flag);
-	void store();
-	String getText();
+public abstract class NBSSignedEntity {
+	public final static int ACTION_INIT = 1, ACTION_PROCESS = 2, ACTION_END = 3;
+	private int _action = ACTION_INIT;
+	
+	public abstract void setXmlSignedData(String data);
+	public abstract void setSignedBy(int userId);
+	public abstract void setSignedDate(Date time);
+	public abstract void setSignedFlag(boolean flag);
+	public abstract void store();
+	public abstract String getText();
+	
+	public int getAction(){
+		return _action;	
+	}
+	
+	public void setNextAction(){
+		switch(_action){
+			case ACTION_INIT:
+				_action = ACTION_PROCESS;
+				break;
+			case ACTION_PROCESS:
+				_action = ACTION_END;
+				break;
+			case ACTION_END:
+				_action = ACTION_INIT;
+				break;
+		}
+		System.out.println("Action set to: " + _action);
+		
+	
+	}
 }
