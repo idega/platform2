6dc6914d5f1c0c8579b7dea2f58591a34d62cac3
Roar Skullestad <roar@idega.com>
2003-06-10 11:01:00 +0000

    *** empty log message ***

---
+++ src/se/idega/idegaweb/commune/childcare/presentation/ChildCareWindowBig.java
@@ -0,0 +1,22 @@
+/*
+ * Created on 9.6.2003
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package se.idega.idegaweb.commune.childcare.presentation;
+
+/**
+ * @author Roar
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public class ChildCareWindowBig extends ChildCareWindow {
+	public ChildCareWindowBig() {
+		super();		
+		this.setWidth(700);
+		this.setHeight(500);
+
+	}
+}
