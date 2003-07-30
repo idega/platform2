1b0d4fe0f3ff736b0dc4623a741434de2220d362
Thomas Hilbig <thomas@idega.com>
2003-07-30 11:23:59 +0000

    now create work report division method stores the new entity

--- src/is/idega/idegaweb/member/isi/block/reports/business/WorkReportBusinessBean.java
+++ src/is/idega/idegaweb/member/isi/block/reports/business/WorkReportBusinessBean.java
@@ -319,6 +319,7 @@ public class WorkReportBusinessBean extends MemberUserBusinessBean implements Me
     }
     // fax
     // email
+    workReportDivisionBoard.store();
     return workReportDivisionBoard;
   }
          
