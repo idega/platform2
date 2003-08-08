1d2b05188d53f8bb2a39a202e38bb060da6206db
Aron Birkir Gudmundsson <aron@idega.com>
2003-08-08 13:58:53 +0000

    changing from IDOEntity to IDOLegacyEntity

--- src/is/idega/idegaweb/campus/data/ApplicationSubjectInfo.java
+++ src/is/idega/idegaweb/campus/data/ApplicationSubjectInfo.java
@@ -2,15 +2,15 @@ package is.idega.idegaweb.campus.data;
 
 import javax.ejb.*;
 
-public interface ApplicationSubjectInfo extends com.idega.data.IDOEntity
+public interface ApplicationSubjectInfo extends com.idega.data.IDOLegacyEntity
 {
- public java.sql.Timestamp getFirstChange() throws java.rmi.RemoteException;
- public java.sql.Timestamp getFirstSubmission() throws java.rmi.RemoteException;
- public java.sql.Timestamp getLastSubmission() throws java.rmi.RemoteException;
- public int getNumber() throws java.rmi.RemoteException;
- public java.lang.String getSubjectName() throws java.rmi.RemoteException;
- public java.sql.Timestamp getLastChange() throws java.rmi.RemoteException;
- public java.lang.String getStatus() throws java.rmi.RemoteException;
- public void delete()throws java.sql.SQLException, java.rmi.RemoteException;
- public int getSubjectId() throws java.rmi.RemoteException;
+ public java.sql.Timestamp getFirstChange();
+ public java.sql.Timestamp getFirstSubmission() ;
+ public java.sql.Timestamp getLastSubmission();
+ public int getNumber() ;
+ public java.lang.String getSubjectName() ;
+ public java.sql.Timestamp getLastChange() ;
+ public java.lang.String getStatus() ;
+ public void delete() throws java.sql.SQLException;
+ public int getSubjectId() ;
 }
