eee9482f698de4438e8dce02ac77f6c92ba0e05a
Joakim Johnsson <joakim@idega.com>
2003-08-18 17:06:47 +0000

    Created

---
+++ src/se/idega/idegaweb/commune/accounting/invoice/business/InvoiceBusinessBean.java
@@ -0,0 +1,62 @@
+package se.idega.idegaweb.commune.accounting.invoice.business;
+
+import java.rmi.RemoteException;
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+import javax.ejb.FinderException;
+
+import com.idega.data.IDOLookup;
+
+import se.idega.idegaweb.commune.childcare.data.ChildCareContractArchive;
+import se.idega.idegaweb.commune.childcare.data.ChildCareContractArchiveHome;
+
+/**
+ * @author Joakim
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public class InvoiceBusinessBean {
+	
+	public void createInvoicingData(Date startDate, Date endDate){
+		
+		ChildCareContractArchive contract;
+		Collection archive = new ArrayList();
+
+		//Flag all contracts as 'not processed'
+
+		
+		try {
+			archive = getChildCareContractArchiveHome().findByDateRange(startDate, endDate);
+		} catch (RemoteException e) {
+			// TODO (JJ) 
+			e.printStackTrace();
+		} catch (FinderException e) {
+			// TODO (JJ) create feedback that no contracts were found
+			e.printStackTrace();
+		}
+		Iterator iter = archive.iterator();
+
+		//Fetch the reference at the provider
+
+
+		//Create the invoice header
+
+		
+		//Loop through all contracts
+		while(iter.hasNext())
+		{
+			contract = (ChildCareContractArchive)iter.next();
+			//If contract is used in the given period
+				//Calc days placed in the given period
+		}
+	}
+
+	public ChildCareContractArchiveHome getChildCareContractArchiveHome() throws RemoteException {
+		return (ChildCareContractArchiveHome) IDOLookup.getHome(ChildCareContractArchive.class);
+	}
+
+}
