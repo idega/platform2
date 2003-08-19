92edb871565e459f37c1d3a0ac555d71ffcaa004
Thorhallur Helgason <laddi@idega.com>
2003-08-19 23:01:22 +0000

    Fixed problem with incompatible return types.

--- src/se/idega/idegaweb/commune/accounting/regulations/data/VATRegulation.java
+++ src/se/idega/idegaweb/commune/accounting/regulations/data/VATRegulation.java
@@ -4,10 +4,17 @@ package se.idega.idegaweb.commune.accounting.regulations.data;
 public interface VATRegulation extends com.idega.data.IDOEntity
 {
  public java.lang.String getDescription();
+ public java.lang.String getIDColumnName();
  public se.idega.idegaweb.commune.accounting.regulations.data.PaymentFlowType getPaymentFlowType();
- public java.lang.String getPeriodFrom();
- public java.lang.String getPeriodTo();
+ public java.sql.Date getPeriodFrom();
+ public java.sql.Date getPeriodTo();
  public se.idega.idegaweb.commune.accounting.regulations.data.ProviderType getProviderType();
  public int getVATPercent();
  public void initializeAttributes();
+ public void setDescription(java.lang.String p0);
+ public void setPaymentFlowTypeId(int p0);
+ public void setPeriodFrom(java.sql.Date p0);
+ public void setPeriodTo(java.sql.Date p0);
+ public void setProviderTypeId(int p0);
+ public void setVATPercent(int p0);
 }
