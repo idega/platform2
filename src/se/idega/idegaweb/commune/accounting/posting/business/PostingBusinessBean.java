746eb8d73468adb3f45d401787236346a95e78a4
Kjell Lindman = <kjell@lindman.se>
2003-08-20 11:52:48 +0000

    General adding stuff

--- src/se/idega/idegaweb/commune/accounting/posting/business/PostingBusinessBean.java
+++ src/se/idega/idegaweb/commune/accounting/posting/business/PostingBusinessBean.java
@@ -1,3 +1,12 @@
+/*
+ * $Id: PostingBusinessBean.java,v 1.5 2003/08/20 11:52:48 kjell Exp $
+ *
+ * Copyright (C) 2003 Agura IT. All Rights Reserved.
+ *
+ * This software is the proprietary information of Agura IT AB.
+ * Use is subject to license terms.
+ *
+ */
 package se.idega.idegaweb.commune.accounting.posting.business;
 
 import java.rmi.RemoteException;
@@ -12,6 +21,15 @@ import se.idega.idegaweb.commune.accounting.posting.data.PostingString;
 import se.idega.idegaweb.commune.accounting.posting.data.PostingStringHome;
 import se.idega.idegaweb.commune.accounting.posting.data.PostingParameters;
 import se.idega.idegaweb.commune.accounting.posting.data.PostingParametersHome;
+
+import se.idega.idegaweb.commune.accounting.regulations.data.ActivityTypeHome;
+import se.idega.idegaweb.commune.accounting.regulations.data.ActivityType;
+import se.idega.idegaweb.commune.accounting.regulations.data.CommuneBelongingTypeHome;
+import se.idega.idegaweb.commune.accounting.regulations.data.CommuneBelongingType;
+import se.idega.idegaweb.commune.accounting.regulations.data.CompanyTypeHome;
+import se.idega.idegaweb.commune.accounting.regulations.data.CompanyType;
+import se.idega.idegaweb.commune.accounting.regulations.data.RegulationSpecTypeHome;
+import se.idega.idegaweb.commune.accounting.regulations.data.RegulationSpecType;
 
 /**
  * @author Joakim
@@ -112,7 +130,6 @@ public class PostingBusinessBean extends com.idega.business.IBOServiceBean imple
 	 * @return PostingParameters
 	 * @author Kjell
 	 */
-/*
 	public Object findPostingParameter(int id) throws FinderException {
 		try {
 			PostingParametersHome home = getPostingParametersHome();
@@ -123,7 +140,76 @@ public class PostingBusinessBean extends com.idega.business.IBOServiceBean imple
 			return null;
 		}
 	}
-*/
+
+	/**
+	 * Gets all Activity types
+	 * @return collection of Activity Types
+	 * @see se.idega.idegaweb.commune.accounting.regulations.data.ActivityType 
+	 * @author Kjell
+	 */
+	public Collection findAllActivityTypes() {
+		try {
+			ActivityTypeHome home = getActivityTypeHome();
+			return home.findAllActivityTypes();				
+		} catch (RemoteException e) {
+			return null;
+		} catch (FinderException e) {
+			return null;
+		}
+	}	
+
+	/**
+	 * Gets all Commune belonging types
+	 * @return collection of Commune belonging types
+	 * @see se.idega.idegaweb.commune.accounting.regulations.data.CommuneBelongingType 
+	 * @author Kjell
+	 */
+	public Collection findAllCommuneBelongingTypes() {
+		try {
+			CommuneBelongingTypeHome home = getCommuneBelongingTypeHome();
+			return home.findAllCommuneBelongingTypes();			
+		} catch (RemoteException e) {
+			return null;
+		} catch (FinderException e) {
+			return null;
+		}
+	}	
+
+	/**
+	 * Gets all Company Types
+	 * @return collection of Company Types
+	 * @see se.idega.idegaweb.commune.accounting.regulations.data.CompanyType 
+	 * @author Kjell
+	 */
+	public Collection findAllCompanyTypes() {
+		try {
+			CompanyTypeHome home = getCompanyTypeHome();
+			return home.findAllCompanyTypes();				
+		} catch (RemoteException e) {
+			return null;
+		} catch (FinderException e) {
+			return null;
+		}
+	}	
+
+	/**
+	 * Gets all Regulation specification types
+	 * @return collection of Regulation specification types
+	 * @see se.idega.idegaweb.commune.accounting.regulations.data.RegulationSpecType 
+	 * @author Kjell
+	 */
+	public Collection findAllRegulationSpecTypes() {
+		try {
+			RegulationSpecTypeHome home = getRegulationSpecTypeHome();
+			return home.findAllRegulationSpecTypes();				
+		} catch (RemoteException e) {
+			return null;
+		} catch (FinderException e) {
+			return null;
+		}
+	}	
+
+
 	
 	/**
 	 * Pads the string according to the rules in Posting field
@@ -166,7 +252,21 @@ public class PostingBusinessBean extends com.idega.business.IBOServiceBean imple
 		return ret;
 	}
 
+	protected ActivityTypeHome getActivityTypeHome() throws RemoteException {
+		return (ActivityTypeHome) com.idega.data.IDOLookup.getHome(ActivityType.class);
+	}
 
+	protected CommuneBelongingTypeHome getCommuneBelongingTypeHome() throws RemoteException {
+		return (CommuneBelongingTypeHome) com.idega.data.IDOLookup.getHome(CommuneBelongingType.class);
+	}
+
+	protected CompanyTypeHome getCompanyTypeHome() throws RemoteException {
+		return (CompanyTypeHome) com.idega.data.IDOLookup.getHome(CompanyType.class);
+	}
+
+	protected RegulationSpecTypeHome getRegulationSpecTypeHome() throws RemoteException {
+		return (RegulationSpecTypeHome) com.idega.data.IDOLookup.getHome(RegulationSpecType.class);
+	}
 
 	protected PostingParametersHome getPostingParametersHome() throws RemoteException {
 		return (PostingParametersHome) com.idega.data.IDOLookup.getHome(PostingParameters.class);
