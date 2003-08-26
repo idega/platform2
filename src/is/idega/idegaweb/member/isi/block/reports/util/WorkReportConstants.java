60655533082d5c870722a4d6eb0b3b4858bdd1db
Eirikur Hrafnsson <eiki@idega.com>
2003-08-26 21:18:32 +0000

    A collection of the static variables used in the workreport system. Extends IWMemberConstants for convenience.

---
+++ src/is/idega/idegaweb/member/isi/block/reports/util/WorkReportConstants.java
@@ -0,0 +1,23 @@
+package is.idega.idegaweb.member.isi.block.reports.util;
+
+import is.idega.idegaweb.member.util.IWMemberConstants;
+
+/**
+ * A collection of the static variables used in the workreport system. Extends IWMemberConstants for convenience.
+ * @author <a href="mailto:eiki@idega.is">Eirikur Hrafnsson</a>
+ *
+ */
+public class WorkReportConstants extends IWMemberConstants{
+
+	public static final String WR_STATUS_NOT_DONE = "NOT_DONE";
+	public static final String WR_STATUS_SOME_DONE = "SOME_DONE";
+	public static final String WR_STATUS_DONE = "DONE";
+	public static final String WR_STATUS_CONTINUANCE = "CONTINUANCE";
+	public static final String WR_STATUS_COMPETITION_BAN = "COMPETITION_BAN";
+	public static final String WR_STATUS_AT_REGIONAL_UNION = "AT_REGIONAL_UNION";
+	public static final String WR_STATUS_INVESTIGATE = "INVESTIGATE";
+	public static final String WR_STATUS_NO_REPORT = "NO_REPORT";
+	public static final String WR_STATUS_SHOULD_BE_BANNED = "SHOULD_BE_BANNED";
+	
+
+}
