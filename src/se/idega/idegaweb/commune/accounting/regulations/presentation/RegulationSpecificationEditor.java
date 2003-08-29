d61f036191d9a0c2e4e3234ba9f250a5c1ed5bad
Kjell Lindman = <kjell@lindman.se>
2003-08-29 14:51:12 +0000

    *** empty log message ***

--- src/se/idega/idegaweb/commune/accounting/regulations/presentation/RegulationSpecificationEditor.java
+++ src/se/idega/idegaweb/commune/accounting/regulations/presentation/RegulationSpecificationEditor.java
@@ -1,5 +1,5 @@
 /*
- * $Id: RegulationSpecificationEditor.java,v 1.2 2003/08/29 01:35:24 kjell Exp $
+ * $Id: RegulationSpecificationEditor.java,v 1.3 2003/08/29 14:51:12 kjell Exp $
  *
  * Copyright (C) 2003 Agura IT. All Rights Reserved.
  *
@@ -31,10 +31,10 @@ import se.idega.idegaweb.commune.accounting.regulations.business.RegulationExcep
 /** 
  * RegulationSpecificationEditor is an idegaWeb block that handles RegSpec types
  * <p>
- * $Date: 2003/08/29 01:35:24 $
+ * $Date: 2003/08/29 14:51:12 $
  *
  * @author Kjell Lindman
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
 public class RegulationSpecificationEditor extends AccountingBlock {
 
@@ -225,10 +225,12 @@ public class RegulationSpecificationEditor extends AccountingBlock {
 
 	private String getLocalizedKey(IWContext iwc, String param) {
 		String ret = getParameter(iwc, param);
+/*
 		ret = ret.toLowerCase().replaceAll(" ", "_").replaceAll("å","aa").replaceAll("ä","ae").replaceAll("ö", "oe");
 		ret = ret.replaceAll("Å","aa").replaceAll("Ä","ae").replaceAll("Ö", "oe");
 		ret = ret.replaceAll("'","_").replaceAll(".","_").replaceAll("`", "_");
 		ret = ret.replaceAll("`","_").replaceAll(" ","_").replaceAll("=", "_");
+*/
 		return ret;
 	}
 
