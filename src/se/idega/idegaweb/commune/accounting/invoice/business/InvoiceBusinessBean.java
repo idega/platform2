86cd62c16456983aba881ae6531bd7ee19919ad1
Joakim Johnsson <joakim@idega.com>
2003-09-08 17:13:37 +0000

    Unfucked version...

--- src/se/idega/idegaweb/commune/accounting/invoice/business/InvoiceBusinessBean.java
+++ src/se/idega/idegaweb/commune/accounting/invoice/business/InvoiceBusinessBean.java
@@ -1,5 +1,5 @@
 package se.idega.idegaweb.commune.accounting.invoice.business;
-
+/*
 import java.rmi.RemoteException;
 import java.sql.Date;
 import java.util.ArrayList;
@@ -9,33 +9,37 @@ import java.util.Iterator;
 import javax.ejb.CreateException;
 import javax.ejb.FinderException;
 
-import com.idega.block.school.data.School;
-import com.idega.data.IDOLookup;
-import com.idega.user.data.User;
-import com.idega.util.IWTimestamp;
-
 import se.idega.idegaweb.commune.accounting.invoice.data.InvoiceHeader;
 import se.idega.idegaweb.commune.accounting.invoice.data.InvoiceHeaderHome;
 import se.idega.idegaweb.commune.accounting.invoice.data.InvoiceRecord;
 import se.idega.idegaweb.commune.accounting.invoice.data.InvoiceRecordHome;
 import se.idega.idegaweb.commune.accounting.posting.business.PostingBusiness;
 import se.idega.idegaweb.commune.accounting.posting.business.PostingBusinessHome;
+import se.idega.idegaweb.commune.accounting.posting.data.PostingParameters;
 import se.idega.idegaweb.commune.accounting.regulations.data.KeyMapping;
+import se.idega.idegaweb.commune.accounting.regulations.data.KeyMappingBMPBean;
 import se.idega.idegaweb.commune.accounting.regulations.data.KeyMappingHome;
 import se.idega.idegaweb.commune.childcare.data.ChildCareContractArchive;
 import se.idega.idegaweb.commune.childcare.data.ChildCareContractArchiveHome;
 
+import com.idega.block.school.data.School;
+import com.idega.data.IDOLookup;
+import com.idega.user.data.User;
+import com.idega.util.IWTimestamp;
+*/
 /**
  * @author Joakim
  *
  * To change the template for this generated type comment go to
  * Window>Preferences>Java>Code Generation>Code and Comments
  */
+
 public class InvoiceBusinessBean {
+/*
 	private static final String BATCH_TEXT = "Härledd uppgift";		//Localize this text in the user interface
-//	private static final String HOURS_PER_WEEK = "tim/v";		//Localize this text in the user interface
+	private static final String HOURS_PER_WEEK = "tim/v";		//Localize this text in the user interface
 	
-	public void createInvoicingData(Date startPeriod, Date endPeriod){
+	public void createInvoicingData(Date startPeriod, Date endPeriod, String createdBy){
 		
 		ChildCareContractArchive contract;
 		Collection archive = new ArrayList();
@@ -43,16 +47,17 @@ public class InvoiceBusinessBean {
 		School provider;
 		Date currentDate = new Date( new java.util.Date().getTime());
 		float months;
-//		int days;
+		int days;
+		float totalSum;
 		IWTimestamp time, startTime, endTime;
 		try {
-			/*
+			//TODO (JJ) Probably need to remove the ref to KeyMappingBMPBean. But to what???
 			int childcare = getKeyMappingHome().findValueByCategoryAndKey(
 				KeyMappingBMPBean.CAT_ACTIVITY,KeyMappingBMPBean.KEY_CHILDCARE).getValue();
 			int check = getKeyMappingHome().findValueByCategoryAndKey(
 				KeyMappingBMPBean.CAT_REG_SPEC,KeyMappingBMPBean.KEY_CHECK).getValue();
-			*/
-			//**Flag all contracts as 'not processed'
+	
+			// **Flag all contracts as 'not processed'
 	
 			try {
 				archive = getChildCareContractArchiveHome().findByDateRange(startPeriod, endPeriod);
@@ -83,6 +88,7 @@ public class InvoiceBusinessBean {
 				invoiceHeader.setCustodianId(custodian);
 				invoiceHeader.setReference(provider);//TODO (JJ) Check if this is right. Supposed to be "Responcible person cenrally = BUN"...
 				invoiceHeader.setDateCreated(currentDate);
+				invoiceHeader.setCreatedBy(createdBy);
 				invoiceHeader.setCreatedBy(BATCH_TEXT);
 				//TODO (JJ) invoiceHeader.setOwnPosting();
 				//TODO (JJ) invoiceHeader.setDoublePosting();
@@ -108,36 +114,46 @@ public class InvoiceBusinessBean {
 					months += 1.0;
 					months -= percentOfMonthDone(startTime);
 					months -= 1.0 - percentOfMonthDone(endTime);
-					//days = IWTimestamp.getDaysBetween(startTime, endTime);
+					days = IWTimestamp.getDaysBetween(startTime, endTime);
 //				}
 
 
+				totalSum = 0;
 				//Get all the rules for this contract
 				//TODO (JJ) This is a func that Thomas will provide.
-				//PostingBusiness postingBusiness = getPostingBusinessHome().create();
-				
-/*				
-				PostingParameters rule = postingBusiness.getPostingParameter(new Date(), childcare, check, null, null);
-//				Rule rule = (Rule)ruleIter.next();
+				Rule rule = (Rule)ruleIter.next();
 				// **Create the invoice record
 				InvoiceRecord invoiceRecord = getInvoiceRecordHome().create();
-				invoiceRecord.setAmount(rule.getAmount());
+				totalSum = rule.getAmount();
+				invoiceRecord.setAmount(totalSum);
 				invoiceRecord.setInvoiceText(provider.getName()+", "+contract.getCareTime()+" "+HOURS_PER_WEEK);
 				invoiceRecord.setRuleText(rule.getText());
 				//TODO (JJ) get the "huvudverksamhet" object that laddi will create.
 				invoiceRecord.setDateCreated(currentDate);
+				invoiceRecord.setCreatedBy(createdBy);
 				invoiceRecord.setCreatedBy(BATCH_TEXT);
 				//TODO (JJ) Create a "utbetalningspost" waiting for description from Lotta
 				//TODO (JJ) set the reference to utbetalningsposten
 				invoiceRecord.setInvoiceHeader(invoiceHeader);
 				//TODO (JJ) Some VAT stuff needed here...
 
+				PostingBusiness postingBusiness = getPostingBusinessHome().create();
+				PostingParameters parameters = postingBusiness.getPostingParameter(
+					new Date(new java.util.Date().getTime()), childcare, check, 0, 0);
+				invoiceRecord.setOwnPosting(parameters.getPostingString());
+
 				//TODO (JJ) This is a func that Thomas will provide.
 				Iterator ruleIter = rule.iterator();
 				while(ruleIter.hasNext()){
 //					Rule rule = (Rule)ruleIter.next();
+					totalSum -= rule.getAmount();
 				}
-*/
+				//Make sure that the sum is not less than 0
+				if(totalSum<0){
+//					subvention -= totalSum;
+					//TODO (JJ) have to create some sort of record reference to the subvention row.
+				}
+
 		
 			}
 				
@@ -146,6 +162,9 @@ public class InvoiceBusinessBean {
 			e1.printStackTrace();
 		} catch (CreateException e) {
 			// TODO (JJ) Auto-generated catch block
+			e.printStackTrace();
+		} catch (FinderException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
@@ -182,5 +201,5 @@ public class InvoiceBusinessBean {
 	public PostingBusinessHome getPostingBusinessHome() throws RemoteException {
 		return (PostingBusinessHome) IDOLookup.getHome(PostingBusiness.class);
 	}
-
+*/
 }
