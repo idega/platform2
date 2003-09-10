895236a81ec4c7276a7d8d5f99ad396040f83c4a
Kjell Lindman = <kjell@lindman.se>
2003-09-10 16:04:56 +0000

    Added constants for Conditions

--- src/se/idega/idegaweb/commune/accounting/regulations/business/RuleTypeConstant.java
+++ src/se/idega/idegaweb/commune/accounting/regulations/business/RuleTypeConstant.java
@@ -9,4 +9,13 @@ public class RuleTypeConstant {
 	public static final String MANUAL ="ruleType.manual";
 	public static final String VAT ="ruleType.vat";
 	public static final String FORMULA ="ruleType.formula";
+
+	public final static String CONDITION_ID_OPERATION = "1";
+	public final static String CONDITION_ID_RESOURCE = "2";
+	public final static String CONDITION_ID_VAT = "3";
+	public final static String CONDITION_ID_SCHOOL_YEAR = "4";
+	public final static String CONDITION_ID_HOURS = "5";
+	public final static String CONDITION_ID_SIBLINGS = "6";	
+	public final static String CONDITION_ID_AGE_INTERVAL = "7";
+	
 }
