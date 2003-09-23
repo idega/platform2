7e41774139a7ed8f406e39ce4dab52dc855622b1
Grimur Jonsson <gimmi@idega.com>
2003-09-23 22:22:17 +0000

    setHeaderImage(Link, imageID) added

--- src/is/idega/idegaweb/travel/presentation/TravelCurrencyCalculatorWindow.java
+++ src/is/idega/idegaweb/travel/presentation/TravelCurrencyCalculatorWindow.java
@@ -2,6 +2,7 @@ package is.idega.idegaweb.travel.presentation;
 
 import com.idega.block.trade.presentation.CurrencyCalculator;
 import com.idega.presentation.*;
+import com.idega.presentation.text.Link;
 /**
  * Title:        idegaWeb TravelBooking
  * Description:
@@ -13,14 +14,26 @@ import com.idega.presentation.*;
 
 public class TravelCurrencyCalculatorWindow extends TravelWindow {
 
+	public static String PARAMETER_IMAGE_ID = "tccw_im";
+
   public TravelCurrencyCalculatorWindow() {
     this.setTitle("Currency Calculator");
   }
 
   public void main(IWContext iwc) throws Exception {
+		if (iwc.isParameterSet(PARAMETER_IMAGE_ID)) {
+			Image image = new Image(Integer.parseInt(iwc.getParameter(PARAMETER_IMAGE_ID)));
+			super.setHeaderImage(image);
+		}
     super.main(iwc);
+    
     CurrencyCalculator cc = new CurrencyCalculator();
+    cc.maintainParameter(PARAMETER_IMAGE_ID);
     add(cc);
+  }
+  
+  public static void setHeaderImage(Link link, int imageId) {
+  	link.addParameter(PARAMETER_IMAGE_ID, imageId);
   }
 
 }
