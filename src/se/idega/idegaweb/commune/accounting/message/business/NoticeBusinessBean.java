a0f82f53a62c1c348b7c8640c03b3872948b307b
Anders Lindman <anders.lindman@ncmedia.com>
2003-09-25 10:12:34 +0000

    Added new checks for providers

--- src/se/idega/idegaweb/commune/accounting/message/business/NoticeBusinessBean.java
+++ src/se/idega/idegaweb/commune/accounting/message/business/NoticeBusinessBean.java
@@ -1,5 +1,5 @@
 /*
- * $Id: NoticeBusinessBean.java,v 1.3 2003/09/08 08:10:07 laddi Exp $
+ * $Id: NoticeBusinessBean.java,v 1.4 2003/09/25 10:12:34 anders Exp $
  *
  * Copyright (C) 2003 Agura IT. All Rights Reserved.
  *
@@ -15,21 +15,22 @@ import java.util.ArrayList;
 import java.rmi.RemoteException;
 
 import com.idega.block.school.business.SchoolBusiness;
+import com.idega.block.school.data.SchoolCategory;
 import com.idega.block.school.data.SchoolType;
 import com.idega.block.school.data.School;
 import com.idega.user.business.UserBusiness;
 import com.idega.user.data.User;
 
 import se.idega.idegaweb.commune.message.business.MessageBusiness;
-//import se.idega.idegaweb.commune.message.data.Message;
+import se.idega.idegaweb.commune.accounting.school.data.Provider;
 
 /** 
- * Business logic for VAT values and regulations.
+ * Business logic for notice messages.
  * <p>
- * Last modified: $Date: 2003/09/08 08:10:07 $ by $Author: laddi $
+ * Last modified: $Date: 2003/09/25 10:12:34 $ by $Author: anders $
  *
  * @author Anders Lindman
- * @version $Revision: 1.3 $
+ * @version $Revision: 1.4 $
  */
 public class NoticeBusinessBean extends com.idega.business.IBOServiceBean implements NoticeBusiness  {
 
@@ -57,11 +58,14 @@ public class NoticeBusinessBean extends com.idega.business.IBOServiceBean implem
 		try {
 			UserBusiness ub = getUserBusiness();
 			SchoolBusiness sb = getSchoolBusiness();
+			SchoolCategory childCareCategory = sb.getCategoryChildcare();
+			String childCareCategoryId = childCareCategory.getCategory();
 			Collection schoolTypes = sb.findAllSchoolTypes();
 			Iterator iter = schoolTypes.iterator();
 			while (iter.hasNext()) {
 				SchoolType st = (SchoolType) iter.next();
-				if (st.getSchoolTypeName().equals("Grundskola") || st.getSchoolTypeName().equals("Gymnasieskola")) {
+				String sc = st.getSchoolCategory();				
+				if (sc.equals(childCareCategoryId)) {
 					int schoolTypeId = ((Integer) st.getPrimaryKey()).intValue();
 					Collection schools = sb.findAllSchoolsByType(schoolTypeId);
 					Iterator iter2 = schools.iterator();
@@ -69,13 +73,16 @@ public class NoticeBusinessBean extends com.idega.business.IBOServiceBean implem
 						School school = (School) iter2.next();
 						int headmasterUserId = school.getHeadmasterUserId();
 						if (headmasterUserId > 0) {
-							User headmaster = ub.getUser(headmasterUserId);
-							String[] s = new String[2];
-							s[0] = school.getName();
-							s[1] = headmaster.getName();
-							c.add(s);
-// remove comment			Message message = getMessageBusiness().createUserMessage(headmaster, subject, body);
-// to activate message		message.store();							
+							Provider provider = new Provider(school);
+							if (!school.getCentralizedAdministration() && !provider.getPaymentByInvoice()) {
+								User headmaster = ub.getUser(headmasterUserId);
+								String[] s = new String[2];
+								s[0] = school.getName();
+								s[1] = headmaster.getName();
+								c.add(s);
+//	   remove comment			Message message = getMessageBusiness().createUserMessage(headmaster, subject, body, false);
+//	   to activate message		message.store();							
+							}
 						}
 					}
 				}
