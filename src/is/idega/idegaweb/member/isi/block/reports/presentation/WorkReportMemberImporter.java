59b383a019df47616637275bd58a5675ddaac099
Pall Helgason <palli@idega.com>
2003-09-26 08:37:02 +0000

    First draft for solution for isi7

--- src/is/idega/idegaweb/member/isi/block/reports/presentation/WorkReportMemberImporter.java
+++ src/is/idega/idegaweb/member/isi/block/reports/presentation/WorkReportMemberImporter.java
@@ -8,8 +8,12 @@ import is.idega.idegaweb.member.isi.block.reports.business.WorkReportImportRepor
 import is.idega.idegaweb.member.isi.block.reports.util.WorkReportConstants;
 
 import java.rmi.RemoteException;
+import java.util.Iterator;
 
 import com.idega.presentation.IWContext;
+import com.idega.presentation.Table;
+import com.idega.presentation.ui.Form;
+import com.idega.presentation.ui.SubmitButton;
 
 /**
  * Description: This class extends WorkReportImporter and takes the uploaded file and reads and imports the member data.
@@ -18,7 +22,7 @@ import com.idega.presentation.IWContext;
  * @author <a href="mailto:eiki@idega.is">Eirikur S. Hrafnsson</a>
  */
 public class WorkReportMemberImporter extends WorkReportImporter {
-	
+
 	private static final String STEP_NAME_LOCALIZATION_KEY = "workreportmemberimporter.step_name";
 	private WorkReportImportReport _report = null;
 
@@ -26,42 +30,70 @@ public class WorkReportMemberImporter extends WorkReportImporter {
 		super();
 		setStepNameLocalizableKey(STEP_NAME_LOCALIZATION_KEY);
 	}
-	
-	
+
 	public void main(IWContext iwc) throws Exception {
 		super.main(iwc);
-		
-		if(getWorkReportFileId()!=-1){ //do nothing before we have the file id
+
+		if (getWorkReportFileId() != -1 && !iwc.isParameterSet("MEMBER_IMPORT_CONFIRM") && !iwc.isParameterSet("MEMBER_IMPORT_REJECT")) { //do nothing before we have the file id
 			//sets this step as bold, if another class calls it this will be overridden
 			setAsCurrentStepByStepLocalizableKey(STEP_NAME_LOCALIZATION_KEY);
-		
+
 			try {
 				String mainBoardName = getBundle(iwc).getProperty(WorkReportConstants.WR_MAIN_BOARD_NAME);
-				WorkReportImportReport report = getWorkReportImportBusiness(iwc).importMemberPart(getWorkReportFileId(),getWorkReportId(),mainBoardName);
-				if(report != null){
-//					add(iwrb.getLocalizedString("WorkReportMemberImporter.import_successful","Importing members completed successfully."));
+				WorkReportImportReport report = getWorkReportImportBusiness(iwc).importMemberPart(getWorkReportFileId(), getWorkReportId(), mainBoardName);
+				if (report != null) {
+					//					add(iwrb.getLocalizedString("WorkReportMemberImporter.import_successful","Importing members completed successfully."));
 					setWorkReportMemberImportReport(report);
 				}
-				else{
-					add(iwrb.getLocalizedString("WorkReportMemberImporter.import_failed","Importing members failed!"));
+				else {
+					add(iwrb.getLocalizedString("WorkReportMemberImporter.import_failed", "Importing members failed!"));
 				}
 			}
 			catch (RemoteException e) {
 				e.printStackTrace();
 			}
 			catch (WorkReportImportException e) {
-//				e.printStackTrace();
-				add( iwrb.getLocalizedString(e.getMessage(),e.getMessage()));
+				//				e.printStackTrace();
+				add(iwrb.getLocalizedString(e.getMessage(), e.getMessage()));
 			}
 		}
 	}
-	
+
 	protected WorkReportImportReport getWorkReportMemberImportReport() {
 		return _report;
 	}
-	
+
 	protected void setWorkReportMemberImportReport(WorkReportImportReport report) {
 		_report = report;
 	}
-	
+
+	protected void showReport() {
+		Form f = new Form();
+		Table t = new Table();
+		t.add(iwrb.getLocalizedString("workreportmemberimport.import_statistics"), 1, 1);
+		t.add(iwrb.getLocalizedString("workreportmemberimport.number_of_members"), 1, 3);
+		t.add(Integer.toString(getWorkReportMemberImportReport().numberOfMembers), 2, 3);
+		t.add(iwrb.getLocalizedString("workreportmemberimport.number_of_players"), 1, 5);
+		t.add(Integer.toString(getWorkReportMemberImportReport().numberOfPlayers), 2, 5);
+		t.add(iwrb.getLocalizedString("workreportmemberimport.number_of_divisions"), 1, 7);
+		t.add(Integer.toString(getWorkReportMemberImportReport().leaguesMap.size()), 2, 7);
+		t.add(iwrb.getLocalizedString("workreportmemberimport.not_imported"), 1, 9);
+		int i = 9;
+		if (getWorkReportMemberImportReport().notRead != null) {
+			Iterator it = getWorkReportMemberImportReport().notRead.iterator();
+			while (it.hasNext()) {
+				t.add((String)it.next(), 2, i++);
+			}
+		}
+
+		i++;
+		SubmitButton confirm = new SubmitButton("MEMBER_IMPORT_CONFIRM");
+		SubmitButton reject = new SubmitButton("MEMBER_IMPORT_REJECT");
+		t.add(confirm,2,i);
+		t.add(reject,2,i);
+			
+		f.add(t);	
+		add(f);
+	}
+
 }
--- src/is/idega/idegaweb/member/isi/block/reports/presentation/WorkReportMemberImporterConfirm.java
+++ src/is/idega/idegaweb/member/isi/block/reports/presentation/WorkReportMemberImporterConfirm.java
@@ -6,10 +6,7 @@
  */
 package is.idega.idegaweb.member.isi.block.reports.presentation;
 
-import java.util.Iterator;
-
 import com.idega.presentation.IWContext;
-import com.idega.presentation.Table;
 
 /**
  * @author palli
@@ -30,18 +27,15 @@ public class WorkReportMemberImporterConfirm extends WorkReportMemberImporter {
 	public void main(IWContext iwc) throws Exception {
 		super.main(iwc);
 		
-		if (getWorkReportMemberImportReport() != null) { 
+		if (iwc.isParameterSet("MEMBER_IMPORT_CONFIRM") || iwc.isParameterSet("MEMBER_IMPORT_REJECT")) { 
 			//sets this step as bold, if another class calls it this will be overridden
 			setAsCurrentStepByStepLocalizableKey(STEP_NAME_LOCALIZATION_KEY);
 		
 			if (iwc.isParameterSet("MEMBER_IMPORT_CONFIRM")) {
-				
+				add(iwrb.getLocalizedString("WorkReportMemberImporter.import_successful","Importing members completed successfully."));
 			}
 			else if  (iwc.isParameterSet("MEMBER_IMPORT_REJECT")) {
-				
-			}
-			else {
-				showReport();
+				add(iwrb.getLocalizedString("WorkReportMemberImporter.import_failed","Importing members failed!"));
 			}
 /*			try {
 				String mainBoardName = getBundle(iwc).getProperty(WorkReportConstants.WR_MAIN_BOARD_NAME);
@@ -61,26 +55,5 @@ public class WorkReportMemberImporterConfirm extends WorkReportMemberImporter {
 				add( iwrb.getLocalizedString(e.getMessage(),e.getMessage()));
 			}
 */		}
-	}
-	
-	protected void showReport() {
-		Table t = new Table();
-		t.add(iwrb.getLocalizedString("workreportmemberimport.import_statistics"),1,1);
-		t.add(iwrb.getLocalizedString("workreportmemberimport.number_of_members"),1,3);
-		t.add(Integer.toString(getWorkReportMemberImportReport().numberOfMembers),2,3);
-		t.add(iwrb.getLocalizedString("workreportmemberimport.number_of_players"),1,5);
-		t.add(Integer.toString(getWorkReportMemberImportReport().numberOfPlayers),2,5);
-		t.add(iwrb.getLocalizedString("workreportmemberimport.number_of_divisions"),1,7);
-		t.add(Integer.toString(getWorkReportMemberImportReport().leaguesMap.size()),2,7);
-		t.add(iwrb.getLocalizedString("workreportmemberimport.not_imported"),1,9);
-		if (getWorkReportMemberImportReport().notRead != null) {
-			Iterator it = getWorkReportMemberImportReport().notRead.iterator();
-			int i = 9;
-			while (it.hasNext()) {
-				t.add((String)it.next(),2,i++);
-			}
 		}
-		
-		add(t);
 	}
-}
