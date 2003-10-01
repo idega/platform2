81a29367afd5d60d395c5faadc4d319569aaf4a3
Roar Skullestad <roar@idega.com>
2003-10-01 15:05:33 +0000

    *** empty log message ***

---
+++ src/se/idega/idegaweb/commune/accounting/invoice/data/RegularInvoiceEntryHome.java
@@ -0,0 +1,17 @@
+/*
+ * Created on 1.10.2003
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package se.idega.idegaweb.commune.accounting.invoice.data;
+
+/**
+ * @author Roar
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public interface RegularInvoiceEntryHome extends com.idega.data.IDOHome{
+
+}
---
+++ src/se/idega/idegaweb/commune/accounting/invoice/data/RegularInvoiceEntryHomeImpl.java
@@ -0,0 +1,24 @@
+/*
+ * Created on 1.10.2003
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package se.idega.idegaweb.commune.accounting.invoice.data;
+
+/**
+ * @author Roar
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public class RegularInvoiceEntryHomeImpl  extends com.idega.data.IDOFactory implements RegularInvoiceEntryHome{
+
+	/* (non-Javadoc)
+	 * @see com.idega.data.IDOFactory#getEntityInterfaceClass()
+	 */
+	protected Class getEntityInterfaceClass() {
+		return RegularInvoiceEntry.class;
+	}
+
+}
