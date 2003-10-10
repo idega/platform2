beb00d92e5bd2f1b7ba4e69d2907a958a9b6991d
Joakim Johnsson <joakim@idega.com>
2003-10-10 16:33:21 +0000

    Doc

--- src/se/idega/idegaweb/commune/accounting/posting/business/MissingMandatoryFieldException.java
+++ src/se/idega/idegaweb/commune/accounting/posting/business/MissingMandatoryFieldException.java
@@ -1,13 +1,11 @@
 package se.idega.idegaweb.commune.accounting.posting.business;
 
 /**
+ * Exception thrown when a posting string is missing a field which is marked as mandatory
  * @author Joakim
- *
  */
 public class MissingMandatoryFieldException extends Exception {
 	/**
-	 * Exception thrown when a posting string has incorrect format
-	 * 
 	 * @see se.idega.idegaweb.commune.accounting.business.AccountingException
 	 */
 	public MissingMandatoryFieldException(String textKey) {
