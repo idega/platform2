b0d22d136d53c4aedf4297784352e520e6fbbc73
Grimur Jonsson <gimmi@idega.com>
2003-10-12 11:41:23 +0000

    Moved to com.idega.location.business

--- src/se/idega/idegaweb/commune/business/CommuneBusinessHome.java
+++
@@ -1,8 +0,0 @@
-package se.idega.idegaweb.commune.business;
-
-
-public interface CommuneBusinessHome extends com.idega.business.IBOHome
-{
- public CommuneBusiness create() throws javax.ejb.CreateException, java.rmi.RemoteException;
-
-}
