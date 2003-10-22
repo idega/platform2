649f074f0d5270f62daf14db5a8112157a28d8f0
Grimur Jonsson <gimmi@idega.com>
2003-10-22 09:59:37 +0000

    *** empty log message ***

--- src/se/idega/idegaweb/commune/account/business/AccountBusiness.java
+++ src/se/idega/idegaweb/commune/account/business/AccountBusiness.java
@@ -1,20 +1,20 @@
 package se.idega.idegaweb.commune.account.business;
 
-import java.rmi.RemoteException;
-import java.util.Collection;
-import java.util.Iterator;
 
-import javax.ejb.*;
-
-public interface AccountBusiness extends com.idega.business.IBOService
-{
-	
-	public  Collection getAllPendingApplications()throws FinderException,RemoteException;
-	public Iterator getAllPendingApplicationsIterator() throws FinderException, RemoteException;
-	
-	public  Collection getAllRejectedApplications()throws FinderException,RemoteException;
-	public Iterator getAllRejectedApplicationsIterator() throws FinderException, RemoteException;
-	
-	public  Collection getAllAcceptedApplications()throws FinderException,RemoteException;
-	public Iterator getAllAcceptedApplicationsIterator() throws FinderException, RemoteException;
+public interface AccountBusiness extends com.idega.business.IBOService {
+ public void acceptApplication(int p0,com.idega.user.data.User p1,boolean p2,boolean p3)throws java.rmi.RemoteException,javax.ejb.CreateException,javax.ejb.FinderException, java.rmi.RemoteException;
+ public void acceptApplication(int p0,com.idega.user.data.User p1)throws java.rmi.RemoteException,javax.ejb.CreateException,javax.ejb.FinderException, java.rmi.RemoteException;
+ public void acceptApplication(int p0,com.idega.user.data.User p1,boolean p2)throws java.rmi.RemoteException,javax.ejb.CreateException,javax.ejb.FinderException, java.rmi.RemoteException;
+ public java.lang.String getAcceptMessageSubject() throws java.rmi.RemoteException;
+ public java.util.Collection getAllAcceptedApplications()throws javax.ejb.FinderException,java.rmi.RemoteException, java.rmi.RemoteException;
+ public java.util.Iterator getAllAcceptedApplicationsIterator()throws javax.ejb.FinderException,java.rmi.RemoteException, java.rmi.RemoteException;
+ public java.util.Collection getAllPendingApplications()throws javax.ejb.FinderException,java.rmi.RemoteException, java.rmi.RemoteException;
+ public java.util.Iterator getAllPendingApplicationsIterator()throws javax.ejb.FinderException,java.rmi.RemoteException, java.rmi.RemoteException;
+ public java.util.Collection getAllRejectedApplications()throws javax.ejb.FinderException,java.rmi.RemoteException, java.rmi.RemoteException;
+ public java.util.Iterator getAllRejectedApplicationsIterator()throws javax.ejb.FinderException,java.rmi.RemoteException, java.rmi.RemoteException;
+ public java.lang.String getBundleIdentifier() throws java.rmi.RemoteException;
+ public se.idega.idegaweb.commune.message.business.MessageBusiness getMessageBusiness()throws java.rmi.RemoteException, java.rmi.RemoteException;
+ public java.lang.String getRejectMessageSubject() throws java.rmi.RemoteException;
+ public void rejectApplication(int p0,com.idega.user.data.User p1,java.lang.String p2)throws java.rmi.RemoteException,javax.ejb.CreateException,javax.ejb.FinderException, java.rmi.RemoteException;
+ public void rejectApplication(int p0,com.idega.user.data.User p1)throws java.rmi.RemoteException,javax.ejb.CreateException,javax.ejb.FinderException, java.rmi.RemoteException;
 }
