e9facad404fedf7533114a0df2ac3d73d2f6a029
Jonas Kari Blandon <jonas@idega.com>
2003-10-30 12:06:34 +0000

    *** empty log message ***

--- src/com/idega/block/category/business/CategoryComparator.java
+++ src/com/idega/block/category/business/CategoryComparator.java
@@ -1,10 +1,9 @@
 package com.idega.block.category.business;
-
 import java.util.Comparator;
 
+import com.idega.block.presentation.CategoryBlock;
 import com.idega.core.category.data.ICCategory;
 import com.idega.util.IsCollator;
-
 /**
  * Title:
  * Description:
@@ -13,26 +12,41 @@ import com.idega.util.IsCollator;
  * @author       <a href="mailto:aron@idega.is">aron@idega.is</a>
  * @version 1.0
  */
-
 public class CategoryComparator implements Comparator {
-
-
-  public CategoryComparator() {
-   }
-
-  public int compare(Object o1, Object o2) {
-      ICCategory p1 = (ICCategory) o1;
-      ICCategory p2 = (ICCategory) o2;
-
-    String one = p1.getName();
-    String two = p2.getName();
-
-    return IsCollator.getIsCollator().compare(one,two);
-
-  }
-
-  public boolean equals(Object obj) {
-    return true;
-  }
-
+	private String blockId = null;
+	boolean orderManually = false;
+	
+	public CategoryComparator(CategoryBlock block) {
+		orderManually = block.getOrderManually();
+		if(orderManually) {
+			blockId = Integer.toString(block.getICObjectInstanceID());
+		}
+	}
+	
+	public int compare(Object o1, Object o2) {
+		ICCategory p1 = (ICCategory) o1;
+		ICCategory p2 = (ICCategory) o2;
+		
+		if(orderManually) {
+			try {
+				int n1 = CategoryFinder.getInstance().getCategoryOrderNumber(p1, blockId);
+				int n2 = CategoryFinder.getInstance().getCategoryOrderNumber(p2, blockId);
+				if(n1!=n2) {
+					return n1-n2;
+				}
+			} catch(Exception e) {
+				System.out.println("Error comparing category order by number");
+				e.printStackTrace();
+			}
+		}
+		
+		String one = p1.getName();
+		String two = p2.getName();
+		
+		return IsCollator.getIsCollator().compare(one, two);
+	}
+	
+	public boolean equals(Object obj) {
+		return true;
+	}
 }
