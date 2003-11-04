6ca5e3e8fa06bfd437d44b4ee4e4dfc20260aef2
Eirikur Hrafnsson <eiki@idega.com>
2003-11-04 16:57:54 +0000

    An inputhandler for valid workreport years

---
+++ src/is/idega/idegaweb/member/isi/block/reports/presentation/inputhandler/WorkReportYearDropDownMenu.java
@@ -0,0 +1,101 @@
+package is.idega.idegaweb.member.isi.block.reports.presentation.inputhandler;
+
+import is.idega.idegaweb.member.isi.block.reports.business.WorkReportBusiness;
+
+import java.rmi.RemoteException;
+import java.util.Collection;
+import java.util.Iterator;
+
+import javax.ejb.FinderException;
+
+import com.idega.business.IBOLookup;
+import com.idega.business.InputHandler;
+import com.idega.data.IDOLookupException;
+import com.idega.idegaweb.IWApplicationContext;
+import com.idega.presentation.Block;
+import com.idega.presentation.IWContext;
+import com.idega.presentation.PresentationObject;
+import com.idega.presentation.ui.DropdownMenu;
+import com.idega.user.business.GroupBusiness;
+import com.idega.user.data.Group;
+import com.idega.util.IWTimestamp;
+/**
+ * A presentation object for dynamic reports to choose groups. By default it
+ * creates a selectionbox with all groups but subclassing it or using the
+ * setGroupType method can filter the list to only show a desired type.
+ * 
+ * @author <a href="mailto:eiki@idega.is">Eirikur S. Hrafnsson</a>
+ */
+public class WorkReportYearDropDownMenu extends Block implements InputHandler {
+
+	private String groupType = null;
+	protected GroupBusiness groupBiz = null;
+	private boolean useShortName = false;
+	private String displayNameSeperator = ",";
+	private int year = IWTimestamp.getTimestampRightNow().getYear();
+
+	
+	private WorkReportBusiness workBiz = null;
+	
+	public WorkReportYearDropDownMenu() {
+		super();
+	}
+
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.idega.business.InputHandler#getHandlerObject(java.lang.String,
+	 *      java.lang.String, com.idega.presentation.IWContext)
+	 */
+	public PresentationObject getHandlerObject(String name, String stringValue, IWContext iwc) {
+		this.setName(name);
+		DropdownMenu yearInput=null;
+		try {
+			yearInput = getWorkReportBusiness(iwc).getYearDropdownMenu(year);
+			
+			yearInput.setName(name);
+			if(stringValue!=null){
+				yearInput.setSelectedElement(stringValue);
+			}
+		}
+		catch (RemoteException e) {
+			e.printStackTrace();
+		}
+		
+		return yearInput;
+	}
+
+	/**
+	 * @return a Collection of Group's
+	 *  
+	 */
+	public Object getResultingObject(String[] values, IWContext iwc) throws Exception {
+		if(values!=null && values.length>0){
+			return values[0];
+		}
+		else return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.idega.business.InputHandler#getDisplayNameOfValue(java.lang.String,
+	 *      com.idega.presentation.IWContext)
+	 */
+	public String getDisplayNameOfValue(Object value, IWContext iwc) {
+		if(value!=null){
+			return (String) value;
+		}
+		else return "";
+	}
+	
+	private WorkReportBusiness getWorkReportBusiness(IWApplicationContext iwac) throws RemoteException {
+		if (workBiz == null) {
+			workBiz = (WorkReportBusiness) IBOLookup.getServiceInstance(iwac, WorkReportBusiness.class);
+		}
+
+		return workBiz;
+	}
+
+}
