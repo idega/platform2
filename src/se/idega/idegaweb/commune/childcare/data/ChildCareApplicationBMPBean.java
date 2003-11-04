8794cfda1b30d428ae219afb34cb5cb95d2c6fb4
Thorhallur Helgason <laddi@idega.com>
2003-11-04 22:08:55 +0000

    Changed method to return applications with missing placement relation to return distinct values.

--- src/se/idega/idegaweb/commune/childcare/data/ChildCareApplicationBMPBean.java
+++ src/se/idega/idegaweb/commune/childcare/data/ChildCareApplicationBMPBean.java
@@ -494,7 +494,7 @@ public class ChildCareApplicationBMPBean extends AbstractCaseBMPBean implements 
 	
 	public Collection ejbFindApplicationsWithoutPlacing() throws FinderException {
 		IDOQuery sql = idoQuery();
-		sql.appendSelectAllFrom(this).append(" c, comm_childcare_archive a");
+		sql.appendSelect().append(" distinct c.* ").appendFrom().append(getEntityName()).append(" c, comm_childcare_archive a");
 		sql.appendWhereEquals("c."+getIDColumnName(), "a.application_id");
 		sql.appendAnd().append("a.sch_class_member_id").appendIsNull();
 
