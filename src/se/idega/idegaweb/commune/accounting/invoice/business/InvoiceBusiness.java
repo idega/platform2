e5e57b44611a559d70e7b3bd748f656f41728b4f
Joakim Johnsson <joakim@idega.com>
2003-11-05 15:32:47 +0000

    added getPaymentRecordsByCategoryProviderAndPeriod

--- src/se/idega/idegaweb/commune/accounting/invoice/business/InvoiceBusiness.java
+++ src/se/idega/idegaweb/commune/accounting/invoice/business/InvoiceBusiness.java
@@ -3,19 +3,21 @@ package se.idega.idegaweb.commune.accounting.invoice.business;
 
 public interface InvoiceBusiness extends com.idega.business.IBOService
 {
+ public void createInvoiceHeader(java.lang.String p0,com.idega.user.data.User p1,int p2,java.lang.String p3,java.lang.String p4,java.sql.Date p5)throws javax.ejb.CreateException, java.rmi.RemoteException;
  public se.idega.idegaweb.commune.accounting.invoice.data.BatchRun getBatchRunByCategory(java.lang.String p0)throws com.idega.data.IDOLookupException,javax.ejb.FinderException, java.rmi.RemoteException;
+ public com.idega.user.data.User getChildByInvoiceRecord(se.idega.idegaweb.commune.accounting.invoice.data.InvoiceRecord p0)throws java.rmi.RemoteException, java.rmi.RemoteException;
+ public se.idega.idegaweb.commune.accounting.invoice.data.InvoiceHeaderHome getInvoiceHeaderHome()throws java.rmi.RemoteException, java.rmi.RemoteException;
+ public se.idega.idegaweb.commune.accounting.invoice.data.InvoiceHeader[] getInvoiceHeadersByCustodianOrChild(com.idega.user.data.User p0,java.util.Date p1,java.util.Date p2) throws java.rmi.RemoteException;
+ public se.idega.idegaweb.commune.accounting.invoice.data.InvoiceRecordHome getInvoiceRecordHome()throws java.rmi.RemoteException, java.rmi.RemoteException;
+ public se.idega.idegaweb.commune.accounting.invoice.data.InvoiceRecord[] getInvoiceRecordsByInvoiceHeader(se.idega.idegaweb.commune.accounting.invoice.data.InvoiceHeader p0) throws java.rmi.RemoteException;
  public int getNoPlacements(se.idega.idegaweb.commune.accounting.invoice.data.BatchRun p0)throws java.rmi.RemoteException,javax.ejb.FinderException,com.idega.data.IDOException, java.rmi.RemoteException;
  public int getNoProviders(se.idega.idegaweb.commune.accounting.invoice.data.BatchRun p0)throws java.rmi.RemoteException,javax.ejb.FinderException,com.idega.data.IDOException, java.rmi.RemoteException;
+ public java.util.Collection getPaymentRecordsByCategoryProviderAndPeriod(com.idega.block.school.data.SchoolCategory p0,com.idega.block.school.data.School p1,java.sql.Date p2)throws java.rmi.RemoteException,javax.ejb.FinderException, java.rmi.RemoteException;
+ public java.util.Collection getPaymentRecordsByCategoryProviderAndPeriod(java.lang.String p0,java.lang.String p1,java.sql.Date p2)throws java.rmi.RemoteException,javax.ejb.FinderException, java.rmi.RemoteException;
  public int getTotAmountWithoutVAT(se.idega.idegaweb.commune.accounting.invoice.data.BatchRun p0)throws java.rmi.RemoteException,javax.ejb.FinderException,com.idega.data.IDOException, java.rmi.RemoteException;
  public boolean isHighShool(java.lang.String p0)throws com.idega.data.IDOLookupException,javax.ejb.FinderException, java.rmi.RemoteException;
+ public void removeInvoiceRecord(se.idega.idegaweb.commune.accounting.invoice.data.InvoiceRecord p0)throws java.rmi.RemoteException,javax.ejb.RemoveException, java.rmi.RemoteException;
+ public void removePreliminaryInvoice(se.idega.idegaweb.commune.accounting.invoice.data.InvoiceHeader p0)throws javax.ejb.RemoveException, java.rmi.RemoteException;
  public void removePreliminaryInvoice(java.sql.Date p0,java.lang.String p1)throws javax.ejb.RemoveException, java.rmi.RemoteException;
- void removePreliminaryInvoice (se.idega.idegaweb.commune.accounting.invoice.data.InvoiceHeader header) throws javax.ejb.RemoveException;
-    void removeInvoiceRecord (se.idega.idegaweb.commune.accounting.invoice.data.InvoiceRecord invoiceRrecord) throws java.rmi.RemoteException, javax.ejb.RemoveException;
  public void startPostingBatch(java.sql.Date p0,java.lang.String p1,com.idega.presentation.IWContext p2)throws com.idega.data.IDOLookupException,javax.ejb.FinderException,se.idega.idegaweb.commune.accounting.invoice.business.SchoolCategoryNotFoundException, java.rmi.RemoteException;
-    se.idega.idegaweb.commune.accounting.invoice.data.InvoiceRecordHome getInvoiceRecordHome () throws java.rmi.RemoteException;
-    se.idega.idegaweb.commune.accounting.invoice.data.InvoiceHeaderHome getInvoiceHeaderHome () throws java.rmi.RemoteException;
- se.idega.idegaweb.commune.accounting.invoice.data.InvoiceHeader [] getInvoiceHeadersByCustodianOrChild (com.idega.user.data.User user, java.util.Date fromPeriod, java.util.Date toPeriod);
- se.idega.idegaweb.commune.accounting.invoice.data.InvoiceRecord [] getInvoiceRecordsByInvoiceHeader (se.idega.idegaweb.commune.accounting.invoice.data.InvoiceHeader header);
- com.idega.user.data.User getChildByInvoiceRecord (se.idega.idegaweb.commune.accounting.invoice.data.InvoiceRecord record) throws java.rmi.RemoteException;
-    void createInvoiceHeader (java.lang.String p0,com.idega.user.data.User p1,int p2,java.lang.String p3,java.lang.String p4,java.sql.Date p5) throws javax.ejb.CreateException;
 }
--- src/se/idega/idegaweb/commune/accounting/invoice/business/InvoiceBusinessBean.java
+++ src/se/idega/idegaweb/commune/accounting/invoice/business/InvoiceBusinessBean.java
@@ -26,8 +26,10 @@ import se.idega.idegaweb.commune.accounting.regulations.business.RegSpecConstant
 import se.idega.idegaweb.commune.childcare.data.ChildCareContract;
 import se.idega.idegaweb.commune.childcare.data.ChildCareContractHome;
 
+import com.idega.block.school.data.School;
 import com.idega.block.school.data.SchoolCategory;
 import com.idega.block.school.data.SchoolCategoryHome;
+import com.idega.block.school.data.SchoolHome;
 import com.idega.business.IBOServiceBean;
 import com.idega.data.IDOException;
 import com.idega.data.IDOLookup;
@@ -40,90 +42,94 @@ import com.idega.user.data.User;
  * base for invoicing and payment data, that is sent to external finance system.
  * Now moved to InvoiceThread
  * <p>
- * Last modified: $Date: 2003/11/05 15:01:16 $ by $Author: staffan $
+ * Last modified: $Date: 2003/11/05 15:32:47 $ by $Author: joakim $
  *
  * @author Joakim
- * @version $Revision: 1.32 $
+ * @version $Revision: 1.33 $
  * @see se.idega.idegaweb.commune.accounting.invoice.business.InvoiceThread
  */
-public class InvoiceBusinessBean extends IBOServiceBean implements InvoiceBusiness{
+public class InvoiceBusinessBean extends IBOServiceBean implements InvoiceBusiness {
 
 	/**
 	 * Spawns a new thread and starts the execution of the posting calculation and then returns
 	 * @param month
 	 */
-	public void startPostingBatch(Date month, String schoolCategory, IWContext iwc) throws IDOLookupException, FinderException, SchoolCategoryNotFoundException{
+	public void startPostingBatch(Date month, String schoolCategory, IWContext iwc)
+		throws IDOLookupException, FinderException, SchoolCategoryNotFoundException {
 		//Select correct thread to start
-		SchoolCategoryHome sch =(SchoolCategoryHome) IDOLookup.getHome(SchoolCategory.class);
-		if(sch.findChildcareCategory().getCategory().equals(schoolCategory)) {
+		SchoolCategoryHome sch = (SchoolCategoryHome) IDOLookup.getHome(SchoolCategory.class);
+		if (sch.findChildcareCategory().getCategory().equals(schoolCategory)) {
 			new InvoiceChildcareThread(month, iwc).start();
-		} else if(sch.findElementarySchoolCategory().getCategory().equals(schoolCategory)){
+		} else if (sch.findElementarySchoolCategory().getCategory().equals(schoolCategory)) {
 			new PaymentThreadElementarySchool(month, iwc).start();
-		} else if(sch.findHighSchoolCategory().getCategory().equals(schoolCategory)){
+		} else if (sch.findHighSchoolCategory().getCategory().equals(schoolCategory)) {
 			new PaymentThreadHighSchool(month, iwc).start();
 		} else {
-			System.out.println("Error: couldn't find any Schoolcategory for billing named "+schoolCategory);
-			throw new SchoolCategoryNotFoundException("Couldn't find any Schoolcategory for billing named "+schoolCategory);
+			System.out.println("Error: couldn't find any Schoolcategory for billing named " + schoolCategory);
+			throw new SchoolCategoryNotFoundException(
+				"Couldn't find any Schoolcategory for billing named " + schoolCategory);
 		}
 	}
-	
+
 	/**
 	 * removes all the invoice records and header and the related information in 
 	 * the payment records for the given month where the status was set to preliminary
 	 * 
 	 * @param month
 	 */
-	public void removePreliminaryInvoice(Date month, String category) throws RemoveException{
+	public void removePreliminaryInvoice(Date month, String category) throws RemoveException {
 		//Remove invoices
 		//PaymentRecord paymentRecord;
 		Iterator headerIter;
 		InvoiceHeader header;
 		//Iterator recordIter;
 		//InvoiceRecord invoiceRrecord;
-		
+
 		try {
-			SchoolCategory schoolCategory =((SchoolCategoryHome) IDOLookup.getHome(SchoolCategory.class)).findByLocalizedKey(category);
+			SchoolCategory schoolCategory =
+				((SchoolCategoryHome) IDOLookup.getHome(SchoolCategory.class)).findByLocalizedKey(category);
 			headerIter = getInvoiceHeaderHome().findByMonthAndSchoolCategory(month, schoolCategory).iterator();
-			while(headerIter.hasNext()){
-				header = (InvoiceHeader)headerIter.next();
+			while (headerIter.hasNext()) {
+				header = (InvoiceHeader) headerIter.next();
 				removePreliminaryInvoice(header);
 			}
-/*
-			//Remove payments... Probably shouldn't remove them since they can hold info that has 
-			// already been attested.
-			recordIter = getPaymentRecordHome().findByMonth(month).iterator();
-			while(recordIter.hasNext()){
-				paymentRecord = (PaymentRecord) recordIter.next();
-				paymentRecord.remove();
-			}
-*/
+			/*
+						//Remove payments... Probably shouldn't remove them since they can hold info that has 
+						// already been attested.
+						recordIter = getPaymentRecordHome().findByMonth(month).iterator();
+						while(recordIter.hasNext()){
+							paymentRecord = (PaymentRecord) recordIter.next();
+							paymentRecord.remove();
+						}
+			*/
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw new RemoveException("Could not remove the records.");
 		}
 	}
-	
+
 	/**
 	 * Removes the invoice records and header and the related information in 
 	 * the payment records if the given header status was set to preliminary
 	 * 
 	 * @param header invoice to remove
 	 */
-	public void removePreliminaryInvoice (InvoiceHeader header) throws RemoveException {
+	public void removePreliminaryInvoice(InvoiceHeader header) throws RemoveException {
 		Iterator recordIter;
 		InvoiceRecord invoiceRrecord;
 		UserTransaction transaction = null;
 		try {
 			transaction = getSessionContext().getUserTransaction();
 			transaction.begin();
-            if(header.getStatus() == ConstantStatus.PRELIMINARY);{
-                recordIter = getInvoiceRecordHome().findByInvoiceHeader(header).iterator();
-                while(recordIter.hasNext()){
-                    invoiceRrecord = (InvoiceRecord) recordIter.next();
-                    removeInvoiceRecord (invoiceRrecord);
-                }
-                header.remove();
-            }
+			if (header.getStatus() == ConstantStatus.PRELIMINARY);
+			{
+				recordIter = getInvoiceRecordHome().findByInvoiceHeader(header).iterator();
+				while (recordIter.hasNext()) {
+					invoiceRrecord = (InvoiceRecord) recordIter.next();
+					removeInvoiceRecord(invoiceRrecord);
+				}
+				header.remove();
+			}
 			transaction.commit();
 		} catch (Exception e) {
 			if (transaction != null) {
@@ -132,119 +138,135 @@ public class InvoiceBusinessBean extends IBOServiceBean implements InvoiceBusine
 				} catch (SystemException se) {
 					se.printStackTrace();
 				}
-            }
-            e.printStackTrace ();
-            throw new RemoveException("Could not remove the records.");
-        }
+			}
+			e.printStackTrace();
+			throw new RemoveException("Could not remove the records.");
+		}
 	}
 
-    public void removeInvoiceRecord (InvoiceRecord invoiceRrecord) throws RemoteException, RemoveException {
+	public void removeInvoiceRecord(InvoiceRecord invoiceRrecord) throws RemoteException, RemoveException {
 		PaymentRecord paymentRecord;
-        String ruleSpecType = invoiceRrecord.getRuleSpecType();
-        if(null != ruleSpecType && RegSpecConstant.CHECK.equals (ruleSpecType)) {
-            try {
-                paymentRecord = getPaymentRecordHome().findByPrimaryKey(new Integer(invoiceRrecord.getPaymentRecordId()));
-                //Remove this instance from the payment record
-                paymentRecord.setPlacements(paymentRecord.getPlacements()-1);
-                paymentRecord.setTotalAmount(paymentRecord.getTotalAmount()-invoiceRrecord.getAmount());
-                paymentRecord.setTotalAmountVAT(paymentRecord.getTotalAmountVAT()-invoiceRrecord.getAmountVAT());
-            } catch (FinderException e1) {}
-        }
-        invoiceRrecord.remove();
-    }
+		String ruleSpecType = invoiceRrecord.getRuleSpecType();
+		if (null != ruleSpecType && RegSpecConstant.CHECK.equals(ruleSpecType)) {
+			try {
+				paymentRecord =
+					getPaymentRecordHome().findByPrimaryKey(new Integer(invoiceRrecord.getPaymentRecordId()));
+				//Remove this instance from the payment record
+				paymentRecord.setPlacements(paymentRecord.getPlacements() - 1);
+				paymentRecord.setTotalAmount(paymentRecord.getTotalAmount() - invoiceRrecord.getAmount());
+				paymentRecord.setTotalAmountVAT(paymentRecord.getTotalAmountVAT() - invoiceRrecord.getAmountVAT());
+			} catch (FinderException e1) {
+			}
+		}
+		invoiceRrecord.remove();
+	}
 
-	public boolean isHighShool(String category) throws IDOLookupException, FinderException{
-		SchoolCategory highSchool = ((SchoolCategoryHome) IDOLookup.getHome(SchoolCategory.class)).findHighSchoolCategory();
-		if(((String)highSchool.getPrimaryKey()).equals(category)){
+	public boolean isHighShool(String category) throws IDOLookupException, FinderException {
+		SchoolCategory highSchool =
+			((SchoolCategoryHome) IDOLookup.getHome(SchoolCategory.class)).findHighSchoolCategory();
+		if (((String) highSchool.getPrimaryKey()).equals(category)) {
 			return true;
 		}
 		return false;
 	}
-	
-	public BatchRun getBatchRunByCategory(String category) throws IDOLookupException, FinderException{
-		SchoolCategory schoolCategory =((SchoolCategoryHome) IDOLookup.getHome(SchoolCategory.class)).findByPrimaryKey(category);
-		BatchRun batchRun =((BatchRunHome) IDOLookup.getHome(BatchRun.class)).findBySchoolCategory(schoolCategory);
+
+	public BatchRun getBatchRunByCategory(String category) throws IDOLookupException, FinderException {
+		SchoolCategory schoolCategory =
+			((SchoolCategoryHome) IDOLookup.getHome(SchoolCategory.class)).findByPrimaryKey(category);
+		BatchRun batchRun = ((BatchRunHome) IDOLookup.getHome(BatchRun.class)).findBySchoolCategory(schoolCategory);
 		return batchRun;
 	}
-	
-	public int getNoProviders(BatchRun batchRun) throws RemoteException, FinderException, IDOException{
+
+	public int getNoProviders(BatchRun batchRun) throws RemoteException, FinderException, IDOException {
 		Date period = batchRun.getPeriod();
 		int schoolCategoryID = batchRun.getSchoolCategoryID();
-		return getPaymentHeaderHome().getProviderCountForSchoolCategoryAndPeriod(schoolCategoryID,period);
+		return getPaymentHeaderHome().getProviderCountForSchoolCategoryAndPeriod(schoolCategoryID, period);
 	}
 
-	public int getNoPlacements(BatchRun batchRun) throws RemoteException, FinderException, IDOException{
+	public int getNoPlacements(BatchRun batchRun) throws RemoteException, FinderException, IDOException {
 		Date period = batchRun.getPeriod();
 		int schoolCategoryID = batchRun.getSchoolCategoryID();
-		return getPaymentRecordHome().getPlacementCountForSchoolCategoryAndPeriod(schoolCategoryID,period);
+		return getPaymentRecordHome().getPlacementCountForSchoolCategoryAndPeriod(schoolCategoryID, period);
 	}
 
-	public int getTotAmountWithoutVAT(BatchRun batchRun) throws RemoteException, FinderException, IDOException{
+	public int getTotAmountWithoutVAT(BatchRun batchRun) throws RemoteException, FinderException, IDOException {
 		Date period = batchRun.getPeriod();
 		int schoolCategoryID = batchRun.getSchoolCategoryID();
-		return getPaymentRecordHome().getTotAmountForSchoolCategoryAndPeriod(schoolCategoryID,period);
+		return getPaymentRecordHome().getTotAmountForSchoolCategoryAndPeriod(schoolCategoryID, period);
 	}
 
-    /**
-     * Retreives an array of all InvoiceHeaders where the user given is either
-     * custodian or the child and in the period. If any of the dates
-     * are null, that constraint will be ignored. An empty list is returned if
-     * no invoice header was found.
-     *
-     * @param user the user to search for
-     * @param fromDate first month in search span
-     * @param toDate last month in search span
-     * @return array of invoice headers
-     */
-    public InvoiceHeader [] getInvoiceHeadersByCustodianOrChild
-        (final User user, final java.util.Date fromPeriod,
-         java.util.Date toPeriod) {
-        Collection collection = null;
-        try {
-            collection = getInvoiceHeaderHome ().findByCustodianOrChild
-                    (user, fromPeriod, toPeriod);
-        } catch (RemoteException exception) {
-            exception.printStackTrace ();
-        } catch (FinderException exception) {
-            // no problem, return empty array
-        }
-        return null == collection ? new InvoiceHeader [0]
-                : (InvoiceHeader []) collection.toArray (new InvoiceHeader [0]);
-    }
+	/**
+	 * Retreives an array of all InvoiceHeaders where the user given is either
+	 * custodian or the child and in the period. If any of the dates
+	 * are null, that constraint will be ignored. An empty list is returned if
+	 * no invoice header was found.
+	 *
+	 * @param user the user to search for
+	 * @param fromDate first month in search span
+	 * @param toDate last month in search span
+	 * @return array of invoice headers
+	 */
+	public InvoiceHeader[] getInvoiceHeadersByCustodianOrChild(
+		final User user,
+		final java.util.Date fromPeriod,
+		java.util.Date toPeriod) {
+		Collection collection = null;
+		try {
+			collection = getInvoiceHeaderHome().findByCustodianOrChild(user, fromPeriod, toPeriod);
+		} catch (RemoteException exception) {
+			exception.printStackTrace();
+		} catch (FinderException exception) {
+			// no problem, return empty array
+		}
+		return null == collection ? new InvoiceHeader[0] : (InvoiceHeader[]) collection.toArray(new InvoiceHeader[0]);
+	}
 
-    /**
-     * Retreives an array of all InvoiceRecords connected to this InvoiceHeader
-     * An empty list is returned if no invoice header was found.
-     *
-     * @param header the header to find records connected to
-     * @return array of invoice records
-     */
-    public InvoiceRecord [] getInvoiceRecordsByInvoiceHeader
-        (final InvoiceHeader header) {
-        Collection collection = null;
-        try {
-            collection = getInvoiceRecordHome ().findByInvoiceHeader (header);
-        } catch (RemoteException exception) {
-            exception.printStackTrace ();
-        } catch (FinderException exception) {
-            // no problem, return empty array
-        }
-        return null == collection ? new InvoiceRecord [0]
-                : (InvoiceRecord []) collection.toArray (new InvoiceRecord [0]);
-    }
+	/**
+	 * Retreives an array of all InvoiceRecords connected to this InvoiceHeader
+	 * An empty list is returned if no invoice header was found.
+	 *
+	 * @param header the header to find records connected to
+	 * @return array of invoice records
+	 */
+	public InvoiceRecord[] getInvoiceRecordsByInvoiceHeader(final InvoiceHeader header) {
+		Collection collection = null;
+		try {
+			collection = getInvoiceRecordHome().findByInvoiceHeader(header);
+		} catch (RemoteException exception) {
+			exception.printStackTrace();
+		} catch (FinderException exception) {
+			// no problem, return empty array
+		}
+		return null == collection ? new InvoiceRecord[0] : (InvoiceRecord[]) collection.toArray(new InvoiceRecord[0]);
+	}
 
-    public User getChildByInvoiceRecord (final InvoiceRecord record)
-        throws RemoteException {
-        final ChildCareContractHome contractHome = (ChildCareContractHome)
-                IDOLookup.getHome (ChildCareContract.class);
-        try {
-            final ChildCareContract contract = contractHome
-                    .findApplicationByContract (record.getContractId ());
-            return null != contract ? contract.getChild () : null;
-        } catch (final FinderException e) {
-            return null;
-        }
-    }
+	public User getChildByInvoiceRecord(final InvoiceRecord record) throws RemoteException {
+		final ChildCareContractHome contractHome = (ChildCareContractHome) IDOLookup.getHome(ChildCareContract.class);
+		try {
+			final ChildCareContract contract = contractHome.findApplicationByContract(record.getContractId());
+			return null != contract ? contract.getChild() : null;
+		} catch (final FinderException e) {
+			return null;
+		}
+	}
+
+	public Collection getPaymentRecordsByCategoryProviderAndPeriod(String category, String provider, Date period)
+		throws RemoteException, FinderException {
+		SchoolCategory schoolCategory =
+			((SchoolCategoryHome) IDOLookup.getHome(SchoolCategory.class)).findByLocalizedKey(category);
+		School school = ((SchoolHome) IDOLookup.getHome(School.class)).findByPrimaryKey(provider);
+		return getPaymentRecordsByCategoryProviderAndPeriod(schoolCategory, school, period);
+	}
+
+	public Collection getPaymentRecordsByCategoryProviderAndPeriod(
+		SchoolCategory category,
+		School provider,
+		Date period)
+		throws RemoteException, FinderException {
+		PaymentHeader paymentHeader =
+			getPaymentHeaderHome().findBySchoolCategorySchoolPeriod(provider, category, period);
+		return getPaymentRecordHome().findByPaymentHeader(paymentHeader);
+	}
 
     public void createInvoiceHeader
         (final String schoolCategory, final User createdBy,
@@ -272,23 +294,19 @@ public class InvoiceBusinessBean extends IBOServiceBean implements InvoiceBusine
         }
     }
 
-	protected PaymentHeaderHome getPaymentHeaderHome() throws RemoteException
-	{
+	protected PaymentHeaderHome getPaymentHeaderHome() throws RemoteException {
 		return (PaymentHeaderHome) IDOLookup.getHome(PaymentHeader.class);
 	}
-	
-	protected PaymentRecordHome getPaymentRecordHome() throws RemoteException
-	{
+
+	protected PaymentRecordHome getPaymentRecordHome() throws RemoteException {
 		return (PaymentRecordHome) IDOLookup.getHome(PaymentRecord.class);
 	}
-	
-	public InvoiceHeaderHome getInvoiceHeaderHome() throws RemoteException
-	{
+
+	public InvoiceHeaderHome getInvoiceHeaderHome() throws RemoteException {
 		return (InvoiceHeaderHome) IDOLookup.getHome(InvoiceHeader.class);
 	}
-	
-	public InvoiceRecordHome getInvoiceRecordHome() throws RemoteException
-	{
+
+	public InvoiceRecordHome getInvoiceRecordHome() throws RemoteException {
 		return (InvoiceRecordHome) IDOLookup.getHome(InvoiceRecord.class);
-	}	
+	}
 }
