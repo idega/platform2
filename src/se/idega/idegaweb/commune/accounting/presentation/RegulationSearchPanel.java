840feae32e14217d2d60b35fe0039702d40e23b1
Roar Skullestad <roar@idega.com>
2003-11-17 12:50:34 +0000

    Moved 	getPostingStrings(...) to PostingBusiness

--- src/se/idega/idegaweb/commune/accounting/presentation/RegulationSearchPanel.java
+++ src/se/idega/idegaweb/commune/accounting/presentation/RegulationSearchPanel.java
@@ -13,15 +13,9 @@ import java.util.List;
 
 import javax.ejb.FinderException;
 
-import se.idega.idegaweb.commune.accounting.export.data.ExportDataMapping;
-import se.idega.idegaweb.commune.accounting.posting.business.MissingMandatoryFieldException;
 import se.idega.idegaweb.commune.accounting.posting.business.PostingBusiness;
 import se.idega.idegaweb.commune.accounting.posting.business.PostingException;
-import se.idega.idegaweb.commune.accounting.posting.business.PostingParametersException;
-import se.idega.idegaweb.commune.accounting.posting.data.PostingParameters;
 import se.idega.idegaweb.commune.accounting.regulations.business.RegulationsBusiness;
-import se.idega.idegaweb.commune.accounting.regulations.data.CommuneBelongingType;
-import se.idega.idegaweb.commune.accounting.regulations.data.CommuneBelongingTypeHome;
 import se.idega.idegaweb.commune.accounting.regulations.data.Regulation;
 import se.idega.idegaweb.commune.accounting.regulations.data.RegulationHome;
 import se.idega.idegaweb.commune.accounting.school.data.Provider;
@@ -32,7 +26,6 @@ import com.idega.block.school.data.SchoolCategory;
 import com.idega.block.school.data.SchoolHome;
 import com.idega.block.school.data.SchoolType;
 import com.idega.business.IBOLookup;
-import com.idega.core.location.data.Commune;
 import com.idega.data.IDOLookup;
 import com.idega.presentation.IWContext;
 import com.idega.presentation.PresentationObject;
@@ -163,7 +156,10 @@ public class RegulationSearchPanel extends AccountingBlock {
 						ex.printStackTrace();
 					}
 					try{
-						_currentPosting = getPosting(iwc, getCurrentSchoolCategory(iwc), type, _currentRegulation, new Provider(_currentSchool), _validDate);	
+						PostingBusiness postingBiz = (PostingBusiness) IBOLookup.getServiceInstance(iwc, PostingBusiness.class);
+						_currentPosting = postingBiz.getPostingStrings(iwc, getCurrentSchoolCategory(iwc), type, _currentRegulation, new Provider(_currentSchool), _validDate);	
+					}catch (RemoteException ex){
+						ex.printStackTrace();
 					}catch (PostingException ex){
 						_postingException = ex;
 					}
@@ -336,67 +332,7 @@ public class RegulationSearchPanel extends AccountingBlock {
 		return _currentRegulation;
 	}
 	
-	/**
-	 * Does the search in the postings
-	 * @param iwc
-	 * @param TODO_USE_THIS_PARAMETER
-	 * @param reg
-	 * @param provider
-	 * @param date
-	 * @return the Posting strings as String[]
-	 */
-	private String[] getPosting(IWContext iwc, SchoolCategory category, SchoolType type, Regulation reg, Provider provider, Date date) throws PostingException{
-		
 
-		String ownPosting = null, doublePosting = null;
-		if (reg != null){
-			int regSpecType = new Integer("" + reg.getRegSpecType().getPrimaryKey()).intValue();
-		
-			try{
-				Commune commune = provider.getSchool().getCommune();
-				CommuneBelongingTypeHome home = (CommuneBelongingTypeHome) IDOLookup.getHome(CommuneBelongingType.class);
-				CommuneBelongingType cbt = commune.getIsDefault() ? home.findHomeCommune() : home.findNoHomeCommune();
-				
-				
-				ExportDataMapping categoryPosting = (ExportDataMapping) IDOLookup.getHome(ExportDataMapping.class).
-					findByPrimaryKeyIDO(category.getPrimaryKey());				
-				//Set the posting strings
-				PostingBusiness postingBusiness = (PostingBusiness) IBOLookup.getServiceInstance(iwc.getApplicationContext(), PostingBusiness.class);
-		
-				PostingParameters parameters;
-				parameters = postingBusiness.getPostingParameter(date, ((Integer) type.getPrimaryKey()).intValue(), regSpecType, provider.getSchool().getManagementTypeId(), ((Integer) cbt.getPrimaryKey()).intValue());
-		
-				ownPosting = parameters.getPostingString();
-				ownPosting = postingBusiness.generateString(ownPosting, provider.getOwnPosting(), date);
-				ownPosting = postingBusiness.generateString(ownPosting, categoryPosting.getAccount(), date);
-				postingBusiness.validateString(ownPosting,date);
-		
-				doublePosting = parameters.getDoublePostingString();
-				doublePosting = postingBusiness.generateString(doublePosting, provider.getDoublePosting(), date);
-				doublePosting = postingBusiness.generateString(doublePosting, categoryPosting.getCounterAccount(), date);
-				postingBusiness.validateString(doublePosting,date);
-			}catch(NullPointerException ex){
-				ex.printStackTrace();
-				throw new PostingException("TODO...", ex.getMessage()); 
-			}catch(RemoteException ex){
-				ex.printStackTrace();
-				throw new PostingException("", ex.getMessage()); 				
-			}catch(FinderException ex){			
-				ex.printStackTrace();
-				throw new PostingException("", ex.getMessage()); 				
-			}catch(PostingException ex){
-				ex.printStackTrace();
-				throw new PostingException("", ex.getMessage()); 				
-			}catch(MissingMandatoryFieldException ex){
-				ex.printStackTrace();
-				throw new PostingException("", ex.getMessage()); 				
-			}catch(PostingParametersException ex){
-				ex.printStackTrace();
-				throw new PostingException("", ex.getMessage()); 				
-			}
-		}
-		return new String[] {ownPosting, doublePosting};
-	}
 			
 	
 	/**
