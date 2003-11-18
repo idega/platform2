15ca47b9786b6eb3c168aa1b5f44e6c8cb66833a
Thorhallur Helgason <laddi@idega.com>
2003-11-18 09:33:03 +0000

    Commented out unused local variable.

--- src/se/idega/idegaweb/commune/school/presentation/TerminateClassMembership.java
+++ src/se/idega/idegaweb/commune/school/presentation/TerminateClassMembership.java
@@ -25,10 +25,10 @@ import se.idega.idegaweb.commune.school.business.SchoolCommuneBusiness;
  * TerminateClassMembership is an IdegaWeb block were the user can terminate a
  * membership in a school class. 
  * <p>
- * Last modified: $Date: 2003/11/15 12:30:48 $ by $Author: staffan $
+ * Last modified: $Date: 2003/11/18 09:33:03 $ by $Author: laddi $
  *
  * @author <a href="http://www.staffannoteberg.com">Staffan Nöteberg</a>
- * @version $Revision: 1.17 $
+ * @version $Revision: 1.18 $
  * @see com.idega.block.school.data.SchoolClassMember
  * @see se.idega.idegaweb.commune.school.businessSchoolCommuneBusiness
  * @see javax.ejb
@@ -165,8 +165,7 @@ public class TerminateClassMembership extends SchoolCommuneBlock {
         final String childName = child.getFirstName () + " "
                 + child.getLastName ();
         final User adminUser = context.getCurrentUser ();
-        final String adminUserName = adminUser == null ? "Admin"
-                : adminUser.getFirstName () + " " + adminUser.getLastName ();
+        //final String adminUserName = adminUser == null ? "Admin" : adminUser.getFirstName () + " " + adminUser.getLastName ();
         final String schoolName
                 = member.getSchoolClass ().getSchool ().getName ();
         final String schoolClassName = member.getSchoolClass ().getName ();
