220e261dcad4860aef9b9fe1e86ec7224e35e8d9
Thomas Hilbig <thomas@idega.com>
2003-11-19 12:33:34 +0000

    *** empty log message ***

---
+++ src/com/idega/block/dataquery/data/xml/QueryBooleanExpressionPart.java
@@ -0,0 +1,48 @@
+package com.idega.block.dataquery.data.xml;
+
+import com.idega.xml.XMLElement;
+
+/**
+ * <p>Title: idegaWeb</p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2003</p>
+ * <p>Company: idega Software</p>
+ * @author <a href="thomas@idega.is">Thomas Hilbig</a>
+ * @version 1.0
+ * Created on Nov 19, 2003
+ */
+public class QueryBooleanExpressionPart implements QueryPart {
+
+	/* (non-Javadoc)
+	 * @see com.idega.block.dataquery.data.xml.QueryPart#getQueryElement()
+	 */
+	public XMLElement getQueryElement() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.idega.block.dataquery.data.xml.QueryPart#encode()
+	 */
+	public String encode() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.idega.block.dataquery.data.xml.QueryPart#isLocked()
+	 */
+	public boolean isLocked() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.idega.block.dataquery.data.xml.QueryPart#setLocked(boolean)
+	 */
+	public void setLocked(boolean locked) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
