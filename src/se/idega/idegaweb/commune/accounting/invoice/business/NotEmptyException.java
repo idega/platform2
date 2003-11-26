572b397d6c617b7e8503989a4ce5234b218817f3
Joakim Johnsson <joakim@idega.com>
2003-11-26 12:09:38 +0000

    Created

---
+++ src/se/idega/idegaweb/commune/accounting/invoice/business/NotEmptyException.java
@@ -0,0 +1,11 @@
+package se.idega.idegaweb.commune.accounting.invoice.business;
+
+/**
+ * @author Joakim
+ *
+ */
+public class NotEmptyException extends Exception{
+	public NotEmptyException(String s){
+		super(s);
+	}
+}
