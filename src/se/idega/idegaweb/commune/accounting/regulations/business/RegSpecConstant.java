41df00e0b8a878f87d08fc1c293252b45fd19e87
Joakim Johnsson <joakim@idega.com>
2003-12-10 18:05:07 +0000

    z_ fix

--- src/se/idega/idegaweb/commune/accounting/regulations/business/RegSpecConstant.java
+++ src/se/idega/idegaweb/commune/accounting/regulations/business/RegSpecConstant.java
@@ -5,17 +5,18 @@ package se.idega.idegaweb.commune.accounting.regulations.business;
  *
  */
 public class RegSpecConstant {
-	// TODO Need to get rid of the 'z_' here...
-//	public static final String CHECK ="regspec.check";
 	public static final String CHECK ="cacc_reg_spec_type.check";
-	public static final String MOTHER_TONGUE ="regspec.modersmal";
-	public static final String SWEDISH_2 ="regspec.svenska_2";
-	public static final String LOWINCOME_PROTECTION="regspec.laginkomstskydd";
-	public static final String SIBLING_REBATE ="regspec.syskonrabatt";
-	public static final String MAXLIMIT ="regspec.maxtaxa";
-	public static final String SUBVENTION ="z_cacc_reg_spec.grundsubvention";
-	public static final String RESOURCE = "cacc_main_rule.resurs";
-	public static final String CHECKTAXA ="z_cacc_reg_spec.checktaxa";
+	public static final String SUBVENTION ="cacc_reg_spec_type.grundsubvention";
+	public static final String CHECKTAXA ="cacc_reg_spec_type.checktaxa";
 	public static final String LAGINKOMSTSKYDD =	"cacc_reg_spec_type.laginkomstskydd";
+	public static final String RESOURCE = "cacc_main_rule.resurs";
 
+//Not used
+/*
+	public static final String MOTHER_TONGUE ="cacc_reg_spec_type.modersmal";
+	public static final String SWEDISH_2 ="cacc_reg_spec_type.svenska_2";
+	public static final String LOWINCOME_PROTECTION="cacc_reg_spec_type.laginkomstskydd";
+	public static final String SIBLING_REBATE ="cacc_reg_spec_type.syskonrabatt";
+	public static final String MAXLIMIT ="cacc_reg_spec_type.maxtaxa";
+*/
 }
