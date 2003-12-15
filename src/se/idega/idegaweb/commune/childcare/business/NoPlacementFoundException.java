c45ac3510de9effb0a51b3c8721ccba1ba250e04
Grimur Jonsson <gimmi@idega.com>
2003-12-15 17:47:56 +0000

    Exception ... basic

---
+++ src/se/idega/idegaweb/commune/childcare/business/NoPlacementFoundException.java
@@ -0,0 +1,50 @@
+/*
+ * Created on 15.12.2003
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+package se.idega.idegaweb.commune.childcare.business;
+
+/**
+ * @author gimmi
+ *
+ * To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+public class NoPlacementFoundException extends Exception {
+
+	/**
+	 * 
+	 */
+	public NoPlacementFoundException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param arg0
+	 */
+	public NoPlacementFoundException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param arg0
+	 * @param arg1
+	 */
+	public NoPlacementFoundException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param arg0
+	 */
+	public NoPlacementFoundException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+}
