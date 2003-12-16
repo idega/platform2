59c7bd80343065bae7521bf3946c56322b1314db
Jonas Kari Blandon <jonas@idega.com>
2003-12-16 16:10:29 +0000

    *** empty log message ***

---
+++ src/is/idega/idegaweb/member/isi/block/reports/presentation/inputhandler/ReportGeneratorRegionalUnionSelectionBox.java
@@ -0,0 +1,38 @@
+/*
+ * Created on Dec 16, 2003
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+package is.idega.idegaweb.member.isi.block.reports.presentation.inputhandler;
+
+import is.idega.idegaweb.member.util.IWMemberConstants;
+
+import com.idega.business.InputHandler;
+
+/**
+ * @author jonas
+ *
+ * To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+public class ReportGeneratorRegionalUnionSelectionBox extends GroupSelectionBox implements InputHandler {
+	/**
+	 * Creates a new <code>LeagueSelectionBox</code> with all leagues.
+	 * @param name	The name of the <code>LeagueSelectionBox</code>
+	 */
+	public ReportGeneratorRegionalUnionSelectionBox(String name) {
+		super(name,IWMemberConstants.GROUP_TYPE_REGIONAL_UNION);
+		setResultAsString();
+	}
+	
+	/**
+	 * 
+	 */
+	public ReportGeneratorRegionalUnionSelectionBox() {
+		super();
+		setGroupType(IWMemberConstants.GROUP_TYPE_REGIONAL_UNION);
+		setName(IWMemberConstants.GROUP_TYPE_REGIONAL_UNION);
+		setResultAsString();
+	}
+}
