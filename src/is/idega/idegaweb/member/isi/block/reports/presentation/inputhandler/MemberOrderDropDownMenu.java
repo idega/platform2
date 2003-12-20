4f025ffbca33f79be584a6b81758ed76910eaf31
Thorhallur Helgason <laddi@idega.com>
2003-12-20 15:43:45 +0000

    An input handler for member ordering.

---
+++ src/is/idega/idegaweb/member/isi/block/reports/presentation/inputhandler/MemberOrderDropDownMenu.java
@@ -0,0 +1,94 @@
+package is.idega.idegaweb.member.isi.block.reports.presentation.inputhandler;
+
+import is.idega.idegaweb.member.isi.block.reports.business.WorkReportBusiness;
+import is.idega.idegaweb.member.util.IWMemberConstants;
+
+import com.idega.business.InputHandler;
+import com.idega.idegaweb.IWResourceBundle;
+import com.idega.presentation.IWContext;
+import com.idega.presentation.PresentationObject;
+import com.idega.presentation.ui.DropdownMenu;
+/**
+ * A presentation object for dynamic reports to genders. Both,male or female. both is default.
+ * 
+ * @author <a href="mailto:eiki@idega.is">Eirikur S. Hrafnsson</a>
+ */
+public class MemberOrderDropDownMenu extends DropdownMenu implements InputHandler {
+
+	private static final String NAME_ORDER = IWMemberConstants.ORDER_BY_NAME; //same as in workreportmember
+	private static final String ADDRESS_ORDER = IWMemberConstants.ORDER_BY_ADDRESS; //same as in workreportmember
+	private static final String POSTAL_CODE_ORDER = IWMemberConstants.ORDER_BY_POSTAL_CODE;
+
+	protected static String IW_BUNDLE_IDENTIFIER = "is.idega.idegaweb.member.isi";
+	private WorkReportBusiness workBiz = null;
+
+	public MemberOrderDropDownMenu() {
+		super();
+	}
+
+	public void main(IWContext iwc) throws Exception {
+		IWResourceBundle iwrb = getResourceBundle(iwc);
+		this.addMenuElement(NAME_ORDER, iwrb.getLocalizedString("MemberOrderDropDownMenu.name_order", "Name"));
+		this.addMenuElement(ADDRESS_ORDER, iwrb.getLocalizedString("MemberOrderDropDownMenu.address_order", "Address"));
+		this.addMenuElement(POSTAL_CODE_ORDER, iwrb.getLocalizedString("MemberOrderDropDownMenu.postal_code_order", "Postal code"));
+		this.setSelectedElement(POSTAL_CODE_ORDER);
+	}
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.idega.business.InputHandler#getHandlerObject(java.lang.String, java.lang.String, com.idega.presentation.IWContext)
+	 */
+	public PresentationObject getHandlerObject(String name, String stringValue, IWContext iwc) {
+		this.setName(name);
+
+		if (stringValue != null) {
+			this.setSelectedElement(stringValue);
+		}
+
+		return this;
+	}
+
+	/**
+	 * @return the year, Integer
+	 *  
+	 */
+	public Object getResultingObject(String[] values, IWContext iwc) throws Exception {
+		if (values != null && values.length > 0) {
+			return values[0];
+		}
+		else
+			return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.idega.business.InputHandler#getDisplayNameOfValue(java.lang.String, com.idega.presentation.IWContext)
+	 */
+	public String getDisplayNameOfValue(Object value, IWContext iwc) {
+		IWResourceBundle iwrb = getResourceBundle(iwc);
+		if (value != null) {
+			
+			String displayName = "";
+			if (NAME_ORDER.equals(value)) {
+				displayName = iwrb.getLocalizedString("MemberOrderDropDownMenu.name_order", "Name");
+			}
+			else if (ADDRESS_ORDER.equals(value)) {
+					displayName = iwrb.getLocalizedString("MemberOrderDropDownMenu.address_order", "Address");
+			}
+			else {
+				displayName = iwrb.getLocalizedString("MemberOrderDropDownMenu.postal_code_order", "Postal code");
+			}
+
+			return displayName;
+		}
+		else
+			return "";
+	}
+	/* (non-Javadoc)
+	 * @see com.idega.presentation.PresentationObject#getBundleIdentifier()
+	 */
+	public String getBundleIdentifier() {
+		return IW_BUNDLE_IDENTIFIER;
+	}
+}
