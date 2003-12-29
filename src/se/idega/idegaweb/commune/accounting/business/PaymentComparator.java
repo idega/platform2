579f7d9cb0887b72909b4fd79dcdeeb69a315b86
Sigtryggur Simonarson <sigtryggur@idega.com>
2003-12-29 16:12:22 +0000

    *** empty log message ***

---
+++ src/se/idega/idegaweb/commune/accounting/business/PaymentComparator.java
@@ -0,0 +1,37 @@
+package se.idega.idegaweb.commune.accounting.business;
+
+import java.text.Collator;
+import java.util.Comparator;
+
+import se.idega.idegaweb.commune.accounting.invoice.data.PaymentHeader;
+import se.idega.idegaweb.commune.accounting.invoice.data.PaymentRecord;
+
+import com.idega.util.LocaleUtil;
+
+/**
+ * A class to compare a collection of PaymentRecords or PaymentHeaders objects.
+ * @author Sigtryggur
+ */
+public class PaymentComparator implements Comparator {
+
+	private Collator collator;
+	private String compareString1;
+	private String compareString2;
+	/** 
+	 * @see java.util.Comparator#compare(java.lang.Object, java.lang.Object)
+	 */
+	
+	public int compare(Object o1, Object o2) {		
+		collator = Collator.getInstance(LocaleUtil.getSwedishLocale());
+
+		if (o1 instanceof PaymentHeader) {
+		compareString1 = ((PaymentHeader) o1).getSchool().getName();
+		compareString2 = ((PaymentHeader) o2).getSchool().getName();
+		}
+		else if (o1 instanceof PaymentRecord){
+			compareString1 = ((PaymentRecord) o1).getPaymentText();
+			compareString2 = ((PaymentRecord) o2).getPaymentText();
+		}
+		return collator.compare(compareString1, compareString2);
+	}
+}
