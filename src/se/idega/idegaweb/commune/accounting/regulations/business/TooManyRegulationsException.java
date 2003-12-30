7a542e00f12cc4235d142c802641e7be0de69db0
Pall Helgason <palli@idega.com>
2003-12-30 11:48:39 +0000

    Added method to store the names of the regulations returned

--- src/se/idega/idegaweb/commune/accounting/regulations/business/TooManyRegulationsException.java
+++ src/se/idega/idegaweb/commune/accounting/regulations/business/TooManyRegulationsException.java
@@ -1,11 +1,14 @@
 /*
  * Copyright (C) 2003 Idega software. All Rights Reserved.
- *
- * This software is the proprietary information of Idega software.
- * Use is subject to license terms.
- *
+ * 
+ * This software is the proprietary information of Idega software. Use is
+ * subject to license terms.
+ *  
  */
 package se.idega.idegaweb.commune.accounting.regulations.business;
+
+import java.util.ArrayList;
+import java.util.Iterator;
 
 import se.idega.idegaweb.commune.accounting.business.AccountingException;
 
@@ -13,11 +16,38 @@ import se.idega.idegaweb.commune.accounting.business.AccountingException;
  * @author palli
  */
 public class TooManyRegulationsException extends AccountingException {
+	protected ArrayList _regNames = null;
+
 	/**
 	 * @param textKey
 	 * @param defaultText
 	 */
 	public TooManyRegulationsException(String textKey, String defaultText) {
 		super(textKey, defaultText);
+	}
+
+	public void addRegulationName(String name) {
+		if (_regNames == null)
+			_regNames = new ArrayList();
+		
+		_regNames.add(name);
+	}
+	
+	public ArrayList getRegulationNames() {
+		return _regNames;
+	}
+	
+	public String getRegulationNamesString() {
+		StringBuffer names = new StringBuffer("");
+		if (_regNames != null) {
+			Iterator it =_regNames.iterator();
+			while (it.hasNext()) {
+				String name = (String)it.next();
+				names.append(name);
+				names.append(";");
+			}
+		}
+		
+		return names.toString();
 	}
 }
