a360cac1b9c4c4bb41580425a5d9d25101f2f3e3
Pall Helgason <palli@idega.com>
2004-01-03 16:40:12 +0000

    Removed unused plugin

--- src/is/idega/idegaweb/member/isi/block/accounting/business/plugin/ClubCreditCardContractPluginBusinessHome.java
+++
@@ -1,8 +0,0 @@
-package is.idega.idegaweb.member.isi.block.accounting.business.plugin;
-
-
-public interface ClubCreditCardContractPluginBusinessHome extends com.idega.business.IBOHome
-{
- public ClubCreditCardContractPluginBusiness create() throws javax.ejb.CreateException, java.rmi.RemoteException;
-
-}
