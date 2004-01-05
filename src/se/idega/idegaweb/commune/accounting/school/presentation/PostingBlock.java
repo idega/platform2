b2e81998945de9226eb313f45b743fe12ac86f63
Roar Skullestad <roar@idega.com>
2004-01-05 17:20:41 +0000

    Added constructors without parameters

--- src/se/idega/idegaweb/commune/accounting/school/presentation/PostingBlock.java
+++ src/se/idega/idegaweb/commune/accounting/school/presentation/PostingBlock.java
@@ -1,5 +1,5 @@
 /*
- * $Id: PostingBlock.java,v 1.3 2003/11/19 12:13:49 roar Exp $
+ * $Id: PostingBlock.java,v 1.4 2004/01/05 17:20:41 roar Exp $
  *
  * Copyright (C) 2003 Agura IT. All Rights Reserved.
  *
@@ -20,16 +20,22 @@ import com.idega.presentation.IWContext;
  * This block is a subclass of PostingParameterListEditor  
  * used for editing own posting and double posting strings.
  * <p>
- * Last modified: $Date: 2003/11/19 12:13:49 $ by $Author: roar $
+ * Last modified: $Date: 2004/01/05 17:20:41 $ by $Author: roar $
  *
  * @author Anders Lindman
- * @version $Revision: 1.3 $
+ * @version $Revision: 1.4 $
  * @see se.idega.idegaweb.commune.accounting.posting.presentation.PostingParameterListEditor
  */
 public class PostingBlock extends PostingParameterListEditor {
 
 	private String ownPosting = null;
 	private String doublePosting = null;
+	
+	/**
+	 * Constructs posting block with empty fields for own and double posting strings
+	 */	
+	public PostingBlock() {
+	}	
 	
 	/**
 	 * Constructs posting block with fields for own and double posting strings by calling generateStrings(IWContext)
