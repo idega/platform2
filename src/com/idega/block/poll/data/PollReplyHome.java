52048d5943e66d020e0cbd3c83ef7a343e34899d
Gudmundur Agust Saemundsson <gummi@idega.com>
2004-01-06 14:32:45 +0000

    New classes for more advanced Poll (Survey)

--- src/com/idega/block/poll/data/PollReplyHome.java
+++
@@ -1,9 +0,0 @@
-package com.idega.block.poll.data;
-
-
-public interface PollReplyHome extends com.idega.data.IDOHome
-{
- public PollReply create() throws javax.ejb.CreateException;
- public PollReply findByPrimaryKey(Object pk) throws javax.ejb.FinderException;
-
-}
