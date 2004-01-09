a66038c53084bf855e7520cbddaaeede0f636e65
Grimur Jonsson <gimmi@idega.com>
2004-01-09 13:54:34 +0000

    Fixed layout

--- src/se/idega/idegaweb/commune/presentation/CommuneUserFinder.java
+++ src/se/idega/idegaweb/commune/presentation/CommuneUserFinder.java
@@ -131,8 +131,8 @@ public abstract class CommuneUserFinder extends CommuneBlock {
 		}
 		
 		if (column > 1) {
-			table.mergeCells(1, 4, column, 3);
-			table.mergeCells(1, 5, column, 4);
+			table.mergeCells(1, 3, column, 3);
+			table.mergeCells(1, 4, column, 4);
 		}
 		table.setRowHeight(3, "12");
 		table.add(getSmallHeader(localize("commune.instructions", "Instructions")+" :"), 1, 4);
