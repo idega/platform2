8cfa71d9d4bc31751aad2dd52d27f668a456fe61
Pall Helgason <palli@idega.com>
2004-01-12 00:11:33 +0000

    Added report links to menu

--- src/is/idega/idegaweb/member/isi/block/accounting/presentation/CashierWindow.java
+++ src/is/idega/idegaweb/member/isi/block/accounting/presentation/CashierWindow.java
@@ -53,6 +53,13 @@ public class CashierWindow extends StyledIWAdminWindow {
 	private static final String ACTION_SELECT_USER = "isi_acc_cw_act_sel_usr";
 	private static final String ACTION_PAYMENT_HISTORY = "cw_act_pay_hist";
 	private static final String ACTION_MEMBER_CREDITCARD = "isi_acc_cw_act_memb_cc";
+	
+	private static final String REPORT_PAYMENT_STATUS = "isi_acc_cw_rep_payment_status";
+	private static final String REPORT_PAYMENT_OVERVIEW = "isi_acc_cw_rep_payment_overview";
+	private static final String REPORT_DEBT_OVERVIEW = "isi_acc_cw_rep_debt_overview";
+	private static final String REPORT_ENTRY_OVERVIEW = "isi_acc_cw_rep_entry_overview";
+	private static final String REPORT_LATE_PAYMENT_LIST = "isi_acc_cw_rep_late_payment_list";
+	private static final String REPORT_PAYMENT_LIST = "isi_acc_cw_rep_payment_list";
 
 	private String PARAMETER_PID = "cw_pid";
 	private String PARAMETER_STATUS = "cw_sta";
@@ -114,102 +121,139 @@ public class CashierWindow extends StyledIWAdminWindow {
 	}
 
 	private Table getMenuTable(IWContext iwc) {
-		Table menu = new Table(1, 15);
+		Table menu = new Table(1, 21);
 		menu.setWidth(Table.HUNDRED_PERCENT);
 		menu.setCellpadding(3);
 		menu.setCellspacing(0);
 
 		Text clubOperations = formatText(_iwrb.getLocalizedString("isi_acc_cashierwindow.club_operations", "Club operations"), true);
-		Lists clubOpList = new Lists();
 
 		Text memberOperations = formatText(_iwrb.getLocalizedString("isi_acc_cashierwindow.member_operations", "Member operations"), true);
-		Lists memberOpList = new Lists();
 		
 		Text reports = formatText(_iwrb.getLocalizedString("isi_acc_cashierwindow.reports", "Reports"), true);
-		Lists reportsList = new Lists();
 		
 		LinkContainer editTariffType = new LinkContainer();
 		editTariffType.setStyleClass(styledLink);
 		editTariffType.add(formatText(_iwrb.getLocalizedString("isi_acc_cashierwindow.edit_tariff_type", "Edit club tariff type (A.12)")));
 		editTariffType.addParameter(ACTION, ACTION_TARIFF_TYPE);
 		editTariffType.addParameter(PARAMETER_GROUP_ID,((Integer)_group.getPrimaryKey()).toString());
-		clubOpList.add(editTariffType);
 		
 		LinkContainer editTariff = new LinkContainer();
 		editTariff.setStyleClass(styledLink);
 		editTariff.add(formatText(_iwrb.getLocalizedString("isi_acc_cashierwindow.edit_tariff", "Edit club tariff list (A.12)")));
 		editTariff.addParameter(ACTION, ACTION_TARIFF);
 		editTariff.addParameter(PARAMETER_GROUP_ID,((Integer)_group.getPrimaryKey()).toString());
-		clubOpList.add(editTariff);
 
 		LinkContainer autoAss = new LinkContainer();
 		autoAss.setStyleClass(styledLink);
 		autoAss.add(formatText(_iwrb.getLocalizedString("isi_acc_cashierwindow.auto_assessment", "Automatic assessment (A.15)")));
 		autoAss.addParameter(ACTION, ACTION_AUTOMATIC_ASSESSMENT);
 		autoAss.addParameter(PARAMETER_GROUP_ID,((Integer)_group.getPrimaryKey()).toString());
-		clubOpList.add(autoAss);
 
 		LinkContainer ccContract = new LinkContainer();
 		ccContract.setStyleClass(styledLink);
 		ccContract.add(formatText(_iwrb.getLocalizedString("isi_acc_cashierwindow.cc_contract", "Edit creditcard company contract (A.24)")));
 		ccContract.addParameter(ACTION, ACTION_CREDITCARD_COMPANY_CONTRACT);
 		ccContract.addParameter(PARAMETER_GROUP_ID,((Integer)_group.getPrimaryKey()).toString());
-		clubOpList.add(ccContract);
 
 		LinkContainer selectUser = new LinkContainer();
 		selectUser.setStyleClass(styledLink);
 		selectUser.add(formatText(_iwrb.getLocalizedString("isi_acc_cashierwindow.select_user", "Select user to work with")));
 		selectUser.addParameter(ACTION, ACTION_SELECT_USER);
 		selectUser.addParameter(PARAMETER_GROUP_ID,((Integer)_group.getPrimaryKey()).toString());
-		memberOpList.add(selectUser);
 
 		LinkContainer manAss = new LinkContainer();
 		manAss.setStyleClass(styledLink);
 		manAss.add(formatText(_iwrb.getLocalizedString("isi_acc_cashierwindow.manual_assessment", "Manual assessment (A.14)")));
 		manAss.addParameter(ACTION, ACTION_MANUAL_ASSESSMENT);
 		manAss.addParameter(PARAMETER_GROUP_ID,((Integer)_group.getPrimaryKey()).toString());
-		memberOpList.add(manAss);
 		
 		LinkContainer insertContract = new LinkContainer();
 		insertContract.setStyleClass(styledLink);
 		insertContract.add(formatText(_iwrb.getLocalizedString("isi_acc_cashierwindow.insert_contract", "Insert/edit member contract (new/A.10)")));
 		insertContract.addParameter(ACTION, ACTION_CONTRACT);
 		insertContract.addParameter(PARAMETER_GROUP_ID,((Integer)_group.getPrimaryKey()).toString());
-		memberOpList.add(insertContract);
 
 		LinkContainer registerPayment = new LinkContainer();
 		registerPayment.setStyleClass(styledLink);
 		registerPayment.add(formatText(_iwrb.getLocalizedString("isi_acc_cashierwindow.payment", "Register payment (A.11)")));
 		registerPayment.addParameter(ACTION, ACTION_PAYMENT);
 		registerPayment.addParameter(PARAMETER_GROUP_ID,((Integer)_group.getPrimaryKey()).toString());
-		memberOpList.add(registerPayment);
 
 		LinkContainer paymentHistory = new LinkContainer();
 		paymentHistory.setStyleClass(styledLink);
 		paymentHistory.add(formatText(_iwrb.getLocalizedString("isi_acc_cashierwindow.paymentHistory", "Payment history (3.11)")));
 		paymentHistory.addParameter(ACTION, ACTION_PAYMENT_HISTORY);
 		paymentHistory.addParameter(PARAMETER_GROUP_ID,((Integer)_group.getPrimaryKey()).toString());
-		memberOpList.add(paymentHistory);
 
 		LinkContainer memberCreditCard = new LinkContainer();
 		memberCreditCard.setStyleClass(styledLink);
 		memberCreditCard.add(formatText(_iwrb.getLocalizedString("isi_acc_cashierwindow.memberCreditCard", "Member credit card info (A.10)")));
 		memberCreditCard.addParameter(ACTION, ACTION_MEMBER_CREDITCARD);
 		memberCreditCard.addParameter(PARAMETER_GROUP_ID,((Integer)_group.getPrimaryKey()).toString());
-		memberOpList.add(memberCreditCard);
 
+		//reports
+		LinkContainer paymentStatus = new LinkContainer();
+		paymentStatus.setStyleClass(styledLink);
+		paymentStatus.add(formatText(_iwrb.getLocalizedString("isi_acc_cashierwindow.paymentStatus", "Payment status (A.29)")));
+		paymentStatus.addParameter(ACTION, REPORT_PAYMENT_STATUS);
+		paymentStatus.addParameter(PARAMETER_GROUP_ID,((Integer)_group.getPrimaryKey()).toString());
+		
+		LinkContainer paymentOverview = new LinkContainer();
+		paymentOverview.setStyleClass(styledLink);
+		paymentOverview.add(formatText(_iwrb.getLocalizedString("isi_acc_cashierwindow.paymentOverview", "Payment overview (A.29)")));
+		paymentOverview.addParameter(ACTION, REPORT_PAYMENT_OVERVIEW);
+		paymentOverview.addParameter(PARAMETER_GROUP_ID,((Integer)_group.getPrimaryKey()).toString());
+
+		LinkContainer debtOverview = new LinkContainer();
+		debtOverview.setStyleClass(styledLink);
+		debtOverview.add(formatText(_iwrb.getLocalizedString("isi_acc_cashierwindow.debtOverview", "Debt overview (A.29)")));
+		debtOverview.addParameter(ACTION, REPORT_DEBT_OVERVIEW);
+		debtOverview.addParameter(PARAMETER_GROUP_ID,((Integer)_group.getPrimaryKey()).toString());
+		
+		LinkContainer entryOverview = new LinkContainer();
+		entryOverview.setStyleClass(styledLink);
+		entryOverview.add(formatText(_iwrb.getLocalizedString("isi_acc_cashierwindow.entryOverview", "Entry overview (A.29)")));
+		entryOverview.addParameter(ACTION, REPORT_ENTRY_OVERVIEW);
+		entryOverview.addParameter(PARAMETER_GROUP_ID,((Integer)_group.getPrimaryKey()).toString());
+		
+		LinkContainer latePaymentList = new LinkContainer();
+		latePaymentList.setStyleClass(styledLink);
+		latePaymentList.add(formatText(_iwrb.getLocalizedString("isi_acc_cashierwindow.latePaymentList", "Late payment list (A.29)")));
+		latePaymentList.addParameter(ACTION, REPORT_LATE_PAYMENT_LIST);
+		latePaymentList.addParameter(PARAMETER_GROUP_ID,((Integer)_group.getPrimaryKey()).toString());
+		
+		LinkContainer paymentList = new LinkContainer();
+		paymentList.setStyleClass(styledLink);
+		paymentList.add(formatText(_iwrb.getLocalizedString("isi_acc_cashierwindow.paymentList", "Payment list (A.29)")));
+		paymentList.addParameter(ACTION, REPORT_LATE_PAYMENT_LIST);
+		paymentList.addParameter(PARAMETER_GROUP_ID,((Integer)_group.getPrimaryKey()).toString());
+		
 		//add to window
 		menu.add(clubOperations, 1, 1);
 		menu.setRowColor(1, COLOR_MIDDLE);
-		menu.add(clubOpList, 1, 2);
-
-		menu.add(memberOperations, 1, 4);
-		menu.setRowColor(4, COLOR_MIDDLE);
-		menu.add(memberOpList, 1, 5);
-
-		menu.add(reports, 1, 6);
-		menu.setRowColor(6, COLOR_MIDDLE);
-		menu.add(reportsList, 1, 7);
+		menu.add(editTariffType, 1, 2);
+		menu.add(editTariff, 1, 3);
+		menu.add(autoAss, 1, 4);
+		menu.add(ccContract, 1, 5);
+		
+		menu.add(memberOperations, 1, 7);
+		menu.setRowColor(7, COLOR_MIDDLE);
+		menu.add(selectUser, 1, 8);
+		menu.add(manAss, 1, 9);
+		menu.add(insertContract, 1, 10);
+		menu.add(registerPayment, 1, 11);
+		menu.add(paymentHistory, 1, 12);
+		menu.add(memberCreditCard, 1, 13);
+		
+		menu.add(reports, 1, 15);
+		menu.setRowColor(15, COLOR_MIDDLE);
+		menu.add(paymentStatus, 1, 16);
+		menu.add(paymentOverview, 1, 17);
+		menu.add(debtOverview, 1, 18);
+		menu.add(entryOverview, 1, 19);
+		menu.add(latePaymentList, 1, 20);
+		menu.add(paymentList, 1, 21);
 		
 		return menu;
 	}
