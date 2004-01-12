fb1909560f993058e7ae4ca3474c3ec3698a4a96
Sigtryggur Simonarson <sigtryggur@idega.com>
2004-01-12 16:40:59 +0000

    *** empty log message ***

--- src/se/idega/idegaweb/commune/accounting/business/InvoiceComparator.java
+++ src/se/idega/idegaweb/commune/accounting/business/InvoiceComparator.java
@@ -24,11 +24,11 @@ public class InvoiceComparator implements Comparator {
 	public int compare(Object o1, Object o2) {		
 		collator = Collator.getInstance(LocaleUtil.getSwedishLocale());
 
-		if (o1 instanceof InvoiceHeader) {
-		compareString1 = ((InvoiceRecord) o1).getProvider().getName();
-		compareString2 = ((InvoiceRecord) o2).getProvider().getName();
-		}
-		else if (o1 instanceof InvoiceRecord){
+		//if (o1 instanceof InvoiceHeader) {
+		//compareString1 = ((InvoiceRecord) o1).getProvider().getName();
+		//compareString2 = ((InvoiceRecord) o2).getProvider().getName();
+		//}
+		if (o1 instanceof InvoiceRecord){
 			compareString1 = ((InvoiceRecord) o1).getInvoiceText();
 			compareString2 = ((InvoiceRecord) o2).getInvoiceText();
 		}
