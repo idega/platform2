26fa3210f9f4abf5cb0688f14c3cda67134c265e
Joakim Johnsson <joakim@idega.com>
2004-01-15 14:09:17 +0000

    Bugfix

--- src/se/idega/idegaweb/commune/accounting/invoice/business/PaymentThreadHighSchool.java
+++ src/se/idega/idegaweb/commune/accounting/invoice/business/PaymentThreadHighSchool.java
@@ -144,7 +144,7 @@ public class PaymentThreadHighSchool extends PaymentThreadSchool {
 	}	
 	
 	protected String[] getPostingStrings(SchoolCategory category, SchoolType schoolType, int regSpecTypeId, Provider provider, Date calculationDate, int schoolYearId, int studyPathId) throws PostingException, RemoteException {
-		if (studyPathId == -1){
+		if (studyPathId != -1){
 			try {
 				return getPostingBusiness().getPostingStrings(category, schoolType, regSpecTypeId, provider, calculationDate,schoolYearId, studyPathId, false);
 			} catch (PostingException e) {
