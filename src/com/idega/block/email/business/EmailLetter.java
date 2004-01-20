7c7fa3b13473a4d284a9192b1f0c98c156e5fb38
Aron Birkir Gudmundsson <aron@idega.com>
2004-01-20 19:22:33 +0000

    empty lines removed

--- src/com/idega/block/email/business/EmailLetter.java
+++ src/com/idega/block/email/business/EmailLetter.java
@@ -1,149 +1,58 @@
 package com.idega.block.email.business;
-
-
-
 /**
-
  *  Title: Description: Copyright: Copyright (c) 2001 Company:
-
  *
-
  * @author     <br>
-
  *      <a href="mailto:aron@idega.is">Aron Birkir</a> <br>
-
- *
-
  * @created    14. mars 2002
-
  * @version    1.0
 
  */
-
-
-
-public interface EmailLetter{
-
-
-
-
-
-  /**  @todo Description of the Field */
-
-  public final static int TYPE_RECEIVED = 0;
-
-  /**  @todo Description of the Field */
-
-  public final static int TYPE_SENT = 1;
-
-  /**  @todo Description of the Field */
-
-  public final static int TYPE_SUBSCRIPTION = 2;
-
-  /**  @todo Description of the Field */
-
-  public final static int TYPE_UNSUBSCRIBE = 3;
-
-
-
-  public final static int TYPE_DRAFT = 4;
-
-
-  public final static int TYPE_LIST = 5;
-
-  /**
-
-   *  Gets the body of the EmailLetter object
-
-   *
-
-   * @return    The body value
-
-   */
-
-  public String getBody();
-
-
-
-
-
-
-
-  /**
-
-   *  Gets the subject of the EmailLetter object
-
-   *
-
-   * @return    The subject value
-
-   */
-
-  public String getSubject();
-
-
-
-
-
-  /**
-
-   *  Gets the fromAddress of the EmailLetter object
-
-   *
-
-   * @return    The from address value
-
-   */
-
-  public String getFromAddress();
-
-
-
-
-
-  /**
-
-   *  Gets the fromName of the EmailLetter object
-
-   *
-
-   * @return    The from name value
-
-   */
-
-  public String getFromName();
-
-
-
-
-
-  /**
-
-   *  Gets the type of the EmailLetter object
-
-   *
-
-   * @return    The type value
-
-   */
-
-  public int getType();
-
-
-
-
-
-  /**
-
-   *  Gets the created of the EmailLetter object
-
-   *
-
-   * @return    The created value
-
-   */
-
-  public java.sql.Timestamp getCreated();
-
+public interface EmailLetter {
+	/**  @todo Description of the Field */
+	public final static int TYPE_RECEIVED = 0;
+	/**  @todo Description of the Field */
+	public final static int TYPE_SENT = 1;
+	/**  @todo Description of the Field */
+	public final static int TYPE_SUBSCRIPTION = 2;
+	/**  @todo Description of the Field */
+	public final static int TYPE_UNSUBSCRIBE = 3;
+	public final static int TYPE_DRAFT = 4;
+	public final static int TYPE_LIST = 5;
+	/**
+	 *  Gets the body of the EmailLetter object
+	 *
+	 * @return    The body value
+	 */
+	public String getBody();
+	/**
+	 *  Gets the subject of the EmailLetter object
+	 *
+	 * @return    The subject value
+	 */
+	public String getSubject();
+	/**
+	 *  Gets the fromAddress of the EmailLetter object
+	 *
+	 * @return    The from address value
+	 */
+	public String getFromAddress();
+	/**
+	 *  Gets the fromName of the EmailLetter object
+	 *
+	 * @return    The from name value
+	 */
+	public String getFromName();
+	/**
+	 *  Gets the type of the EmailLetter object
+	 *
+	 * @return    The type value
+	 */
+	public int getType();
+	/**
+	 *  Gets the created of the EmailLetter object
+	 *
+	 * @return    The created value
+	 */
+	public java.sql.Timestamp getCreated();
 }
-
