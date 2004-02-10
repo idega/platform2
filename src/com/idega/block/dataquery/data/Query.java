ece596da1c7c0dd1a69521518523f907683af3ef
Thomas Hilbig <thomas@idega.com>
2004-02-10 15:01:05 +0000

    *** empty log message ***

--- src/com/idega/block/dataquery/data/Query.java
+++ src/com/idega/block/dataquery/data/Query.java
@@ -1,10 +1,9 @@
 package com.idega.block.dataquery.data;
 
 import com.idega.core.file.data.ICFile;
-import com.idega.util.xml.XMLFile;
 
 
-public interface Query extends XMLFile,ICFile
+public interface Query extends ICFile
 {
  public int getXMLSchemaId();
  public void setXMLSchemaId(int p0);
---
+++ src/com/idega/block/dataquery/data/QueryConstants.java
@@ -0,0 +1,17 @@
+package com.idega.block.dataquery.data;
+
+/**
+ * <p>Title: idegaWeb</p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2003</p>
+ * <p>Company: idega Software</p>
+ * @author <a href="thomas@idega.is">Thomas Hilbig</a>
+ * @version 1.0
+ * Created on Feb 2, 2004
+ */
+public abstract class QueryConstants {
+
+	public final static String PERMISSION_PRIVATE_QUERY = "private";
+	public final static String PERMISSION_PUBLIC_QUERY = "public";
+
+}
