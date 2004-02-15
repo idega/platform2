7ca4eef415a0970c1831e6dc7c20f4790d4d9b11
Aron Birkir Gudmundsson <aron@idega.com>
2004-02-15 15:57:32 +0000

    Commune specific family relations window

---
+++ src/se/idega/idegaweb/commune/user/presentation/CommuneFamilyRelationConnector.java
@@ -0,0 +1,60 @@
+/*
+ * Created on Feb 15, 2004
+ *
+ */
+package se.idega.idegaweb.commune.user.presentation;
+
+import java.rmi.RemoteException;
+
+import se.idega.idegaweb.commune.business.CommuneFamilyService;
+
+import com.idega.business.IBOLookup;
+import com.idega.idegaweb.IWApplicationContext;
+import com.idega.presentation.IWContext;
+import com.idega.user.data.User;
+
+import is.idega.idegaweb.member.business.MemberFamilyLogic;
+import is.idega.idegaweb.member.presentation.FamilyRelationConnector;
+
+/**
+ * CommuneFamilyRelationConnector
+ * @author aron 
+ * @version 1.0
+ */
+public class CommuneFamilyRelationConnector extends FamilyRelationConnector {
+	
+	
+	/**
+	 * 
+	 */
+	public CommuneFamilyRelationConnector() {
+		super();
+	}
+
+	/* (non-Javadoc)
+	 * @see is.idega.idegaweb.member.presentation.FamilyRelationConnector#getMemberFamilyLogic(com.idega.idegaweb.IWApplicationContext)
+	 */
+	protected MemberFamilyLogic getMemberFamilyLogic(IWApplicationContext iwac) throws RemoteException {
+		return (MemberFamilyLogic)IBOLookup.getServiceInstance(iwac,CommuneFamilyService.class);
+	}
+
+	/* (non-Javadoc)
+	 * @see is.idega.idegaweb.member.presentation.UserRelationConnector#isRelationshipLegal(com.idega.presentation.IWContext, com.idega.user.data.User, com.idega.user.data.User, java.lang.String)
+	 */
+	protected boolean isRelationshipLegal(IWContext iwc, User roleUser, User victimUser, String relationType) {
+		try {
+			MemberFamilyLogic familyService = getMemberFamilyLogic(iwc);
+			if(relationType.equalsIgnoreCase(familyService.getCohabitantRelationType())){
+				return !familyService.isSpouseOf(roleUser,victimUser);
+			}
+			else if(relationType.equalsIgnoreCase(familyService.getSpouseRelationType())){
+				return !familyService.isCohabitantOf(roleUser,victimUser);
+			}
+		}
+		catch (RemoteException e) {
+			e.printStackTrace();
+		}
+		return true;
+	}
+
+}
