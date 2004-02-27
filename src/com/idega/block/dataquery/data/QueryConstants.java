1055bc244c1162fc832c7758d39cb4281d7251f7
Thomas Hilbig <thomas@idega.com>
2004-02-27 10:55:26 +0000

    new constant

--- src/com/idega/block/dataquery/data/QueryConstants.java
+++ src/com/idega/block/dataquery/data/QueryConstants.java
@@ -14,5 +14,8 @@ public abstract class QueryConstants {
 	public final static String PERMISSION_PRIVATE_QUERY = "private";
 	public final static String PERMISSION_PUBLIC_QUERY = "public";
 	public final static String ENTITY_PATH_DELIMITER = "#";
+	// used for adding a counter to a name if the name is not unique
+	// e.g. reykjavik (already existing) -> reykjavik+COUNTER_TOKEN+1 
+	public final static String COUNTER_TOKEN = "_";
 
 }
