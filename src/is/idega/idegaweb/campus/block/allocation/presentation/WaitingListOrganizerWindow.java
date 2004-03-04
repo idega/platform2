0710ee1daecf3bafbbb88e956daf1ab99a53ab00
Aron Birkir Gudmundsson <aron@idega.com>
2004-03-04 13:11:52 +0000

    update priority level if number on list enters another priority zone ( where records have other prioritylevel than selected records)

--- src/is/idega/idegaweb/campus/block/allocation/presentation/WaitingListOrganizerWindow.java
+++ src/is/idega/idegaweb/campus/block/allocation/presentation/WaitingListOrganizerWindow.java
@@ -33,6 +33,7 @@ import com.idega.presentation.IWContext;
 import com.idega.presentation.PresentationObject;
 import com.idega.presentation.text.Text;
 import com.idega.presentation.ui.DataTable;
+import com.idega.presentation.ui.DropdownMenu;
 import com.idega.presentation.ui.Form;
 import com.idega.presentation.ui.SubmitButton;
 import com.idega.presentation.ui.TextInput;
@@ -51,6 +52,7 @@ public class WaitingListOrganizerWindow extends Window {
 	public static String WL_ORDER = "order";
 	public static String WL_ID = "wlId";
 	public static String NUMBER_ON_LIST = "onlist";
+	public static String PRIORITY = "priority";
 	public static String MAX_LIST = "maxlist";
 
 	private final static String IW_BUNDLE_IDENTIFIER = "is.idega.idegaweb.campus";
@@ -96,41 +98,77 @@ public class WaitingListOrganizerWindow extends Window {
 		int complexId = Integer.parseInt(iwc.getParameter(COMPLEX_ID));
 		int aprtTypeId = Integer.parseInt(iwc.getParameter(APARTMENT_TYPE_ID));
 		int id = Integer.parseInt(iwc.getParameter(WL_ID));
-		int moveTo = Integer.parseInt(iwc.getParameter("move_to"));
+		int moveTo = -1;
+		try {
+			moveTo = Integer.parseInt(iwc.getParameter("move_to"));
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		}
+		//String priority = iwc.getParameter(PRIORITY);
 		
 		Collection L = CampusApplicationFinder.listOfWaitinglist(aprtTypeId, complexId);
 		if (L != null) {
 			WaitingList currentListEntry = null;
-			Integer beforeOrder = null;
-			Integer afterOrder = null;
-			Integer lastOrder = null;
+			WaitingList beforeEntry = null;
+			WaitingList afterEntry = null;
+			WaitingList lastEntry = null;
 			int count = 1;
-			if(moveTo <= L.size()){
+			if(moveTo <= L.size() ){
 				for (Iterator iter = L.iterator(); iter.hasNext();) {
 					WaitingList entry = (WaitingList) iter.next();
 					if(count == moveTo){
-						afterOrder = entry.getOrder();
-						beforeOrder = lastOrder;
+						afterEntry = entry;
+						beforeEntry = lastEntry;
 					}
 					if(String.valueOf(id).equals(entry.getPrimaryKey().toString())){
 						currentListEntry = entry;
 					}
-					lastOrder = entry.getOrder();
+					lastEntry = entry;
+					
 					count++;
 				}
 				// update order number
 				if(currentListEntry!=null){
 					// TODO move to business
-					if(beforeOrder !=null && afterOrder!=null){
-						int order = (int) (afterOrder.intValue()+beforeOrder.intValue())/2;
-						currentListEntry.setOrder(order);
-						currentListEntry.store();
+					if(moveTo>0){
+						if(beforeEntry !=null && afterEntry!=null){
+							int order = (int) (afterEntry.getOrder().intValue()+beforeEntry.getOrder().intValue())/2;
+							currentListEntry.setOrder(order);
+							if(currentListEntry.getPriorityLevel().compareTo(beforeEntry.getPriorityLevel())!=0){
+								currentListEntry.setSamePriority(beforeEntry);
+							}
+							currentListEntry.store();
+						}
 					}
 				}
 			}
 		}		
-		
 		return false;
+	}
+	/**
+	 * Returns the level with the higher priority
+	 * @param one
+	 * @param two
+	 * @return
+	 */
+	//TODO put into business classes
+	private String getHigherPriorityLevel(String one, String two){
+		if(one.compareTo(two)<0)
+			return one;
+		else
+			return two;
+	}
+	// TODO move to general CampusBlock / ApplicationBlock
+	private DropdownMenu priorityDrop(String name, String selected) {
+		DropdownMenu drp = new DropdownMenu(name);
+		drp.addMenuElement("A", "A");
+		drp.addMenuElement("B", "B");
+		drp.addMenuElement("C", "C");
+		drp.addMenuElement("D", "D");
+		drp.addMenuElement("E", "E");
+		drp.addMenuElement("T", "T");
+		drp.setSelectedElement(selected);
+		return drp;
 	}
 
 	protected PresentationObject getRenumberTable(IWContext iwc) {
@@ -151,12 +189,15 @@ public class WaitingListOrganizerWindow extends Window {
 		table.add(Edit.formatText(iwrb.getLocalizedString("no_on_list","No. On list")),1,2);
 		table.add(Edit.formatText(iwrb.getLocalizedString("list_count","List count")),1,3);
 		table.add(Edit.formatText(iwrb.getLocalizedString("move_to_no","Move to No.")),1,4);
+		table.add(Edit.formatText(iwrb.getLocalizedString("priority","Priority")),1,5);
 
 		WaitingList item = null;
 		Applicant applicant = null;
+		String prioritySelected = "";
 
 		try {
 			item = ((WaitingListHome)IDOLookup.getHomeLegacy(WaitingList.class)).findByPrimaryKeyLegacy(id);
+			prioritySelected = item.getPriorityLevel();
 			applicant = ((ApplicantHome)IDOLookup.getHomeLegacy(Applicant.class)).findByPrimaryKeyLegacy(item.getApplicantId().intValue());
 		}
 		catch (SQLException e) {
@@ -168,6 +209,9 @@ public class WaitingListOrganizerWindow extends Window {
 		TextInput input = new TextInput("move_to");
 		Edit.setStyle(input);
 		table.add(input,2,4);
+		DropdownMenu priorities = priorityDrop(PRIORITY,prioritySelected);
+		Edit.setStyle(priorities);
+		table.add(priorities,2,5);
 		
 		table.addTitle(iwrb.getLocalizedString("move_person_on_waitinglist","Move person on waitinglist"));
 		
@@ -177,6 +221,8 @@ public class WaitingListOrganizerWindow extends Window {
 		table.addButton(cancel);
 		
 		form.add(table);
+		form.add(Text.getBreak());
+		form.add( Edit.formatText(iwrb.getLocalizedString("waitinglist_priority_will_be_upated","Priority level of will be changed to be the same as in the target area")));
 		
 		return form;
 	}
@@ -184,6 +230,7 @@ public class WaitingListOrganizerWindow extends Window {
 	public String getBundleIdentifier() {
 		return IW_BUNDLE_IDENTIFIER;
 	}
+	
 
 	public void main(IWContext iwc) {
 		eUser = iwc.getUser();
