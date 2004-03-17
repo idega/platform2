d48962865e46cecb2ca2c34c970fbe9a2536ad66
Jonas Kari Blandon <jonas@idega.com>
2004-03-17 14:29:42 +0000

    member status views

---
+++ src/is/idega/idegaweb/member/isi/block/members/data/MemberGroupData.java
@@ -0,0 +1,115 @@
+/*
+ * Created on Feb 17, 2004
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+package is.idega.idegaweb.member.isi.block.members.data;
+
+import is.idega.idegaweb.member.util.IWMemberConstants;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+import com.idega.user.data.Group;
+import com.idega.user.data.User;
+
+/**
+ * @author jonas
+ *
+ * To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+public class MemberGroupData {
+	
+	/* A list of Strings with membership info */
+	private List _memberInfo = new ArrayList();
+	
+	public MemberGroupData(User user) {
+		_user = user;
+		Collection parentGroups = user.getParentGroups();
+		Iterator iter = parentGroups.iterator();
+		StringBuffer buf = new StringBuffer();
+		while(iter.hasNext()) {
+			Group group = (Group) iter.next();
+			String memberInfo = processGroup(group);
+			if(memberInfo!=null && memberInfo.length()>0) {
+				_memberInfo.add(memberInfo);
+			}
+			buf.setLength(0);
+		}
+	}
+	
+	/**
+	 * Returns a String with infor on membership based on a group. Searches all ascendant groups and includes clubs, 
+	 * leagues, regional unions, unions, federations and divisions
+	 * @param group an imediate parent group of the member
+	 */
+	private String processGroup(Group group) {
+		boolean ok = processGroup(group, _buf, true);
+		String result = ok?_buf.toString():null;
+		_buf.setLength(0);
+		return result;
+	}
+	
+	/**
+	 * Returns a String with infor on membership based on a group. Searches all ascendant groups and includes clubs, 
+	 * leagues, regional unions, unions, federations and divisions
+	 * @param group the group to categorize
+	 * @param buf a StringBuffer to collect the member group info into
+	 * @param isFirstGroup if the group is an imediate parent of the member
+	 * @returns true if a club, league, regional union, union or federation was found among groups ancestors (including
+	 * group itself)
+	 */
+	private boolean processGroup(Group group, StringBuffer buf, boolean isFirstGroup) {
+		String type = group.getGroupType();
+		
+		boolean isFinalGroup = IWMemberConstants.GROUP_TYPE_CLUB.equals(type) ||
+		                       IWMemberConstants.GROUP_TYPE_LEAGUE.equals(type) ||
+		                       IWMemberConstants.GROUP_TYPE_REGIONAL_UNION.equals(type) ||
+		                       IWMemberConstants.GROUP_TYPE_UNION.equals(type) ||
+		                       IWMemberConstants.GROUP_TYPE_FEDERATION.equals(type);
+		boolean isDivision = IWMemberConstants.GROUP_TYPE_CLUB_DIVISION.equals(type);
+		
+		boolean ok = true; // assume final group will be found
+		
+		if(!isFinalGroup) {
+			Collection groups = group.getParentGroups();
+			if(groups==null || groups.size()==0) {
+				// final group not found
+				ok = false;
+			} else {
+				Iterator iter = groups.iterator();
+				while(iter.hasNext()) {
+					ok = processGroup((Group) iter.next(), buf, false);
+				}
+			}
+		} else {
+			buf.append(group.getName());
+		}
+		
+		if(isDivision || isFirstGroup) {
+			buf.append(" - ").append(group.getName());
+		}
+		
+		return ok;
+	}
+	
+	/**
+	 * gets the user that the groups in this ISIMemberGroups instance apply to
+	 * @return the user
+	 */
+	public User getUser() {
+		return _user;
+	}
+	
+	public List getMemberInfo() {
+		return _memberInfo;
+	}
+	
+	private User _user;
+	
+	private StringBuffer _buf = new StringBuffer();
+}
---
+++ src/is/idega/idegaweb/member/isi/block/members/presentation/MemberOverview.java
@@ -0,0 +1,133 @@
+/*
+ * Created on Feb 17, 2004
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+package is.idega.idegaweb.member.isi.block.members.presentation;
+
+import is.idega.idegaweb.member.isi.block.members.data.MemberGroupData;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+import com.idega.data.IDOLookup;
+import com.idega.idegaweb.IWResourceBundle;
+import com.idega.presentation.Block;
+import com.idega.presentation.IWContext;
+import com.idega.presentation.PresentationObject;
+import com.idega.presentation.Table;
+import com.idega.presentation.text.Text;
+import com.idega.user.data.GroupRelation;
+import com.idega.user.data.GroupRelationHome;
+import com.idega.user.data.User;
+import com.idega.user.data.UserHome;
+import com.idega.util.IWTimestamp;
+
+/**
+ * @author jonas
+ *
+ * To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+public class MemberOverview extends Block {
+	
+	public static final String IW_BUNDLE_IDENTIFIER = "is.idega.idegaweb.member.isi";
+	
+	private IWResourceBundle _iwrb = null;
+	
+	public void main(IWContext iwc) {
+		_iwrb = getResourceBundle(iwc);
+		_data = new MemberGroupData(getUser());
+		Text regText = new Text(_iwrb.getLocalizedString("member_overview_registration", "Skraningar"));
+		add(regText);
+		add(getMemberRegistrationStatus());
+		addBreak();
+		Text histText = new Text(_iwrb.getLocalizedString("member_overview_history", "Saga"));
+		add(histText);
+		add(getMemberHistory());
+	}
+	
+	public PresentationObject getMemberHistory() {
+		Collection history = null;
+		int rows = 0;
+		try {
+			history = (Collection) ((GroupRelationHome) com.idega.data.IDOLookup.getHome(GroupRelation.class)).findAllGroupsRelationshipsByRelatedGroup(getUser().getID(),"GROUP_PARENT");
+			rows = history.size();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		if(rows>0) {
+			Table table = new Table(3, rows+1);
+			table.setCellpadding(2);
+			table.setBorder(2);
+			int row = 1;
+			table.add(_iwrb.getLocalizedString("member_overview_group", "Hopur"), 1, row);
+			//table.add(_iwrb.getLocalizedString("member_overview_state", "Virkni"), 2, row);
+			table.add(_iwrb.getLocalizedString("member_overview_begin_date", "Byrjadi"), 2, row);
+			table.add(_iwrb.getLocalizedString("member_overview_end_date", "Haetti"), 3, row++);
+			
+			Iterator historyIter = history.iterator();
+			while(historyIter.hasNext()) {
+				GroupRelation rel = (GroupRelation) historyIter.next();
+				
+				IWTimestamp from = new IWTimestamp(rel.getInitiationDate());
+				IWTimestamp to = null;
+				if (rel.getTerminationDate() != null)
+					to = new IWTimestamp(rel.getTerminationDate());
+
+				table.add(rel.getGroup().getName(), 1, row);
+				table.add(from.getDateString("dd-MM-yyyy"), 2, row);
+				if (to != null) {
+					table.add(to.getDateString("dd-MM-yyyy"), 3, row);
+				}
+				row++;
+			}
+			
+			return table;
+		} else {
+			return null;
+		}
+	}
+	
+	public PresentationObject getMemberRegistrationStatus() {
+		try {
+			List memberInfo = _data.getMemberInfo();
+			Table table = new Table(1, memberInfo.size());
+			//table.setBorder(2);
+			table.setCellpadding(2);
+			table.setBorder(2);
+			Iterator miIter = memberInfo.iterator();
+			int row = 1;
+			while(miIter.hasNext()) {
+				String mi = (String) miIter.next();
+				table.add(mi, 1, row++);
+			}
+			return table;
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public String getBundleIdentifier() {
+		return IW_BUNDLE_IDENTIFIER;
+	}
+	
+	private User getUser() {
+		if(_user==null) {
+			try {
+				_user = ((UserHome) IDOLookup.getHome(User.class)).findByPrimaryKey(_userId);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return _user;
+	}
+	
+	private Integer _userId = new Integer(338609);
+	private User _user = null;
+	private MemberGroupData _data = null;
+}
