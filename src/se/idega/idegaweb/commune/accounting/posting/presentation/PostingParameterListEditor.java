59510bf85ab9b7adcf1bd4d06524b2dcff369aae
Anders Lindman <anders.lindman@ncmedia.com>
2004-03-19 10:49:25 +0000

    Added support for two new posting strings.

--- src/se/idega/idegaweb/commune/accounting/posting/presentation/PostingParameterListEditor.java
+++ src/se/idega/idegaweb/commune/accounting/posting/presentation/PostingParameterListEditor.java
@@ -1,5 +1,5 @@
 /*
- * $Id: PostingParameterListEditor.java,v 1.40 2004/02/05 16:17:45 roar Exp $
+ * $Id: PostingParameterListEditor.java,v 1.41 2004/03/19 10:49:25 anders Exp $
  *
  * Copyright (C) 2003 Agura IT. All Rights Reserved.
  *
@@ -48,10 +48,10 @@ import se.idega.idegaweb.commune.accounting.school.business.StudyPathBusiness;
  * It handles posting variables for both own and double entry accounting
  *  
  * <p>
- * $Id: PostingParameterListEditor.java,v 1.40 2004/02/05 16:17:45 roar Exp $
+ * $Id: PostingParameterListEditor.java,v 1.41 2004/03/19 10:49:25 anders Exp $
  *
  * @author <a href="http://www.lindman.se">Kjell Lindman</a>
- * @version $Revision: 1.40 $
+ * @version $Revision: 1.41 $
  */
 public class PostingParameterListEditor extends AccountingBlock {
 
@@ -76,6 +76,8 @@ public class PostingParameterListEditor extends AccountingBlock {
 	private final static String KEY_HEADER = "posting_parm_edit.header";
 	private final static String KEY_HEADER_OWN_ENTRY = "posting_parm_edit.header_own_entry";
 	private final static String KEY_HEADER_DOUBLE_ENTRY = "posting_parm_edit.header_double_entry";
+	private final static String KEY_HEADER_PAYABLE_ACCOUNT = "export.payable_account";
+	private final static String KEY_HEADER_CUSTOMER_CLAIM_ACCOUNT = "export.customer_claim_account";
 	private final static String KEY_FROM_DATE = "posting_parm_edit.from_date";
 	private final static String KEY_TO_DATE = "posting_parm_edit.to_date";
 	private final static String KEY_CHANGE_DATE = "posting_parm_edit.change_date";
@@ -105,6 +107,8 @@ public class PostingParameterListEditor extends AccountingBlock {
 
 	public final static String PARAM_OWN_STRING = "own_string";
 	public final static String PARAM_DOUBLE_STRING = "double_string";
+	public final static String PARAM_PAYABLE_ACCOUNT = "payable_account";
+	public final static String PARAM_CUSTOMER_CLAIM_ACCOUNT = "customer_claim_account";
 
 	private final static String PARAM_SELECTOR_ACTIVITY = "selector_activity";
 	private final static String PARAM_SELECTOR_REGSPEC = "selector_regspec";
@@ -118,7 +122,11 @@ public class PostingParameterListEditor extends AccountingBlock {
 	private String _errorText = "";
 	private String _theOwnString = "";
 	private String _theDoubleString = "";
+	private String _thePayableAccountString = "";
+	private String _theCustomerClaimAccountString = "";
 	private Map _pMap;
+	private boolean addPayableAccount = false;
+	private boolean addCustomerClaimAccount = false;
 	
 	private boolean _setToEmpty = false;
 	
@@ -128,6 +136,14 @@ public class PostingParameterListEditor extends AccountingBlock {
 
 	public ICPage getResponsePage() {
 		return _responsePage;
+	}
+	
+	public void setAddPayableAccount(boolean b) {
+		addPayableAccount = b;
+	}
+	
+	public void setAddCustomerClaimAccount(boolean b) {
+		addCustomerClaimAccount = b;
 	}
 	
 	/**
@@ -216,6 +232,8 @@ public class PostingParameterListEditor extends AccountingBlock {
 
 		_theOwnString = "";
 		_theDoubleString = "";
+		_thePayableAccountString = "";
+		_theCustomerClaimAccountString = "";
 		try {
 			int index = 1;
 			PostingBusiness pBiz = getPostingBusiness(iwc);
@@ -243,6 +261,12 @@ public class PostingParameterListEditor extends AccountingBlock {
 				}
 				_theOwnString += pBiz.pad(iwc.getParameter(PARAM_OWN_STRING + "_" + index), field);
 				_theDoubleString += pBiz.pad(iwc.getParameter(PARAM_DOUBLE_STRING + "_" + index), field);
+				if (addPayableAccount) {
+					_thePayableAccountString += pBiz.pad(iwc.getParameter(PARAM_PAYABLE_ACCOUNT + "_" + index), field);
+				}
+				if (addCustomerClaimAccount) {
+					_theCustomerClaimAccountString += pBiz.pad(iwc.getParameter(PARAM_CUSTOMER_CLAIM_ACCOUNT + "_" + index), field);
+				}
 				index++;
 			}
 			
@@ -263,6 +287,20 @@ public class PostingParameterListEditor extends AccountingBlock {
 	 */
 	public String getDoublePosting() {
 		return _theDoubleString;
+	}
+
+	/**
+	 * Returns the payable account string.
+	 */
+	public String getPayableAccount() {
+		return _thePayableAccountString;
+	}
+
+	/**
+	 * Returns the customer claim account string.
+	 */
+	public String getCustomerClaimAccount() {
+		return _theCustomerClaimAccountString;
 	}
 
 	private void closeMe(IWContext iwc) {
@@ -365,12 +403,12 @@ public class PostingParameterListEditor extends AccountingBlock {
 
 		table.add(getLocalizedLabel(KEY_CHANGE_SIGN, "Ändringssignatur"),3 ,row);
 		table.add(getText(pp != null ? pp.getChangedSign() : ""), 4, row);
-		table.add(new HiddenInput(PARAM_SIGNED, ""+userName));
+		table.add(new HiddenInput(PARAM_SIGNED, ""+userName), 1, 1);
 		if (iwc.isParameterSet(PARAM_MODE_COPY)) {
-			table.add(new HiddenInput(PARAM_MODE_COPY, ""+iwc.getParameter(PARAM_MODE_COPY)));
+			table.add(new HiddenInput(PARAM_MODE_COPY, ""+iwc.getParameter(PARAM_MODE_COPY)), 1, 1);
 		}
 		if (iwc.isParameterSet(PARAM_EDIT_ID)) {
-			table.add(new HiddenInput(PARAM_EDIT_ID, ""+iwc.getParameter(PARAM_EDIT_ID)));
+			table.add(new HiddenInput(PARAM_EDIT_ID, ""+iwc.getParameter(PARAM_EDIT_ID)), 1, 1);
 		}
 		return table;	
 	}
@@ -467,9 +505,27 @@ public class PostingParameterListEditor extends AccountingBlock {
 	 * @see se.idega.idegaweb.commune.accounting.posting.data.PostingParameters
 	 */
 	protected Table getPostingParameterForm(IWContext iwc, PostingParameters pp, String postingString, String doublePostingString) {
+		return getPostingParameterForm(iwc, pp, postingString, doublePostingString, "", "");
+	}
+	
+	/*
+	 * Returns the posting form with selectors edit fields 
+	 * for "own entry" account as well as "double entry" accounts 
+	 * and additional payable account and customer claim account 
+	 * @see se.idega.idegaweb.commune.accounting.posting.data.PostingParameters
+	 */
+	protected Table getPostingParameterForm(
+				IWContext iwc,
+				PostingParameters pp,
+				String postingString,
+				String doublePostingString,
+				String payableAccountString,
+				String customerClaimAccountString) {
 		Table accounts = new Table();
 		ListTable list1 = null;
 		ListTable list2 = null;
+		ListTable list3 = null;
+		ListTable list4 = null;
 		
 		try {
 			int index = 1;
@@ -487,16 +543,22 @@ public class PostingParameterListEditor extends AccountingBlock {
 			int size = fields.size();
 			list1 = new ListTable(this, size);
 			list2 = new ListTable(this, size);
+			list3 = new ListTable(this, size);
+			list4 = new ListTable(this, size);
 			Iterator iter = fields.iterator();
 			int readPointer = 0;
 			while (iter.hasNext()) {
 				PostingField field = (PostingField) iter.next();
 				list1.setHeader(field.getFieldTitle(), index);
 				list2.setHeader(field.getFieldTitle(), index);
+				list3.setHeader(field.getFieldTitle(), index);
+				list4.setHeader(field.getFieldTitle(), index);
 
 				int fieldLength = field.getLen();
 				String theData1 = "";
 				String theData2 = "";
+				String theData3 = "";
+				String theData4 = "";
 				if (! _setToEmpty){
 				
 					if(iwc.isParameterSet(PARAM_OWN_STRING+"_"+index)) {
@@ -505,9 +567,21 @@ public class PostingParameterListEditor extends AccountingBlock {
 					if(iwc.isParameterSet(PARAM_DOUBLE_STRING+"_"+index)) {
 						theData2 = (String) _pMap.get(PARAM_DOUBLE_STRING+"_"+index);
 					}
+					if(iwc.isParameterSet(PARAM_PAYABLE_ACCOUNT+"_"+index)) {
+						theData3 = (String) _pMap.get(PARAM_PAYABLE_ACCOUNT+"_"+index);
+					}
+					if(iwc.isParameterSet(PARAM_CUSTOMER_CLAIM_ACCOUNT+"_"+index)) {
+						theData4 = (String) _pMap.get(PARAM_CUSTOMER_CLAIM_ACCOUNT+"_"+index);
+					}
 					if (postingString != null && !iwc.isParameterSet(PARAM_BUTTON_SAVE)) {
 						theData1 = pBiz.extractField(postingString,readPointer, fieldLength, field);
 						theData2 = pBiz.extractField(doublePostingString,readPointer, fieldLength, field);
+						if (addPayableAccount) {
+							theData3 = pBiz.extractField(payableAccountString,readPointer, fieldLength, field);							
+						}
+						if (addCustomerClaimAccount) {
+							theData4 = pBiz.extractField(customerClaimAccountString,readPointer, fieldLength, field);							
+						}
 					}
 				}
 				readPointer += fieldLength;
@@ -519,6 +593,18 @@ public class PostingParameterListEditor extends AccountingBlock {
 						getTextInput(PARAM_DOUBLE_STRING+"_"+index, theData2, 80, field.getLen()),
 						field
 						));
+				if (addPayableAccount) {
+					list3.add(inputTextFieldValidation(
+							getTextInput(PARAM_PAYABLE_ACCOUNT+"_"+index, theData3, 80, field.getLen()),
+							field
+							));					
+				}
+				if (addCustomerClaimAccount) {
+					list4.add(inputTextFieldValidation(
+							getTextInput(PARAM_CUSTOMER_CLAIM_ACCOUNT+"_"+index, theData4, 80, field.getLen()),
+							field
+							));					
+				}
 				index++;
 			}
 			
@@ -528,7 +614,14 @@ public class PostingParameterListEditor extends AccountingBlock {
 		accounts.add(list1, 1, 2);
 		accounts.add(getLocalizedText(KEY_HEADER_DOUBLE_ENTRY, "Mot kontering"), 1, 3);
 		accounts.add(list2, 1, 4);
-				
+		if (addPayableAccount) {
+			accounts.add(getLocalizedText(KEY_HEADER_PAYABLE_ACCOUNT, "Konto leverantörsreskontra"), 1, 5);
+			accounts.add(list3, 1, 6);
+		}				
+		if (addCustomerClaimAccount) {
+			accounts.add(getLocalizedText(KEY_HEADER_CUSTOMER_CLAIM_ACCOUNT, "Konto kundfodran"), 1, 7);
+			accounts.add(list4, 1, 8);
+		}				
 		return accounts;
 	}
 
