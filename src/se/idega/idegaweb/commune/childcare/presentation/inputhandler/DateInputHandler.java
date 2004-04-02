af74dcf9ae30c7e5f167d0a2314ec1acd335c368
Thorhallur Helgason <laddi@idega.com>
2004-04-02 09:20:27 +0000

    Changed to use DatePicker instead of DateInput.

--- src/se/idega/idegaweb/commune/childcare/presentation/inputhandler/DateInputHandler.java
+++ src/se/idega/idegaweb/commune/childcare/presentation/inputhandler/DateInputHandler.java
@@ -3,20 +3,40 @@
  */
 package se.idega.idegaweb.commune.childcare.presentation.inputhandler;
 
+import java.text.DateFormat;
+
+import com.idega.business.InputHandler;
 import com.idega.presentation.IWContext;
-import com.idega.presentation.ui.DateInput;
+import com.idega.presentation.PresentationObject;
+import com.idega.presentation.ui.DatePicker;
 
 
 /**
  * @author laddi
  */
-public class DateInputHandler extends DateInput {
+public class DateInputHandler implements InputHandler {
 
 	/* (non-Javadoc)
-	 * @see com.idega.presentation.PresentationObject#main(com.idega.presentation.IWContext)
+	 * @see com.idega.business.InputHandler#getDisplayForResultingObject(java.lang.Object, com.idega.presentation.IWContext)
 	 */
-	public void main(IWContext iwc) throws Exception {
-		super.main(iwc);
-		setToDisplayDayLast(true);
+	public String getDisplayForResultingObject(Object value, IWContext iwc) {
+		if (value != null) {
+			return value.toString();
+		}
+		return "";
+	}
+	/* (non-Javadoc)
+	 * @see com.idega.business.InputHandler#getHandlerObject(java.lang.String, java.lang.String, com.idega.presentation.IWContext)
+	 */
+	public PresentationObject getHandlerObject(String name, String stringValue, IWContext iwc) {
+		DatePicker picker = new DatePicker(name, iwc.getCurrentLocale());
+		picker.setDateFormatStyle(DateFormat.SHORT);
+		return picker;
+	}
+	/* (non-Javadoc)
+	 * @see com.idega.business.InputHandler#getResultingObject(java.lang.String[], com.idega.presentation.IWContext)
+	 */
+	public Object getResultingObject(String[] value, IWContext iwc) throws Exception {
+		return value[0];
 	}
 }
