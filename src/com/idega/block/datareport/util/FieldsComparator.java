80863bd09bf4a8b9cfe93a5ded2d3c1a4383ad2e
Jonas Kari Blandon <jonas@idega.com>
2004-04-06 16:24:58 +0000

    added constructor to provide comparators for specific fields

--- src/com/idega/block/datareport/util/FieldsComparator.java
+++ src/com/idega/block/datareport/util/FieldsComparator.java
@@ -11,16 +11,35 @@ import java.util.Comparator;
 import java.util.Locale;
 
 /*
- * A Comparator for ReportableFields, used to sort ReportableData by one or more ReportableFields. Comparations
- * are done in the order of  the ReportableFields in the array.
- * If a field value is a number or a string starting with a number and a whitespace, the nubers are used for the
- * comparison, otherwise the comparison is alphanumerical using icelandic locale
+ * A Comparator for ReportableFields, used to sort ReportableData by one or more ReportableFields.
+ * The default comparisons works as follows.
+ * If a field value is a String containing a number, the numbers are used for the comparison. if the field is a String starting with a number and a whitespace,
+ * first the String after the whitespace is compared, then the number before the whitespace. In other cases the comparison is alphanumerical using 
+ * icelandic locale.
  * @author jonas
  */
 public class FieldsComparator implements Comparator {
 	
+	/**
+	 * Constructs a FieldsComparator that compares ReportableData objects using the given fields,
+	 * comparing each of the fields using one of the given comparators
+	 * @param reportableFields The fields to use for comparing 
+	 * @param comparators The comparators to use, array must be same length as <code>reportableFields</code> array.
+	 *        The i-th Comparator is used to compare the i-th ReportableField. If the i-th is <code>null</code>, then 
+	 *        default comparison is used (same as if other constructor was used).
+	 */
+	public FieldsComparator(ReportableField[] reportableFields, Comparator[] comparators) {
+		_reportableFields = reportableFields;
+		_comparators = comparators;
+	}
+	
+	/**
+	 * Constructs a FieldsComparator that compares ReportableData objects using the given fields.
+	 * @param reportableFields The fields to use for comparing 
+	 */
 	public FieldsComparator(ReportableField[] reportableFields) {
-		this.reportableFields = reportableFields;
+		_reportableFields = reportableFields;
+		_comparators = null;
 	}
 	
 	public int compare(Object o1, Object o2) {
@@ -34,11 +53,11 @@ public class FieldsComparator implements Comparator {
 		} else if (data2==null) {
 			return 1;
 		}
-		int count = reportableFields.length;
+		int count = _reportableFields.length;
 		for(int i=0; i<count; i++) {
-			ReportableField field = reportableFields[i];
-			String fieldValue1 = (String) data1.getFieldValue(field);
-			String fieldValue2 = (String) data2.getFieldValue(field);
+			ReportableField field = _reportableFields[i];
+			Object fieldValue1 = data1.getFieldValue(field);
+			Object fieldValue2 = data2.getFieldValue(field);
 			if(fieldValue1==fieldValue2) {
 				continue;
 			}
@@ -47,27 +66,37 @@ public class FieldsComparator implements Comparator {
 			} else if(fieldValue2==null) {
 				return 1;
 			}
-			int i1 = getInt(fieldValue1);
-			int i2 = getInt(fieldValue2);
-			if(i1!=-1 && i2!=-1) {
-				// found numbers to compare, use them
-				int dstr = collator.compare(getStringAfterInt(fieldValue1), getStringAfterInt(fieldValue2));
-				if(dstr==0) {
-					if(i1!=i2) {
-						return i1-i2;
-					} else {
-						continue;
-					}
+			int comp;
+			if(_comparators == null || _comparators[i]==null) {
+				if(fieldValue1 instanceof String) {
+					// the two fieldvalues are always of the same type
+					comp = defaultStringCompare((String) fieldValue1, (String) fieldValue2);
 				} else {
-					return dstr; 
+					comp = _collator.compare(fieldValue1, fieldValue2);
 				}
+			} else {
+				comp = _comparators[i].compare(fieldValue1, fieldValue2);
 			}
-			int comp = collator.compare(fieldValue1, fieldValue2);
 			if(comp!=0) {
 				return comp;
 			}
 		}
 		return 0;
+	}
+	
+	private int defaultStringCompare(String str0, String str1) {
+		int comp = 0;
+		
+		int i1 = getInt(str0);
+		int i2 = getInt(str1);
+		if(i1!=-1 && i2!=-1) {
+			// found numbers to compare, use them
+			int dstr = _collator.compare(getStringAfterInt(str0), getStringAfterInt(str1));
+			comp = dstr==0?(i1-i2):dstr;
+		} else {
+			comp = _collator.compare(str0, str1);
+		}
+		return comp;
 	}
 	
 	private int getInt(String str) {
@@ -97,6 +126,7 @@ public class FieldsComparator implements Comparator {
 		return str.substring(i+1);
 	}
 	
-	private ReportableField[] reportableFields;	
-	private Collator collator = Collator.getInstance(new Locale("is","IS"));
+	private ReportableField[] _reportableFields;
+	private Comparator[] _comparators;
+	private Collator _collator = Collator.getInstance(new Locale("is","IS"));
 }
