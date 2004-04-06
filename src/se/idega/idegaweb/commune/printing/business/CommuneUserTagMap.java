93bb0b8b8a4158ccc317d84593b00355dff0e72d
Thorhallur Helgason <laddi@idega.com>
2004-04-06 09:00:24 +0000

    Changed so that if the street name is null from the mail address, the main address is always returned.

--- src/se/idega/idegaweb/commune/printing/business/CommuneUserTagMap.java
+++ src/se/idega/idegaweb/commune/printing/business/CommuneUserTagMap.java
@@ -1,4 +1,3 @@
-
 package se.idega.idegaweb.commune.printing.business;
 
 import java.rmi.RemoteException;
@@ -14,96 +13,103 @@ import com.lowagie.text.ElementTags;
 import com.lowagie.text.xml.XmlPeer;
 
 /**
- * The <CODE>Tags</CODE>-class maps several XHTML-tags to iText-objects.
+ * The <CODE>Tags</CODE> -class maps several XHTML-tags to iText-objects.
  */
 
 public class CommuneUserTagMap extends HashMap {
-    
-/**
- * Constructs an HtmlTagMap.
- */
-    
-    public CommuneUserTagMap(IWApplicationContext iwac,User user) {
-        super();
-        DateFormat df = DateFormat.getDateInstance(DateFormat.LONG,iwac.getApplicationSettings().getDefaultLocale());
-        UserBusiness ub = getUserBusiness(iwac);
-        
-        XmlPeer peer = new XmlPeer(ElementTags.ITEXT, CommuneUserTags.USERLETTER);
-        put(peer.getAlias(), peer);
-        
-        peer = new XmlPeer(ElementTags.CHUNK, CommuneUserTags.NAME);
-        peer.setContent(user.getName());
-        put(peer.getAlias(), peer);
-        
-        peer = new XmlPeer(ElementTags.CHUNK, CommuneUserTags.FULLNAME);
-        peer.setContent(user.getName());
-        put(peer.getAlias(), peer);
-        
-        peer = new XmlPeer(ElementTags.CHUNK, CommuneUserTags.FIRSTNAME);
-        peer.setContent(user.getFirstName());
-        put(peer.getAlias(), peer);
-        
-        peer = new XmlPeer(ElementTags.CHUNK, CommuneUserTags.MIDDLENAME);
-        peer.setContent(user.getMiddleName());
-        put(peer.getAlias(), peer);
-        
-        peer = new XmlPeer(ElementTags.CHUNK, CommuneUserTags.LASTNAME);
-        peer.setContent(user.getLastName());
-        put(peer.getAlias(), peer);
-        
-        peer = new XmlPeer(ElementTags.CHUNK, CommuneUserTags.PERSONALID);
-        peer.setContent(user.getPersonalID());
-        put(peer.getAlias(), peer);   
-        
-        if(user.getDateOfBirth()!=null){
-        	peer = new XmlPeer(ElementTags.CHUNK, CommuneUserTags.BIRTHDATE);
-        	peer.setContent(df.format(user.getDateOfBirth()));
-        	put(peer.getAlias(), peer);        
-        }
-               
-        if(ub!=null){
-        	try{
-        	peer = new XmlPeer(ElementTags.CHUNK, CommuneUserTags.EMAIL);
-        	peer.setContent(ub.getUsersMainEmail(user).getEmailAddress());
-        	put(peer.getAlias(), peer); 
-        	
-        	Address address = getUserMailReceiveAddress(ub,user);
-        	
-        	peer = new XmlPeer(ElementTags.CHUNK, CommuneUserTags.STREETADDRESS);
-        	peer.setContent(address.getStreetAddress());
-        	put(peer.getAlias(), peer);
-        	
-        	peer = new XmlPeer(ElementTags.CHUNK, CommuneUserTags.POSTALADDRESS);
-        	peer.setContent(address.getPostalAddress());
-        	put(peer.getAlias(), peer);
-        	
-        	peer = new XmlPeer(ElementTags.CHUNK, CommuneUserTags.ADDRESSCOUNTRY);
-        	peer.setContent(address.getCountry().getName());
-        	put(peer.getAlias(), peer);
-        	
-        	peer = new XmlPeer(ElementTags.CHUNK, CommuneUserTags.PHONE);
-        	peer.setContent(ub.getUsersHomePhone(user).getNumber());
-        	put(peer.getAlias(), peer);  
-        	}
-        	catch(Exception ex){}
-        
-        }
-   
-    }
-    
-    private Address getUserMailReceiveAddress(UserBusiness ub,User user)throws RemoteException{
-    	int userID =(( Integer) user.getPrimaryKey()).intValue();
-    	Address addr = ub.getUserAddressByAddressType(userID,ub.getAddressHome().getAddressType2());
-    	if(addr==null)
-    		addr = ub.getUsersMainAddress(userID);
-    	return addr;
-    }
-    
-    private UserBusiness getUserBusiness(IWApplicationContext iwac) {
-    	try{
-    	return (UserBusiness)IBOLookup.getServiceInstance(iwac,UserBusiness.class);
-    	}catch(RemoteException ex){}
-    	return null;
-    }
-    
+
+	/**
+	 * Constructs an HtmlTagMap.
+	 */
+
+	public CommuneUserTagMap(IWApplicationContext iwac, User user) {
+		super();
+		DateFormat df = DateFormat.getDateInstance(DateFormat.LONG, iwac.getApplicationSettings().getDefaultLocale());
+		UserBusiness ub = getUserBusiness(iwac);
+
+		XmlPeer peer = new XmlPeer(ElementTags.ITEXT, CommuneUserTags.USERLETTER);
+		put(peer.getAlias(), peer);
+
+		peer = new XmlPeer(ElementTags.CHUNK, CommuneUserTags.NAME);
+		peer.setContent(user.getName());
+		put(peer.getAlias(), peer);
+
+		peer = new XmlPeer(ElementTags.CHUNK, CommuneUserTags.FULLNAME);
+		peer.setContent(user.getName());
+		put(peer.getAlias(), peer);
+
+		peer = new XmlPeer(ElementTags.CHUNK, CommuneUserTags.FIRSTNAME);
+		peer.setContent(user.getFirstName());
+		put(peer.getAlias(), peer);
+
+		peer = new XmlPeer(ElementTags.CHUNK, CommuneUserTags.MIDDLENAME);
+		peer.setContent(user.getMiddleName());
+		put(peer.getAlias(), peer);
+
+		peer = new XmlPeer(ElementTags.CHUNK, CommuneUserTags.LASTNAME);
+		peer.setContent(user.getLastName());
+		put(peer.getAlias(), peer);
+
+		peer = new XmlPeer(ElementTags.CHUNK, CommuneUserTags.PERSONALID);
+		peer.setContent(user.getPersonalID());
+		put(peer.getAlias(), peer);
+
+		if (user.getDateOfBirth() != null) {
+			peer = new XmlPeer(ElementTags.CHUNK, CommuneUserTags.BIRTHDATE);
+			peer.setContent(df.format(user.getDateOfBirth()));
+			put(peer.getAlias(), peer);
+		}
+
+		if (ub != null) {
+			try {
+				peer = new XmlPeer(ElementTags.CHUNK, CommuneUserTags.EMAIL);
+				peer.setContent(ub.getUsersMainEmail(user).getEmailAddress());
+				put(peer.getAlias(), peer);
+
+				Address address = getUserMailReceiveAddress(ub, user);
+
+				peer = new XmlPeer(ElementTags.CHUNK, CommuneUserTags.STREETADDRESS);
+				peer.setContent(address.getStreetAddress());
+				put(peer.getAlias(), peer);
+
+				peer = new XmlPeer(ElementTags.CHUNK, CommuneUserTags.POSTALADDRESS);
+				peer.setContent(address.getPostalAddress());
+				put(peer.getAlias(), peer);
+
+				peer = new XmlPeer(ElementTags.CHUNK, CommuneUserTags.ADDRESSCOUNTRY);
+				peer.setContent(address.getCountry().getName());
+				put(peer.getAlias(), peer);
+
+				peer = new XmlPeer(ElementTags.CHUNK, CommuneUserTags.PHONE);
+				peer.setContent(ub.getUsersHomePhone(user).getNumber());
+				put(peer.getAlias(), peer);
+			}
+			catch (Exception ex) {
+			}
+
+		}
+
+	}
+
+	private Address getUserMailReceiveAddress(UserBusiness ub, User user) throws RemoteException {
+		int userID = ((Integer) user.getPrimaryKey()).intValue();
+		Address addr = ub.getUserAddressByAddressType(userID, ub.getAddressHome().getAddressType2());
+		if (addr == null) {
+			addr = ub.getUsersMainAddress(userID);
+		}
+		else if (addr.getStreetName() == null) {
+			addr = ub.getUsersMainAddress(userID);
+		}
+		return addr;
+	}
+
+	private UserBusiness getUserBusiness(IWApplicationContext iwac) {
+		try {
+			return (UserBusiness) IBOLookup.getServiceInstance(iwac, UserBusiness.class);
+		}
+		catch (RemoteException ex) {
+		}
+		return null;
+	}
+
 }
