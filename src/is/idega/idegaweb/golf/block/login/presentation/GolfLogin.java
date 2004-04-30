5b8f8f4a98fa37c9bdb2cfdfc15dff06139af65c
Thorhallur Helgason <laddi@idega.com>
2004-04-30 13:09:47 +0000

    Changed layout.

--- src/is/idega/idegaweb/golf/block/login/presentation/GolfLogin.java
+++ src/is/idega/idegaweb/golf/block/login/presentation/GolfLogin.java
@@ -10,6 +10,7 @@ import is.idega.idegaweb.golf.entity.Member;
 import is.idega.idegaweb.golf.presentation.GolfBlock;
 
 import com.idega.presentation.IWContext;
+import com.idega.presentation.Script;
 import com.idega.presentation.Table;
 import com.idega.presentation.text.Link;
 import com.idega.presentation.text.Text;
@@ -29,7 +30,8 @@ import com.idega.presentation.ui.TextInput;
  */
 public class GolfLogin extends GolfBlock {
 
-
+	private Boolean _showFormWhenLoggedOn;
+	private boolean _enterSubmit = false;
 
 	public GolfLogin() {
 		super();
@@ -38,74 +40,102 @@ public class GolfLogin extends GolfBlock {
 
 
 	private void startState() {
-		String userText = getResourceBundle().getLocalizedString("user", "User");
-		String passwordText = getResourceBundle().getLocalizedString("password", "Password");
-
-		Form myForm = new Form();
-		myForm.setEventListener(GolfLoginBusiness.class);
-
-		myForm.setMethod("post");
-		myForm.maintainAllParameters();
-
-		Text loginTexti = getSmallHeader(userText);
-		Text passwordTexti = getSmallHeader(passwordText);
-
-		Table myTable = new Table(6,1);
-		myTable.setCellpadding(0);
-		myTable.setCellspacing(0);
-		myTable.setCellpaddingRight(1,1,5);
-		myTable.setCellpaddingRight(2,1,5);
-		myTable.setCellpaddingRight(3,1,5);
-		myTable.setCellpaddingRight(4,1,5);
-		myTable.setCellpaddingRight(5,1,5);
-		myTable.setCellpaddingRight(6,1,5);
-
-		myTable.add(loginTexti, 1, 1);
-
-
-		TextInput login = (TextInput) getStyledSmallInterface(new TextInput("login"));
-		login.setSize(8);
-		myTable.add(login, 2, 1);
-
-
-		myTable.add(passwordTexti, 3, 1);
-
-		PasswordInput passw = (PasswordInput) getStyledSmallInterface(new PasswordInput("password"));
-		passw.setSize(8);
-		myTable.add(passw, 4, 1);
-
-		/*GenericButton loginButton = getSubmitButton("tengja");
-		loginButton.setContent(localize("login.login","Login"));
-		myTable.add(loginButton, 5, 1);*/
-		
-		Text spacer = getStyleText("|", this.STYLENAME_TEMPLATE_HEADER2);
-		myTable.add(spacer, 5, 1);
-		
-		Link loginLink = getStyleLink(localize("login.login","Login"), this.STYLENAME_TEMPLATE_LINK);
-		loginLink.setToFormSubmit(myForm);
-		myTable.add(loginLink, 6, 1);
-		
-//		if (showNewUserImage) {
-//			GenericButton button = getSubmitButton();
-//			button.setName(GolfLoginBusiness.newLoginStateParameter);
-//			button.setContent(localize("login.register","Register"));
-//			myTable.add(button, 6, 1);
-//		} else if (forgotPasswordUrl != null) {
-//			Link l = getSmallLink(localize("login.forgot","Forgot"));
-//			l.setURL(forgotPasswordUrl);
-//			myTable.add(l, 6, 1);
-//		}
-
-		myTable.add(new Parameter(GolfLoginBusiness.LoginStateParameter, "login"));
-
-
-
-		myForm.add(myTable);
-		add(myForm);
-
+		if (showForm(false)) {
+			String userText = getResourceBundle().getLocalizedString("user", "User");
+			String passwordText = getResourceBundle().getLocalizedString("password", "Password");
+	
+			Form myForm = new Form();
+			myForm.setEventListener(GolfLoginBusiness.class);
+	
+			myForm.setMethod("post");
+			myForm.maintainAllParameters();
+	
+			Table myTable = new Table(5,1);
+			myTable.setCellpadding(0);
+			myTable.setCellspacing(0);
+			myTable.setWidth(Table.HUNDRED_PERCENT);
+			myTable.setRowStyleClass(1, getLoginRowClass());
+			myTable.setWidth(5, Table.HUNDRED_PERCENT);
+			myTable.setAlignment(5, 1, Table.HORIZONTAL_ALIGN_RIGHT);
+	
+			Text loginTexti = getSmallText(userText);
+			Text passwordTexti = getSmallText(passwordText);
+	
+			myTable.add(loginTexti, 1, 1);
+	
+			TextInput login = (TextInput) getStyledSmallInterface(new TextInput("login"));
+			login.setSize(8);
+			if (_enterSubmit) {
+				login.setOnKeyPress("return enterSubmit(this,event)");
+			}
+			myTable.add(login, 2, 1);
+	
+			myTable.add(passwordTexti, 3, 1);
+	
+			PasswordInput passw = (PasswordInput) getStyledSmallInterface(new PasswordInput("password"));
+			passw.setSize(8);
+			if (_enterSubmit) {
+				passw.setOnKeyPress("return enterSubmit(this,event)");
+			}
+			myTable.add(passw, 4, 1);
+	
+			/*GenericButton loginButton = getSubmitButton("tengja");
+			loginButton.setContent(localize("login.login","Login"));
+			myTable.add(loginButton, 5, 1);*/
+			
+			/*Text spacer = getStyleText("|", this.STYLENAME_TEMPLATE_HEADER2);
+			myTable.add(spacer, 5, 1);*/
+			
+			Link loginLink = getStyleLink(localize("login.login","Login"), this.STYLENAME_TEMPLATE_LINK);
+			loginLink.setToFormSubmit(myForm);
+			myTable.add(loginLink, 5, 1);
+			
+	//		if (showNewUserImage) {
+	//			GenericButton button = getSubmitButton();
+	//			button.setName(GolfLoginBusiness.newLoginStateParameter);
+	//			button.setContent(localize("login.register","Register"));
+	//			myTable.add(button, 6, 1);
+	//		} else if (forgotPasswordUrl != null) {
+	//			Link l = getSmallLink(localize("login.forgot","Forgot"));
+	//			l.setURL(forgotPasswordUrl);
+	//			myTable.add(l, 6, 1);
+	//		}
+	
+			myTable.add(new Parameter(GolfLoginBusiness.LoginStateParameter, "login"));
+	
+	
+	
+			myForm.add(myTable);
+			add(myForm);
+		}
+	}
+	
+	private boolean showForm(boolean loggedOn) {
+		if (_showFormWhenLoggedOn != null) {
+			if (loggedOn && _showFormWhenLoggedOn.booleanValue()) {
+				return true;
+			}
+			else if (!loggedOn && !_showFormWhenLoggedOn.booleanValue()) {
+				return true;
+			}
+			return false;
+		}
+		return true;
 	}
 
 	public void main(IWContext modinfo) throws Exception {
+
+		if (getParentPage() != null) {
+			Script script = null;
+			if (getParentPage().getAssociatedScript() != null)
+				script = getParentPage().getAssociatedScript();
+			else {
+				script = new Script();
+				getParentPage().setAssociatedScript(script);
+			}
+			script.addFunction("enterSubmit", "function enterSubmit(myfield,e) { var keycode; if (window.event) keycode = window.event.keyCode; else if (e) keycode = e.which; else return true; if (keycode == 13) { myfield.form.submit(); return false; } else return true; }");
+			_enterSubmit = true;
+		}
 
 		String state = internalGetState(modinfo);
 		if (state != null) {
@@ -142,86 +172,88 @@ public class GolfLogin extends GolfBlock {
 	}
 
 	private void isLoggedOn(IWContext modinfo) {
-
-		Form myForm = new Form();
-		myForm.setEventListener(GolfLoginBusiness.class);
-
-		myForm.setMethod("post");
-		myForm.maintainAllParameters();
-
-		Table myTable = new Table();
-		myTable.setCellspacing(0);
-		myTable.setCellpadding(0);
-		myTable.setCellpaddingLeft(1,1,5);
-		myTable.setCellpaddingRight(2,1,5);
-
-		Member member = (Member) modinfo.getSession().getAttribute("member_login");
-		Text userName = getSmallHeader(member.getName());
-		myTable.add(userName,1,1);
-		myTable.setNoWrap(1,1);
-
-		Text spacer = getStyleText("|", this.STYLENAME_TEMPLATE_HEADER2);
-		myTable.add(spacer, 2, 1);
-		
-		Link logout = getStyleLink(localize("login.logout","Log out"), this.STYLENAME_TEMPLATE_LINK);
-		logout.setToFormSubmit(myForm);
-		myTable.add(logout, 3, 1);
-
-		/*GenericButton button = getSubmitButton();
-		button.setContent(localize("login.logout","Log out"));
-		myTable.add(button, 2, 1);*/
-		myTable.add(new Parameter(GolfLoginBusiness.LoginStateParameter, "logoff"));
-
-		myForm.add(myTable);
-		add(myForm);
+		if (showForm(true)) {
+			Form myForm = new Form();
+			myForm.setEventListener(GolfLoginBusiness.class);
+	
+			myForm.setMethod("post");
+			myForm.maintainAllParameters();
+	
+			Table myTable = new Table();
+			myTable.setCellspacing(0);
+			myTable.setCellpadding(0);
+			myTable.setCellpaddingLeft(1, 1, 5);
+			myTable.setCellpaddingRight(2, 1, 5);
+	
+			Member member = (Member) modinfo.getSession().getAttribute("member_login");
+			Text userName = getSmallHeader(member.getName());
+			myTable.add(userName, 1, 1);
+			myTable.setNoWrap(1, 1);
+	
+			Text spacer = getStyleText("|", this.STYLENAME_TEMPLATE_HEADER2);
+			myTable.add(spacer, 2, 1);
+			
+			Link logout = getStyleLink(localize("login.logout","Log out"), this.STYLENAME_TEMPLATE_LINK);
+			logout.setToFormSubmit(myForm);
+			myTable.add(logout, 3, 1);
+	
+			/*GenericButton button = getSubmitButton();
+			button.setContent(localize("login.logout","Log out"));
+			myTable.add(button, 2, 1);*/
+			myTable.add(new Parameter(GolfLoginBusiness.LoginStateParameter, "logoff"));
+	
+			myForm.add(myTable);
+			add(myForm);
+		}
 	}
 
 
 	private void loginFailed(String what) {
-		String userText = getResourceBundle().getLocalizedString("user", "User");
-		String passwordText = getResourceBundle().getLocalizedString("password", "Password");
-
-
-		Form myForm = new Form();
-		myForm.setEventListener(GolfLoginBusiness.class);
-
-		myForm.setMethod("post");
-		myForm.maintainAllParameters();
-
-		Text loginTexti = getSmallHeader(userText);
-		Text passwordTexti =getSmallHeader(passwordText);
-
-		Table myTable = new Table();
-		myTable.setCellspacing(0);
-		myTable.setCellpadding(0);
-		myTable.setCellpaddingRight(1,1,5);
-		myTable.setCellpaddingRight(2,1,5);
-		
-		Text failed = getMessageText(getResourceBundle().getLocalizedString("loginfailed", "Login failed"));
-		//failed.setFontSize(1);
-		if (what.equals("empty")) {
-			failed.setText(getResourceBundle().getLocalizedString("id_needed", "Identification needed"));
-		} else if (what.equals("toBig")) {
-			failed.setText(getResourceBundle().getLocalizedString("wrong_format", "Wrong format"));
+		if (showForm(false)) {
+			String userText = getResourceBundle().getLocalizedString("user", "User");
+			String passwordText = getResourceBundle().getLocalizedString("password", "Password");
+	
+			Form myForm = new Form();
+			myForm.setEventListener(GolfLoginBusiness.class);
+	
+			myForm.setMethod("post");
+			myForm.maintainAllParameters();
+	
+			Text loginTexti = getSmallHeader(userText);
+			Text passwordTexti =getSmallHeader(passwordText);
+	
+			Table myTable = new Table(2, 1);
+			myTable.setWidth(Table.HUNDRED_PERCENT);
+			myTable.setCellspacing(0);
+			myTable.setCellpadding(0);
+			myTable.setRowStyleClass(1, getLoginRowClass());
+			myTable.setAlignment(2, 1, Table.HORIZONTAL_ALIGN_RIGHT);
+			
+			Text failed = getSmallText(getResourceBundle().getLocalizedString("loginfailed", "Login failed"));
+			//failed.setFontSize(1);
+			if (what.equals("empty")) {
+				failed.setText(getResourceBundle().getLocalizedString("id_needed", "Identification needed"));
+			} else if (what.equals("toBig")) {
+				failed.setText(getResourceBundle().getLocalizedString("wrong_format", "Wrong format"));
+			}
+			myTable.add(failed,1,1);
+			myTable.setAlignment(1, 1, "center");
+			
+			/*Text spacer = getStyleText("|", this.STYLENAME_TEMPLATE_HEADER2);
+			myTable.add(spacer, 2, 1);
+			
+			Link tryAgain = getStyleLink(localize("login.try_again","Try again"), this.STYLENAME_TEMPLATE_LINK);
+			tryAgain.setToFormSubmit(myForm);
+			myTable.add(tryAgain, 3, 1);
+	
+			/*GenericButton tryAgain = getSaveButton();
+			tryAgain.setContent(localize("login.try_again","Try again"));
+			myTable.add(tryAgain, 3, 1);*/
+			myTable.add(new Parameter(GolfLoginBusiness.LoginStateParameter, "tryagain"));
+	
+			myForm.add(myTable);
+			add(myForm);
 		}
-		myTable.add(failed,1,1);
-		myTable.setAlignment(1, 1, "center");
-		
-		Text spacer = getStyleText("|", this.STYLENAME_TEMPLATE_HEADER2);
-		myTable.add(spacer, 2, 1);
-		
-		Link tryAgain = getStyleLink(localize("login.try_again","Try again"), this.STYLENAME_TEMPLATE_LINK);
-		tryAgain.setToFormSubmit(myForm);
-		myTable.add(tryAgain, 3, 1);
-
-		/*GenericButton tryAgain = getSaveButton();
-		tryAgain.setContent(localize("login.try_again","Try again"));
-		myTable.add(tryAgain, 3, 1);*/
-		myTable.add(new Parameter(GolfLoginBusiness.LoginStateParameter, "tryagain"));
-
-		myForm.add(myTable);
-		add(myForm);
-
 	}
 
 	///// additional mothods /////////
@@ -230,4 +262,10 @@ public class GolfLogin extends GolfBlock {
 		GolfLoginBusiness.logOut2(modinfo);
 	}
 
+	/**
+	 * @param formWhenLoggedOn The _showFormWhenLoggedOn to set.
+	 */
+	public void setShowFormWhenLoggedOn(boolean formWhenLoggedOn) {
+		this._showFormWhenLoggedOn = new Boolean(formWhenLoggedOn);
+	}
 }
