2e5987ae975d1de39aa7a35bcca26bdbc87651fc
Thomas Hilbig <thomas@idega.com>
2004-05-11 18:03:02 +0000

    some unused constructors removed

--- src/com/idega/block/dataquery/data/xml/QueryConditionPart.java
+++ src/com/idega/block/dataquery/data/xml/QueryConditionPart.java
@@ -68,30 +68,30 @@ public class QueryConditionPart implements QueryPart {
 	}
 	
 	
-	public QueryConditionPart(int idNumber, String entity,String path, String field, String type, String pattern, String description) {
-		this(new StringBuffer(PREFIX).append(idNumber).toString(), entity, path, field, type, pattern, description);
-	}
+//	public QueryConditionPart(int idNumber, String entity,String path, String field, String type, String pattern, String description) {
+//		this(new StringBuffer(PREFIX).append(idNumber).toString(), entity, path, field, type, pattern, description);
+//	}
 	
-	public QueryConditionPart(String id, String entity,String path, String field, String type, String pattern, String description){
-		this.id = id;
-		this.entity = entity;
-		this.path = path;
-		this.field = field;
-		this.type = type;
-		this.pattern = pattern;
-		this.description = description;
-	}
+//	public QueryConditionPart(String id, String entity,String path, String field, String type, String pattern, String description){
+//		this.id = id;
+//		this.entity = entity;
+//		this.path = path;
+//		this.field = field;
+//		this.type = type;
+//		this.pattern = pattern;
+//		this.description = description;
+//	}
 	
 	
-	public QueryConditionPart(int idNumber, String entity,String path, String field, String type, Collection patterns, String description) {
-		this(new StringBuffer(PREFIX).append(idNumber).toString(), entity, path, field, type, patterns, description);
-	}
+//	public QueryConditionPart(int idNumber, String entity,String path, String field, String type, Collection patterns, String description) {
+//		this(new StringBuffer(PREFIX).append(idNumber).toString(), entity, path, field, type, patterns, description);
+//	}
 
 	
-	public QueryConditionPart(String id, String entity,String path, String field, String type, Collection patterns, String description){
-		this(id, entity, path, field, type, (String) null, description);
-		this.patterns = patterns;
-	}
+//	public QueryConditionPart(String id, String entity,String path, String field, String type, Collection patterns, String description){
+//		this(id, entity, path, field, type, (String) null, description);
+//		this.patterns = patterns;
+//	}
 
 	
 	public QueryConditionPart(XMLElement xml){
@@ -321,30 +321,30 @@ public class QueryConditionPart implements QueryPart {
 		return buffer.toString();
 	}
 	
-	public static QueryConditionPart decode(String encoded){
-		StringTokenizer toker = new StringTokenizer(encoded,";");
-		int tokenNumber = toker.countTokens();
-		String id = toker.nextToken();
-		String entity = toker.nextToken();
-		String path = toker.nextToken();
-		String field = toker.nextToken();
-		String type = toker.nextToken();
-		if (tokenNumber == 7) {
-			String pattern = toker.nextToken();
-			String description = toker.nextToken();
-			return new QueryConditionPart(id, entity, path, field, type, pattern, description);
-		}
-		else {
-			List patterns = new ArrayList();
-			int counter = 6;
-			while (toker.hasMoreTokens() && counter < tokenNumber) {
-				patterns.add(toker.nextToken());
-				counter++;
-			}
-			String description = toker.nextToken();
-			return new QueryConditionPart(id, entity, path, field, type, patterns, description);
-		}
-	}
+//	public static QueryConditionPart decode(String encoded){
+//		StringTokenizer toker = new StringTokenizer(encoded,";");
+//		int tokenNumber = toker.countTokens();
+//		String id = toker.nextToken();
+//		String entity = toker.nextToken();
+//		String path = toker.nextToken();
+//		String field = toker.nextToken();
+//		String type = toker.nextToken();
+//		if (tokenNumber == 7) {
+//			String pattern = toker.nextToken();
+//			String description = toker.nextToken();
+//			return new QueryConditionPart(id, entity, path, field, type, pattern, description);
+//		}
+//		else {
+//			List patterns = new ArrayList();
+//			int counter = 6;
+//			while (toker.hasMoreTokens() && counter < tokenNumber) {
+//				patterns.add(toker.nextToken());
+//				counter++;
+//			}
+//			String description = toker.nextToken();
+//			return new QueryConditionPart(id, entity, path, field, type, patterns, description);
+//		}
+//	}
 	
 
 	/* (non-Javadoc)
