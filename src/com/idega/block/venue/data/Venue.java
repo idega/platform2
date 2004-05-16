e8ec1a2a704c91469c3c86578e77b7470f055807
Thorhallur Helgason <laddi@idega.com>
2004-05-16 12:12:11 +0000

    First draft for venue system (arenas, golfcourses, sportshalls etc.)

---
+++ src/com/idega/block/venue/data/Venue.java
@@ -0,0 +1,55 @@
+/*
+ * Created on 18.4.2004
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+package com.idega.block.venue.data;
+
+import com.idega.data.IDOEntity;
+
+
+import com.idega.user.data.Group;
+
+
+import com.idega.core.location.data.Address;
+
+
+
+/** * @author laddiTODO To change the template for this generated type comment go toWindow - Preferences - Java - Code Generation - Code and Comments */
+public interface Venue extends IDOEntity {
+
+	String getName();
+
+	void setName(String name);
+
+	Group getOwner();
+
+	void setOwner(Group owner);
+
+	String getDescription();
+
+	void setDescription(String description);
+
+	/**
+	 *  
+	 * @uml.property name="venueType"
+	 * @uml.associationEnd multiplicity="(0 1)" inverse="venue:com.idega.block.venue.data.VenueType"
+	 */
+	VenueType getVenueType();
+
+	/**
+	 *  
+	 * @uml.property name="venueType"
+	 */
+	void setVenueType(VenueType venueType);
+
+	Address getAddress();
+
+	void setAddress(Address address);
+
+	boolean isDeleted();
+
+	void setDeleted(boolean deleted);
+
+}
---
+++ src/com/idega/block/venue/data/VenueType.java
@@ -0,0 +1,23 @@
+/*
+ * Created on 18.4.2004
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+package com.idega.block.venue.data;
+
+import com.idega.data.IDOEntity;
+
+
+/** * @author laddiTODO To change the template for this generated type comment go toWindow - Preferences - Java - Code Generation - Code and Comments */
+public interface VenueType extends IDOEntity {
+
+	String getName();
+
+	void setName(String name);
+
+	String getDescription();
+
+	void setDescription(String description);
+
+}
