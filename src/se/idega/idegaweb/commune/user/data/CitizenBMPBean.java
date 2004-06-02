e6f3800675a73dd28ce77043a0e2e93ff1aac1db
Joakim Johnsson <joakim@idega.com>
2004-06-02 14:46:02 +0000

    Change date input to be able to use dropdowns through the report generator

--- src/se/idega/idegaweb/commune/user/data/CitizenBMPBean.java
+++ src/se/idega/idegaweb/commune/user/data/CitizenBMPBean.java
@@ -182,6 +182,147 @@ public class CitizenBMPBean extends UserBMPBean implements Citizen {
 			query.append(".* ");
 			//from
 			query.appendFrom(tables,variables);
+			
+			IDOQuery subQuery = this.idoQuery();
+			
+			subQuery.appendSelect();
+			subQuery.append(" usr.");
+			subQuery.append(usrIdColumn);
+			subQuery.append(" user_id");
+			subQuery.appendFrom();
+			subQuery.append(scmDef.getSQLTableName());
+			subQuery.append(" cm, ");
+			subQuery.append(usrDef.getSQLTableName());
+			subQuery.append(" usr ");
+			subQuery.appendWhere();
+			subQuery.append("usr.");
+			subQuery.append(usrIdColumn);
+			subQuery.appendEqualSign();
+			subQuery.append("cm.");
+			subQuery.append(memberField);
+			
+			subQuery.appendAnd();
+			subQuery.append("cm.");
+			subQuery.append(registerDateField);
+			subQuery.appendLessThanOrEqualsSign();
+			subQuery.append(date);
+			
+			subQuery.appendAnd();
+			subQuery.append("cm.");
+			subQuery.append(schoolClassField);
+			subQuery.appendInCollection(classes);
+			
+			query.append(", (");
+			query.append(subQuery);
+			query.append(") sub");
+			
+			
+			//where
+			query.appendWhere();
+			query.append(variables[1]);
+			query.append(".");
+			query.append(grRelDef.findFieldByUniqueName(GroupRelation.FIELD_GROUP).getSQLFieldName());
+			query.appendEqualSign();
+			query.append(citizenGroup.getPrimaryKey());
+			//and
+			query.appendAnd();
+			query.append(variables[1]);
+			query.append(".");
+			query.append(grRelDef.findFieldByUniqueName(GroupRelation.FIELD_RELATED_GROUP).getSQLFieldName());
+			query.appendEqualSign();
+			query.append(variables[0]);
+			query.append(".");
+			query.append(usrIdColumn);
+			
+			
+			//and if relationstatus
+			if(relationStatus!= null){
+				//and
+				query.appendAnd();
+				query.append(variables[1]);
+				query.append(".");
+				query.append(grRelDef.findFieldByUniqueName(GroupRelation.FIELD_STATUS).getSQLFieldName());
+				query.appendInArrayWithSingleQuotes(relationStatus);		
+			}
+
+			query.appendAnd();
+			query.append(variables[0]);
+			query.append(".");
+			query.append(dateOfBirthField);
+			query.appendGreaterThanOrEqualsSign();
+			query.append(firstDateOfBirth);
+			
+			query.appendAnd();
+			query.append(variables[0]);
+			query.append(".");
+			query.append(dateOfBirthField);
+			query.appendLessThanOrEqualsSign();
+			query.append(lastDateOfBirth);
+			
+			query.appendAnd();
+			query.append(usrIdColumn);
+			
+
+			query.appendNotIn("sub.user_id ");
+			
+//			query.appendNotIn(subQuery);
+			
+			//order by usr.last_name, usr.first_name, usr.middle_name
+			String[] order = new String[3];
+			order[0] = variables[0]+"."+usrDef.findFieldByUniqueName(User.FIELD_LAST_NAME).getSQLFieldName();
+			order[1] = variables[0]+"."+usrDef.findFieldByUniqueName(User.FIELD_MIDDLE_NAME).getSQLFieldName();
+			order[2] = variables[0]+"."+usrDef.findFieldByUniqueName(User.FIELD_MIDDLE_NAME).getSQLFieldName();
+			query.appendOrderBy(order); 
+
+			
+			System.out.println("SQL -> "+this.getClass()+":"+query);
+					
+			return idoFindPKsByQuery(query);
+			//Temp debug
+			//return idoFindPKsByQuery(query,100);
+		} catch (IDOCompositePrimaryKeyException e) {
+			e.printStackTrace();
+		} 
+		return ListUtil.getEmptyList();
+	}
+
+	public Collection ejbFindCitizensNotAssignedToClassOnGivenDateOld(Group citizenGroup, Date date, Collection classes, Date firstDateOfBirth, Date lastDateOfBirth) throws IDOLookupException, FinderException{
+		try {
+			
+			IDOEntityDefinition usrDef = IDOLookup.getEntityDefinitionForClass(User.class);
+			IDOEntityDefinition scmDef = IDOLookup.getEntityDefinitionForClass(SchoolClassMember.class);
+			IDOEntityDefinition grRelDef = IDOLookup.getEntityDefinitionForClass(GroupRelation.class);
+
+			String usrIdColumn = usrDef.getPrimaryKeyDefinition().getField().getSQLFieldName();
+			IDOEntityField dateOfBirthField = usrDef.findFieldByUniqueName(User.FIELD_DATE_OF_BIRTH);
+
+			IDOEntityField memberField = scmDef.findFieldByUniqueName(SchoolClassMember.FIELD_MEMBER);
+			IDOEntityField registerDateField = scmDef.findFieldByUniqueName(SchoolClassMember.FIELD_REGISTER_DATE);
+			IDOEntityField schoolClassField = scmDef.findFieldByUniqueName(SchoolClassMember.FIELD_SCHOOLCLASS);
+			
+			//relationStatus could be as parameter to this method
+			String[] relationStatus = new String[1];
+			relationStatus[0] = GroupRelation.STATUS_ACTIVE;
+		  	
+			String[] tables = new String[2];
+			String[] variables = new String[2];
+			//table name
+			tables[0] = usrDef.getSQLTableName();
+			//	as variable
+			variables[0] = "u";
+			//table name
+			tables[1] = grRelDef.getSQLTableName();
+			//	as variable
+			variables[1] = "gr_rel";
+			  	
+			//constructing query
+			IDOQuery query = idoQuery();
+			//select
+			query.appendSelect();
+			query.append(variables[0]);
+			query.append(".* ");
+			//from
+			query.appendFrom(tables,variables);
 			//where
 			query.appendWhere();
 			query.append(variables[1]);
