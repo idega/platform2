6db48ee284057657c27b5c427472827195fb6dd1
Thorhallur Helgason <laddi@idega.com>
2004-06-05 08:06:45 +0000

    A class to show various handicap statistics for a golfer.

---
+++ src/is/idega/idegaweb/golf/handicap/presentation/HandicapMemberStatistics.java
@@ -0,0 +1,182 @@
+/*
+ * Created on 4.6.2004
+ */
+package is.idega.idegaweb.golf.handicap.presentation;
+
+import java.rmi.RemoteException;
+
+import is.idega.idegaweb.golf.business.StatisticsBusiness;
+import is.idega.idegaweb.golf.entity.Member;
+import is.idega.idegaweb.golf.presentation.GolfBlock;
+
+import com.idega.business.IBOLookup;
+import com.idega.business.IBOLookupException;
+import com.idega.business.IBORuntimeException;
+import com.idega.idegaweb.IWApplicationContext;
+import com.idega.presentation.IWContext;
+import com.idega.presentation.Table;
+
+
+/**
+ * @author laddi
+ */
+public class HandicapMemberStatistics extends GolfBlock {
+
+	private boolean iShowHoleStatistics = true;
+	private boolean iShowTotalStatistics = false;
+	
+	private StatisticsBusiness statBusiness;
+	String iMemberID;
+	
+	/* (non-Javadoc)
+	 * @see com.idega.presentation.PresentationObject#main(com.idega.presentation.IWContext)
+	 */
+	public void main(IWContext iwc) throws Exception {
+		statBusiness = getStatisticsBusiness(iwc);
+		
+		iMemberID = iwc.getRequest().getParameter("member_id");
+		if (iMemberID == null) {
+			iMemberID = (String) iwc.getSession().getAttribute("member_id");
+		}
+		if (iMemberID == null) {
+			Member member = (Member) iwc.getSession().getAttribute("member_login");
+			if (member != null) {
+				iMemberID = String.valueOf(member.getID());
+				if (iMemberID == null) {
+					iMemberID = "1";
+				}
+			}
+			else {
+				iMemberID = "1";
+			}
+		}
+
+		if (iShowHoleStatistics) {
+			getHoleStatistics(iwc);
+		}
+		else if (iShowTotalStatistics) {
+			getTotalStatistics(iwc);
+		}
+	}
+	
+	private void getHoleStatistics(IWContext iwc) throws RemoteException {
+		int eagles = statBusiness.getNumberOfEaglesByMember(Integer.parseInt(iMemberID));
+		int birdies = statBusiness.getNumberOfBirdiesByMember(Integer.parseInt(iMemberID));
+		int pars = statBusiness.getNumberOfParsByMember(Integer.parseInt(iMemberID));
+		int bogeys = statBusiness.getNumberOfBogeysByMember(Integer.parseInt(iMemberID));
+		int doubleBogeys = statBusiness.getNumberOfDoubleBogeysByMember(Integer.parseInt(iMemberID));
+		int totalStrokes = eagles + birdies + pars + bogeys + doubleBogeys;
+		
+		double averageEagles = 0;
+		double averageBirdies = 0;
+		double averagePars = 0;
+		double averageBogeys = 0;
+		double averageDoubleBogeys = 0;
+		if (totalStrokes > 0) {
+			averageEagles = (double) eagles / (double) totalStrokes;
+			averageBirdies = (double) birdies / (double) totalStrokes;
+			averagePars = (double) pars / (double) totalStrokes;
+			averageBogeys = (double) bogeys / (double) totalStrokes;
+			averageDoubleBogeys = (double) doubleBogeys / (double) totalStrokes;
+		}
+
+		Table table = new Table(3, 6);
+		table.setWidth(Table.HUNDRED_PERCENT);
+		table.setCellpadding(0);
+		table.setCellspacing(0);
+		table.setColumnAlignment(2, Table.HORIZONTAL_ALIGN_CENTER);
+		table.setColumnAlignment(3, Table.HORIZONTAL_ALIGN_CENTER);
+		int row = 1;
+		
+		table.add(getSmallHeader(localize("handicap.count", "Count")), 2, row);
+		table.add(getSmallHeader(localize("handicap.average_of_total", "Total")), 3, row);
+		table.setRowStyleClass(row++, getHeaderRowClass());
+		
+		table.add(getSmallText(localize("handicap.eagles", "Eagles")), 1, row);
+		table.add(getSmallText(String.valueOf(eagles)), 2, row);
+		table.add(getSmallText(statBusiness.getPercentText(averageEagles)), 3, row);
+		table.setRowStyleClass(row++, getLightRowClass());
+
+		table.add(getSmallText(localize("handicap.birdies", "Birdies")), 1, row);
+		table.add(getSmallText(String.valueOf(birdies)), 2, row);
+		table.add(getSmallText(statBusiness.getPercentText(averageBirdies)), 3, row);
+		table.setRowStyleClass(row++, getDarkRowClass());
+
+		table.add(getSmallText(localize("handicap.pars", "Pars")), 1, row);
+		table.add(getSmallText(String.valueOf(pars)), 2, row);
+		table.add(getSmallText(statBusiness.getPercentText(averagePars)), 3, row);
+		table.setRowStyleClass(row++, getLightRowClass());
+		
+		table.add(getSmallText(localize("handicap.bogeys", "Bogeys")), 1, row);
+		table.add(getSmallText(String.valueOf(bogeys)), 2, row);
+		table.add(getSmallText(statBusiness.getPercentText(averageBogeys)), 3, row);
+		table.setRowStyleClass(row++, getDarkRowClass());
+
+		table.add(getSmallText(localize("handicap.double_bogeys", "Double bogeys")), 1, row);
+		table.add(getSmallText(String.valueOf(doubleBogeys)), 2, row);
+		table.add(getSmallText(statBusiness.getPercentText(averageDoubleBogeys)), 3, row);
+		table.setRowStyleClass(row++, getLightRowClass());
+		
+		add(table);
+	}
+	
+	private void getTotalStatistics(IWContext iwc) throws RemoteException {
+		int strokes = statBusiness.getSumOfStrokesByMember(Integer.parseInt(iMemberID));
+		int putts = statBusiness.getSumOfPuttsByMember(Integer.parseInt(iMemberID));
+		int holesPlayed = statBusiness.getNumberOfHolesPlayedByMember(Integer.parseInt(iMemberID));
+		int roundsPlayed = statBusiness.getNumberOfRoundsByMember(Integer.parseInt(iMemberID));
+
+		Table table = new Table(2, 5);
+		table.setWidth(Table.HUNDRED_PERCENT);
+		table.setCellpadding(0);
+		table.setCellspacing(0);
+		table.setColumnAlignment(2, Table.HORIZONTAL_ALIGN_CENTER);
+		int row = 1;
+		
+		table.add(getSmallHeader(localize("handicap.count", "Count")), 2, row);
+		table.setRowStyleClass(row++, getHeaderRowClass());
+		
+		table.add(getSmallText(localize("handicap.strokes", "Strokes")), 1, row);
+		table.add(getSmallText(String.valueOf(strokes)), 2, row);
+		table.setRowStyleClass(row++, getLightRowClass());
+
+		table.add(getSmallText(localize("handicap.putts", "Putts")), 1, row);
+		table.add(getSmallText(String.valueOf(putts)), 2, row);
+		table.setRowStyleClass(row++, getDarkRowClass());
+
+		table.add(getSmallText(localize("handicap.holes_played_total", "Holes played")), 1, row);
+		table.add(getSmallText(String.valueOf(holesPlayed)), 2, row);
+		table.setRowStyleClass(row++, getLightRowClass());
+
+		table.add(getSmallText(localize("handicap.rounds_played_total", "Rounds played")), 1, row);
+		table.add(getSmallText(String.valueOf(roundsPlayed)), 2, row);
+		table.setRowStyleClass(row++, getDarkRowClass());
+
+		add(table);
+	}
+	
+	private StatisticsBusiness getStatisticsBusiness(IWApplicationContext iwac) {
+		try {
+			return (StatisticsBusiness) IBOLookup.getServiceInstance(iwac, StatisticsBusiness.class);
+		}
+		catch (IBOLookupException ile) {
+			throw new IBORuntimeException(ile);
+		}
+	}
+
+	/**
+	 * @param showHoleStatistics The iShowHoleStatistics to set.
+	 */
+	public void setShowHoleStatistics(boolean showHoleStatistics) {
+		iShowHoleStatistics = showHoleStatistics;
+		iShowTotalStatistics = !showHoleStatistics;
+	}
+	
+	/**
+	 * @param showTotalStatistics The iShowTotalStatistics to set.
+	 */
+	public void setShowTotalStatistics(boolean showTotalStatistics) {
+		iShowTotalStatistics = showTotalStatistics;
+		iShowHoleStatistics = !showTotalStatistics;
+	}
+}
