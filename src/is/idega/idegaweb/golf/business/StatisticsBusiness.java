388f034567b5510666c5dde8a14d9ed7b57256b3
Thorhallur Helgason <laddi@idega.com>
2004-06-05 08:08:50 +0000

    Added new statistical methods

--- src/is/idega/idegaweb/golf/business/StatisticsBusiness.java
+++ src/is/idega/idegaweb/golf/business/StatisticsBusiness.java
@@ -4,11 +4,24 @@ package is.idega.idegaweb.golf.business;
 public interface StatisticsBusiness extends com.idega.business.IBOService
 {
  public java.lang.String getDecimalText(double p0) throws java.rmi.RemoteException;
+ public int getHolesPlayedByMember(int p0) throws java.rmi.RemoteException;
+ public int getNumberOfBirdiesByMember(int p0) throws java.rmi.RemoteException;
+ public int getNumberOfBogeysByMember(int p0) throws java.rmi.RemoteException;
+ public int getNumberOfDoubleBogeysByMember(int p0) throws java.rmi.RemoteException;
+ public int getNumberOfEaglesByMember(int p0) throws java.rmi.RemoteException;
+ public int getNumberOfHolesPlayedByMember(int p0) throws java.rmi.RemoteException;
+ public int getNumberOfParsByMember(int p0) throws java.rmi.RemoteException;
+ public int getNumberOfRoundsByMember(int p0) throws java.rmi.RemoteException;
+ public double getNumberOnFairwayByMember(int p0) throws java.rmi.RemoteException;
  public double getNumberOnFairwayByTeeID(java.util.Collection p0) throws java.rmi.RemoteException;
+ public double getNumberOnGreenByMember(int p0) throws java.rmi.RemoteException;
  public double getNumberOnGreenByTeeID(java.util.Collection p0) throws java.rmi.RemoteException;
  public java.lang.String getPercentText(double p0) throws java.rmi.RemoteException;
+ public double getPuttAverageByMember(int p0) throws java.rmi.RemoteException;
  public double getPuttAverageByTeeID(java.util.Collection p0) throws java.rmi.RemoteException;
  public java.util.Collection getStatisticsByTeeID(java.util.Collection p0) throws java.rmi.RemoteException;
+ public int getSumOfPuttsByMember(int p0) throws java.rmi.RemoteException;
+ public int getSumOfStrokesByMember(int p0) throws java.rmi.RemoteException;
  public is.idega.idegaweb.golf.entity.TeeColor getTeeColor(is.idega.idegaweb.golf.entity.Tee p0) throws java.rmi.RemoteException;
  public java.util.Collection getTeeFromFieldIDAndHoleNumber(int p0,int p1) throws java.rmi.RemoteException;
 }
--- src/is/idega/idegaweb/golf/business/StatisticsBusinessBean.java
+++ src/is/idega/idegaweb/golf/business/StatisticsBusinessBean.java
@@ -1,8 +1,12 @@
 package is.idega.idegaweb.golf.business;
 
 
+import is.idega.idegaweb.golf.entity.Scorecard;
+import is.idega.idegaweb.golf.entity.ScorecardHome;
 import is.idega.idegaweb.golf.entity.Statistic;
 import is.idega.idegaweb.golf.entity.StatisticHome;
+import is.idega.idegaweb.golf.entity.Stroke;
+import is.idega.idegaweb.golf.entity.StrokeHome;
 import is.idega.idegaweb.golf.entity.Tee;
 import is.idega.idegaweb.golf.entity.TeeColor;
 import is.idega.idegaweb.golf.entity.TeeColorHome;
@@ -18,6 +22,7 @@ import com.idega.business.IBOServiceBean;
 import com.idega.data.IDOException;
 import com.idega.data.IDOLookup;
 import com.idega.data.IDOLookupException;
+import com.idega.data.query.MatchCriteria;
 
 /**
  * @author Anna
@@ -97,7 +102,154 @@ public class StatisticsBusinessBean extends IBOServiceBean implements Statistics
 		return null;
 	}
 	
-	public String getPercentText(double number) {
+	public double getNumberOnFairwayByMember(int member){
+		double fairwayPercent = 0;
+		try {			
+			int totalCount = getHome().getCountFairwaysByMember(member);
+			int fairwayCount = getHome().getNumberOnFairwayByMember(member);
+			fairwayPercent = ((double) fairwayCount / (double) totalCount);
+			return fairwayPercent;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return fairwayPercent;
+	}
+
+	public double getNumberOnGreenByMember(int member){
+		double greenPercent = 0;
+		try {
+			int totalCount = getHome().getCountOnGreenByMember(member);
+			int greenCount = getHome().getNumberOnGreenByMember(member);
+			greenPercent = ((double) greenCount / (double) totalCount);
+			return greenPercent;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return greenPercent;
+	}
+	
+	public double getPuttAverageByMember(int member){
+		try {
+			return getHome().getPuttAverageByMember(member);
+		} catch (IDOLookupException e) {
+			e.printStackTrace();
+		} catch (IDOException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
+	
+	public int getNumberOfEaglesByMember(int member){
+		try {
+			return getStrokesHome().getCountDifferenceByMember(member, -2, MatchCriteria.LESSEQUAL);
+		} catch (IDOLookupException e) {
+			e.printStackTrace();
+		} catch (IDOException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
+	
+	public int getNumberOfBirdiesByMember(int member){
+		try {
+			return getStrokesHome().getCountDifferenceByMember(member, -1, MatchCriteria.EQUALS);
+		} catch (IDOLookupException e) {
+			e.printStackTrace();
+		} catch (IDOException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
+	
+	public int getNumberOfParsByMember(int member){
+		try {
+			return getStrokesHome().getCountDifferenceByMember(member, 0, MatchCriteria.EQUALS);
+		} catch (IDOLookupException e) {
+			e.printStackTrace();
+		} catch (IDOException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
+	
+	public int getNumberOfBogeysByMember(int member){
+		try {
+			return getStrokesHome().getCountDifferenceByMember(member, 1, MatchCriteria.EQUALS);
+		} catch (IDOLookupException e) {
+			e.printStackTrace();
+		} catch (IDOException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
+	
+	public int getNumberOfDoubleBogeysByMember(int member){
+		try {
+			return getStrokesHome().getCountDifferenceByMember(member, 2, MatchCriteria.GREATEREQUAL);
+		} catch (IDOLookupException e) {
+			e.printStackTrace();
+		} catch (IDOException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
+	
+	public int getNumberOfRoundsByMember(int member){
+		try {
+			return getScorecardHome().getCountRoundsPlayedByMember(member);
+		} catch (IDOLookupException e) {
+			e.printStackTrace();
+		} catch (IDOException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
+	
+	public int getSumOfStrokesByMember(int member) {
+		try {
+			return getStrokesHome().getSumOfStrokesByMember(member);
+		} catch (IDOLookupException e) {
+			e.printStackTrace();
+		} catch (IDOException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
+	
+	public int getNumberOfHolesPlayedByMember(int member) {
+		try {
+			return getStrokesHome().getCountOfHolesPlayedByMember(member);
+		} catch (IDOLookupException e) {
+			e.printStackTrace();
+		} catch (IDOException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
+	
+	public int getSumOfPuttsByMember(int member) {
+		try {
+			return getHome().getPuttSumByMember(member);
+		} catch (IDOLookupException e) {
+			e.printStackTrace();
+		} catch (IDOException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
+	
+	public int getHolesPlayedByMember(int member) {
+		try {
+			return getStrokesHome().getCountOfHolesPlayedByMember(member);
+		} catch (IDOLookupException e) {
+			e.printStackTrace();
+		} catch (IDOException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
+	
+		public String getPercentText(double number) {
 		return nf.format(number);
 	}
 	
@@ -116,4 +268,12 @@ public class StatisticsBusinessBean extends IBOServiceBean implements Statistics
 	private TeeColorHome getTeeColorHome()throws IDOLookupException{
 		return (TeeColorHome) IDOLookup.getHome(TeeColor.class);
 	}
+
+	private StrokeHome getStrokesHome()throws IDOLookupException{
+		return (StrokeHome) IDOLookup.getHome(Stroke.class);
+	}	
+
+	private ScorecardHome getScorecardHome()throws IDOLookupException{
+		return (ScorecardHome) IDOLookup.getHome(Scorecard.class);
+	}	
 }
