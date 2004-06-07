95b90aad817443072d847e9b0be3f72ee3b6a12c
Thorhallur Helgason <laddi@idega.com>
2004-06-07 06:38:26 +0000

    Added hole number if found in request.

--- src/is/idega/idegaweb/golf/field/presentation/FieldNameText.java
+++ src/is/idega/idegaweb/golf/field/presentation/FieldNameText.java
@@ -7,6 +7,7 @@ import is.idega.idegaweb.golf.entity.Field;
 import is.idega.idegaweb.golf.entity.FieldHome;
 
 import com.idega.data.IDOLookup;
+import com.idega.idegaweb.IWResourceBundle;
 import com.idega.presentation.IWContext;
 import com.idega.presentation.text.Text;
 
@@ -16,13 +17,18 @@ import com.idega.presentation.text.Text;
  */
 public class FieldNameText extends Text {
 
-	/* (non-Javadoc)
+  private final static String IW_BUNDLE_IDENTIFIER="is.idega.idegaweb.golf";
+
+  /* (non-Javadoc)
 	 * @see com.idega.presentation.PresentationObject#main(com.idega.presentation.IWContext)
 	 */
 	public void main(IWContext iwc) throws Exception {
+		IWResourceBundle iwrb = getResourceBundle(iwc);
+		
 		String field_id = iwc.getParameter("field_id");
 		if (field_id == null)
 			field_id = "50";
+		String hole_number = iwc.getParameter("hole_number");
 
 		Field field = ((FieldHome) IDOLookup.getHomeLegacy(Field.class)).findByPrimaryKey(Integer.parseInt(field_id));
 		if (field != null) {
@@ -31,5 +37,14 @@ public class FieldNameText extends Text {
 		else {
 			setText("No field found");
 		}
+
+		if (hole_number != null) {
+			setText(Text.NON_BREAKING_SPACE + "-" + Text.NON_BREAKING_SPACE);
+			setText(iwrb.getLocalizedString("field.hole", "Hole") + Text.NON_BREAKING_SPACE + hole_number);
+		}
 	}
+
+  public String getBundleIdentifier(){
+    return IW_BUNDLE_IDENTIFIER;
+  }
 }
