9db3fb0e7fb3451d2899a4b0325a0a4d9dd90f90
Thorhallur Helgason <laddi@idega.com>
2004-06-08 08:46:19 +0000

    Changed to go straight to selected tournament in the tournament admin from tournament info.

--- src/is/idega/idegaweb/golf/tournament/presentation/TournamentAdministrator.java
+++ src/is/idega/idegaweb/golf/tournament/presentation/TournamentAdministrator.java
@@ -10,28 +10,33 @@ import com.idega.presentation.text.Text;
  * @author gimmi
  */
 public class TournamentAdministrator extends TournamentBlock {
+
 	protected boolean tournamentMustBeSet() {
 		return false;
 	}
 
 	public void main(IWContext modinfo) throws Exception {
-	  String view = modinfo.getParameter("tournament_admin_view");
-	  String URI = modinfo.getRequestURI();
+		String view = modinfo.getParameter("tournament_admin_view");
+		String URI = modinfo.getRequestURI();
 
 		modinfo.removeSessionAttribute("tournament_id");
 
-	  if (isAdmin() || isClubAdmin()) {
-	    //add(getResourceBundle().getImage("tournament/adminlogo.gif"));
-	  	add(getMessageText(localize("tournament.loading_tournament_list...","Loading tournament list...")));
-		add(Text.getBreak());
-	  	add(Text.getBreak());
-	  	add(modinfo.getIWMainApplication().getCoreBundle().getImage("busy.gif"));
-		getParentPage().setToRedirect(modinfo.getIWMainApplication().getObjectInstanciatorURI(TournamentSelectorWindow.class) + "&" + PARAMETER_CLASS_NAME + "=" + TournamentSelectorWindow.class.getName());
-	  }
-	  else {
-	    add(getErrorText(localize("tournament.no_permission","No Permission")));
-	    this.getParentPage().setToClose(5);
-	  }
+		if (isAdmin() || isClubAdmin()) {
+			//add(getResourceBundle().getImage("tournament/adminlogo.gif"));
+			add(getMessageText(localize("tournament.loading_tournament_list...", "Loading tournament list...")));
+			add(Text.getBreak());
+			add(Text.getBreak());
+			add(modinfo.getIWMainApplication().getCoreBundle().getImage("busy.gif"));
+			String URL = modinfo.getIWMainApplication().getObjectInstanciatorURI(TournamentSelectorWindow.class) + "&" + PARAMETER_CLASS_NAME + "=" + TournamentSelectorWindow.class.getName();
+			if (modinfo.isParameterSet(TournamentSelector.PARAMETER_TOURNAMENT)) {
+				URL += "&" + TournamentSelector.PARAMETER_TOURNAMENT + "=" + modinfo.getParameter(TournamentSelector.PARAMETER_TOURNAMENT);
+			}
+			getParentPage().setToRedirect(URL);
+		}
+		else {
+			add(getErrorText(localize("tournament.no_permission", "No Permission")));
+			this.getParentPage().setToClose(5);
+		}
 
 	}
 }
