7a030c479476c0bb188c235b6137a6965f6da120
Aron Birkir Gudmundsson <aron@idega.com>
2004-06-09 18:19:38 +0000

    Upgrading excel document printing for scorecards

---
+++ src/is/idega/idegaweb/golf/tournament/business/PrintStickersWriter.java
@@ -0,0 +1,221 @@
+/*
+ * Created on 9.6.2004
+ *
+ * Copyright (C) 2004 Idega hf. All Rights Reserved.
+ *
+ *  This software is the proprietary information of Idega hf.
+ *  Use is subject to license terms.
+ */
+package is.idega.idegaweb.golf.tournament.business;
+
+import is.idega.idegaweb.golf.entity.Tournament;
+import is.idega.idegaweb.golf.entity.TournamentRound;
+import is.idega.idegaweb.golf.entity.TournamentRoundHome;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.ejb.FinderException;
+import javax.servlet.http.HttpServletRequest;
+
+import com.idega.data.IDOLookup;
+import com.idega.idegaweb.IWResourceBundle;
+import com.idega.io.MediaWritable;
+import com.idega.io.MemoryFileBuffer;
+import com.idega.io.MemoryInputStream;
+import com.idega.io.MemoryOutputStream;
+import com.idega.presentation.IWContext;
+import com.idega.util.IWTimestamp;
+
+
+/**
+ * @author aron
+ *
+ * PrintStickersWriter TODO Describe this type
+ */
+public class PrintStickersWriter implements MediaWritable {
+	
+	String tournamentRoundID = null;
+	MemoryFileBuffer buffer = null;
+	IWResourceBundle iwrb = null;
+
+	/* (non-Javadoc)
+	 * @see com.idega.io.MediaWritable#getMimeType()
+	 */
+	public String getMimeType() {
+		return "application/x-msexcel";
+	}
+	
+	private String localize(String key,String defaultString){
+		return iwrb.getLocalizedString(key,defaultString);
+	}
+
+	/* (non-Javadoc)
+	 * @see com.idega.io.MediaWritable#init(javax.servlet.http.HttpServletRequest, com.idega.presentation.IWContext)
+	 */
+	public void init(HttpServletRequest req, IWContext iwc) {
+		iwrb = iwc.getIWMainApplication().getBundle("is.idega.idegaweb.golf").getResourceBundle(iwc);
+		String tournamentRoundID = iwc.getParameter("tournament_round_id");
+		Connection Conn = null;
+
+		  try{
+			Conn = com.idega.util.database.ConnectionBroker.getConnection();
+			buffer = new MemoryFileBuffer();
+			MemoryOutputStream ous = new MemoryOutputStream(buffer);
+			OutputStreamWriter out = new OutputStreamWriter(ous);
+
+			char[] c  = null;
+
+			Statement stmt = Conn.createStatement();
+
+			TournamentRound tournamentRound = ((TournamentRoundHome) IDOLookup.getHomeLegacy(TournamentRound.class)).findByPrimaryKey(Integer.parseInt(tournamentRoundID));
+			Tournament tournament = tournamentRound.getTournament();
+			int interval = tournament.getInterval();
+
+//			TODO: Gimmi : Why not use TournamentController.getTournamentRoundParticipants() here?
+
+			String sql = "select m.member_id, m.social_security_number, m.first_name, m.middle_name,"+
+			  " m.last_name, u.abbrevation,tm.tournament_id, tm.tournament_group_id, tg.name as tournament_group_name, tc.tee_color_name, s.scorecard_id,"+
+			  " s.scorecard_date, tr.tournament_round_id, tr.round_number, count(str.stroke_count) as holes_played,"+
+			  " cast((( s.handicap_before * s.slope / 113 ) + ( s.course_rating - f.field_par )) as numeric (4,0)) as round_handicap,"+
+			  " sum(str.stroke_count) as strokes_without_handicap, sum(str.stroke_count) - cast((( s.handicap_before * s.slope / 113 ) + ( s.course_rating - f.field_par ))as numeric (4,0)) as strokes_with_handicap,"+
+			  " s.total_points, sum(str.hole_par) as total_par, sum(str.stroke_count) - sum(str.hole_par) as difference, start.grup_num, start.startingtime_id, tg.name as group_name"+
+			  " from tournament_round tr, tee_color tc,"+
+			  " member_info mi, member m, field f, union_ u, tournament_member tm, tournament t,tournament_group tg, startingtime start, tournament_ROUND_startingtime ts,"+
+			  " scorecard s left join stroke str on str.scorecard_id = s.scorecard_id"+
+			  " where s.tee_color_id = tc.tee_color_id and s.tournament_round_id = tr.tournament_round_id and tr.tournament_id = t.tournament_id"+
+			  " and tm.tournament_group_id = tg.tournament_group_id and tm.union_id = u.union_id and t.tournament_id = tm.tournament_id and tm.member_id = mi.member_id"+
+			  " and mi.member_id = s.member_id and s.member_id = m.member_id and s.field_id = f.field_id"+
+			  " AND tR.tournament_ROUND_id = ts.tournament_ROUND_id AND ts.startingtime_id = start.startingtime_id"+
+			  " AND start.field_id = t.field_id"+
+			  //" AND start.startingtime_date >= cast (tr.round_date as datetime)"+
+			  //" AND start.startingtime_date <= cast (tr.round_end_date as datetime)"+
+			  " AND m.member_id = start.member_id and tm.tournament_group_id = tg.tournament_group_id"+
+			  " and tr.tournament_round_id = "+tournamentRoundID+
+			  " group by m.member_id, m.social_security_number, m.first_name, m.middle_name, m.last_name, u.abbrevation, tm.tournament_id, tm.tournament_group_id, tg.name,tc.tee_color_name,s.scorecard_id, s.scorecard_date, f.field_par,tr.tournament_round_id, tr.round_number, s.total_points, s.handicap_before, s.slope, s.course_rating, start.grup_num, start.startingtime_id, tg.name"+
+			  " order by grup_num,start.startingtime_id";
+
+			ResultSet RS  = stmt.executeQuery(sql);
+
+			int count = 0;
+			String tournName = tournament.getName();
+			StringBuffer data = new StringBuffer();
+			data.append(localize("tournament.tournament","Tournament")); data.append("\t");
+			data.append(localize("tournament.name","Name")); data.append("\t");
+			data.append(localize("tournament.ssn","Social security number")); data.append("\t");
+			data.append(localize("tournament.club","Club")); data.append("\t");
+			data.append(localize("tournament.group","Group")); data.append("\t");
+			data.append(localize("tournament.tee","Tee")); data.append("\t");
+			data.append(localize("tournament.gamehandicap","Gamehandicap"));data.append("\t");
+			data.append(localize("tournament.teetime_with_date","Teetime with date")); data.append("\t");
+			data.append(localize("tournament.teetime","Teetime")); data.append("\t");
+			data.append("\n");
+			out.write(data.toString().toCharArray());
+
+			while(RS.next()){
+
+			  IWTimestamp stampur2 = new IWTimestamp(tournamentRound.getRoundDate());
+
+			  int addMinutes = (RS.getInt("grup_num")-1) * interval;
+			  stampur2.addMinutes(addMinutes);
+
+			  String minutes = stampur2.getMinute()+"";
+			  if ( stampur2.getMinute() < 10 ) {
+				minutes = "0"+minutes;
+			  }
+
+			  data = new StringBuffer();
+			  String s = tournName; // tournName
+			  data.append(s);       data.append("\t");
+			  s = RS.getString("first_name"); // first_name
+			  if(!RS.wasNull()){   data.append(s);       data.append(" ");     }
+			  s = RS.getString("middle_name"); // middle_name
+			  if(!RS.wasNull()){   data.append(s);       data.append(" ");     }
+			  s = RS.getString("last_name"); // last_name
+			  if(!RS.wasNull()){   data.append(s); }
+			  data.append("\t");
+			  String ssc = RS.getString("social_security_number");
+			  if ( ssc.length() == 10 ) {
+				s = ssc.substring(0,6)+"-"+ssc.substring(6,ssc.length());
+			  }
+			  else {
+				s =  ssc; // social_security_number
+			  }
+			  if(!RS.wasNull()){   data.append(s); }
+			  data.append("\t");
+			  s = RS.getString("abbrevation"); // abbrevation
+			  if(!RS.wasNull()){   data.append(s); }
+			  data.append("\t");
+			  s = RS.getString(9); // tournament_group_name
+			  if(!RS.wasNull()){   data.append(s); }
+			  data.append("\t");
+			  s = RS.getString("tee_color_name"); // tee_color_name
+			  if(!RS.wasNull()){   data.append(s); }
+			  data.append("\t");
+			  s = RS.getString(16); // round_handicap
+			  if(!RS.wasNull()){   data.append(s); }
+			  data.append("\t");
+			  s = stampur2.getYear()+"-"+stampur2.getMonth()+"-"+stampur2.getDate()+" kl. "+stampur2.getHour()+"."+minutes; // startingtime
+			  data.append(s);
+			  data.append("\t");
+			  s = stampur2.getHour()+":"+minutes; // startingtime
+			  data.append(s);
+			  data.append("\n");
+			  c = data.toString().toCharArray();
+			  out.write(c);
+			  count++;
+
+			}
+
+			if(RS!=null)
+			  RS.close();
+			stmt.close();
+			if(c!=null)
+			  out.write(c);
+
+			out.close();
+
+		  }
+		  catch(IOException io){
+			io.printStackTrace();
+		  }
+		  catch(SQLException sql){
+			sql.printStackTrace();
+		  }
+		  catch(FinderException e){
+		  	e.printStackTrace();
+		  }
+		  finally {
+			com.idega.util.database.ConnectionBroker.freeConnection(Conn);
+		  }
+
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.idega.io.MediaWritable#writeTo(java.io.OutputStream)
+	 */
+	public void writeTo(OutputStream out) throws IOException {
+		if (buffer != null) {
+			MemoryInputStream mis = new MemoryInputStream(buffer);
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			// Read the entire contents of the file.
+			while (mis.available() > 0) {
+				baos.write(mis.read());
+			}
+			baos.writeTo(out);
+		}
+		else
+			System.err.println("buffer is null");
+
+	}
+
+	public static void main(String[] args) {
+	}
+}
--- src/is/idega/idegaweb/golf/tournament/presentation/Printing.java
+++ src/is/idega/idegaweb/golf/tournament/presentation/Printing.java
@@ -6,6 +6,7 @@ package is.idega.idegaweb.golf.tournament.presentation;
 import is.idega.idegaweb.golf.entity.Tournament;
 import is.idega.idegaweb.golf.entity.TournamentHome;
 import is.idega.idegaweb.golf.entity.TournamentRound;
+import is.idega.idegaweb.golf.tournament.business.PrintStickersWriter;
 import is.idega.idegaweb.golf.tournament.business.TournamentController;
 
 import java.io.IOException;
@@ -13,7 +14,9 @@ import java.sql.SQLException;
 
 import javax.ejb.FinderException;
 
+import com.idega.block.reports.business.ReportWriter;
 import com.idega.data.IDOLookup;
+import com.idega.idegaweb.IWMainApplication;
 import com.idega.idegaweb.IWResourceBundle;
 import com.idega.presentation.IWContext;
 import com.idega.presentation.Table;
@@ -24,6 +27,7 @@ import com.idega.presentation.ui.Form;
 import com.idega.presentation.ui.GenericButton;
 import com.idega.presentation.ui.HiddenInput;
 import com.idega.presentation.ui.SubmitButton;
+import com.idega.presentation.ui.Window;
 
 /**
  * @author gimmi
@@ -124,10 +128,14 @@ public class Printing extends TournamentBlock {
 	      Link scorecards = getLink(iwrb.getLocalizedString("tournament.stickers_on_scorecards","Stickers on scorecards"));
 	        scorecards.setWindowToOpen(PrintStickersWindow.class);
 	        scorecards.addParameter("tournament_round_id",tournamentRounds[a].getID());
-	      Link scorecardsExcel = getLink(iwrb.getLocalizedString("tournament.stickers_on_scorecards","Stickers on scorecards")+" (Excel)");
-	        scorecardsExcel.setWindowToOpen(PrintStickersExcelWindow.class);
+	      
+	      //Link scorecardsExcel = getLink(iwrb.getLocalizedString("tournament.stickers_on_scorecards","Stickers on scorecards")+" (Excel)");
+	        //scorecardsExcel.setWindowToOpen(PrintStickersExcelWindow.class);
+	        Link scorecardsExcel = getPrintScoreCardExcelLink(modinfo,iwrb.getLocalizedString("tournament.stickers_on_scorecards","Stickers on scorecards")+" (Excel)");
 	        scorecardsExcel.addParameter("tournament_round_id",tournamentRounds[a].getID());
 	        scorecardsExcel.setTarget("_blank");
+	      
+	        
 	      Link excel = getLink(iwrb.getLocalizedString("tournament.unfilled_leaderboard","Unfilled leaderboard")+" (Excel)");
 	        excel.setWindowToOpen(PrintUnfilledLeaderBoardExcelWindow.class);
 	        excel.addParameter("tournament_round_id",tournamentRounds[a].getID());
@@ -184,6 +192,18 @@ public class Printing extends TournamentBlock {
 
 	    add(myTable);
 
+	  }
+	  
+	  private Link getPrintScoreCardExcelLink(IWContext iwc,String label){
+	  	 Window window = new Window("TournamentAdmin",iwc.getIWMainApplication().getMediaServletURI());
+	     window.setResizable(true);
+	     window.setMenubar(true);
+	     window.setHeight(500);
+	     window.setWidth(700);
+	     Link link = getLink( label );
+	     link.setWindow(window);
+	     link.addParameter(ReportWriter.PRM_WRITABLE_CLASS,IWMainApplication.getEncryptedClassName(PrintStickersWriter.class));
+	     return link;
 	  }
 
 }
