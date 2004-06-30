9f38510bbfddcf0333e2eea36c0ec471ad9fea52
Jonas Kari Blandon <jonas@idega.com>
2004-06-30 21:54:36 +0000

    fixed bug in appending calling domain

--- src/is/idega/idegaweb/member/isi/block/clubs/presentation/ClubPageIncluder.java
+++ src/is/idega/idegaweb/member/isi/block/clubs/presentation/ClubPageIncluder.java
@@ -1,6 +1,6 @@
 /*
  * Created on Mar 28, 2004
-*/
+ */
 package is.idega.idegaweb.member.isi.block.clubs.presentation;
 
 import com.idega.core.builder.data.ICPage;
@@ -17,83 +17,87 @@ import com.idega.presentation.ui.PageIncluder;
  * @author <a href="eiki@idega.is>Eirikur Hrafnsson</a>
  */
 public class ClubPageIncluder extends PageIncluder {
-
-    public static String BUNDLE_PROPERTY_ROOT_CLUB_ID = "ROOT_CLUB_ID";
-    public static String PARAM_ROOT_CLUB_ID = "RO_CL_ID";
-    public static String PARAM_CALLING_PAGE_ID = "CL_PA_ID";
-    public static String PARAM_CALLING_DOMAIN = "CL_DM";
+	
+	public static String BUNDLE_PROPERTY_ROOT_CLUB_ID = "ROOT_CLUB_ID";
+	public static String PARAM_ROOT_CLUB_ID = "RO_CL_ID";
+	public static String PARAM_CALLING_PAGE_ID = "CL_PA_ID";
+	public static String PARAM_CALLING_DOMAIN = "CL_DM";
 	public static final String IW_BUNDLE_IDENTIFIER = "is.idega.idegaweb.member.isi";
-    
+	
 	private Page parentPage;
 	private String menuStyleSrc = "cssmenu/CSSMultiLevelMenu.css";
-    private ICPage page;
-    private String rootClubParamName = null; // id of club is sent with this parametere (if not null)
-    private String _callingDomain = null;
+	private ICPage page;
+	private String rootClubParamName = null; // id of club is sent with this parametere (if not null)
+	private String _callingDomain = null;
 	
-    public ClubPageIncluder() {
-        super();
-    }
-
-    public ClubPageIncluder(String URL) {
-        super(URL);
-    }
-    
-    public void main(IWContext iwc) throws Exception {
-    	super.main(iwc);
-    	
-    	parentPage = this.getParentPage();
+	public ClubPageIncluder() {
+		super();
+	}
+	
+	public ClubPageIncluder(String URL) {
+		super(URL);
+	}
+	
+	public void main(IWContext iwc) throws Exception {
+		super.main(iwc);
+		
+		parentPage = this.getParentPage();
 		parentPage.addStyleSheetURL(iwc.getIWMainApplication().getCoreBundle().getVirtualPathWithFileNameString(menuStyleSrc));
-    }
-    
-    protected String finalizeLocationString(String location, IWContext iwc) {
-        IWBundle bundle = this.getBundle(iwc);
-        String groupId = bundle.getProperty(BUNDLE_PROPERTY_ROOT_CLUB_ID);
-        StringBuffer finalUrl = new StringBuffer(location);
-        
-        if(groupId!=null) {
-            if(location.endsWith("/")){//check if the url ends with a slash
-                finalUrl.append("?");
-              }
-              else{//no slash at end
-                if( finalUrl.indexOf("?")==-1 ){//check if the url contains a ?
-                  if(finalUrl.indexOf("/",8)!=-1){//check if the url contains a slash
-                      finalUrl.append("?");
-                  }
-                  else{
-                      finalUrl.append("/?");
-                  }
-                }
-                else{//just add to the parameters
-                    finalUrl.append("&");
-                }
-              }
-              //add the extra parameters
-            //the division id
-            finalUrl.append(PARAM_ROOT_CLUB_ID).append("=").append(groupId);
-            if(rootClubParamName!=null) {
-            	finalUrl.append("&").append(rootClubParamName).append("=").append(groupId);
-            }
-            
-            if(page!=null) {
-                finalUrl.append("&").append(PARAM_CALLING_PAGE_ID).append("=").append( page.getPrimaryKey().toString());
-                finalUrl.append("&").append(PARAM_CALLING_DOMAIN).append("=").append( _callingDomain );
-            }
-            /*else {
-	            //the page the includer is currently on
-	            IWApplicationContext iwac = iwc.getIWMainApplication().getIWApplicationContext();
-	            BuilderService bs;
-	            try {
-	                bs = BuilderServiceFactory.getBuilderService(iwac);
-	                finalUrl.append("&").append(PARAM_CALLING_PAGE_ID).append("=").append(bs.getCurrentPageId(iwc));
-	            } catch (Exception e) {
-	                e.printStackTrace();
-	            }
-            }*/
-        }
-        
-        return finalUrl.toString();
-    }
-    
+	}
+	
+	protected String finalizeLocationString(String location, IWContext iwc) {
+		IWBundle bundle = this.getBundle(iwc);
+		String groupId = bundle.getProperty(BUNDLE_PROPERTY_ROOT_CLUB_ID);
+		StringBuffer finalUrl = new StringBuffer(location);
+		
+		if(groupId!=null) {
+			if(location.endsWith("/")){//check if the url ends with a slash
+				finalUrl.append("?");
+			}
+			else{//no slash at end
+				if( finalUrl.indexOf("?")==-1 ){//check if the url contains a ?
+					if(finalUrl.indexOf("/",8)!=-1){//check if the url contains a slash
+						finalUrl.append("?");
+					}
+					else{
+						finalUrl.append("/?");
+					}
+				}
+				else{//just add to the parameters
+					finalUrl.append("&");
+				}
+			}
+			//add the extra parameters
+			//the division id
+			finalUrl.append(PARAM_ROOT_CLUB_ID).append("=").append(groupId);
+			if(rootClubParamName!=null) {
+				finalUrl.append("&").append(rootClubParamName).append("=").append(groupId);
+			}
+			
+			if(page!=null) {
+				finalUrl.append("&").append(PARAM_CALLING_PAGE_ID).append("=").append( page.getPrimaryKey().toString());
+			}
+			
+			if(_callingDomain!=null) {
+				finalUrl.append("&").append(PARAM_CALLING_DOMAIN).append("=").append( _callingDomain );
+			}
+			/*else {
+			 //the page the includer is currently on
+			  IWApplicationContext iwac = iwc.getIWMainApplication().getIWApplicationContext();
+			  BuilderService bs;
+			  try {
+			  bs = BuilderServiceFactory.getBuilderService(iwac);
+			  finalUrl.append("&").append(PARAM_CALLING_PAGE_ID).append("=").append(bs.getCurrentPageId(iwc));
+			  } catch (Exception e) {
+			  e.printStackTrace();
+			  }
+			  }*/
+		}
+		String strFinalUrl = finalUrl.toString();
+		System.out.println("ClubPageIncluder: finalized url is \"" + strFinalUrl + "\"");
+		return strFinalUrl;
+	}
+	
 	public String getBundleIdentifier() {
 		return IW_BUNDLE_IDENTIFIER;
 	}
@@ -103,7 +107,7 @@ public class ClubPageIncluder extends PageIncluder {
 	}
 	
 	public void setCallingPage(ICPage page) {
-	    this.page = page;
+		this.page = page;
 	}
 	
 	public void setCallingDomain(String callingDomain) {
