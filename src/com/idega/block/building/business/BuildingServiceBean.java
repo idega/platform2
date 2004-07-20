684e57ee8d3d03dac11e1d4bf1950167a8d80704
Aron Birkir Gudmundsson <aron@idega.com>
2004-07-20 14:59:32 +0000

    store methods implemented

--- src/com/idega/block/building/business/BuildingServiceBean.java
+++ src/com/idega/block/building/business/BuildingServiceBean.java
@@ -6,6 +6,9 @@ package com.idega.block.building.business;
 
 import java.rmi.RemoteException;
 
+import javax.ejb.CreateException;
+import javax.ejb.FinderException;
+
 import com.idega.block.building.data.Apartment;
 import com.idega.block.building.data.ApartmentCategory;
 import com.idega.block.building.data.ApartmentCategoryHome;
@@ -25,6 +28,7 @@ import com.idega.block.building.data.FloorHome;
 import com.idega.block.building.data.Room;
 import com.idega.block.building.data.RoomHome;
 import com.idega.business.IBOServiceBean;
+import com.idega.data.IDOStoreException;
 
 /**
  * BuildingServiceBean
@@ -34,26 +38,209 @@ import com.idega.business.IBOServiceBean;
 public class BuildingServiceBean extends IBOServiceBean implements BuildingService {
 	
 	public Complex storeComplex(Integer complexID, String name, String info, Integer imageID, Integer textID) {
+		try {
+			Complex complex = null;
+			if (complexID!= null && complexID.intValue() > 0) {
+				complex = getComplexHome().findByPrimaryKey(complexID);
+			}
+			else{
+				complex = getComplexHome().create();
+			}
+			complex.setName(name);
+			complex.setInfo(info);
+			if (imageID!=null && imageID.intValue() > 0)
+				complex.setImageId(imageID);
+			if(textID!=null && textID.intValue()>0)
+				complex.setTextId(textID.intValue());
+			complex.store();
+		} catch (IDOStoreException e) {
+			e.printStackTrace();
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		} catch (FinderException e) {
+			e.printStackTrace();
+		} catch (CreateException e) {
+			e.printStackTrace();
+		}
+	
 		return null;
 	}
 	
-	public Building storeBuilding(Integer buildingID, String name, String address, String info, Integer imageID, Integer complexid, Integer textID){
+	public Building storeBuilding(Integer buildingID, String name, String address, String info, Integer imageID, Integer complexID, Integer textID){
+		try {
+			Building building = null;
+			
+			if (buildingID!=null && buildingID.intValue() > 0) {
+				building = getBuildingHome().findByPrimaryKey(buildingID);
+			}
+			else{
+				building = getBuildingHome().create();
+			}
+			building.setName(name);
+			building.setStreet(address);
+			building.setInfo(info);
+			if (imageID!=null && imageID.intValue() > 0)
+				building.setImageId(imageID);
+			if(complexID!=null && complexID.intValue()>0)
+				building.setComplexId(complexID.intValue());
+			
+			if(textID!=null && textID.intValue()>0)
+				building.setTextId(textID.intValue());
+			building.store();
+			return building;
+		} catch (IDOStoreException e) {
+			e.printStackTrace();
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		} catch (CreateException e) {
+			e.printStackTrace();
+		} catch (FinderException e) {
+			e.printStackTrace();
+		}
 		return null;
+		
 	}
 	
 	public Floor storeFloor(Integer floorID, String name, Integer buildingID, String info, Integer imageID, Integer textID) {
+		try {
+			Floor floor = null;
+			if (floorID!=null && floorID.intValue() > 0) {
+				floor = getFloorHome().findByPrimaryKey(floorID);
+			}
+			else{
+				floor = getFloorHome().create();
+			}
+			floor.setName(name);
+			floor.setBuildingId(buildingID);
+			floor.setInfo(info);
+			if (imageID!=null && imageID.intValue() > 0)
+				floor.setImageId(imageID);
+			if(textID!=null && textID.intValue()>0)
+				floor.setTextId(textID.intValue());
+			floor.store();
+			return floor;
+		} catch (IDOStoreException e) {
+			e.printStackTrace();
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		} catch (CreateException e) {
+			e.printStackTrace();
+		} catch (FinderException e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 	
-	public ApartmentCategory saveApartmentCategory(Integer categoryID, String name, String info, Integer imageID, Integer textID) {
+	public ApartmentCategory storeApartmentCategory(Integer categoryID, String name, String info, Integer imageID, Integer textID) {
+		try {
+			ApartmentCategory category = null;
+			if (categoryID!=null && categoryID.intValue() > 0) {
+				category = getApartmentCategoryHome().findByPrimaryKey(categoryID);
+			}
+			else{
+				category = getApartmentCategoryHome().create();
+			}
+			category.setName(name);
+			category.setInfo(info);
+			if (imageID!=null && imageID.intValue() > 0)
+				category.setImageId(imageID);
+			if(textID!=null && textID.intValue()>0)
+				category.setTextId(textID.intValue());
+			category.store();
+			return category;
+		} catch (IDOStoreException e) {
+			e.printStackTrace();
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		} catch (CreateException e) {
+			e.printStackTrace();
+		} catch (FinderException e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 	
-	public ApartmentType storeApartmentType(Integer typeID, String name, String info, String extraInfo, Integer planID, Integer imageID, Integer categoryID, Integer textID, Float area, Integer roomcount, Integer rent, Boolean balcony, Boolean bath, Boolean kitchen, Boolean storage, Boolean study, Boolean furniture, Boolean loft) {
+	public ApartmentType storeApartmentType(Integer typeID, String name, String info,String abbrev, String extraInfo, Integer planID, Integer imageID, Integer categoryID, Integer textID, Float area, Integer roomcount, Integer rent, Boolean balcony, Boolean bath, Boolean kitchen, Boolean storage, Boolean study, Boolean furniture, Boolean loft) {
+		
+		try {
+			ApartmentType type = null;
+			if(typeID!=null && typeID.intValue()>0){
+				type = getApartmentTypeHome().findByPrimaryKey(typeID);
+			}
+			else{
+				type = getApartmentTypeHome().create();
+			}
+				
+			type.setName(name);
+			type.setInfo(info);
+			type.setAbbreviation(abbrev);
+			type.setExtraInfo(extraInfo);
+			if (planID!=null && planID.intValue() > 0)
+				type.setFloorPlanId(planID);
+			if (imageID!=null && imageID.intValue() > 0)
+				type.setImageId(imageID);
+			if(textID!=null && textID.intValue()>0)
+				type.setTextId(textID.intValue());
+			type.setApartmentCategoryId(categoryID);
+			type.setArea(area);
+			type.setRoomCount(roomcount);
+			type.setRent(rent);
+
+			type.setBalcony(balcony.booleanValue());
+			type.setBathRoom(bath.booleanValue());
+			type.setKitchen(kitchen.booleanValue());
+			type.setLoft(loft.booleanValue());
+			type.setStorage(storage.booleanValue());
+			type.setStudy(study.booleanValue());
+			type.setFurniture(furniture.booleanValue());
+
+			type.store();
+			return type;
+		} catch (IDOStoreException e) {
+			e.printStackTrace();
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		} catch (CreateException e) {
+			e.printStackTrace();
+		} catch (FinderException e) {
+			e.printStackTrace();
+		}
 		return null;
+
 	}
 	
 	public Apartment storeApartment(Integer apartmentID, String name, String info, Integer floorID, Integer typeID, Boolean rentable, Integer imageID, Integer textID) {
+		try {
+			Apartment apartment =null;
+			if (apartmentID!=null && apartmentID.intValue() > 0) {
+				apartment =getApartmentHome().findByPrimaryKey(apartmentID);
+			}
+			else{
+				apartment =getApartmentHome().create();
+			}
+			apartment.setName(name);
+			apartment.setFloorId(floorID);
+			apartment.setApartmentTypeId(typeID);
+			apartment.setInfo(info);
+			apartment.setRentable(rentable.booleanValue());
+			if (imageID!=null && imageID.intValue() > 0)
+				apartment.setImageId(imageID);
+			
+
+			if(textID!=null && textID.intValue()>0)
+				apartment.setTextId(textID.intValue());
+
+			apartment.store();
+			return apartment;
+		} catch (IDOStoreException e) {
+			e.printStackTrace();
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		} catch (FinderException e) {
+			e.printStackTrace();
+		} catch (CreateException e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 	
