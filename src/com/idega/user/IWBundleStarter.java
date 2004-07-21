74f1ef117da2cebd8cc30b33e6d7399824001dd3
Thomas Hilbig <thomas@idega.com>
2004-07-21 12:35:34 +0000

    *** empty log message ***

---
+++ src/com/idega/user/IWBundleStarter.java
@@ -0,0 +1,30 @@
+package com.idega.user;
+
+import com.idega.development.presentation.DeveloperHomepageGenerator;
+import com.idega.idegaweb.IWBundle;
+import com.idega.idegaweb.IWBundleStartable;
+import com.idega.repository.data.ImplementorRepository;
+import com.idega.user.block.homepage.presentation.HomePageGenerator;
+
+/**
+ * <p>Title: idegaWeb</p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2003</p>
+ * <p>Company: idega Software</p>
+ * @author <a href="thomas@idega.is">Thomas Hilbig</a>
+ * @version 1.0
+ * Created on Jun 10, 2004
+ */
+public class IWBundleStarter implements IWBundleStartable {
+
+	public void start(IWBundle starterBundle) {
+		
+		// implementors
+		ImplementorRepository repository = ImplementorRepository.getInstance();
+		repository.addImplementor(DeveloperHomepageGenerator.class, HomePageGenerator.class);
+	}
+	
+	public void stop(IWBundle starterBundle) {
+		// nothing to do
+	}
+}
