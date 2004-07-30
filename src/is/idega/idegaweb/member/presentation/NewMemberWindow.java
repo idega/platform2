cce9a77e8b84f425c8916cbd761f3d47ada07a31
Aron Birkir Gudmundsson <aron@idega.com>
2004-07-30 12:08:47 +0000

    usage of deprecated  getID() removed

--- src/is/idega/idegaweb/member/presentation/NewMemberWindow.java
+++ src/is/idega/idegaweb/member/presentation/NewMemberWindow.java
@@ -218,7 +218,7 @@ public class NewMemberWindow extends IWAdminWindow {
 						stat = sHome.findByPrimaryKey(new Integer(sStat));
 					}
 					group.addGroup(user);
-					if (stat != null && (!usb.setUserGroupStatus(user.getID(), ((Integer) group.getPrimaryKey()).intValue(), ((Integer) stat.getPrimaryKey()).intValue()))) {
+					if (stat != null && (!usb.setUserGroupStatus(((Integer)user.getPrimaryKey()).intValue(), ((Integer) group.getPrimaryKey()).intValue(), ((Integer) stat.getPrimaryKey()).intValue()))) {
 						failedInserts.add(user);
 						errorFree = false;
 					}
