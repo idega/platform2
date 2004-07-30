12491aede979251d7ce3a87c6446a5ef16e2c647
Aron Birkir Gudmundsson <aron@idega.com>
2004-07-30 13:59:20 +0000

    *** empty log message ***

---
+++ src/is/idega/idegaweb/campus/business/CampusGroupException.java
@@ -0,0 +1,51 @@
+/*
+ * Created on 30.7.2004
+ *
+ * Copyright (C) 2004 Idega hf. All Rights Reserved.
+ *
+ *  This software is the proprietary information of Idega hf.
+ *  Use is subject to license terms.
+ */
+package is.idega.idegaweb.campus.business;
+
+/**
+ * @author aron
+ *
+ * CampusGroupException TODO Describe this type
+ */
+public class CampusGroupException extends Exception {
+
+	/**
+	 * 
+	 */
+	public CampusGroupException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param arg0
+	 */
+	public CampusGroupException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param arg0
+	 * @param arg1
+	 */
+	public CampusGroupException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param arg0
+	 */
+	public CampusGroupException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+}
