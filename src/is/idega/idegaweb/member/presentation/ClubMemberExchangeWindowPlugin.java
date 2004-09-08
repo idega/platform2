4c80b2db098ac400401bbc35b8719e7fb9302b9d
Thomas Hilbig <thomas@idega.com>
2004-09-08 10:46:37 +0000

    new validation for this plugin

--- src/is/idega/idegaweb/member/presentation/ClubMemberExchangeWindowPlugin.java
+++ src/is/idega/idegaweb/member/presentation/ClubMemberExchangeWindowPlugin.java
@@ -1,12 +1,21 @@
 package is.idega.idegaweb.member.presentation;
 
+import is.idega.idegaweb.member.util.IWMemberConstants;
+import java.rmi.RemoteException;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.Map;
+import com.idega.business.IBOLookup;
+import com.idega.business.IBOLookupException;
 import com.idega.idegaweb.IWBundle;
 import com.idega.idegaweb.IWMainApplicationSettings;
 import com.idega.idegaweb.IWResourceBundle;
 import com.idega.presentation.IWContext;
 import com.idega.presentation.Image;
 import com.idega.user.app.ToolbarElement;
+import com.idega.user.business.UserBusiness;
+import com.idega.user.data.Group;
+import com.idega.user.data.User;
 
 
 /**
@@ -57,7 +66,15 @@ public class ClubMemberExchangeWindowPlugin implements ToolbarElement {
 	 */
 	public boolean isValid(IWContext iwc) {
 		IWMainApplicationSettings settings = iwc.getApplicationSettings();
-		return (settings.getProperty("temp_show_is_related_stuff") != null);
+		if (settings.getProperty("temp_show_is_related_stuff") == null) {
+			return false;
+		}		
+		try {
+			return checkUsersPermission(iwc);
+		}
+		catch (RemoteException e) {
+		      throw new RuntimeException("[ClubMemberExchangeWindowPlugin]: Can't check user's permission");
+		}
 	}
 
 	/* (non-Javadoc)
@@ -73,4 +90,33 @@ public class ClubMemberExchangeWindowPlugin implements ToolbarElement {
 	public boolean isButton(IWContext iwc) {
 		return false;
 	}
+
+
+	private boolean checkUsersPermission(IWContext iwc) throws RemoteException {
+		if (iwc.isSuperAdmin()) {
+			return true;
+		}
+		UserBusiness userBusiness = getUserBusiness(iwc);
+		User user = iwc.getCurrentUser();
+		Collection groups = userBusiness.getUsersTopGroupNodesByViewAndOwnerPermissions(user, iwc);
+		Iterator iterator = groups.iterator();
+		while (iterator.hasNext()) {
+			Group group = (Group) iterator.next();
+			String groupType = group.getGroupType();
+			if (IWMemberConstants.GROUP_TYPE_FEDERATION.equals(groupType) ||
+				IWMemberConstants.GROUP_TYPE_LEAGUE.equals(groupType)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private UserBusiness getUserBusiness(IWContext iwc)	{
+		try {
+			return (UserBusiness) IBOLookup.getServiceInstance(iwc, UserBusiness.class);
+		}
+		catch (IBOLookupException ex)	{
+      throw new RuntimeException("[ClubMemberExchangeWindowPlugin]: Can't retrieve UserBusiness");
+		}
+	}	
 }
