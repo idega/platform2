1e65616f71d17b0dc4a1bf78b637bde3f13e1359
Thorhallur Helgason <laddi@idega.com>
2004-09-14 10:57:32 +0000

    Removed old junk.

--- src/is/idega/idegaweb/golf/templates/GolfMainJSPModule1a.java
+++
@@ -1,7 +0,0 @@
-package is.idega.idegaweb.golf.templates;
-
-
-public class GolfMainJSPModule1a extends GolfMainJSPModule{
-
-
-}  // class GolfMainJSPModule1a
--- src/is/idega/idegaweb/golf/templates/GolfMainJSPModule1b.java
+++
@@ -1,5 +0,0 @@
-package is.idega.idegaweb.golf.templates;
-
-public class GolfMainJSPModule1b extends GolfMainJSPModule3{
-
-}  // class GolfMainJSPModule1a
