cc806c7f1ae839196471af555d840876b18ad8a4
Sigtryggur Simonarson <sigtryggur@idega.com>
2004-09-15 18:45:36 +0000

    Rollback with few changes to make the cashier visible

--- src/is/idega/idegaweb/member/isi/block/accounting/business/AccountingBusinessHome.java
+++ src/is/idega/idegaweb/member/isi/block/accounting/business/AccountingBusinessHome.java
@@ -1,21 +1,8 @@
-/*
- * Created on Sep 10, 2004
- *
- * To change the template for this generated file go to
- * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
- */
 package is.idega.idegaweb.member.isi.block.accounting.business;
 
-import com.idega.business.IBOHome;
 
-/**
- * @author IBM
- *
- * To change the template for this generated type comment go to
- * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
- */
-public interface AccountingBusinessHome extends IBOHome {
-    public AccountingBusiness create() throws javax.ejb.CreateException,
-            java.rmi.RemoteException;
+public interface AccountingBusinessHome extends com.idega.business.IBOHome
+{
+ public AccountingBusiness create() throws javax.ejb.CreateException, java.rmi.RemoteException;
 
 }
--- src/is/idega/idegaweb/member/isi/block/accounting/business/AccountingBusinessHomeImpl.java
+++ src/is/idega/idegaweb/member/isi/block/accounting/business/AccountingBusinessHomeImpl.java
@@ -1,27 +1,17 @@
-/*
- * Created on Sep 10, 2004
- *
- * To change the template for this generated file go to
- * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
- */
 package is.idega.idegaweb.member.isi.block.accounting.business;
 
-import com.idega.business.IBOHomeImpl;
 
-/**
- * @author IBM
- *
- * To change the template for this generated type comment go to
- * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
- */
-public class AccountingBusinessHomeImpl extends IBOHomeImpl implements
-        AccountingBusinessHome {
-    protected Class getBeanInterfaceClass() {
-        return AccountingBusiness.class;
-    }
+public class AccountingBusinessHomeImpl extends com.idega.business.IBOHomeImpl implements AccountingBusinessHome
+{
+ protected Class getBeanInterfaceClass(){
+  return AccountingBusiness.class;
+ }
 
-    public AccountingBusiness create() throws javax.ejb.CreateException {
-        return (AccountingBusiness) super.createIBO();
-    }
+
+ public AccountingBusiness create() throws javax.ejb.CreateException{
+  return (AccountingBusiness) super.createIBO();
+ }
+
+
 
 }
