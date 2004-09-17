3a2d99027c368122f90941c664f9487dd7856b91
Thomas Hilbig <thomas@idega.com>
2004-09-17 15:20:49 +0000

    unused classes

--- src/is/idega/idegaweb/golf/block/file/data/FileCatagory.java
+++
@@ -1,8 +0,0 @@
-package is.idega.idegaweb.golf.block.file.data;
-
-
-public interface FileCatagory extends com.idega.data.IDOLegacyEntity
-{
- public java.lang.String getFileCatagoryName();
- public void initializeAttributes();
-}
