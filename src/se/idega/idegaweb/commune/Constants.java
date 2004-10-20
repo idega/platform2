06dcfc3dc953873de56a0513452a25930540b55a
Aron Birkir Gudmundsson <aron@idega.com>
2004-10-20 14:50:00 +0000

    Global commune constants

---
+++ src/se/idega/idegaweb/commune/Constants.java
@@ -0,0 +1,21 @@
+/*
+ * $Id: Constants.java,v 1.1 2004/10/20 14:50:00 aron Exp $
+ * Created on 15.10.2004
+ *
+ * Copyright (C) 2004 Idega Software hf. All Rights Reserved.
+ *
+ * This software is the proprietary information of Idega hf.
+ * Use is subject to license terms.
+ */
+package se.idega.idegaweb.commune;
+
+/**
+ * 
+ *  Last modified: $Date: 2004/10/20 14:50:00 $ by $Author: aron $
+ * 
+ * @author <a href="mailto:aron@idega.com">aron</a>
+ * @version $Revision: 1.1 $
+ */
+public class Constants {
+    public final static String IW_BUNDLE_IDENTIFIER = "se.idega.idegaweb.commune";
+}
