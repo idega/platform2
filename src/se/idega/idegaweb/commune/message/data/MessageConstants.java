42887e451fcf984a966127bbd1575f275a776bba
Aron Birkir Gudmundsson <aron@idega.com>
2004-10-20 15:33:19 +0000

    *** empty log message ***

---
+++ src/se/idega/idegaweb/commune/message/data/MessageConstants.java
@@ -0,0 +1,24 @@
+/*
+ * $Id: MessageConstants.java,v 1.1 2004/10/20 15:33:19 aron Exp $
+ * Created on 15.10.2004
+ *
+ * Copyright (C) 2004 Idega Software hf. All Rights Reserved.
+ *
+ * This software is the proprietary information of Idega hf.
+ * Use is subject to license terms.
+ */
+package se.idega.idegaweb.commune.message.data;
+
+/**
+ * 
+ *  Last modified: $Date: 2004/10/20 15:33:19 $ by $Author: aron $
+ * 
+ * @author <a href="mailto:aron@idega.com">aron</a>
+ * @version $Revision: 1.1 $
+ */
+public class MessageConstants {
+
+    public static final String LETTER_TYPE_DEFAULT="DEFA";
+    public static final String LETTER_TYPE_PASSWORD="PASS";
+
+}
