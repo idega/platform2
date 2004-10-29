56c1125f29efd897dcccf99febc41f6b17eb88ef
Thomas Hilbig <thomas@idega.com>
2004-10-29 10:22:01 +0000

    new Constants class added

--- src/is/idega/idegaweb/golf/util/GolfConstants.java
+++ src/is/idega/idegaweb/golf/util/GolfConstants.java
@@ -1,5 +1,5 @@
 /*
- * $Id: GolfConstants.java,v 1.2 2004/10/28 12:03:53 eiki Exp $
+ * $Id: GolfConstants.java,v 1.3 2004/10/29 10:22:01 thomas Exp $
  * Created on Sep 17, 2004
  *
  * Copyright (C) 2004 Idega Software hf. All Rights Reserved.
@@ -10,14 +10,17 @@
 package is.idega.idegaweb.golf.util;
 
 import com.idega.user.data.GroupTypeConstants;
+import com.idega.user.data.MetadataConstants;
 
 
 /**
  * 
- *  Last modified: $Date: 2004/10/28 12:03:53 $ by $Author: eiki $
+ *  Last modified: $Date: 2004/10/29 10:22:01 $ by $Author: thomas $
  * 
  * @author <a href="mailto:thomas@idega.com">thomas</a>
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
+ * 
+ * 
  */
 public interface GolfConstants {
 	
@@ -29,9 +32,9 @@ public interface GolfConstants {
 	
 	public static final String UNION_TYPE_NONE = "none";
 	
-	public static final String META_PREFIX = "golf_";
-	public static final String SUB_CLUBS_META_DATA_KEY = META_PREFIX+"sub_clubs";
-	public static final String MAIN_CLUB_META_DATA_KEY = META_PREFIX+"main_club";
-	public static final String HANDICAP_META_DATA_KEY = META_PREFIX+"handicap";
+	public static final String META_PREFIX = MetadataConstants.GOLF_META_PREFIX;
+	public static final String SUB_CLUBS_META_DATA_KEY = MetadataConstants.SUB_CLUBS_GOLF_META_DATA_KEY;
+	public static final String MAIN_CLUB_META_DATA_KEY = MetadataConstants.MAIN_CLUB_GOLF_META_DATA_KEY;
+	public static final String HANDICAP_META_DATA_KEY = MetadataConstants.HANDICAP_GOLF_META_DATA_KEY;
 
 }
