9d7c5845146cdc8e8b4237aaccca6e87c5cf4c37
Thomas Hilbig <thomas@idega.com>
2004-11-03 16:21:33 +0000

    *** empty log message ***

---
+++ src/com/idega/manager/bean/InstallUpdateBean.java
@@ -0,0 +1,21 @@
+/*
+ * $Id: InstallUpdateBean.java,v 1.1 2004/11/03 16:21:33 thomas Exp $
+ * Created on Nov 3, 2004
+ *
+ * Copyright (C) 2004 Idega Software hf. All Rights Reserved.
+ *
+ * This software is the proprietary information of Idega hf.
+ * Use is subject to license terms.
+ */
+package com.idega.manager.bean;
+
+
+/**
+ * 
+ *  Last modified: $Date: 2004/11/03 16:21:33 $ by $Author: thomas $
+ * 
+ * @author <a href="mailto:thomas@idega.com">thomas</a>
+ * @version $Revision: 1.1 $
+ */
+public class InstallUpdateBean {
+}
