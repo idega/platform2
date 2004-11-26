14775a285768a7c0381cea527391ab45217f160a
Aron Birkir Gudmundsson <aron@idega.com>
2004-11-26 09:10:30 +0000

    Moving placement helpers to care bundle

---
+++ src/se/idega/idegaweb/commune/care/business/PlacementHelper.java
@@ -0,0 +1,51 @@
+/*
+ * $Id: PlacementHelper.java,v 1.1 2004/11/26 09:10:30 aron Exp $
+ * Created on 5.10.2004
+ *
+ * Copyright (C) 2004 Idega Software hf. All Rights Reserved.
+ *
+ * This software is the proprietary information of Idega hf.
+ * Use is subject to license terms.
+ */
+package se.idega.idegaweb.commune.care.business;
+
+import java.util.Date;
+
+import se.idega.idegaweb.commune.care.data.ChildCareApplication;
+import se.idega.idegaweb.commune.care.data.ChildCareContract;
+
+import com.idega.idegaweb.IWResourceMessage;
+import com.idega.util.TimePeriod;
+
+/**
+ * 
+ *  Last modified: $Date: 2004/11/26 09:10:30 $ by $Author: aron $
+ * 
+ * @author <a href="mailto:aron@idega.com">aron</a>
+ * @version $Revision: 1.1 $
+ */
+public interface PlacementHelper {
+
+    public void setApplication(ChildCareApplication app);
+    public ChildCareApplication getApplication();
+    public ChildCareContract getContract();
+    public void setContract(ChildCareContract contract);
+    public boolean hasEarliestPlacementDate();
+    	public Date getEarliestPlacementDate();
+    	public boolean hasLatestPlacementDate();
+    	public Date getLatestPlacementDate();
+    	public Integer getMaximumCareTimeHours();
+    	public Integer getCurrentCareTimeHours();
+    	public Integer getCurrentClassID();
+    	public Integer getCurrentSchoolTypeID();
+    	public Integer getCurrentProviderID();
+    	public Integer getCurrentEmploymentID();
+    	public IWResourceMessage getEarliestPlacementMessage();
+    	public IWResourceMessage getLatestPlacementMessage();
+    	
+    	public boolean hasDeadlinePassed();
+    	public TimePeriod getValidPeriod();
+    	
+    	public IWResourceMessage getMessageWhenDeadlinePassed();
+    	
+}
