59c0a3daeb28d94ca9ee41040363a1529612f0b0
Thorhallur Helgason <laddi@idega.com>
2004-12-05 09:04:57 +0000

    Removed unused imports.

--- src/se/idega/idegaweb/commune/care/data/CareTime.java
+++ src/se/idega/idegaweb/commune/care/data/CareTime.java
@@ -1,5 +1,5 @@
 /*
- * $Id: CareTime.java,v 1.1 2004/12/02 12:39:08 laddi Exp $
+ * $Id: CareTime.java,v 1.2 2004/12/05 09:05:33 laddi Exp $
  * Created on 1.12.2004
  *
  * Copyright (C) 2004 Idega Software hf. All Rights Reserved.
@@ -9,23 +9,14 @@
  */
 package se.idega.idegaweb.commune.care.data;
 
-import java.util.Collection;
-
-import javax.ejb.FinderException;
-
-import com.idega.data.GenericEntity;
 import com.idega.data.IDOEntity;
-import com.idega.data.IDOLookup;
-import com.idega.data.query.SelectQuery;
-import com.idega.data.query.Table;
-import com.idega.data.query.WildCardColumn;
 
 
 /**
- * Last modified: $Date: 2004/12/02 12:39:08 $ by $Author: laddi $
+ * Last modified: $Date: 2004/12/05 09:05:33 $ by $Author: laddi $
  * 
  * @author <a href="mailto:laddi@idega.com">laddi</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public interface CareTime extends IDOEntity {
 
