b4112a6df2b05ab2daa5586860d6c723ec5a2748
Malin Anulf <malin.anulf@agurait.com>
2004-12-28 14:45:11 +0000

    merger of hantera kontrakt och se placerade barn.

--- src/se/idega/idegaweb/commune/childcare/presentation/ChildCareContracts.java
+++ src/se/idega/idegaweb/commune/childcare/presentation/ChildCareContracts.java
@@ -9,13 +9,17 @@ import java.util.Iterator;
 
 import se.idega.idegaweb.commune.care.data.ChildCareApplication;
 import se.idega.idegaweb.commune.care.data.ChildCareContract;
+import se.idega.idegaweb.commune.childcare.business.ChildCareGroupWriter;
 import se.idega.idegaweb.commune.childcare.event.ChildCareEventListener;
 
 import com.idega.block.school.data.SchoolClassMember;
 import com.idega.presentation.IWContext;
 import com.idega.presentation.Table;
+import com.idega.presentation.text.DownloadLink;
 import com.idega.presentation.text.Link;
 import com.idega.presentation.text.Text;
+import com.idega.presentation.ui.DropdownMenu;
+import com.idega.presentation.ui.Form;
 import com.idega.user.data.User;
 import com.idega.util.IWTimestamp;
 import com.idega.util.PersonalIDFormatter;
@@ -31,24 +35,50 @@ public class ChildCareContracts extends ChildCareBlock {
 	private int allowedFutureContracts = 2;
 	//private boolean showCreateGroupBtn = true;
 	
+	protected static final int STATUS_ACTIVE = 1;
+	protected static final int STATUS_NOT_YET_ACTIVE = 2;
+
+	protected static final String PARAMETER_CHILD_ID = "cc_remove_child_id";
+	protected static final String PARAMETER_STATUS_SORT = "cc_status_sort";
+	
+	private int sort = -1;
+	private boolean showNotYetActive = false;
+	
+	
 	/**
 	 * @see se.idega.idegaweb.commune.childcare.presentation.ChildCareBlock#init(com.idega.presentation.IWContext)
 	 */
 	public void init(IWContext iwc) throws Exception {
+		if (iwc.isParameterSet(PARAMETER_STATUS_SORT))
+			sort = Integer.parseInt(iwc.getParameter(PARAMETER_STATUS_SORT));
+		
+		if (sort != -1) {
+			switch (sort) {
+			case STATUS_ACTIVE :
+				showNotYetActive = false;
+				break;
+			case STATUS_NOT_YET_ACTIVE :
+				showNotYetActive = true;
+				break;
+			}
+		}
 		if (canSeeContracts(iwc)) {
 			Table table = new Table();
 			table.setWidth(getWidth());
 			table.setCellpadding(getCellpadding());
 			table.setCellspacing(getCellspacing());
+			
 			if (allowAlter)
 				table.setColumns(9);
 			else
 				table.setColumns(8);
+			
+			
 			if (useStyleNames()) {
-				table.setRowStyleClass(1, getHeaderRowClass());
+				table.setRowStyleClass(3, getHeaderRowClass());
 			}
 			else {
-				table.setRowColor(1, getHeaderColor());
+				table.setRowColor(3, getHeaderColor());
 			}
 			int column = 1;
 			int row = 1;
@@ -58,13 +88,17 @@ public class ChildCareContracts extends ChildCareBlock {
 				table.setCellpaddingLeft(1, row, 12);
 				table.setCellpaddingRight(table.getColumns(), row, 12);
 			}
+			table.mergeCells(column, row, table.getColumns(), row);
+			table.add(getNavigationTable(), column, row++);
+			table.setHeight(column, row++, "5");
 			table.add(getLocalizedSmallHeader("child_care.name","Name"), column++, row);
 			table.add(getLocalizedSmallHeader("child_care.personal_id","Personal ID"), column++, row);
 			table.add(getLocalizedSmallHeader("child_care.created","Created"), column++, row);
 			table.add(getLocalizedSmallHeader("child_care.valid_from","Valid from"), column++, row);
 			table.add(getLocalizedSmallHeader("child_care.status","Status"), column++, row);
 			table.add(getLocalizedSmallHeader("child_care.care_time","Care time"), column++, row);
-						
+			
+				
 			if(isCommuneAdministrator(iwc))
 				table.add(getLocalizedSmallHeader("child_care.invoice_receiver","Invoice Receiver"), column++, row);
 			
@@ -74,7 +108,12 @@ public class ChildCareContracts extends ChildCareBlock {
 			boolean showNotActiveComment = false;
 			boolean showRemovedComment = false;
 
-			Collection contracts = getBusiness().getAcceptedApplicationsByProvider(getSession().getChildCareID());
+			IWTimestamp stampNow = new IWTimestamp();
+			Collection contracts = null;
+			
+			//contracts = getBusiness().getAcceptedApplicationsByProvider(getSession().getChildCareID());
+			contracts = getBusiness().getAcceptedApplicationsByProvider(getSession().getChildCareID(), stampNow.getDate(), showNotYetActive, getSession().getGroupID(), sort);
+			
 			if (contracts != null) {
 				ChildCareApplication application;
 				ChildCareContract contract;
@@ -96,16 +135,15 @@ public class ChildCareContracts extends ChildCareBlock {
 				//boolean hasComments = false;
 				IWTimestamp stamp = new IWTimestamp();
 				SchoolClassMember student;
-				//Collection students;
-				
-				Iterator iter = contracts.iterator();
+							
+				Iterator iter = contracts.iterator();								
 				while (iter.hasNext()) {
 					column = 1;
 					application = (ChildCareApplication) iter.next();
 					contract = getBusiness().getValidContract(((Integer)application.getPrimaryKey()).intValue());
 					child = application.getChild();
 					//provider = application.getProviderId();
-					
+
 					hasComment = true;
 
 					if (useStyleNames()) {
@@ -129,6 +167,7 @@ public class ChildCareContracts extends ChildCareBlock {
 						student = contract.getSchoolClassMember();
 						
 						created = new IWTimestamp(contract.getCreatedDate());
+						student = contract.getSchoolClassMember();
 						if (contract.getValidFromDate() != null)
 							validFrom = new IWTimestamp(contract.getValidFromDate());
 						else
@@ -258,8 +297,8 @@ public class ChildCareContracts extends ChildCareBlock {
 								table.add(getInformationIcon(localize("child_care.to_many_future_contracts","To many future contracts")),column,row);
 						}
 						
-						//if (student.getRemovedDate() == null)
-						//	table.add(delete, column++, row);
+						if (student.getRemovedDate() == null)
+							table.add(delete, column++, row);
 						
 						
 						row++;
@@ -322,6 +361,86 @@ public class ChildCareContracts extends ChildCareBlock {
 		else {
 			add(getSmallErrorText(localize("child_care.prognosis_must_be_set","Prognosis must be set or updated before you can continue!")));
 		}
+	}
+	
+	protected Form getNavigationTable() throws RemoteException {
+		Form form = new Form();
+		form.setEventListener(ChildCareEventListener.class);
+		
+		Table table = new Table(10,1);
+		table.setWidth(Table.HUNDRED_PERCENT);
+		table.setCellpadding(0);
+		table.setCellspacing(0);
+		table.setWidth(2, 2);
+		table.setWidth(4, 16);
+		table.setWidth(6, 2);
+		table.setWidth(6, 2);
+		table.setWidth(9, Table.HUNDRED_PERCENT);
+		table.setNoWrap(10, 1);
+		form.add(table);
+
+		table.add(getSmallHeader(localize("child_care.group","Group")+":"),1,1);
+		table.add(getChildCareGroups(), 3, 1);
+		
+		DropdownMenu statusSort = (DropdownMenu) getStyledInterface(new DropdownMenu(PARAMETER_STATUS_SORT));
+		statusSort.addMenuElement("-1", localize("child_care.show_all_statuses","Show all"));
+		statusSort.addMenuElement(STATUS_ACTIVE, localize("child_care.show_active","Show active"));
+		statusSort.addMenuElement(STATUS_NOT_YET_ACTIVE, localize("child_care.show_not_yet_active","Show not yet active"));
+		statusSort.setSelectedElement(sort);
+		statusSort.setToSubmit();
+		
+		table.add(getSmallHeader(localize("child_care.status_sort","Sorting")+":"), 5, 1);
+		table.add(statusSort, 7, 1);
+		
+		boolean hasShowNotYetActive = false;
+		if (sort != -1)
+			hasShowNotYetActive = true;
+		
+		table.add(getPDFLink(showNotYetActive, hasShowNotYetActive), 10, 1);
+		table.add(Text.getNonBrakingSpace(), 10, 1);
+		table.add(getXSLLink(showNotYetActive, hasShowNotYetActive), 10, 1);
+
+		return form;
+	}
+	
+	
+	protected DropdownMenu getChildCareGroups() throws RemoteException {
+		DropdownMenu menu = getGroups(getSession().getGroupID(), -1);
+		menu.addMenuElementFirst("-1", localize("child_care.show_all_groups","Show all"));
+		menu.setToSubmit();
+		
+		return menu;	
+	}
+	
+	protected Link getPDFLink(boolean showNotYetActive, boolean hasShowNotYetActive) throws RemoteException {
+		
+		//Link link = new Link(getBundle().getImage("shared/pdf.gif"),iwc.getIWMainApplication().getMediaServletURI());
+		//link.setWindow(getFileWindow());
+		DownloadLink link = new DownloadLink(getBundle().getImage("shared/pdf.gif"));
+		link.addParameter(ChildCareGroupWriter.PARAMETER_TYPE, ChildCareGroupWriter.PDF);
+		link.setMediaWriterClass(ChildCareGroupWriter.class);
+		//link.addParameter(MediaWritable.PRM_WRITABLE_CLASS, IWMainApplication.getEncryptedClassName(ChildCareGroupWriter.class));
+		link.addParameter(ChildCareGroupWriter.PARAMETER_PROVIDER_ID, getSession().getChildCareID());
+		link.addParameter(ChildCareGroupWriter.PARAMETER_GROUP_ID, getSession().getGroupID());
+		if (hasShowNotYetActive)
+			link.addParameter(ChildCareGroupWriter.PARAMETER_SHOW_NOT_YET_ACTIVE, String.valueOf(showNotYetActive));
+		
+		return link;
+	}
+	
+	protected Link getXSLLink(boolean showNotYetActive, boolean hasShowNotYetActive) throws RemoteException {
+		DownloadLink link = new DownloadLink(getBundle().getImage("shared/xls.gif"));
+		link.setMediaWriterClass(ChildCareGroupWriter.class);
+		//Link link = new Link(getBundle().getImage("shared/xls.gif"),iwc.getIWMainApplication().getMediaServletURI());
+		//link.setWindow(getFileWindow());
+		link.addParameter(ChildCareGroupWriter.PARAMETER_TYPE, ChildCareGroupWriter.XLS);
+		//link.addParameter(MediaWritable.PRM_WRITABLE_CLASS, IWMainApplication.getEncryptedClassName(ChildCareGroupWriter.class));
+		link.addParameter(ChildCareGroupWriter.PARAMETER_PROVIDER_ID, getSession().getChildCareID());
+		link.addParameter(ChildCareGroupWriter.PARAMETER_GROUP_ID, getSession().getGroupID());
+		if (hasShowNotYetActive)
+			link.addParameter(ChildCareGroupWriter.PARAMETER_SHOW_NOT_YET_ACTIVE, String.valueOf(showNotYetActive));
+		
+		return link;
 	}
 	
 	protected boolean canSeeContracts(IWContext iwc) {
