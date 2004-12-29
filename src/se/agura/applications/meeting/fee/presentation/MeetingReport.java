df4391f1edc93f97e35ac3777ad39d40c68c8463
Thorhallur Helgason <laddi@idega.com>
2004-12-29 15:53:11 +0000

    Fixed bugs with not being sent to next phases properly.Changed so that meeting reports can not be filled ahead in time.

--- src/se/agura/applications/meeting/fee/presentation/MeetingReport.java
+++ src/se/agura/applications/meeting/fee/presentation/MeetingReport.java
@@ -1,5 +1,5 @@
 /*
- * $Id: MeetingReport.java,v 1.10 2004/12/21 14:01:57 laddi Exp $ Created on
+ * $Id: MeetingReport.java,v 1.11 2004/12/29 15:53:11 laddi Exp $ Created on
  * 24.11.2004
  * 
  * Copyright (C) 2004 Idega Software hf. All Rights Reserved.
@@ -14,6 +14,7 @@ import java.util.Collection;
 import java.util.Iterator;
 
 import javax.ejb.CreateException;
+import javax.ejb.FinderException;
 
 import se.agura.applications.meeting.fee.data.MeetingFeeFormula;
 
@@ -38,7 +39,7 @@ import com.idega.util.PersonalIDFormatter;
  * Last modified: 24.11.2004 13:46:01 by: anna
  * 
  * @author <a href="mailto:anna@idega.com">anna </a>
- * @version $Revision: 1.10 $
+ * @version $Revision: 1.11 $
  */
 public class MeetingReport extends MeetingFeeBlock {
 
@@ -57,7 +58,7 @@ public class MeetingReport extends MeetingFeeBlock {
 		if (action.equals(ACTION_NEXT)) {
 			add(formPageTwo(iwc));
 		}
-		if(action.equals(ACTION_SHOW_REPORT)) {
+		else if(action.equals(ACTION_SHOW_REPORT)) {
 			add(formPageThree(iwc));
 		}
 		else if (action.equals(ACTION_SAVE)) {
@@ -137,7 +138,6 @@ public class MeetingReport extends MeetingFeeBlock {
 			log(re);
 		}
 		form.add(new Break());
-		form.add(form);
 		form.add(getBackButton());
 		form.add(getSaveButton());
 		
@@ -161,8 +161,11 @@ public class MeetingReport extends MeetingFeeBlock {
 		RadioButton meetingPlaceIn = (RadioButton) getRadioButton(new RadioButton(PARAMETER_MEETING_FEE_MEETING_LOCATION, Boolean.TRUE.toString()));
 		meetingPlaceIn.setMustBeSelected(getResourceBundle().getLocalizedString("meeting.fee.place_not_empty", "Meeting place can not be empty"));
 		RadioButton meetingPlaceOut = (RadioButton) getRadioButton(new RadioButton(PARAMETER_MEETING_FEE_MEETING_LOCATION, Boolean.FALSE.toString()));
+		
 		IWTimestamp stamp = new IWTimestamp();
 		DateInput meetingDateInput = (DateInput) getInput(new DateInput(PARAMETER_MEETING_FEE_DATE));
+		meetingDateInput.setYearRange(stamp.getYear(), stamp.getYear() - 5);
+		
 		DropdownMenu participantsMenu = getParticipantsMenu(iwc, iwc.getCurrentUser());
 		TextArea comment = new TextArea(PARAMETER_MEETING_FEE_COMMENT);
 		comment.setWidth(Table.HUNDRED_PERCENT);
@@ -284,65 +287,86 @@ public class MeetingReport extends MeetingFeeBlock {
 		table.setWidth(iWidth);
 		table.setCellpadding(iCellpadding);
 		table.setCellspacing(0);
+		table.setColumns(6);
 		int row = 1;
 	
-		String concregation = iwc.getParameter(PARAMETER_MEETING_FEE_CONGREGATION);
+		String congregation = null;
+		try {
+			congregation = getUserBusiness(iwc).getGroupBusiness().getGroupByGroupID(Integer.parseInt(iwc.getParameter(PARAMETER_MEETING_FEE_CONGREGATION))).getName();
+		}
+		catch (RemoteException re) {
+			log(re);
+		}
+		catch (FinderException fe) {
+			log(fe);
+		}
+		catch (NumberFormatException nfe) {
+			log(nfe);
+		}
+		
 		String userName = iwc.getCurrentUser().getName();
-		String location = iwc.getParameter(PARAMETER_MEETING_FEE_MEETING_LOCATION);
+
+		boolean inCommune = new Boolean(iwc.getParameter(PARAMETER_MEETING_FEE_MEETING_LOCATION)).booleanValue();
+		String location = null;
+		if (inCommune) {
+			location = getResourceBundle().getLocalizedString("meeting.fee.in_commune", "In commune");
+		}
+		else {
+			location = getResourceBundle().getLocalizedString("meeting.fee.outside_commune", "Outside commune");
+		}
+		
 		IWTimestamp meetingDate = new IWTimestamp(iwc.getParameter(PARAMETER_MEETING_FEE_DATE));
 		String comment = iwc.getParameter(PARAMETER_MEETING_FEE_COMMENT);
 		
+		table.mergeCells(2, row, table.getColumns(), row);
 		table.add(getHeader(getResourceBundle().getLocalizedString("meeting.fee.assignment_from","Assignment from")),1,row);
-		table.add(getText(concregation), 2, row++);
+		table.add(getText(congregation), 2, row++);
 		table.setHeight(row++, 12);
 		
+		table.mergeCells(2, row, table.getColumns(), row);
 		table.add(getHeader(getResourceBundle().getLocalizedString("meeting.fee.speaker","Speaker")),1,row);
 		table.add(getText(userName), 2, row++); 
 		table.setHeight(row++, 12);
 		
+		table.mergeCells(2, row, table.getColumns(), row);
 		table.add(getHeader(getResourceBundle().getLocalizedString("meeting.fee.meeting_location","Meeting location")), 1, row);
 		table.add(getText(location), 2, row++);
 		table.setHeight(row++, 12);
 		
+		table.mergeCells(2, row, table.getColumns(), row);
 		table.add(getHeader(getResourceBundle().getLocalizedString("meeting.fee.meeting_date","Meeting date")), 1, row);
 		table.add(getText(meetingDate.getLocaleDate(iwc.getCurrentLocale())), 2, row++); 
 		table.setHeight(row++, 12);
 		
+		table.setVerticalAlignment(1, row, Table.VERTICAL_ALIGN_TOP);
+		table.mergeCells(2, row, table.getColumns(), row);
 		table.add(getHeader(getResourceBundle().getLocalizedString("meeting.fee.comment","Comment")), 1, row);
 		table.add(getText(comment), 2, row++);
-		table.setHeight(row++, 12);
+		table.setHeight(row++, 18);
 		
-		table.add(getHeader(getResourceBundle().getLocalizedString("meeting.fee.participants","Participants")),1,row++);
-		table.setHeight(row++, 3);
+		table.add(getHeader(getResourceBundle().getLocalizedString("meeting.fee.participants","Participants")),1,row);
 		
-		table.add(getHeader(getResourceBundle().getLocalizedString("meeting.fee.participants.last_name","Last name")),1, row);
-		table.add(getHeader(getResourceBundle().getLocalizedString("meeting.fee.participants.first_name","First name")),2,row);
+		table.add(getHeader(getResourceBundle().getLocalizedString("meeting.fee.participants.full_name","Name")),2, row);
 		table.add(getHeader(getResourceBundle().getLocalizedString("meeting.fee.participants.personal_number","Personal number")),3, row);
 		table.add(getHeader(getResourceBundle().getLocalizedString("meeting.fee.participants.hours","Hours")), 4,row);
 		table.add(getHeader(getResourceBundle().getLocalizedString("meeting.fee.participants.minutes","Minutes")), 5, row);
 		table.add(getHeader(getResourceBundle().getLocalizedString("meeting.fee.participants.sum","Sum")), 6, row++);
-		
-		String[] participants = iwc.getParameterValues(PARAMETER_PARTICIPANT_USER_ID);
-		String[] hours = new String[participants.length];
-		String[] minutes = new String[participants.length];
 		
 		int groupID = Integer.parseInt(iwc.getParameter(PARAMETER_MEETING_FEE_PARTICIPANTS));
 		Collection users = getUserBusiness(iwc).getUsersInGroup(groupID);
 		Iterator iter = users.iterator();		
 		
 		while (iter.hasNext()) {
-			int i = 0;
-		
 			User user = (User) iter.next();
-			String lastName = user.getLastName();
-			String firstName = user.getFirstName();
+			String name = user.getName();
 			String pId = PersonalIDFormatter.format(user.getPersonalID(), iwc.getCurrentLocale());
-			String hour = hours[i];
-			String minute = minutes[i];
+			String hour = iwc.getParameter(PARAMETER_HOURS + "_" + user.getPrimaryKey());
+			String minute = iwc.getParameter(PARAMETER_MINUTES + "_" + user.getPrimaryKey());
 			
 			table.add(new HiddenInput(PARAMETER_PARTICIPANT_USER_ID, user.getPrimaryKey().toString()), 1, row);
-			table.add(getText(lastName), 1, row);
-			table.add(getText(firstName), 2, row);
+			table.add(new HiddenInput(PARAMETER_HOURS + "_" + user.getPrimaryKey(), hour), 1, row);
+			table.add(new HiddenInput(PARAMETER_MINUTES + "_" + user.getPrimaryKey(), minute), 1, row);
+			table.add(getText(name), 2, row);
 			table.add(getText(pId), 3, row);
 			table.add(hour, 4, row);
 			table.add(minute, 5, row);
@@ -351,12 +375,12 @@ public class MeetingReport extends MeetingFeeBlock {
 		}
 
 		table.setCellpaddingLeft(1, 0);
-		
+
 		return table;
 	}
 	
 	private SubmitButton getShowReportButton() {
-		SubmitButton showReportButton = (SubmitButton) getButton(new SubmitButton(getResourceBundle().getLocalizedString("meeting.fee.show_report", "Next"), PARAMETER_ACTION, ACTION_NEXT));
+		SubmitButton showReportButton = (SubmitButton) getButton(new SubmitButton(getResourceBundle().getLocalizedString("meeting.fee.show_report", "Next"), PARAMETER_ACTION, ACTION_SHOW_REPORT));
 		showReportButton.setToolTip(getResourceBundle().getLocalizedString("meeting.fee.show_report.tooltip","shows your report"));
 		return showReportButton;
 	}
