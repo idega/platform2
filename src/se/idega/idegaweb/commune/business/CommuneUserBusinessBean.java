b01e8ac42464c1d5e64db09aa31b22677baa727f
Pall Helgason <palli@idega.com>
2005-01-02 22:17:41 +0000

    Trying to fix the import stuff in taby

--- src/se/idega/idegaweb/commune/business/CommuneUserBusinessBean.java
+++ src/se/idega/idegaweb/commune/business/CommuneUserBusinessBean.java
@@ -151,17 +151,21 @@ public class CommuneUserBusinessBean extends UserBusinessBean implements Commune
 			final UserHome home = getUserHome();
 			user = home.findByPersonalID(personalID);
 			//update if found
-			firstName = (firstName == null) ? "" : firstName;
-			middleName = (middleName == null) ? "" : middleName;
-			lastName = (lastName == null) ? "" : lastName;
-			
-			StringBuffer fullName = new StringBuffer();
-			firstName = (firstName == null) ? "" : firstName;
-			middleName = (middleName == null) ? "" : middleName;
-			lastName = (lastName == null) ? "" : lastName;
-			fullName.append(firstName).append(" ").append(middleName).append(" ").append(lastName);
-			if (fullName.toString().trim().length() > 0) {
-				user.setFullName(fullName.toString());
+			if ((firstName != null && !firstName.trim().equals("")) || 
+			    (middleName != null && !middleName.trim().equals("")) || 
+			    (lastName != null && !lastName.trim().equals(""))) {
+				firstName = (firstName == null) ? "" : firstName;
+				middleName = (middleName == null) ? "" : middleName;
+				lastName = (lastName == null) ? "" : lastName;
+				
+				StringBuffer fullName = new StringBuffer();
+				firstName = (firstName == null) ? "" : firstName;
+				middleName = (middleName == null) ? "" : middleName;
+				lastName = (lastName == null) ? "" : lastName;
+				fullName.append(firstName).append(" ").append(middleName).append(" ").append(lastName);
+				if (fullName.toString().trim().length() > 0) {
+					user.setFullName(fullName.toString());
+				}
 			}
 			
 			if (gender != null)
