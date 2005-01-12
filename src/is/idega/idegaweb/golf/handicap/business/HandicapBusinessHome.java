3b57c166afcdde31aa060f6b3b1d72de0d372d56
Thorhallur Helgason <laddi@idega.com>
2005-01-12 11:39:58 +0000

    Removed new golf classes.

--- src/is/idega/idegaweb/golf/handicap/business/HandicapBusinessHome.java
+++
@@ -1,8 +0,0 @@
-package is.idega.idegaweb.golf.handicap.business;
-
-
-public interface HandicapBusinessHome extends com.idega.business.IBOHome
-{
- public HandicapBusiness create() throws javax.ejb.CreateException, java.rmi.RemoteException;
-
-}
