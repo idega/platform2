de6b3383ab830e8b2e26204c6e8a2aa2c19b0e9b
Thorhallur Helgason <laddi@idega.com>
2005-01-17 10:20:26 +0000

    Moved current season from bean to property.

--- src/se/idega/idegaweb/commune/care/business/CareConstants.java
+++ src/se/idega/idegaweb/commune/care/business/CareConstants.java
@@ -1,5 +1,5 @@
 /*
- * $Id: CareConstants.java,v 1.2 2004/10/20 15:13:36 thomas Exp $
+ * $Id: CareConstants.java,v 1.3 2005/01/17 10:20:27 laddi Exp $
  * Created on Oct 19, 2004
  *
  * Copyright (C) 2004 Idega Software hf. All Rights Reserved.
@@ -12,14 +12,16 @@ package se.idega.idegaweb.commune.care.business;
 
 /**
  * 
- *  Last modified: $Date: 2004/10/20 15:13:36 $ by $Author: thomas $
+ *  Last modified: $Date: 2005/01/17 10:20:27 $ by $Author: laddi $
  * 
  * @author <a href="mailto:thomas@idega.com">thomas</a>
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
 public class CareConstants {
 	
 	public final static String CASE_CODE_KEY = "MBANBOP";
 	
 	public final static String AFTER_SCHOOL_CASE_CODE_KEY = "MBFRITV";
+	
+	public final static String PROPERTY_CURRENT_SEASON = "current_school_season";
 }
