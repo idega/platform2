bc83d035f95a9224e3964320d2988cdccea59ebb
Malin Anulf <malin.anulf@agurait.com>
2005-02-07 21:12:13 +0000

    window to open statistics on not updated queues

---
+++ src/se/idega/idegaweb/commune/childcare/presentation/ChildCarePrognosisBeforeUpdateWindow.java
@@ -0,0 +1,27 @@
+/*
+ * Created on 28.5.2003
+ */
+package se.idega.idegaweb.commune.childcare.presentation;
+
+import com.idega.presentation.IWContext;
+import com.idega.presentation.ui.Window;
+
+/**
+ * @author laddi
+ */
+public class ChildCarePrognosisBeforeUpdateWindow extends Window {
+
+	public ChildCarePrognosisBeforeUpdateWindow() {
+		this.setWidth(680);
+		this.setHeight(450);
+		this.setScrollbar(true);
+		this.setResizable(true);	
+	}
+
+	/**
+	 * @see com.idega.presentation.PresentationObject#main(IWContext)
+	 */
+	public void main(IWContext iwc) throws Exception {
+		add(new ChildCarePrognosisStatisticsBeforeUpdate());
+	}
+}
