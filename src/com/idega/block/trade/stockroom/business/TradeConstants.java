b57735f7a0219cdb30028c050b527b69dfdfe3e3
Grimur Jonsson <gimmi@idega.com>
2005-05-31 19:20:52 +0000

    constants for the trade bundle

---
+++ src/com/idega/block/trade/stockroom/business/TradeConstants.java
@@ -0,0 +1,27 @@
+/*
+ * $Id: TradeConstants.java,v 1.1 2005/05/31 19:20:52 gimmi Exp $
+ * Created on 9.2.2005
+ *
+ * Copyright (C) 2005 Idega Software hf. All Rights Reserved.
+ *
+ * This software is the proprietary information of Idega hf.
+ * Use is subject to license terms.
+ */
+package com.idega.block.trade.stockroom.business;
+
+
+/**
+ * 
+ *  Last modified: $Date: 2005/05/31 19:20:52 $ by $Author: gimmi $
+ * 
+ * @author <a href="mailto:gimmi@idega.com">gimmi</a>
+ * @version $Revision: 1.1 $
+ */
+public class TradeConstants {
+	
+	public static String SUPPLIER_MANAGER_ROLE_KEY = "supp_man_edit_role";
+//	public static String ROLE_CREATE_EDIT_PRODUCTS = "supp_man_role_create_edit_products";
+//	public static String ROLE_EDIT_SUPPLIES = "supp_man_role_edit_supplies";
+//	public static String ROLE_TRAVEL_AGENCY = "supp_man_role_agency";
+	public static String ROLE_BOOKING_BASKET = "supp_man_role_book_basket";
+}
