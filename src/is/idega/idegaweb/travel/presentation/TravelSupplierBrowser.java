5f862f5be6cc5ac710e2018c03ce959ccc61ba5b
Grimur Jonsson <gimmi@idega.com>
2005-07-05 22:56:36 +0000

    abstract class to get the plugin name ...

--- src/is/idega/idegaweb/travel/presentation/TravelSupplierBrowser.java
+++ src/is/idega/idegaweb/travel/presentation/TravelSupplierBrowser.java
@@ -1,24 +1,30 @@
 package is.idega.idegaweb.travel.presentation;
 
 import com.idega.presentation.IWContext;
-import com.idega.presentation.Table;
 import com.idega.presentation.text.Text;
 
-public class TravelSupplierBrowser extends TravelManager {
+public abstract class TravelSupplierBrowser extends TravelManager {
 
-		public void main(IWContext iwc) throws Exception {
-			super.main(iwc);
-			SupplierBrowser sb = new SupplierBrowser();
-			
-			String plug = iwc.getParameter("tmp_plugin");
-			
-			Table table = new Table();
-			table.setColor("WHITE");
-			sb.setSupplierManager(super.getSupplierManager());
-			sb.setPlugin(plug);
-			table.add(sb, 1, 1);
-			add(Text.BREAK);
-			add(table);
-		}
+	protected abstract Class getPlugin();	
+
+	public void main(IWContext iwc) throws Exception {
+		super.main(iwc);
 		
+		add(Text.BREAK);
+
+		if (super.isSupplierManagerBookerStaff()) {
+			SupplierBrowser sb = new SupplierBrowser();
+			sb.setWidth("90%");
+			sb.setUseTravelLook(true);
+			sb.setSupplierManager(super.getSupplierManager());
+			sb.setPlugin(getPlugin().getName());
+			
+			add(sb);
+		} else if (!super.isLoggedOn(iwc)) {
+			add(super.getResourceBundle().getLocalizedString("travel.not_logged_on", "Not logged on"));
+		} else {
+			add(super.getResourceBundle().getLocalizedString("travel.no_permission", "No permission"));
+		}
+	}
+	
 }
