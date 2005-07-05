3aaaa94f5c1d52f1b1d017701d07c0c5b1125522
Grimur Jonsson <gimmi@idega.com>
2005-07-05 22:52:25 +0000

    new stuff :D

---
+++ src/is/idega/idegaweb/travel/presentation/SupplierBrowserBookingForm.java
@@ -0,0 +1,322 @@
+/*
+ * $Id: SupplierBrowserBookingForm.java,v 1.1 2005/07/05 22:52:25 gimmi Exp $
+ * Created on Jul 4, 2005
+ *
+ * Copyright (C) 2005 Idega Software hf. All Rights Reserved.
+ *
+ * This software is the proprietary information of Idega hf.
+ * Use is subject to license terms.
+ */
+package is.idega.idegaweb.travel.presentation;
+
+import is.idega.idegaweb.travel.block.search.business.SearchEventListener;
+import is.idega.idegaweb.travel.block.search.business.ServiceSearchBusiness;
+import is.idega.idegaweb.travel.block.search.presentation.AbstractSearchForm;
+import is.idega.idegaweb.travel.data.GeneralBooking;
+import is.idega.idegaweb.travel.interfaces.Booking;
+import is.idega.idegaweb.travel.service.presentation.BookingForm;
+import java.util.Collection;
+import java.util.Iterator;
+import com.idega.block.basket.business.BasketBusiness;
+import com.idega.block.basket.data.BasketEntry;
+import com.idega.block.creditcard.business.CreditCardAuthorizationException;
+import com.idega.block.trade.stockroom.data.Product;
+import com.idega.business.IBOLookup;
+import com.idega.business.IBOLookupException;
+import com.idega.business.IBORuntimeException;
+import com.idega.idegaweb.IWApplicationContext;
+import com.idega.idegaweb.IWUserContext;
+import com.idega.presentation.IWContext;
+import com.idega.presentation.Table;
+import com.idega.presentation.text.Link;
+import com.idega.presentation.text.Text;
+import com.idega.presentation.ui.DropdownMenu;
+import com.idega.presentation.ui.Form;
+import com.idega.presentation.ui.HiddenInput;
+import com.idega.presentation.ui.SubmitButton;
+import com.idega.presentation.ui.TextArea;
+import com.idega.presentation.ui.TextInput;
+import com.idega.util.IWTimestamp;
+
+
+public class SupplierBrowserBookingForm extends TravelManager {
+	
+	private String textStyleClass 	= "sbrowser_text";
+	private String headerStyleClass = "sbrowser_header";
+	private String ioStyleClass 	= "sbrowser_interface";
+	
+	public void main (IWContext iwc) throws Exception {
+		super.main(iwc);
+		add(Text.BREAK);
+		if (super.isSupplierManagerBookerStaff()) {
+			addBookingForm(iwc);
+		} else if (!super.isLoggedOn(iwc)) {
+			add(super.getResourceBundle().getLocalizedString("travel.not_logged_on", "Not logged on"));
+		} else {
+			add(super.getResourceBundle().getLocalizedString("travel.no_permission", "No permission"));
+		}
+	}
+	
+	private void addBookingForm(IWContext iwc) throws Exception {
+		Form form = new Form();
+		Table btable = new Table();
+		btable.setWidth("90%");
+		btable.setCellpaddingAndCellspacing(0);
+		btable.setColor(super.WHITE);
+		form.add(btable);
+		
+		TravelBasket b = new TravelBasket();
+		b.setShowCheckoutLink(false);
+		b.setUseTravelLook(true);
+		b.setWidth("100%");
+		btable.add(b);
+		add(form);
+		
+		Table table = getTable();
+		int row = 1;
+		
+		if (iwc.isParameterSet(AbstractSearchForm.ACTION)) {
+			form.add(Text.BREAK);
+			
+			try {
+				getServiceSearchBusiness(iwc).getSearchSession(iwc).throwException();
+				Collection bookings = getServiceSearchBusiness(iwc).getSearchSession(iwc).getBookingsSavedFromBasket(); 
+				table.mergeCells(1, row, 2, row);
+				table.setRowColor(row, backgroundColor);
+				table.add(getHeaderText(getResourceBundle().getLocalizedString("travel.authorization_successful", "Authorization Successful")), 1, row++);
+				
+				boolean ccAuthAdded = false;
+				int localeID = iwc.getCurrentLocaleId();
+				GeneralBooking gBooking;
+				Product product;
+				Iterator iter = bookings.iterator();
+				while (iter.hasNext()) {
+					gBooking = (GeneralBooking) iter.next();
+					if (!ccAuthAdded && gBooking.getPaymentTypeId() == Booking.PAYMENT_TYPE_ID_CREDIT_CARD) {
+						table.mergeCells(1, row, 2, row);
+						table.setRowColor(row, GRAY);
+						table.add(getText(getResourceBundle().getLocalizedString("travel.credidcard_authorization_number_is","Creditcard authorization number is")), 1, row);
+						table.add(getText(gBooking.getCreditcardAuthorizationNumber()), 1, row++);
+						
+						table.mergeCells(1, row, 2, row);
+						table.setRowColor(row, GRAY);
+						table.setRowHeight(row, "14");
+						++row;
+
+						ccAuthAdded = true;
+					}
+					product = gBooking.getService().getProduct();
+					table.mergeCells(1, row, 2, row);
+					table.setRowColor(row, GRAY);
+					table.add(getText(getResourceBundle().getLocalizedString("travel.service","Service")), 1, row);
+					table.add(getText(" : "), 1, row);
+					table.add(getText(product.getProductName(localeID)+" - "+product.getSupplier().getName()), 1, row);
+					table.add(Text.BREAK, 1, row);
+					
+					table.mergeCells(1, row, 2, row);
+					table.setRowColor(row, GRAY);
+					table.add(getText(getResourceBundle().getLocalizedString("travel.reference_number_is","Reference number")), 1, row);
+					table.add(getText(" : "), 1, row);
+					table.add(getText(gBooking.getReferenceNumber()), 1, row);
+					table.add(Text.BREAK, 1, row);
+//					table.mergeCells(1, row, 2, row);
+//					table.setRowColor(row, GRAY);
+//					table.add(getText(getResourceBundle().getLocalizedString("travel.if_unable_to_print","If you are unable to print the voucher, write the reference number down else proceed to printing the voucher.")), 1, row++);
+					
+					Link printVoucher = new Link(getText(getResourceBundle().getLocalizedString("travel.print_voucher","Print voucher")));
+					printVoucher.addParameter(VoucherWindow.parameterBookingId, gBooking.getID());
+					printVoucher.setWindowToOpen(VoucherWindow.class);
+					
+					table.mergeCells(1, row, 2, row);
+					table.setRowColor(row, GRAY);
+					table.add(printVoucher, 1, row);
+					++row;
+					
+					table.mergeCells(1, row, 2, row);
+					table.setRowColor(row, GRAY);
+					table.setRowHeight(row, "14");
+
+					++row;
+				}
+				
+			} catch (CreditCardAuthorizationException e)  {
+				table.mergeCells(1, row, 2, row);
+				table.setRowColor(row, backgroundColor);
+				table.add(getHeaderText(getResourceBundle().getLocalizedString("travel.authorization_failed", "Authorization Failed")), 1, row++);
+				table.mergeCells(1, row, 2, row);
+				table.setRowColor(row, GRAY);
+				table.add(getText(e.getLocalizedMessage(getResourceBundle())), 1, row++);
+			} catch (Exception e)  {
+				table.mergeCells(1, row, 2, row);
+				table.setRowColor(row, backgroundColor);
+				table.add(getHeaderText(getResourceBundle().getLocalizedString("travel.authorization_failed", "Authorization failed")), 1, row++);
+				table.mergeCells(1, row, 2, row);
+				table.setRowColor(row, GRAY);
+				table.add(getText(e.getMessage()), 1, row++);
+			}
+		}
+		if (!getBasketBusiness(iwc).getBasket().isEmpty()) {
+			TextInput ccName = getStyleTextInput(new TextInput(BookingForm.PARAMETER_NAME_ON_CARD));
+			TextInput ccNumber = getStyleTextInput(new TextInput(BookingForm.parameterCCNumber));
+			TextInput ccYear = getStyleTextInput(new TextInput(BookingForm.parameterCCYear));
+			TextInput ccMonth = getStyleTextInput(new TextInput(BookingForm.parameterCCMonth));
+			TextInput ccCVC = getStyleTextInput(new TextInput(BookingForm.parameterCCCVC));
+			HiddenInput ccRef = new HiddenInput(AbstractSearchForm.PARAMETER_REFERENCE_NUMBER, IWTimestamp.RightNow().toSQLString(false));
+			ccYear.setSize(3);
+			ccMonth.setSize(3);
+			ccCVC.setSize(4);
+			
+			TextInput name = getStyleTextInput(new TextInput(BookingForm.PARAMETER_FIRST_NAME));
+			TextInput email = getStyleTextInput(new TextInput(BookingForm.PARAMETER_EMAIL));
+			TextInput phone = getStyleTextInput(new TextInput(BookingForm.PARAMETER_PHONE));
+			TextArea comment = new TextArea(BookingForm.PARAMETER_COMMENT);
+			if (ioStyleClass != null) {
+				comment.setStyleClass(ioStyleClass);
+			}
+			comment.setWidth("150");
+			comment.setHeight("60");
+			
+			DropdownMenu payType = getBooker(iwc).getPaymentTypeDropdown(getResourceBundle(), BookingForm.PARAMETER_PAYMENT_TYPE);
+			payType.setSelectedElement(Booking.PAYMENT_TYPE_ID_ACCOUNT);
+			payType.setToEnableWhenSelected(ccName, Integer.toString(Booking.PAYMENT_TYPE_ID_CREDIT_CARD));
+			payType.setToEnableWhenSelected(ccNumber, Integer.toString(Booking.PAYMENT_TYPE_ID_CREDIT_CARD));
+			payType.setToEnableWhenSelected(ccYear, Integer.toString(Booking.PAYMENT_TYPE_ID_CREDIT_CARD));
+			payType.setToEnableWhenSelected(ccMonth, Integer.toString(Booking.PAYMENT_TYPE_ID_CREDIT_CARD));
+			payType.setToEnableWhenSelected(ccCVC, Integer.toString(Booking.PAYMENT_TYPE_ID_CREDIT_CARD));
+			
+			name.keepStatusOnAction();
+			email.keepStatusOnAction();
+			phone.keepStatusOnAction();
+			comment.keepStatusOnAction();
+			payType.keepStatusOnAction();
+			
+			ccName.keepStatusOnAction();
+			ccNumber.keepStatusOnAction();
+			ccYear.keepStatusOnAction();
+			ccMonth.keepStatusOnAction();
+			ccCVC.keepStatusOnAction();
+			ccRef.keepStatusOnAction();
+			
+			if (iwc.isParameterSet(BookingForm.PARAMETER_PAYMENT_TYPE) && Integer.parseInt(iwc.getParameter(BookingForm.PARAMETER_PAYMENT_TYPE)) != Booking.PAYMENT_TYPE_ID_CREDIT_CARD) {
+				ccName.setDisabled(true);
+				ccMonth.setDisabled(true);
+				ccYear.setDisabled(true);
+				ccCVC.setDisabled(true);
+				ccNumber.setDisabled(true);
+			}
+			
+			table.mergeCells(1, row, 2, row);
+			table.setRowColor(row, backgroundColor);
+			table.add(getHeaderText(getResourceBundle().getLocalizedString("travel.client_information", "Client Information")), 1, row++);
+			table.add(getText(getResourceBundle().getLocalizedString("travel.name", "Name")), 1, row);
+			table.add(name, 2, row);
+			table.setRowColor(row++, GRAY);
+			table.setRowColor(row, GRAY);
+			table.add(getText(getResourceBundle().getLocalizedString("travel.email", "Email")), 1, row);
+			table.add(email, 2, row++);
+			table.setRowColor(row, GRAY);
+			table.add(getText(getResourceBundle().getLocalizedString("travel.phone", "Phone")), 1, row);
+			table.add(phone, 2, row++);
+			table.setRowColor(row, GRAY);
+			table.add(getText(getResourceBundle().getLocalizedString("travel.comment", "Comment")), 1, row);
+			table.setVerticalAlignment(1, row, Table.VERTICAL_ALIGN_TOP);
+			table.add(comment, 2, row++);
+			
+			
+			
+			table.mergeCells(1, row, 2, row);
+			table.setRowColor(row, backgroundColor);
+			table.add(getHeaderText(getResourceBundle().getLocalizedString("travel.payment", "Payment")), 1, row++);
+			table.setRowColor(row, GRAY);
+			table.add(getText(getResourceBundle().getLocalizedString("travel.payment_type", "Payment type")), 1, row);
+			table.add(payType, 2, row++);
+			table.setRowColor(row, GRAY);
+			table.add(ccRef, 2, row);
+			table.add(getText(getResourceBundle().getLocalizedString("travel.name_on_card", "Name as it appears on the card")), 1, row);
+			table.add(ccName, 2, row++);
+			table.setRowColor(row, GRAY);
+			table.add(getText(getResourceBundle().getLocalizedString("travel.creditcard_number", "Creditcard Number")), 1, row);
+			table.add(ccNumber, 2, row++);
+			table.setRowColor(row, GRAY);
+			table.add(getText(getResourceBundle().getLocalizedString("travel.expire_date", "Creditcard expire date (MM/YY)")), 1, row);
+			table.add(ccMonth, 2, row);
+			table.add(getText(" / "), 2, row);
+			table.add(ccYear, 2, row++);
+			table.setRowColor(row, GRAY);
+			table.add(getText(getResourceBundle().getLocalizedString("travel.creditcard_cvc", "Creditcard CVC code")), 1, row);
+			table.add(ccCVC, 2, row++);
+			table.setRowColor(row, GRAY);
+			table.add(getText(getResourceBundle().getLocalizedString("travel.total_amount", "Total amount")), 1, row);
+			table.add(getText(getTotalPriceString(iwc)), 2, row++);
+			
+			ccCVC.setAsNotEmpty(getResourceBundle().getLocalizedString("travel.creditcard_cvc_number_must_be_provided", "Creditcard CVC number must be provided"));
+			ccMonth.setAsNotEmpty(getResourceBundle().getLocalizedString("travel.month_of_expire_must_be_provided", "Month of expire must be provided"));
+			ccYear.setAsNotEmpty(getResourceBundle().getLocalizedString("travel.year_of_expire_must_be_provided", "Year of expire must be provided"));
+			ccNumber.setAsNotEmpty(getResourceBundle().getLocalizedString("travel.creditcard_number_must_be_provided", "Creditcard number must be provided"));
+			ccName.setAsNotEmpty(getResourceBundle().getLocalizedString("travel.name_on_creditcard_must_be_provided", "Name on card must be provided"));
+			name.setAsNotEmpty(getResourceBundle().getLocalizedString("travel.client_name_must_be_provided", "Client name must be provided"));
+			
+			
+			SubmitButton save = new SubmitButton(getResourceBundle().getLocalizedString("travel.book", "Book"), AbstractSearchForm.ACTION, AbstractSearchForm.ACTION_CONFIRM);
+			form.setEventListener(SearchEventListener.class);
+			
+			table.setRowColor(row, GRAY);
+			table.mergeCells(1, row, 2, row);
+			table.setAlignment(1, row, Table.HORIZONTAL_ALIGN_RIGHT);
+			table.add(save, 1,row++);
+			
+		} 
+		if (row > 1) {
+			form.add(Text.BREAK);
+			form.add(table);
+		}
+		
+	}
+	
+	private String getTotalPriceString(IWContext iwc) throws Exception {
+		float price, totalPrice = 0;
+		String currency = null;
+		
+		Collection values = getBasketBusiness(iwc).getBasket().values();
+		GeneralBooking booking;
+		Iterator iter = values.iterator();
+		while (iter.hasNext()) {
+			booking = (GeneralBooking)  ((BasketEntry) iter.next()).getItem();
+			price = getBooker(iwc).getBookingPrice(getBooker(iwc).getMultibleBookings(booking));
+			totalPrice += price;
+			String curr = getBooker(iwc).getBookingCurrency(booking).getCurrencyAbbreviation();
+			if (curr != null && (currency == null)) {
+				currency = curr;
+			} else if (curr != null && !currency.equals(curr)) {
+				throw new Exception("Too many currencies");
+			}
+		}
+		
+		if (currency != null) {
+			return super.decimalFormat.format(totalPrice) + " " +currency;
+		}
+		return null;
+	}
+	
+	public TextInput getStyleTextInput(TextInput io) {
+		return io;
+	}
+	
+	protected ServiceSearchBusiness getServiceSearchBusiness(IWApplicationContext iwac) {
+		try {
+			return (ServiceSearchBusiness) IBOLookup.getServiceInstance(iwac, ServiceSearchBusiness.class);
+		}
+		catch (IBOLookupException e) {
+			throw new IBORuntimeException(e);
+		}
+	}
+	
+	protected BasketBusiness getBasketBusiness(IWUserContext iwuc) {
+		try {
+			return (BasketBusiness) IBOLookup.getSessionInstance(iwuc, BasketBusiness.class);
+		}
+		catch (IBOLookupException e) {
+			throw new IBORuntimeException(e);
+		}
+	}
+}
---
+++ src/is/idega/idegaweb/travel/presentation/TravelBasket.java
@@ -0,0 +1,343 @@
+/*
+ * $Id: TravelBasket.java,v 1.1 2005/07/05 22:52:25 gimmi Exp $
+ * Created on 22.6.2005
+ *
+ * Copyright (C) 2005 Idega Software hf. All Rights Reserved.
+ *
+ * This software is the proprietary information of Idega hf.
+ * Use is subject to license terms.
+ */
+package is.idega.idegaweb.travel.presentation;
+
+import is.idega.idegaweb.travel.block.search.business.SearchEventListener;
+import is.idega.idegaweb.travel.block.search.business.ServiceSearchBusinessBean;
+import is.idega.idegaweb.travel.block.search.presentation.AbstractSearchForm;
+import is.idega.idegaweb.travel.business.BookingComparator;
+import is.idega.idegaweb.travel.data.GeneralBooking;
+import is.idega.idegaweb.travel.service.presentation.BookingForm;
+import java.rmi.RemoteException;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Locale;
+import java.util.Vector;
+import javax.ejb.FinderException;
+import com.idega.block.basket.business.BasketBusiness;
+import com.idega.block.basket.data.BasketEntry;
+import com.idega.block.trade.stockroom.data.Product;
+import com.idega.block.trade.stockroom.data.Supplier;
+import com.idega.business.IBOLookup;
+import com.idega.business.IBOLookupException;
+import com.idega.business.IBORuntimeException;
+import com.idega.core.builder.data.ICPage;
+import com.idega.idegaweb.IWUserContext;
+import com.idega.presentation.IWContext;
+import com.idega.presentation.Table;
+import com.idega.presentation.text.Link;
+import com.idega.presentation.text.Text;
+import com.idega.util.IWTimestamp;
+
+
+public class TravelBasket extends TravelBlock {
+	
+	private BasketBusiness basketBusiness = null;
+	private Collection bookings = null;
+	
+	private String textStyleClass = null;
+	private String headerStyleClass = null;
+	private String linkStyleClass = null;
+	
+	private String bookingURL = null;
+	private ICPage bookingPage = null;
+	private Class bookingClass = null;
+	
+	private boolean showCheckoutLink = true;
+	private boolean useTravelLook = false;
+	
+	private Vector parName = null;
+	private Vector parValue = null;
+	
+	private boolean checkoutIsOnAnotherServer = false;
+	private String encryptedListenerName = null;
+	
+	public void main(IWContext iwc) throws Exception {
+		
+		super.main(iwc);
+
+		init(iwc);
+		displayBasketContent(iwc);
+	}
+
+	private void init(IWContext iwc) {
+		checkoutIsOnAnotherServer = true;
+		basketBusiness = getBasketBusiness(iwc);
+	}
+	
+	
+	private void displayBasketContent(IWContext iwc) throws Exception {
+		Table table = new Table();
+		if (useTravelLook) {
+			table = TravelManager.getTable();
+		}
+		table.setWidth("100%");
+		table.setBorder(0);
+		int row = 1;
+		Iterator iter = getBookings(iwc).iterator();
+		GeneralBooking booking;
+		Product prod;
+		Supplier supp;
+		BookingForm bf;
+		float totalPrice = 0;
+		float price;
+		int localeID = iwc.getCurrentLocaleId();
+		Locale locale = iwc.getCurrentLocale();
+		Link remove = null;
+		String currency = null;
+		
+		table.add(getHeader(getResourceBundle().getLocalizedString("travel.service", "Service")), 1, row);
+		table.add(getHeader(getResourceBundle().getLocalizedString("travel.date_s", "Date(s)")), 2, row);
+		table.add(getHeader(getResourceBundle().getLocalizedString("travel.prices", "Prices")), 3, row);
+		table.add(getHeader(""), 4, row);
+		if (useTravelLook) {
+			table.setRowColor(row, TravelManager.backgroundColor);
+		}
+		++row;
+		while (iter.hasNext()) {
+			booking = (GeneralBooking) iter.next();
+			prod = booking.getService().getProduct();
+			supp = prod.getSupplier();
+			bf = getServiceHandler(iwc).getBookingForm(iwc, prod, false);
+			table.add(getText(prod.getProductName(localeID)+" - "+supp.getName()), 1, row);
+			addBookingDates(iwc, bf, booking, locale, table, 2, row);
+			price = getBooker(iwc).getBookingPrice(getBooker(iwc).getMultibleBookings(booking));
+			totalPrice += price;
+			String curr = getBooker(iwc).getBookingCurrency(booking).getCurrencyAbbreviation();
+			if (curr != null && (currency == null)) {
+				currency = curr;
+			} else if (curr != null && !currency.equals(curr)) {
+				throw new Exception("Too many currencies");
+			}
+			table.add(getText(decimalFormat.format(price)+" "+currency), 3, row);
+			
+			remove = getLink(getResourceBundle().getLocalizedString("travel.basket.remove", "Remove"));
+			remove.addParameter(ServiceSearchBusinessBean.PARAMETER_BOOKING_ID_REMOVAL, booking.getPrimaryKey().toString());
+			remove.addParameter(AbstractSearchForm.ACTION, ServiceSearchBusinessBean.PARAMETER_BOOKING_ID_REMOVAL);
+			remove.setEventListener(SearchEventListener.class);
+			table.add(remove, 4, row);
+			
+			table.setVerticalAlignment(1, row, Table.VERTICAL_ALIGN_TOP);
+			table.setVerticalAlignment(2, row, Table.VERTICAL_ALIGN_TOP);
+			table.setVerticalAlignment(3, row, Table.VERTICAL_ALIGN_TOP);
+			table.setVerticalAlignment(4, row, Table.VERTICAL_ALIGN_TOP);
+			table.setAlignment(4, row, Table.HORIZONTAL_ALIGN_RIGHT);
+			
+			if (useTravelLook) {
+				table.setRowColor(row, TravelManager.GRAY);
+			}
+			++row;
+		}
+		if (currency != null) {
+			if (useTravelLook) {
+				table.setRowColor(row, TravelManager.GRAY);
+			}
+			table.add(getBoldText(getResourceBundle().getLocalizedString("travel.total", "Total")), 1, row);
+			table.add(getBoldText(decimalFormat.format(totalPrice)+" "+currency), 3, row++);
+
+			if (showCheckoutLink) {
+				Link book = getLink(super.getResourceBundle().getLocalizedString("travel.check_out", "Check out"));
+				if (bookingURL != null) {
+					book.setURL(bookingURL);
+				}
+				if (bookingPage != null) {
+					book.setPage(bookingPage);
+				}
+				if (bookingClass != null) {
+					book.setClassToInstanciate(bookingClass);
+				}
+	 
+				book.addParameter(AbstractSearchForm.ACTION, AbstractSearchForm.ACTION_BOOKING_FORM);
+				book.addParameter(AbstractSearchForm.PARAMETER_REFERENCE_NUMBER, IWTimestamp.RightNow().toString());
+	
+				if (parName != null) {
+					for (int i = 0; i < parName.size(); i++) {
+						book.addParameter(parName.get(i).toString(), parValue.get(i).toString());
+					}
+				}
+				
+				if (checkoutIsOnAnotherServer) {
+					if (encryptedListenerName == null) {
+						book.setEventListener(SearchEventListener.class);
+					} else {
+						book.setEventListener(encryptedListenerName);
+					}
+					Collection values = basketBusiness.getBasket().values();
+					if (values != null) {
+						Iterator viter = values.iterator();
+						while (viter.hasNext()) {
+							BasketEntry e = (BasketEntry) viter.next();
+							book.addParameter(ServiceSearchBusinessBean.PARAMETER_BOOKING_IDS_FOR_BASKET, e.getItem().getItemID().toString());
+						}
+					}
+				}
+				if (useTravelLook) {
+					table.setRowColor(row, TravelManager.GRAY);
+				}
+
+				table.add(book, 3, row);
+				table.mergeCells(3, row, 4, row);
+				table.setAlignment(3, row, Table.HORIZONTAL_ALIGN_RIGHT);
+			}
+
+			++row;
+		
+		} else {
+			table.add(getText(getResourceBundle().getLocalizedString("travel.basket_is_empty", "Basket is empty")), 1, row);
+			if (useTravelLook) {
+				table.setRowColor(row, TravelManager.GRAY);
+			}
+		}
+		
+		table.setWidth(2, "90");
+		table.setWidth(3, "100");
+		table.setWidth(4, "50");
+		
+		add(table);
+	}
+
+	protected void addBookingDates(IWContext iwc, BookingForm bf, GeneralBooking booking, Locale locale, Table table, int column, int row) throws RemoteException {
+		try {
+			List list = getBooker(iwc).getMultibleBookings(booking);
+			table.add(getText(bf.getBookingDateString(list, locale)), column, row);
+		}
+		catch (FinderException e) {
+			e.printStackTrace();
+			table.add(getText(new IWTimestamp(booking.getBookingDate()).getLocaleDate(locale)),column,row);
+			
+		}
+	}
+	
+	private Collection getBookings(IWContext iwc) throws RemoteException {
+		if (bookings == null) {
+			Collection entries = basketBusiness.getBasket().values();
+			bookings = new Vector();
+			BasketEntry entry;
+			GeneralBooking booking;
+			Iterator iter = entries.iterator();
+			while (iter.hasNext()) {
+				entry = (BasketEntry) iter.next();
+				booking = (GeneralBooking) entry.getItem();
+				bookings.add(booking);
+			}
+			Collections.sort((List) bookings, new BookingComparator(iwc, BookingComparator.DATE));
+		}
+		return bookings;
+	}
+	
+	protected Text getText(String content) {
+		if (useTravelLook) {
+			return TravelManager.getText(content);
+		} else {
+			return  getText(content, textStyleClass);
+		}
+	}
+	
+	private Text getHeader(String content) {
+		if (useTravelLook) {
+			return TravelManager.getHeaderText(content);
+		} else {
+			return getText(content, headerStyleClass);
+		}
+	}
+	
+	private Text getBoldText(String content) {
+		if (useTravelLook) {
+			Text text = getText(content);
+			text.setBold(true);
+			return text;
+			
+		} else {
+			return getHeader(content);
+		}
+	}
+	
+	private Link getLink(String content) {
+		return new Link(getText(content, linkStyleClass));
+	}
+	
+	private Text getText(String content, String styleClass) {
+		Text text = new Text(content);
+		if (styleClass != null) {
+			text.setStyleClass(styleClass);
+		}
+		return text;
+	}
+	
+	
+	public void setBookingURL(String url) {
+		this.bookingURL = url;
+	}
+	
+	public void setBookingPage(ICPage page) {
+		this.bookingPage = page;
+	}
+	
+	public void setTextStyleClass(String styleClass) {
+		this.textStyleClass = styleClass;
+	}
+	
+	public void setHeaderStyleClass(String styleClass) {
+		this.headerStyleClass = styleClass;
+	}
+	
+	public void setLinkStyleClass(String styleClass) {
+		this.linkStyleClass = styleClass;
+	}
+	
+	public void setBookingClass(Class bookingClass) {
+		this.bookingClass = bookingClass;
+	}
+	
+	/**
+	 * Set whether or not the checkout/booking form is on another server
+	 */
+	public void setCheckoutIsOnAnotherServer(boolean is) {
+		this.checkoutIsOnAnotherServer = is;
+	}
+	
+	public void addParameter(String name, String value) {
+		if (name != null && !name.equals("") && value != null) {
+			if (parName == null) {
+				parName = new Vector();
+				parValue = new Vector();
+			}
+			parName.add(name);
+			parValue.add(value);
+		}
+	}
+	
+	public void setShowCheckoutLink(boolean show) {
+		this.showCheckoutLink = show;
+	}
+	
+	public void setUseTravelLook(boolean use) {
+		this.useTravelLook = use;
+	}
+	/**
+	 * Set the encrypted name of ActionListener for the other server  
+	 * @param name
+	 */
+	public void setEncryptedListenerName(String name) {
+		this.encryptedListenerName = name;
+	}
+	
+	protected BasketBusiness getBasketBusiness(IWUserContext iwuc) {
+		try {
+			return (BasketBusiness) IBOLookup.getSessionInstance(iwuc, BasketBusiness.class);
+		}
+		catch (IBOLookupException e) {
+			throw new IBORuntimeException(e);
+		}
+	}
+	
+}
