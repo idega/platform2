ec4c08286db7b4c2f9ffe457dd9a7f3ba9702474
Grimur Jonsson <gimmi@idega.com>
2005-09-05 23:05:14 +0000

    added supplierManager info

--- src/is/idega/idegaweb/travel/presentation/TravelItineraryWindow.java
+++ src/is/idega/idegaweb/travel/presentation/TravelItineraryWindow.java
@@ -1,5 +1,5 @@
 /*
- * $Id: TravelItineraryWindow.java,v 1.1 2005/08/27 15:36:14 gimmi Exp $
+ * $Id: TravelItineraryWindow.java,v 1.2 2005/09/05 23:05:14 gimmi Exp $
  * Created on Aug 27, 2005
  *
  * Copyright (C) 2005 Idega Software hf. All Rights Reserved.
@@ -9,7 +9,19 @@
  */
 package is.idega.idegaweb.travel.presentation;
 
+import is.idega.idegaweb.travel.data.GeneralBooking;
+import is.idega.idegaweb.travel.data.GeneralBookingHome;
+import java.util.Collection;
+import com.idega.core.contact.data.Email;
+import com.idega.core.contact.data.Phone;
+import com.idega.core.location.data.Address;
+import com.idega.core.location.data.AddressType;
+import com.idega.core.location.data.AddressTypeHome;
+import com.idega.data.IDOLookup;
 import com.idega.presentation.IWContext;
+import com.idega.presentation.Table;
+import com.idega.presentation.text.Text;
+import com.idega.user.data.Group;
 
 
 public class TravelItineraryWindow extends TravelWindow {
@@ -23,6 +35,48 @@ public class TravelItineraryWindow extends TravelWindow {
 	public void main(IWContext iwc) throws Exception {
 		super.main(iwc);
 		String[] bids = iwc.getParameterValues(PARAMETER_BOOKING_ID);
+		
+		if (bids != null && bids.length > 0) {
+			GeneralBookingHome gbHome = (GeneralBookingHome) IDOLookup.getHome(GeneralBooking.class);
+			GeneralBooking gBook = gbHome.findByPrimaryKey(new Integer(bids[0]));
+			Group supplierManager = gBook.getService().getProduct().getSupplier().getSupplierManager();
+			
+			Table table = new Table();
+			table.setAlignment(Table.HORIZONTAL_ALIGN_CENTER);
+			table.setAlignment(1, 1, Table.HORIZONTAL_ALIGN_CENTER);
+			table.setAlignment(1, 2, Table.HORIZONTAL_ALIGN_CENTER);
+			table.add(getHeader(supplierManager.getName()), 1, 1);
+			
+			AddressTypeHome atHome = (AddressTypeHome) IDOLookup.getHome(AddressType.class);
+			Collection coll = supplierManager.getAddresses(atHome.findAddressType1());
+			boolean addDash = false;
+			if (coll != null && !coll.isEmpty()) {
+				Address a = (Address) coll.iterator().next();
+				table.add(getText(a.getStreetAddress()), 1, 2);
+				addDash = true;
+			}
+			
+			coll = supplierManager.getPhones();
+			if (coll != null && !coll.isEmpty()) {
+				Phone p = (Phone) coll.iterator().next();
+				if (addDash) {
+					table.add(getText(" - "), 1, 2);
+				}
+				addDash = true;
+				table.add(getText(p.getNumber()), 1, 2);
+			}
+			
+			coll = supplierManager.getEmails();
+			if (coll != null && !coll.isEmpty()) {
+				Email e = (Email) coll.iterator().next();
+				if (addDash) {
+					table.add(getText(" - "), 1, 2);
+				}
+				addDash = true;
+				table.add(getText(e.getEmailAddress()), 1, 2);
+			}
+			add(table);
+		}
 		TravelBasket tb = new TravelBasket();
 		tb.setBookingPKs(bids);
 		tb.setShowDeleteLink(false);
@@ -30,6 +84,18 @@ public class TravelItineraryWindow extends TravelWindow {
 		tb.setHeaderStyleClass("sbrowser_header");
 		tb.setTextStyleClass("sbrowser_text");
 		add(tb);
+	}
+	
+	private Text getHeader(String content) {
+		Text text = new Text(content);
+		text.setStyleClass("sbrowser_header");
+		return text;
+	}
+	
+	protected Text getText(String content) {
+		Text text = new Text(content);
+		text.setStyleClass("sbrowser_text");
+		return text;
 	}
 
 }
