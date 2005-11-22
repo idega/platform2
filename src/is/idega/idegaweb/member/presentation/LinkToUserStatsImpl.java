9ff6345431b45f0c6fb2c8a69d83508817f8159e
Sigtryggur Simonarson <sigtryggur@idega.com>
2005-11-22 23:52:21 +0000

    LinkToUserStats made more general

--- src/is/idega/idegaweb/member/presentation/LinkToUserStatsImpl.java
+++ src/is/idega/idegaweb/member/presentation/LinkToUserStatsImpl.java
@@ -13,12 +13,15 @@ import com.idega.user.presentation.LinkToUserStats;
 public class LinkToUserStatsImpl implements LinkToUserStats {
 
     private Group selectedGroup = null;
+    private String invocationFileName = null;
+    private String layoutFileName = null;
+    private String localizableKeyName = null;
     
     public Link getLink() {
         Link link = new Link();
-		link.addParameter(UserStatsWindow.STATS_INVOCATION_NAME_FROM_BUNDLE, "Invocation-UserStats.xml");
-		link.addParameter(UserStatsWindow.STATS_LAYOUT_NAME_FROM_BUNDLE, "Layout-UserStats.xml");
-		link.addParameter(UserStatsWindow.STATS_LOCALIZABLE_KEY_NAME, "userstatswindow.userstats");
+		link.addParameter(UserStatsWindow.STATS_INVOCATION_NAME_FROM_BUNDLE, invocationFileName);
+		link.addParameter(UserStatsWindow.STATS_LAYOUT_NAME_FROM_BUNDLE, layoutFileName);
+		link.addParameter(UserStatsWindow.STATS_LOCALIZABLE_KEY_NAME, localizableKeyName);
 		link.setWindowToOpen(UserStatsWindow.class);
 		if (selectedGroup!= null) {
 		    link.addParameter("dr_group", selectedGroup.getNodeID());
@@ -28,5 +31,14 @@ public class LinkToUserStatsImpl implements LinkToUserStats {
 
     public void setSelectedGroup(Group selectedGroup) {
         this.selectedGroup = selectedGroup;
+    }
+    public void setInvocationFileName(String invocationFileName) {
+        this.invocationFileName = invocationFileName;
+    }
+    public void setLayoutFileName(String layoutFileName) {
+        this.layoutFileName = layoutFileName;
+    }
+    public void setLocalizableKeyName(String localizableKeyName) {
+        this.localizableKeyName = localizableKeyName;
     }
 }
