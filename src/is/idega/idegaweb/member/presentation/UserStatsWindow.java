f275b48fa5d143d7a35a08daeedd7c7a22e27247
Sigtryggur Simonarson <sigtryggur@idega.com>
2005-11-26 17:22:51 +0000

    Changes to logic regarding selecting a layout for a report

--- src/is/idega/idegaweb/member/presentation/UserStatsWindow.java
+++ src/is/idega/idegaweb/member/presentation/UserStatsWindow.java
@@ -21,11 +21,10 @@ public class UserStatsWindow extends StyledIWAdminWindow {
     
     private IWResourceBundle iwrb;
     private IWBundle iwb;
-    public static final String STATS_INVOCATION_NAME_FROM_BUNDLE = "STATS_INVOCATION_NAME_FROM_BUNDLE";
     public static final String STATS_LOCALIZABLE_KEY_NAME = "STATS_LOCALIZABLE_KEY_NAME";
-    public static final String STATS_LAYOUT_PARAM = "STATS_LAYOUT_PARAM";
-    public static final String STATS_INVOCATION_PARAM = "STATS_INVOCATION_PARAM";
+    public static final String STATS_INVOCATION_NAME_FROM_BUNDLE = "STATS_INVOCATION_NAME_FROM_BUNDLE";
     public static final String STATS_LAYOUT_NAME_FROM_BUNDLE = "STATS_LAYOUT_NAME_FROM_BUNDLE";
+    public static final String STATS_DYNAMIC_LAYOUT_FILE_ID = "dr_dynamicLayoutFile";
     
     public final static String STYLE_2 = "font-family:arial; font-size:8pt; color:#000000; text-align: justify;";
     public static final String IW_BUNDLE_IDENTIFIER = "is.idega.idegaweb.member";
@@ -55,46 +54,33 @@ public class UserStatsWindow extends StyledIWAdminWindow {
         table.mergeCells(1, 1, 1, 2);
 
 		ReportGenerator repGen = new ReportGenerator();	
-		//repGen.setParameterToMaintain("dr_group");
-		repGen.setParameterToMaintain(STATS_INVOCATION_PARAM);
-		repGen.setParameterToMaintain(STATS_LAYOUT_PARAM);
 		repGen.setParameterToMaintain(STATS_LAYOUT_NAME_FROM_BUNDLE);
 		repGen.setParameterToMaintain(STATS_INVOCATION_NAME_FROM_BUNDLE);
 		repGen.setParameterToMaintain(STATS_LOCALIZABLE_KEY_NAME);
-		String invocationKey = iwc.getParameter(STATS_INVOCATION_PARAM);
 		String invocationFileName = iwc.getParameter(STATS_INVOCATION_NAME_FROM_BUNDLE);
-		String layoutKey = iwc.getParameter(STATS_LAYOUT_PARAM);
 		String layoutFileName = iwc.getParameter(STATS_LAYOUT_NAME_FROM_BUNDLE);
-		String localizedNameKey = iwc.getParameter(STATS_LOCALIZABLE_KEY_NAME);
-		if ((invocationKey != null && iwb.getProperty(invocationKey, "-1") != null) || invocationFileName != null) {
-		    if (invocationFileName != null) {
-		        repGen.setMethodInvocationBundleAndFileName(iwb, invocationFileName);
-		    } else {
-		        Integer invocationICFileID = new Integer(iwb.getProperty(invocationKey));
-		        if (invocationICFileID.intValue() > 0) {
-		            repGen.setMethodInvocationICFileID(invocationICFileID);
-		        }
+	    String localizedNameKey = iwc.getParameter(STATS_LOCALIZABLE_KEY_NAME);
+	    repGen.setMethodInvocationBundleAndFileName(iwb, invocationFileName);
+		String layoutFileID = iwc.getParameter(STATS_DYNAMIC_LAYOUT_FILE_ID);
+		if (layoutFileID != null) {
+			if (layoutFileName != null && layoutFileID.equals("-1")) {
+		    	repGen.setLayoutBundleAndFileName(iwb, layoutFileName);
 		    }
-		    if (layoutFileName != null) {
-		        repGen.setLayoutBundleAndFileName(iwb, layoutFileName);
-		    } else if (layoutKey != null && iwb.getProperty(layoutKey, "-1") != null) {
-		        Integer layoutICFileID = new Integer(iwb.getProperty(layoutKey));
-		        if (layoutICFileID.intValue() > 0)
-		                repGen.setLayoutICFileID(layoutICFileID);
+		    else if (layoutFileID != null) {
+		    	repGen.setLayoutICFileID(new Integer(layoutFileID));
 		    }
-		    if (localizedNameKey != null) {
-		        String reportName = iwrb.getLocalizedString(localizedNameKey);
-		        repGen.setReportName(reportName);
-		        table.add(formatHeadline(reportName), 1, 1); //not a selector
-		        table.addBreak(1, 1);
-		    }
+		} else if (layoutFileName != null) {
+			repGen.setLayoutBundleAndFileName(iwb, layoutFileName);
 		}
-		
+	    if (localizedNameKey != null) {
+	        String reportName = iwrb.getLocalizedString(localizedNameKey);
+	        repGen.setReportName(reportName);
+	        table.add(formatHeadline(reportName), 1, 1); //not a selector
+	        table.addBreak(1, 1);
+	    }
 		table.add(repGen, 1, 1); //not a selector
-		
 		add(table, iwc);		
     }
-
 
     public String getBundleIdentifier() {
         return IW_BUNDLE_IDENTIFIER;
