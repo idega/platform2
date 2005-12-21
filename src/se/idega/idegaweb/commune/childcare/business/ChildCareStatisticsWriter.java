8b63259fd7a462c4dc508ddbbd073eb49b8afa66
Dainis Brjuhoveckis <dainis.brjuhoveckis@idega.lv>
2005-12-21 09:13:41 +0000

    Merged in from HEAD: added Contract and Placement changes export.

--- src/se/idega/idegaweb/commune/childcare/business/ChildCareStatisticsWriter.java
+++ src/se/idega/idegaweb/commune/childcare/business/ChildCareStatisticsWriter.java
@@ -5,9 +5,15 @@ package se.idega.idegaweb.commune.childcare.business;
 
 import java.io.InputStream;
 import java.rmi.RemoteException;
+import java.sql.Timestamp;
+import java.util.Calendar;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.Iterator;
 import java.util.Locale;
+import java.util.Vector;
 
 import javax.ejb.FinderException;
 
@@ -22,17 +28,25 @@ import se.idega.idegaweb.commune.business.CommuneUserBusiness;
 import se.idega.idegaweb.commune.care.data.CareTime;
 import se.idega.idegaweb.commune.care.data.ChildCareApplication;
 import se.idega.idegaweb.commune.care.data.ChildCareContract;
+import se.idega.idegaweb.commune.care.data.ChildCareContractHome;
 import se.idega.idegaweb.commune.presentation.CommuneBlock;
 
 import com.idega.block.process.data.CaseLog;
+import com.idega.block.school.business.SchoolBusiness;
 import com.idega.block.school.data.School;
+import com.idega.block.school.data.SchoolClassMember;
+import com.idega.block.school.data.SchoolClassMemberHome;
+import com.idega.block.school.data.SchoolClassMemberLog;
+import com.idega.block.school.data.SchoolClassMemberLogHome;
 import com.idega.business.IBOLookup;
+import com.idega.business.IBOLookupException;
 import com.idega.core.contact.data.Phone;
 import com.idega.core.file.data.ICFile;
 import com.idega.core.file.data.ICFileHome;
 import com.idega.core.location.data.Address;
 import com.idega.core.location.data.PostalCode;
 import com.idega.data.IDOLookup;
+import com.idega.data.IDOLookupException;
 import com.idega.idegaweb.IWApplicationContext;
 import com.idega.idegaweb.IWResourceBundle;
 import com.idega.io.MemoryFileBuffer;
@@ -255,4 +269,305 @@ public class ChildCareStatisticsWriter {
 	protected CommuneUserBusiness getCommuneUserBusiness(IWApplicationContext iwc) throws RemoteException {
 		return (CommuneUserBusiness) IBOLookup.getServiceInstance(iwc, CommuneUserBusiness.class);	
 	}
+	
+	/*
+	 * Contract and placement changes
+	 * This is copy from ContractAndPlacementChangesExportWriter	  	 
+	 */
+	public static final String SEPARATOR = ";";
+	
+	IWContext iwc;
+	
+	private boolean isDebuggingGoingOn = false;
+	
+	public boolean createExportFile(IWContext iwc, ICFile folder) {
+		
+		this.iwc = iwc;
+		
+		try {
+			MemoryFileBuffer buffer = new MemoryFileBuffer();
+			MemoryOutputStream mos = new MemoryOutputStream(buffer);
+			
+			
+			Iterator iter = getExportData().iterator(); 
+			while (iter.hasNext()) {
+				DataForExport row = (DataForExport) iter.next();
+				try {
+					mos.write(row.getContents().getBytes("UTF8"));
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+
+			
+			buffer.setMimeType("text/plain"); 
+			InputStream mis = new MemoryInputStream(buffer);
+	
+			ICFileHome icFileHome = (ICFileHome) IDOLookup.getHome(ICFile.class);
+			ICFile file = icFileHome.create();
+			file.setFileValue(mis);
+			file.setMimeType("text/plain");
+			
+			IWTimestamp toDate = new IWTimestamp();
+			String s = toDate.toString().replaceAll(":", ""); // on windows filenames cannot contain colons
+			
+			file.setName("export " + s + ".txt");			
+			file.setFileSize(buffer.length());
+			file.store();
+			
+			mos.close();
+			mis.close();
+			
+			folder.addChild(file);
+			
+			return true;			
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	
+	private Date getFirstDateOfCurrentMonth() {		
+	    Calendar cal = roundCalendarToDate(new GregorianCalendar());
+	    
+	    if (isDebuggingGoingOn) {
+		    cal.add(Calendar.YEAR, -1);
+		    cal.add(Calendar.MONTH, -1);
+	    }
+	        
+	    int firstDay = cal.getActualMinimum(Calendar.DAY_OF_MONTH); // It will be 1	    
+	    cal.set(Calendar.DAY_OF_MONTH, firstDay);
+	    
+	    return new Date(cal.getTimeInMillis());
+	}	
+
+	private Date getLastDateOfCurrentMonth() {		
+	    Calendar cal = roundCalendarToDate(new GregorianCalendar());
+	    
+	    if (isDebuggingGoingOn) {
+	    	cal.add(Calendar.YEAR, -1);	
+	    } 	    	
+	        
+	    int days = cal.getActualMaximum(Calendar.DAY_OF_MONTH);	    
+	    cal.set(Calendar.DAY_OF_MONTH, days);
+
+	    return new Date(cal.getTimeInMillis());
+	}
+	
+	private Calendar roundCalendarToDate(Calendar cal){
+	    cal.set(Calendar.HOUR, 0);
+	    cal.set(Calendar.MINUTE, 0);
+	    cal.set(Calendar.SECOND, 0);
+	    cal.set(Calendar.MILLISECOND, 0);
+	    return cal;
+	}	
+
+	private Date timestampToRoundedDate(Timestamp stamp) {
+		if (stamp == null) return null;
+		
+		Calendar cal = new GregorianCalendar();
+		cal.setTimeInMillis(stamp.getTime());
+		return roundCalendarToDate(cal).getTime();
+		
+	}
+	
+	/*
+	private Date roundDateToDayOfMonth(Date date) {
+		if (date == null) return null;
+		
+		Calendar cal = new GregorianCalendar();
+		cal.setTimeInMillis(date.getTime());
+		return roundCalendarToDate(cal).getTime();
+	}
+	
+	private String nullStringToEmpty(String s) {
+		if(s == null) {
+			return "";
+		}
+		else {
+			return s;
+		}
+	}
+	*/
+	
+	private String dateToString(Date date) {
+		if(date == null) return "";
+		IWTimestamp stamp = new IWTimestamp(date);
+		return stamp.getDateString("yyyy-MM-dd");	
+	}
+	
+	private Collection getExportData()  {
+		Date firstDateOfCurrentMonth = this.getFirstDateOfCurrentMonth();
+		Date lastDateOfCurrentMonth = this.getLastDateOfCurrentMonth();
+		
+		Vector uberCollection = new Vector();
+		
+		try {
+			//get all placements
+			//retrieve the placements we need and add them to the ubercolection
+
+			Iterator placementsIterator =  getSchoolClassMemberHome().findAllByCategory(getSchoolBusiness().getCategoryChildcare()).iterator();
+			
+			boolean takeThisPlacement = false;
+			
+			for(; placementsIterator.hasNext();) {
+				SchoolClassMember placement = (SchoolClassMember) placementsIterator.next();
+
+				Date registerDate = timestampToRoundedDate(placement.getRegisterDate()); //java.sql.Timestamp; and only contains date
+				if (registerDate == null ) continue;
+
+				Date removedDate = timestampToRoundedDate(placement.getRemovedDate()); //java.sql.Timestamp; contains date and time
+				
+				//XXX this should be done in SQL in the first place
+				if (registerDate.compareTo(lastDateOfCurrentMonth) > 0)	continue;
+				if (removedDate != null && removedDate.compareTo(firstDateOfCurrentMonth) < 0)	continue;
+				
+				Iterator placementLogs = getSchoolClassMemberLogHome().findAllBySchoolClassMember(placement).iterator();
+				while (placementLogs.hasNext()){
+					takeThisPlacement = false;
+					
+					SchoolClassMemberLog log = (SchoolClassMemberLog) placementLogs.next();
+					
+					Date startDate = log.getStartDate(); //java.util.Date, contains date only
+					Date endDate = log.getEndDate(); //java.util.Date, contains date only
+					
+//					2.	IF it’s a new placement where sch_class_member.register_date >= 1st of current month 
+//					AND sch_class_member.register_date <= last date of current month					
+					if (registerDate.equals(startDate) & isDateInInterval(registerDate, firstDateOfCurrentMonth, lastDateOfCurrentMonth))  //placement started
+						takeThisPlacement = true;
+
+//					3.	IF it’s a placement that ends (has removed_date set in sch_class_member) and 
+//					sch_class_member.removed date <= last date of current month
+//					and sch_class_member.removed date >= first date of current month					
+					if (removedDate != null && (isDateInInterval(removedDate, firstDateOfCurrentMonth, lastDateOfCurrentMonth) & removedDate.equals(endDate))) //placement ended
+						takeThisPlacement = true;
+						
+//					4.	IF a group has been changed (new entry in sch_class_member_log with 
+//						sch_class_member_log.start date >= 1st of current month 
+//						AND sch_class_member_log.start date <= last date of current month)
+					if (takeThisPlacement || isDateInInterval(startDate, firstDateOfCurrentMonth, lastDateOfCurrentMonth)) // group was changed
+					{
+						String careTime = "care_time_string";
+						uberCollection.add(new DataForExport(log.getSchoolClassMember(),startDate, endDate, careTime));					
+					}
+					
+				}
+				
+			}
+			
+			//get all contracts
+			//retrieve the contracts we need
+			//... and add them to the ubercollection		
+
+			Iterator contractsIterator = getChildCareContractHome().findChangedBetween(
+					new java.sql.Date(firstDateOfCurrentMonth.getTime()), new java.sql.Date(lastDateOfCurrentMonth.getTime())).iterator(); //TODO: check, if this date conversion is okay.
+			
+			while (contractsIterator.hasNext()) {
+				ChildCareContract contract = (ChildCareContract) contractsIterator.next();
+				
+//				5.	IF a caretime has been changed (new entry in comm_childcare_archive with 
+//					comm_childcare_archive.start date >=1st of current month AND comm_childcare_archive.start date <= last date of current month)
+				Date validFromDate = contract.getValidFromDate(); //java.util.Date , only date
+				if (validFromDate == null) continue;
+				
+				if ( isDateInInterval(validFromDate, firstDateOfCurrentMonth, lastDateOfCurrentMonth)) {
+					uberCollection.add(new DataForExport(contract.getSchoolClassMember(),validFromDate, null, contract.getCareTime()));					
+				}				
+				
+			}	
+			
+			//reshuffle the ubercollection so it is in correct order
+			Collections.sort(uberCollection);
+			
+			return uberCollection;
+			
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			return new Vector();
+		}		
+		
+	}
+
+
+	private boolean isDateInInterval(Date date, Date startDate, Date endDate) {
+		return date.compareTo(startDate) >= 0 || date.compareTo(endDate) <= 0;
+	}	
+	
+	private SchoolClassMemberHome getSchoolClassMemberHome() throws IDOLookupException {
+		return (SchoolClassMemberHome) IDOLookup.getHome(SchoolClassMember.class);
+	}
+	
+	private SchoolClassMemberLogHome getSchoolClassMemberLogHome() throws IDOLookupException {
+		return (SchoolClassMemberLogHome) IDOLookup.getHome(SchoolClassMemberLog.class);
+	}	
+
+	private SchoolBusiness getSchoolBusiness() throws IBOLookupException{
+		return (SchoolBusiness) IBOLookup.getServiceInstance(iwc, SchoolBusiness.class);   
+	}
+	
+	private ChildCareContractHome getChildCareContractHome() throws IDOLookupException {
+		return (ChildCareContractHome) IDOLookup.getHome(ChildCareContract.class);
+	}
+	
+	private class DataForExport implements Comparable {
+		String contents;
+		Date startDate;
+		
+		public DataForExport(SchoolClassMember member, Date startDate, Date endDate, String careTimeString) {
+			this.startDate = startDate;
+			try {
+			this.contents = 
+				member.getSchoolClass().getSchool().getExtraProviderId() + SEPARATOR +
+				member.getSchoolClass().getGroupStringId() + SEPARATOR +
+				member.getStudent().getPersonalID() + SEPARATOR + 
+				dateToString(startDate) + SEPARATOR +
+				dateToString(endDate) + SEPARATOR +
+				careTimeString + SEPARATOR +
+				member.getSchoolType().getTypeStringId() + 
+				"\r\n";	
+			} catch (Exception e) {
+				System.out.println("error in DataForExport");
+				e.printStackTrace();
+			}
+		}
+
+		public int compareTo(Object arg0) { // must return > 0 if argument is smaller
+			try {
+				Date date = ((DataForExport) arg0).getStartDate();
+				if (this.getStartDate().after(date)) {
+					return 1;
+				}
+				else if (this.getStartDate().before(date)) {
+					return -1;
+				}
+				else {
+					return 0;
+				}
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+				return 0;
+			}
+		}
+		
+		public Date getStartDate() {
+			return startDate;
+		}
+		
+		public void setStartDate(Date startDate) {
+			this.startDate = startDate;
+		}
+		
+		public String getContents() {
+			return contents;
+		}
+		
+		public void setContents(String contents) {
+			this.contents = contents;
+		}
+	}		
+	// end of contract and placement changes
 }
--- src/se/idega/idegaweb/commune/childcare/presentation/ChildCareContractStatistics.java
+++ src/se/idega/idegaweb/commune/childcare/presentation/ChildCareContractStatistics.java
@@ -5,11 +5,10 @@ package se.idega.idegaweb.commune.childcare.presentation;
 
 import java.text.DecimalFormat;
 import java.util.Iterator;
-
 import se.idega.idegaweb.commune.childcare.business.ChildCareStatisticsWriter;
-
 import com.idega.core.file.data.ICFile;
 import com.idega.presentation.IWContext;
+import com.idega.presentation.PresentationObjectContainer;
 import com.idega.presentation.Table;
 import com.idega.presentation.text.Link;
 import com.idega.presentation.text.Text;
@@ -27,6 +26,8 @@ public class ChildCareContractStatistics extends ChildCareBlock {
 	private ICFile statisticsFolder;
 	private boolean createReport = false;
 	private boolean reportCreated = false;
+	
+	private boolean useAsContractAndPlacementChangesExport = false;
 	
 	/**
 	 * @return
@@ -46,21 +47,28 @@ public class ChildCareContractStatistics extends ChildCareBlock {
 	 * @see se.idega.idegaweb.commune.childcare.presentation.ChildCareBlock#init(com.idega.presentation.IWContext)
 	 */
 	public void init(IWContext iwc) throws Exception {
-		if (getStatisticsFolder() != null) {
-			parseAction(iwc);
+		if (!isUseAsContractAndPlacementChangesExport()) {
+			if (getStatisticsFolder() != null) {
+				parseAction(iwc);
+				
+				Table table = new Table(1, 3);
+				table.setCellpadding(0);
+				table.setCellspacing(0);
+				table.setHeight(2, 12);
+				table.setWidth(getWidth());
+				
+				table.add(getCreateForm(), 1, 1);
+				table.add(getFileList(iwc), 1, 3);
+				add(table);
+			}
+			else {
+				add(getSmallErrorText("The statistics folder has not been set..."));
+			}
+		} else {
+			setIwc(iwc); 
 			
-			Table table = new Table(1, 3);
-			table.setCellpadding(0);
-			table.setCellspacing(0);
-			table.setHeight(2, 12);
-			table.setWidth(getWidth());
-			
-			table.add(getCreateForm(), 1, 1);
-			table.add(getFileList(iwc), 1, 3);
-			add(table);
-		}
-		else {
-			add(getSmallErrorText("The statistics folder has not been set..."));
+			parseAction();			
+			createGui();
 		}
 	}
 	
@@ -137,4 +145,156 @@ public class ChildCareContractStatistics extends ChildCareBlock {
 			reportCreated = statistics.createReport(iwc, statisticsFolder, iwc.getCurrentLocale());
 		}
 	}
+
+	
+	public boolean isUseAsContractAndPlacementChangesExport() {
+		return useAsContractAndPlacementChangesExport;
+	}
+
+	
+	public void setUseAsContractAndPlacementChangesExport(boolean useAsContractAndPlacementChangesExport) {
+		this.useAsContractAndPlacementChangesExport = useAsContractAndPlacementChangesExport;
+	}
+	
+	/*
+	 * Contract and placement changes
+	 * This is copy from ContractAndPlacementChangesExport	  	 
+	 */
+	
+	//private static final String PARAMETER_CREATE_REPORT = "create_report";	
+
+	private ICFile exportFolder;
+	IWContext iwc = null;
+	
+	private boolean createExport = false;
+	private boolean exportCreated = false;		
+	
+	private void parseAction() {		
+		if (iwc.isParameterSet(PARAMETER_CREATE_REPORT)) {
+			this.createExport = true;
+			
+//			ContractAndPlacementChangesExportWriter writer = 
+//					new ContractAndPlacementChangesExportWriter();
+
+			ChildCareStatisticsWriter writer = 
+				new ChildCareStatisticsWriter();
+			
+			this.exportCreated = writer.createExportFile(this.getIwc(), this.getExportFolder());			
+		}		
+	}
+	
+	private void createGui() {
+		PresentationObjectContainer poc = new PresentationObjectContainer();
+		
+		if (exportFolder != null ) {
+			Table table = new Table(1, 3);
+			table.setCellpadding(0);
+			table.setCellspacing(0);
+			table.setHeight(2, 12);
+			table.setWidth(getWidth());
+			
+			table.add(getCreateContractAndPlacementChangesForm(), 1, 1);
+			table.add(getFileList(), 1, 3);		
+			
+			poc.add(table);
+		} 
+		else {
+			poc.add(getSmallErrorText("The statistics folder has not been set..."));
+		}
+		
+		add(poc);
+		
+	}
+	
+	private Table getFileList() {
+		Table table = new Table();
+		table.setWidth(Table.HUNDRED_PERCENT);
+		table.setCellpadding(getCellpadding());
+		table.setCellspacing(getCellspacing());
+		table.setColumns(3);
+		table.setRowColor(1, getHeaderColor());
+		table.setWidth(1, Table.HUNDRED_PERCENT);
+		int row = 1;
+		int column = 1;
+			
+		table.add(getLocalizedSmallHeader("child_care.file_name", "File name"), column++, row);
+		table.setNoWrap(column, row);
+		table.add(getLocalizedSmallHeader("child_care.file_size", "File size"), column++, row);
+		table.setNoWrap(column, row);
+		table.add(getLocalizedSmallHeader("child_care.created_date", "Created date"), column++, row);
+		//table.setNoWrap(column, row);
+		//table.add(getLocalizedSmallHeader("child_care.mimetype", "Mimetype"), column++, row++);
+		row++;
+		
+		Iterator iter = getExportFolder().getChildrenIterator();
+		
+		if (iter != null) {
+			while (iter.hasNext()) {
+				column = 1;
+				ICFile file = (ICFile) iter.next();
+				
+				double size = 0;
+				if (!file.isFolder()) {
+					size = (double) file.getFileSize().intValue() / (double) 1024;					
+				}
+				DecimalFormat format = new DecimalFormat("0.0 KB");
+
+				if (row % 2 == 0)
+					table.setRowColor(row, getZebraColor1());
+				else
+					table.setRowColor(row, getZebraColor2());
+	
+				Link link = getSmallLink(file.getName());
+				link.setFile(file);
+				
+				table.add(link, column++, row);
+				table.setNoWrap(column, row);
+				table.add(getSmallText(format.format(size)), column++, row);
+				table.setNoWrap(column, row);
+				table.add(getSmallText(new IWTimestamp(file.getCreationDate()).getLocaleDateAndTime(iwc.getCurrentLocale(), IWTimestamp.SHORT, IWTimestamp.SHORT)), column++, row);
+				//table.setNoWrap(column, row);
+				//table.add(getSmallText(file.getMimeType()), column++, mos.write(new String("hello, world").getBytes()););
+				row++;	
+			}
+		}
+		table.setColumnAlignment(2, Table.HORIZONTAL_ALIGN_RIGHT);
+		table.setColumnAlignment(3, Table.HORIZONTAL_ALIGN_CENTER);
+		
+		return table;
+	}
+	
+	private Form getCreateContractAndPlacementChangesForm() {
+		Form form = new Form();
+		form.addParameter(PARAMETER_CREATE_REPORT, "true");
+		
+		SubmitButton button = (SubmitButton) getButton(new SubmitButton(localize("child_care.create_report", "Create report")));
+		form.add(button);
+		
+		if (createExport && !exportCreated) {
+			form.add(Text.getNonBrakingSpace());
+			form.add(getSmallErrorText(localize("child_care.create_report_failed", "Failed to create report. Possibly no changes found.")));
+		}
+		
+		return form;
+	}	
+	
+
+	public ICFile getExportFolder() {
+		return exportFolder;
+	}
+
+	public void setExportFolder(ICFile statisticsFolder) {
+		this.exportFolder = statisticsFolder;
+	}
+	
+	public IWContext getIwc() {
+		return iwc;
+	}
+	
+	public void setIwc(IWContext iwc) {
+		this.iwc = iwc;
+	}	
+
+	// end of Contract and placement changes	
+	
 }
